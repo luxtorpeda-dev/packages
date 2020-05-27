15ceda74dc7e8fa59a87305364d627efb1510f58
d10sfan <sales@circuitcube.com>
2020-05-27 10:54:37 -0500

    Force update of dhewm3

--- engines/dhewm3/build.sh
+++ engines/dhewm3/build.sh
@@ -18,6 +18,5 @@ popd
 # COPY PHASE
 cp -rfv tmp/bin/* "$diststart/9050/dist/"
 cp -rfv tmp/lib/dhewm3/* "$diststart/9050/dist/"
-
 cp -rfv tmp/bin/* "$diststart/9070/dist/"
 cp -rfv tmp/lib/dhewm3/* "$diststart/9070/dist/"
