0df02d626fee816ace7355a53b95b38d472a8c84
d10sfan <sales@circuitcube.com>
2020-06-02 21:17:23 -0500

    Update build.sh

--- engines/dhewm3/build.sh
+++ engines/dhewm3/build.sh
@@ -20,5 +20,3 @@ cp -rfv tmp/bin/* "$diststart/9050/dist/"
 cp -rfv tmp/lib/dhewm3/* "$diststart/9050/dist/"
 cp -rfv tmp/bin/* "$diststart/9070/dist/"
 cp -rfv tmp/lib/dhewm3/* "$diststart/9070/dist/"
-
-
