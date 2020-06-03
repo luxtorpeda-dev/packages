4138a924f84b5938206bf1c605ff737331f440a7
d10sfan <sales@circuitcube.com>
2020-06-02 23:41:02 -0500

    Fix path to license for OpenGothic

--- engines/opengothic/env.sh
+++ engines/opengothic/env.sh
@@ -2,4 +2,4 @@
 
 export STEAM_APP_ID_LIST="39510"
 export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./source/assets/LICENSE.glslang"
+export ADDITIONAL_LICENSES="./assets/LICENSE.glslang"
