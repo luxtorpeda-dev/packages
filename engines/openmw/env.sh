8fccc76cd51f29d0383b256a9cf56bfcc345e214
d10sfan <sales@circuitcube.com>
2020-06-03 00:09:29 -0500

    Update openmw LICENSE_PATH

--- engines/openmw/env.sh
+++ engines/openmw/env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 export STEAM_APP_ID_LIST="22320"
+export LICENSE_PATH="./source/LICENSE"
+export ADDITIONAL_LICENSES="./source/files/mygui/DejaVu\ Font\ License.txt ./boost/LICENSE_1_0.txt ./ffmpeg/COPYING.LGPLv2.1 ./bullet3/LICENSE.txt ./mygui/COPYING.MIT ./osg/LICENSE.txt"
