529d648626ebe205494f1bd5a5f6cd2d2bf997d1
d10sfan <sales@circuitcube.com>
2020-06-03 08:02:35 -0500

    Update openmw license path
    Font license is already provided

--- engines/openmw/env.sh
+++ engines/openmw/env.sh
@@ -2,4 +2,4 @@
 
 export STEAM_APP_ID_LIST="22320"
 export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./source/files/mygui/DejaVu\ Font\ License.txt ./boost/LICENSE_1_0.txt ./ffmpeg/COPYING.LGPLv2.1 ./bullet3/LICENSE.txt ./mygui/COPYING.MIT ./osg/LICENSE.txt"
+export ADDITIONAL_LICENSES="./boost/LICENSE_1_0.txt ./ffmpeg/COPYING.LGPLv2.1 ./bullet3/LICENSE.txt ./mygui/COPYING.MIT ./osg/LICENSE.txt"
