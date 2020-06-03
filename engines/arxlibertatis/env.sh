25f806aaf0ebd73681763de72a5d9cd3f3d1470b
d10sfan <sales@circuitcube.com>
2020-06-02 22:33:58 -0500

    Add LICENSE_PATH for arx libertatis

--- engines/arxlibertatis/env.sh
+++ engines/arxlibertatis/env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 export STEAM_APP_ID_LIST="1700 1710"
+export LICENSE_PATH="./source/LICENSE"
+export ADDITIONAL_LICENSES="./source/LICENSE.DejaVu ./boost/LICENSE_1_0.txt ./glm/copying.txt ./data/LICENSE"
