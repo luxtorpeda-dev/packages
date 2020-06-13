19e3221bcd196dbbbe7406d9ac7fc699e058d5a3
d10sfan <d10sfan@gmail.com>
2020-06-13 09:42:41 -0500

    Support for gzdoom

---
+++ engines/gzdoom/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# From https://gitlab.com/luxtorpeda/packages/gzdoom - See LICENSE file for more information
+
+# CLONE PHASE
+git clone https://github.com/coelckers/gzdoom.git source
+pushd source
+git checkout 5001f36
+popd
+
+# BUILD PHASE
+pushd "source"
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
+for app_id in $STEAM_APP_ID_LIST ; do
+    cp -rfv "source/build"/{gzdoom,soundfonts,*.pk3} "$diststart/$app_id/dist/"
+done
