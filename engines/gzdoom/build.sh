0f334d4870d58c325f97f6d87e8f67343f6fec45
d10sfan <d10sfan@gmail.com>
2020-06-13 10:05:31 -0500

    Update gzdoom version

--- engines/gzdoom/build.sh
+++ engines/gzdoom/build.sh
@@ -5,34 +5,15 @@
 # CLONE PHASE
 git clone https://github.com/coelckers/gzdoom.git source
 pushd source
-git checkout 5001f36
-popd
-
-git clone https://github.com/coelckers/ZMusic.git zmusic
-pushd zmusic
-git checkout c1bf2f8
+git checkout a3b0f21
 popd
 
 # BUILD PHASE
-mkdir -p tmp
-
-pushd "zmusic"
-mkdir -p build
-cd build
-cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=tmp \
-    ..
-make install
-popd
-
 pushd "source"
 mkdir -p build
 cd build
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DZMUSIC_INCLUDE_DIR=tmp/include \
-    -DZMUSIC_LIBRARIES=tmp/lib/libzmusic.so \
     ..
 make -j "$(nproc)"
 popd
@@ -40,5 +21,4 @@ popd
 # COPY PHASE
 for app_id in $STEAM_APP_ID_LIST ; do
     cp -rfv "source/build"/{gzdoom,soundfonts,*.pk3} "$diststart/$app_id/dist/"
-    cp -rfv "source/tmp/lib/libzmusic.so "$diststart/$app_id/dist/"
 done
