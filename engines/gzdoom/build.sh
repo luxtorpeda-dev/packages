c8ff80efcddebe1b2751405ac92e2e481b1fa886
d10sfan <d10sfan@gmail.com>
2020-06-13 09:54:35 -0500

    Support for zmusic

--- engines/gzdoom/build.sh
+++ engines/gzdoom/build.sh
@@ -8,12 +8,30 @@ pushd source
 git checkout 5001f36
 popd
 
+git clone https://github.com/coelckers/ZMusic.git zmusic
+pushd source
+git checkout c1bf2f8
+popd
+
 # BUILD PHASE
+mkdir -p tmp
+
+pushd "zmusic"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=tmp \
+    ..
+popd
+
 pushd "source"
 mkdir -p build
 cd build
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
+    -DZMUSIC_INCLUDE_DIR=tmp/include \
+    -DZMUSIC_LIBRARIES=tmp/lib/libzmusic.so \
     ..
 make -j "$(nproc)"
 popd
@@ -21,4 +39,5 @@ popd
 # COPY PHASE
 for app_id in $STEAM_APP_ID_LIST ; do
     cp -rfv "source/build"/{gzdoom,soundfonts,*.pk3} "$diststart/$app_id/dist/"
+    cp -rfv "source/tmp/lib/libzmusic.so "$diststart/$app_id/dist/"
 done
