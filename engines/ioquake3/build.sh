32cf01aa0b2dea617dfe215743539df767c98414
d10sfan <sales@circuitcube.com>
2020-06-12 21:49:06 -0500

    Add ioquake 3 engine (#26)
    * Add ioquake 3 engine
    
    * Fix for build script
    
    * Update packages and build sequences
    
    * Fix license path
    
    Co-authored-by: d10sfan <d10sfan@gmail.com>

---
+++ engines/ioquake3/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# From https://gitlab.com/luxtorpeda/packages/ioq3 - See LICENSE file for more information
+
+# CLONE PHASE
+git clone https://github.com/ioquake/ioq3.git source
+pushd source
+git checkout f2c61c1
+popd
+
+# BUILD PHASE
+pushd "source"
+make -j "$(nproc)"
+popd
+
+
+# COPY PHASE
+for app_id in $STEAM_APP_ID_LIST ; do
+    COPYDIR="../$app_id/dist" make --directory="source" copyfiles
+done
