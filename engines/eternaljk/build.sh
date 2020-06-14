5eef8a48eef2ba0966ec6724967b66ce865399fc
d10sfan <sales@circuitcube.com>
2020-06-13 21:13:41 -0500

    Eternaljk (#32)
    * Update to eternaljk
    
    * Start of eternaljk
    
    * Disable building of ded server
    
    * Add patch to revert 7za
    
    Co-authored-by: d10sfan <d10sfan@gmail.com>

---
+++ engines/eternaljk/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/eternalcodes/EternalJK.git source
+pushd source
+git checkout 0396e4a
+git am < ../patches/0001-Revert-use-of-7za-from-commit-c2dfd02ebd73b06ba2326c.patch
+popd
+
+# BUILD PHASE
+mkdir -p tmp
+pushd source
+mkdir build
+cd build
+cmake \
+    -DCMAKE_INSTALL_PREFIX=../../tmp \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+# COPY PHASE
+cp -rfv tmp/JediAcademy/* "$diststart/6020/dist/"
