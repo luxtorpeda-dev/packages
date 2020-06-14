27717224efca1f6ea90a1d860a045af66d583560
d10sfan <sales@circuitcube.com>
2020-06-13 20:36:17 -0500

    Openjk (#30)
    * start of openjk
    
    * Install step
    
    * fix
    
    * Add copy phase
    
    * Update packages.json
    
    * Cleanup build script
    
    Co-authored-by: d10sfan <d10sfan@gmail.com>

---
+++ engines/openjk/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/JACoders/OpenJK.git source
+pushd source
+git checkout 24c5b27
+popd
+
+# BUILD PHASE
+mkdir -p tmp
+pushd source
+mkdir build
+cd build
+cmake \
+    -DBuildJK2SPEngine=ON \
+    -DBuildJK2SPGame=ON \
+    -DBuildJK2SPRdVanilla=ON \
+    -DCMAKE_INSTALL_PREFIX=../../tmp \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+# COPY PHASE
+cp -rfv tmp/JediAcademy/* "$diststart/6020/dist/"
+cp -rfv tmp/JediOutcast/* "$diststart/6030/dist/"
+cp -v tmp/JediAcademy/OpenJK/cgamex86_64.so "$diststart/6030/dist/OpenJK/"
+cp -v tmp/JediAcademy/OpenJK/uix86_64.so "$diststart/6030/dist/OpenJK/"
