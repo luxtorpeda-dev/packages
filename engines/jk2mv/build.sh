dd60b95f1908d7d52ec2a45b2d1f5ed6f0be9149
d10sfan <sales@circuitcube.com>
2020-06-13 21:30:00 -0500

    Jk2mv (#33)
    * Update packages for jk2mv
    
    * Initial support for jk2mv
    
    * change name due to collision
    
    * fix submodules
    
    * Set CMake flag
    
    * Fix asset path
    
    Co-authored-by: d10sfan <d10sfan@gmail.com>

---
+++ engines/jk2mv/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/mvdevs/jk2mv.git source
+pushd source
+git checkout 11a108e
+git submodule update --init --recursive
+popd
+
+# BUILD PHASE
+mkdir -p tmp
+pushd source
+mkdir building
+cd building
+cmake \
+    -DCMAKE_INSTALL_PREFIX=../../tmp \
+    -DUseInternalMiniZip=ON \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+# COPY PHASE
+cp -rfv tmp/* "$diststart/6020/dist/"
---
+++ engines/jk2mv/env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="6030"
+export LICENSE_PATH="./source/LICENSE"
