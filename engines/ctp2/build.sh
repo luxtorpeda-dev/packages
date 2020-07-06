a209db0290a758799da839a89ba562fab0e1c1d4
d10sfan <sales@circuitcube.com>
2020-07-06 12:06:49 -0500

    ctp2 (#39)
    * Add ctp2 initial
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Fix pathing
    
    * Packages
    
    * Run script
    
    Co-authored-by: d10sfan <d10sfan@gmail.com>

---
+++ engines/ctp2/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/ptitSeb/ctp2.git source
+pushd source
+git checkout 2584d003041a44123994348957b74ee3b11305fc
+popd
+
+apt-get install -y byacc flex
+
+# BUILD PHASE
+pushd source
+cp -rfv "EULA - Source Code for CTP2.rtf" "EULA.rtf"
+cp -rfv "Activision CTP2 Source Code_Readme.txt" "Activision_CTP2_Source_Code_Readme.txt"
+cp -rfv "Apolyton CTP2 Source Code_Readme.txt" "Apolyton_CTP2_Source_Code_Readme.txt"
+./autogen.sh
+CFLAGS="$CFLAGS -w -fuse-ld=gold" CXXFLAGS="$CXXFLAGS -w -fuse-ld=gold" ./configure --enable-silent-rules
+make
+popd
+
+# COPY PHASE
+mkdir -p "$diststart/572050/dist/ctp2_program/ctp/dll/map"
+cp -rfv source/ctp2_code/ctp2 "$diststart/572050/dist/ctp2_program/ctp/ctp2"
+cp -rfv source/ctp2_data "$diststart/572050/dist/"
+cp -rfv source/ctp2_code/mapgen/.libs/*.so "$diststart/572050/dist/ctp2_program/ctp/dll/map"
+cp -rfv assets/run-ctp2.sh "$diststart/572050/dist/run-ctp2.sh"
---
+++ engines/ctp2/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="572050"
+export LICENSE_PATH="./source/EULA.rtf"
+export ADDITIONAL_LICENSES="./source/Activision_CTP2_Source_Code_Readme.txt ./source/Apolyton_CTP2_Source_Code_Readme.txt"
