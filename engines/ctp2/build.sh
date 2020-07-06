0d2c1bdc6f1f1269b753e99e7dfb2b09c98d5e00
d10sfan <d10sfan@gmail.com>
2020-07-06 11:09:08 -0500

    Add ctp2 initial

---
+++ engines/ctp2/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/ptitSeb/ctp2.git source
+pushd source
+git checkout 2584d003041a44123994348957b74ee3b11305fc
+popd
+
+# BUILD PHASE
+pushd source
+./autogen.sh
+CFLAGS="$CFLAGS -w -fuse-ld=gold" CXXFLAGS="$CXXFLAGS -w -fuse-ld=gold" ./configure --enable-silent-rules
+make
+popd
+
+# COPY PHASE
+mkdir -p "$diststart/572050/dist/ctp2_program/ctp/dll/map"
+cp -rfv source/ctp2_code/ctp2 "$diststart/572050/dist/ctp2_program/ctp/ctp2"
+cp -rfv source/ctp2_data "$diststart/572050/dist/"
+cp -rfv source/ctp2_code/mapgen/.libs/*.so "$diststart/572050/dist/ctp2_program/ctp/dll/map"
---
+++ engines/ctp2/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="572050"
+export LICENSE_PATH="./source/EULA - Source Code for CTP2.rtf"
+export ADDITIONAL_LICENSES="./source/Activision CTP2 Source Code_Readme.txt"
