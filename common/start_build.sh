ab9ce15bffba8e1315e4842ab9740da0ef31bc80
d10sfan <sales@circuitcube.com>
2020-07-08 17:14:24 -0500

    GZDoom Upgrade (#50)
    Add support for gcc 9 and gzdoom fixes

--- common/start_build.sh
+++ common/start_build.sh
@@ -11,6 +11,13 @@ setup_dist_dirs "$STEAM_APP_ID_LIST"
 
 echo "::set-env name=APP_IDS::$STEAM_APP_ID_LIST"
 
+if [ -z "${GCC_9}" ]; then
+    echo "Using default gcc"
+else
+    echo "Installing gcc 9"
+    install_gcc_9
+fi
+
 source ./build.sh
 
 copy_license_file "$STEAM_APP_ID_LIST"
--- engines/gzdoom/build.sh
+++ engines/gzdoom/build.sh
@@ -5,19 +5,59 @@
 # CLONE PHASE
 git clone https://github.com/coelckers/gzdoom.git source
 pushd source
-git checkout f586196
+git checkout 04e53b8
 git am < ../patches/0001-Workaround-for-missing-PRId64.patch
 popd
 
+git clone https://github.com/coelckers/ZMusic.git zmusic
+pushd zmusic
+git checkout -f 9097591
+popd
+
+git clone https://github.com/FluidSynth/fluidsynth.git fluidsynth
+pushd fluidsynth
+git checkout -f 19a20eb
+popd
+
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
 # BUILD PHASE
+pushd "fluidsynth"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)" install
+popd
+
+pushd "zmusic"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    -DFLUIDSYNTH_INCLUDE_DIR="$pfx/include" \
+    -DFLUIDSYNTH_LIBRARIES="$pfx/lib64" \
+    ..
+make -j "$(nproc)" install
+popd
+
 pushd "source"
 mkdir -p build
 cd build
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH="$pfx" \
     ..
 make -j "$(nproc)"
 popd
 
 # COPY PHASE
+mkdir -p "$diststart/common/dist/lib"
 cp -rfv "source/build"/{gzdoom,soundfonts,*.pk3} "$diststart/common/dist/"
+cp -rfv "$pfx/lib/libzmusic.so" "$diststart/common/dist/lib"
+cp -rfv "$pfx/lib64"/libfluidsynth.so* "$diststart/common/dist/lib"
+cp -rfv "assets/run-gzdoom.sh" "$diststart/common/dist/"
