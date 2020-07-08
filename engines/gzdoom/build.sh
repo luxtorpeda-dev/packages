3d6a2c615e9b0d262250c45a8034f9537cb40351
d10sfan <sales@circuitcube.com>
2020-07-08 13:52:05 -0500

    Update build.sh

--- engines/gzdoom/build.sh
+++ engines/gzdoom/build.sh
@@ -2,11 +2,19 @@
 
 # From https://gitlab.com/luxtorpeda/packages/gzdoom - See LICENSE file for more information
 
+sudo apt-get update && \
+sudo apt-get install software-properties-common -y && \
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y && \
+sudo apt-get update && \
+sudo apt-get install gcc-8 g++-8 -y && \
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-8
+sudo update-alternatives --config gcc
+gcc -v
+
 # CLONE PHASE
 git clone https://github.com/coelckers/gzdoom.git source
 pushd source
-git checkout f586196
-git am < ../patches/0001-Workaround-for-missing-PRId64.patch
+git checkout 04e53b8
 popd
 
 # BUILD PHASE
