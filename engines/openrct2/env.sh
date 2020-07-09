4f486ffcf21eec37001d795a65aef8d8b4a9ace7
d10sfan <d10sfan@gmail.com>
2020-07-09 12:25:33 -0500

    fix spelling

--- engines/openrct2/env.sh
+++ engines/openrct2/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export STEAM_APP_ID_LIST="285330"
-export LICENSE_PATH="./source/license.txt"
+export LICENSE_PATH="./source/licence.txt"
 export ADDITIONAL_LICENSES="./jansson/LICENSE ./libzip/LICENSE ./icu/icu4c/LICENSE"
 export GCC_9="1"
