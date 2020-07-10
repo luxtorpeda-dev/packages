0d55e207d869acc625b92737ccb235e3f9709bf3
d10sfan <d10sfan@gmail.com>
2020-07-10 16:03:38 -0500

    Initial

---
+++ engines/openapoc/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/OpenApoc/OpenApoc.git source
+pushd source
+git checkout -f 9183a08
+popd
+
+git clone https://github.com/boostorg/boost boost
+pushd boost
+git checkout -f 62a4b7f
+git submodule update --init --recursive
+popd
+
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
+# BUILD PHASE
+./build-boost.sh
+
+pushd source
+mkdir build
+cd build
+cmake \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DBUILD_LAUNCHER=OFF \
+    ..
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
