c5d079c462e39374be23cc13bba4a5d4b8d78de5
d10sfan <d10sfan@gmail.com>
2020-07-13 10:55:16 -0500

    scripts

--- engines/quake4/build.sh
+++ engines/quake4/build.sh
@@ -2,4 +2,6 @@
 
 # COPY PHASE
 cp -rfv "assets/run-quake4.sh" "$diststart/2210/dist/"
+cp -rfv "assets/setup-quake4.sh" "$diststart/2210/dist/"
+cp -rfv "assets/uninstall-quake4.sh" "$diststart/2210/dist/"
 cp -rfv "assets/Quake4Config.cfg" "$diststart/2210/dist/"
