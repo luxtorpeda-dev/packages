1c79e23e66e6ceb980b5553f0103ea9c282a0b3c
d10sfan <d10sfan@gmail.com>
2020-07-13 17:32:26 -0500

    32 bit force

--- engines/prey2006/build.sh
+++ engines/prey2006/build.sh
@@ -3,7 +3,8 @@
 # CLONE PHASE
 git clone https://github.com/tim241/sdlcl sdlcl
 pushd sdlcl
-git checkout -f 2d3cc735
+git checkout -f 85ca5537
+git am < ../patches/0001-Force-32-bit.patch
 popd
 
 # BUILD PHASE
