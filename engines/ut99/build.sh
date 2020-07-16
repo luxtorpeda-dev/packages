77e48e563561f2b14a4895909c301150143a2085
d10sfan <d10sfan@gmail.com>
2020-07-16 14:09:03 -0500

    ut99 - Change to provide config here

--- engines/ut99/build.sh
+++ engines/ut99/build.sh
@@ -4,3 +4,4 @@
 cp -rfv "assets/run-ut99.sh" "$diststart/13240/dist/"
 cp -rfv "assets/setup-ut99.sh" "$diststart/13240/dist/"
 cp -rfv "assets/uninstall-ut99.sh" "$diststart/13240/dist/"
+cp -rfv "assets/UnrealTournament-override.ini" "$diststart/13240/dist/"
