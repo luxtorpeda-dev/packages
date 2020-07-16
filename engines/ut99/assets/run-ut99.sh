8cc35756c524d95c211ac05e98ccdc6659ba1630
d10sfan <sales@circuitcube.com>
2020-07-15 23:56:40 -0500

    UT 99 (#77)

---
+++ engines/ut99/assets/run-ut99.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ../ # game tries to start in system directory, so have to get out and back to the normal directory
+
+cd linuxdata
+padsp ./ut
---
+++ engines/ut99/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# COPY PHASE
+cp -rfv "assets/run-ut99.sh" "$diststart/13240/dist/"
+cp -rfv "assets/setup-ut99.sh" "$diststart/13240/dist/"
+cp -rfv "assets/uninstall-ut99.sh" "$diststart/13240/dist/"
