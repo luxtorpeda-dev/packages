e5d2dfe22bfe10103c8db82a5a3c7c8f7949967b
d10sfan <sales@circuitcube.com>
2020-07-16 13:02:44 -0500

    ut-99 - Fix for steam overlay not working

--- engines/ut99/assets/run-ut99.sh
+++ engines/ut99/assets/run-ut99.sh
@@ -3,4 +3,4 @@
 cd ../ # game tries to start in system directory, so have to get out and back to the normal directory
 
 cd linuxdata
-LD_PRELOAD=/usr/lib/i386-linux-gnu/pulseaudio/libpulsedsp.so padsp ./ut
+LD_PRELOAD="$LD_PRELOAD /usr/lib/i386-linux-gnu/pulseaudio/libpulsedsp.so" padsp ./ut
