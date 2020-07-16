b8373f2a9d38485a832238c4d845a05f82034653
d10sfan <sales@circuitcube.com>
2020-07-16 17:22:31 -0500

    Upgrade dhewm3 (#67)

--- engines/dhewm3/build.sh
+++ engines/dhewm3/build.sh
@@ -3,14 +3,31 @@
 # CLONE PHASE
 git clone https://github.com/dhewm/dhewm3.git source
 pushd source
-git checkout 3a763fc
+git checkout f3a4d92
+popd
+
+git clone https://github.com/kcat/openal-soft.git openal
+pushd openal
+git checkout -f f5e0eef
 popd
 
 # BUILD PHASE
+pushd "openal"
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
 pushd source/neo
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../../../tmp ..
+cmake \
+    -DCMAKE_INSTALL_PREFIX=../../../tmp \
+    -DCMAKE_PREFIX_PATH=../../../tmp \
+    ..
 make -j "$(nproc)"
 make install
 popd
@@ -18,3 +35,5 @@ popd
 # COPY PHASE
 cp -rfv tmp/bin/* "$diststart/common/dist/"
 cp -rfv tmp/lib/dhewm3/* "$diststart/common/dist/"
+cp -rfv "/usr/local/lib/libopenal.so.1.20.1" "$diststart/common/dist/openal.so"
+cp -rfv "assets/run-dhewm3.sh" "$diststart/common/dist/"
--- engines/dhewm3/env.sh
+++ engines/dhewm3/env.sh
@@ -2,5 +2,5 @@
 
 export STEAM_APP_ID_LIST="9050 9070"
 export LICENSE_PATH="./source/COPYING.txt"
-export ADDITIONAL_LICENSES="./source/README.md"
+export ADDITIONAL_LICENSES="./source/README.md ./openal/COPYING"
 export COMMON_PACKAGE="1"
