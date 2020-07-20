7dab07f853e1410aa94ff88d0ca77f0fc5a1efcc
d10sfan <d10sfan@gmail.com>
2020-07-19 21:50:24 -0500

    Remove tr1

--- engines/opentomb/build.sh
+++ engines/opentomb/build.sh
@@ -6,14 +6,6 @@ pushd source
 git checkout 8e4c249
 popd
 
-git clone https://github.com/hessu/bchunk.git bchunk
-pushd bchunk
-git checkout -f 2d46931
-popd
-
-wget https://www.gnu.org/software/xorriso/xorriso-1.5.2.tar.gz
-tar xvf xorriso-1.5.2.tar.gz
-
 # BUILD PHASE
 pushd source
 mkdir build
@@ -23,15 +15,6 @@ cmake \
 make -j "$(nproc)"
 popd
 
-pushd bchunk
-make
-popd
-
-pushd xorriso-1.5.2
-./configure
-make
-popd
-
 # COPY PHASE
 cp -rfv source/build/OpenTomb "$diststart/common/dist/"
 cp -rfv source/resource "$diststart/common/dist"
@@ -39,6 +22,4 @@ cp -rfv source/shaders "$diststart/common/dist"
 cp -rfv source/autoexec.lua "$diststart/common/dist"
 cp -rfv source/config.lua "$diststart/common/dist"
 cp -rfv source/scripts "$diststart/common/dist"
-cp -rfv bchunk/bchunk "$diststart/common/dist"
-cp -rfv xorriso-1.5.2/xorriso/xorriso "$diststart/common/dist"
 cp -rfv assets/*.sh "$diststart/common/dist"
--- engines/opentomb/env.sh
+++ engines/opentomb/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export STEAM_APP_ID_LIST="224960 225300 225320 224980 22500"
+export STEAM_APP_ID_LIST="225300 225320 224980 22500"
 export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./source/README.md ./source/resource/fonts/LICENSE ./bchunk/COPYING ./xorriso-1.5.2/COPYING"
+export ADDITIONAL_LICENSES="./source/README.md ./source/resource/fonts/LICENSE"
 export COMMON_PACKAGE="1"
