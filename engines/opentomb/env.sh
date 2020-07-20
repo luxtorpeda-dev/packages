1f5b3cf5f464629a9ab648c5463d9e7f8ead2195
d10sfan <d10sfan@gmail.com>
2020-07-19 20:19:58 -0500

    helper packages

--- engines/opentomb/env.sh
+++ engines/opentomb/env.sh
@@ -2,5 +2,5 @@
 
 export STEAM_APP_ID_LIST="224960 225300 225320 224980 22500"
 export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./source/README.md ./source/resource/fonts/LICENSE"
+export ADDITIONAL_LICENSES="./source/README.md ./source/resource/fonts/LICENSE ./bchunk/COPYING ./xorriso-1.5.2/COPYING"
 export COMMON_PACKAGE="1"
