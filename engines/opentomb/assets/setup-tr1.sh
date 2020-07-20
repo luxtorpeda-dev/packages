073f0c786c3312d899e9992fbaf5a07048bbcee1
d10sfan <d10sfan@gmail.com>
2020-07-19 21:20:41 -0500

    Cleanup scripts

--- engines/opentomb/assets/setup-tr1.sh
+++ engines/opentomb/assets/setup-tr1.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-./setup-opentomb.sh tr1
+GAMENAME="tr1"
+
+mkdir -p ./data/"$GAMENAME"
+
+cat ./GAME.DAT | head -n 3 >> ./GAME.cue
+./bchunk ./GAME.GOG ./GAME.cue TRACK
+./xorriso -osirrox on -indev TRACK01.iso -extract / ./data/"$GAMENAME"
+
+mv ./data/"$GAMENAME"/DATA ./data/"$GAMENAME"/data
+rm TRACK01.iso
