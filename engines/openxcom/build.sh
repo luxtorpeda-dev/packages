4afbe962d930cbc19cc93b2e96480f4cf8be8d80
d10sfan <d10sfan@gmail.com>
2020-07-29 14:38:53 -0500

    Initial

---
+++ engines/openxcom/build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/OpenXcom/OpenXcom.git source
+pushd source
+git checkout bd342df
+popd
+
+git clone https://github.com/ferzkopp/SDL_gfx.git sdl_gfx
+pushd sdl_gfx
+git checkout -f 0df23ee
+popd
+
+git clone https://github.com/jbeder/yaml-cpp.git yaml-cpp
+pushd yaml-cpp
+git checkout -f 98acc5a
+popd
+
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
+# BUILD PHASE
+pushd "sdl_gfx"
+./configure --prefix="$pfx"
+make -j "$(nproc)"
+make install
+popd
+
+pushd yaml-cpp
+mkdir build
+cd build
+cmake \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+pushd source
+mkdir build
+cd build
+cmake \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    -DCMAKE_BUILD_TYPE=Release \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+# COPY PHASE
---
+++ engines/openxcom/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="7650 7760"
+export LICENSE_PATH="./source/LICENSE.txt"
+export ADDITIONAL_LICENSES="./sdl_gfx/LICENSE ./yaml-cpp/LICENSE"
+export COMMON_PACKAGE="1"
