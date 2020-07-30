243f725ccb62c1e46d5e8ae9c191a29320979042
d10sfan <d10sfan@gmail.com>
2020-07-29 21:51:11 -0500

    force rebuild

--- engines/openxcom/build.sh
+++ engines/openxcom/build.sh
@@ -50,5 +50,5 @@ popd
 # COPY PHASE
 mkdir -p "$diststart/common/dist/lib"
 cp -rfv "$pfx/lib/"*.so* "$diststart/common/dist/lib"
-cp -rfv "$pfx/share/" "$diststart/common/dist/"
+cp -rfv "$pfx/share/" "$diststart/common/dist"
 cp -rfv "$pfx/bin/openxcom" "$diststart/common/dist"
