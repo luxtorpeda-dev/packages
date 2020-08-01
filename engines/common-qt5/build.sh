a04e8481b8d4d02dddfa4bec6b00cd93ddebb938
d10sfan <d10sfan@gmail.com>
2020-08-01 11:22:54 -0500

    qt5 - Add common

---
+++ engines/common-qt5/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://code.qt.io/qt/qt5.git source
+pushd source
+git checkout -f 5.12
+git submodule update --init --recursive
+popd
+
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
+# BUILD PHASE
+mkdir -p qt5-build
+pushd qt5-build
+../source/configure -opensource -nomake examples -nomake tests -confirm-license --prefix="$pfx"
+make -j$(nproc)
+make install
+popd
+
+# COPY PHASE
---
+++ engines/common-qt5/env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST=""
+export LICENSE_PATH="./source/LICENSE.LGPLv3"
+export ADDITIONAL_LICENSES="./source/LICENSE.GPLv3 ./source/LICENSE.FDL"
+export GCC_9="1"
+export COMMON_PACKAGE="1"
