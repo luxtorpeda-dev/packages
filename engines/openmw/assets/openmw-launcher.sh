a8547c03fc9807bf2563135a96538d7ef0a738c9
d10sfan <sales@circuitcube.com>
2020-08-02 14:40:00 -0500

    openmw - Force re-build

--- engines/openmw/assets/openmw-launcher.sh
+++ engines/openmw/assets/openmw-launcher.sh
@@ -10,5 +10,4 @@ fi
 
 ./openmw-iniimporter Morrowind.ini openmw.cfg
 
-
 LD_LIBRARY_PATH=qt5/lib:$LD_LIBRARY_PATH QT_QPA_PLATFORM_PLUGIN_PATH=./qt5/plugins ./openmw-launcher --data-local "Data Files" "$@" 
