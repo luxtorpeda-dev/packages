68e7ed09116f678bd14b70c0494f4df2abaf07b4
d10sfan <d10sfan@gmail.com>
2020-08-01 21:53:20 -0500

    fix

--- engines/common-qt5/build.sh
+++ engines/common-qt5/build.sh
@@ -15,11 +15,10 @@ sudo apt-get -y install flex gperf bison
 # BUILD PHASE
 mkdir -p qt5-build
 pushd qt5-build
-../source/configure -opensource -nomake examples -nomake tests -confirm-license -prefix "$pfx" -skip qtconnectivity -skip qtandroidextras -skip qtpurchasing -skip qtserialbus -skip qtserialport -skip qtcharts -skip qtcanvas3d -skip qt3d -skip qtwebview -skip qtvirtualkeyboard -skip qtwayland -skip qtcharts -skip qtsensors -skip qtdatavis3d -skip qtdocgallery -skip qtfeedback -skip qtlocation -skip qttools -skip qtwebglplugin -skip qttranslations
+../source/configure -opensource -nomake examples -nomake tests -confirm-license -prefix "$pfx" -skip qtconnectivity -skip qtandroidextras -skip qtpurchasing -skip qtserialbus -skip qtserialport -skip qtcharts -skip qtcanvas3d -skip qt3d -skip qtwebview -skip qtvirtualkeyboard -skip qtwayland -skip qtcharts -skip qtsensors -skip qtdatavis3d -skip qtdocgallery -skip qtfeedback -skip qtlocation -skip qttools -skip qtwebglplugin -skip qttranslations -skip qtwebsockets
 make -j $(nproc)
 make install
 popd
 
 # COPY PHASE
-cp -rfv "$pfx/lib" "$diststart/common/dist"
-cp -rfv "$pfx/include" "$diststart/common/dist"
+cp -rfv "$pfx/"* "$diststart/common/dist"
