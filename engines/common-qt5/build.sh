8de93f10c54137c93662019f62657df7164d9519
d10sfan <sales@circuitcube.com>
2020-08-02 14:34:47 -0500

    qt5 x11 fix (#102)
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh

--- engines/common-qt5/build.sh
+++ engines/common-qt5/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://code.qt.io/qt/qt5.git source
 pushd source
-git checkout -f 5.12
+git checkout -f 5.9
 git submodule update --init --recursive
 popd
 
@@ -15,7 +15,7 @@ sudo apt-get -y install flex gperf bison
 # BUILD PHASE
 mkdir -p qt5-build
 pushd qt5-build
-../source/configure -opensource -nomake examples -nomake tests -confirm-license -prefix "$pfx" -skip qtconnectivity -skip qtandroidextras -skip qtpurchasing -skip qtserialbus -skip qtserialport -skip qtcharts -skip qtcanvas3d -skip qt3d -skip qtwebview -skip qtvirtualkeyboard -skip qtwayland -skip qtcharts -skip qtsensors -skip qtdatavis3d -skip qtdocgallery -skip qtfeedback -skip qtlocation -skip qttools -skip qtwebglplugin -skip qttranslations -skip qtwebsockets
+../source/configure -opensource -nomake examples -nomake tests -confirm-license -prefix "$pfx" -skip qtconnectivity -skip qtandroidextras -skip qtpurchasing -skip qtquick1 -skip qtserialbus -skip qtserialport -skip qtcharts -skip qtcanvas3d -skip qt3d -skip qtwebview -skip qtvirtualkeyboard -skip qtwayland -skip qtcharts -skip qtsensors -skip qtdatavis3d -skip qtdocgallery -skip qtfeedback -skip qtlocation -skip qttools -skip qttranslations -skip qtwebsockets -qt-xcb
 make -j $(nproc)
 make install
 popd
