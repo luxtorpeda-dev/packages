3fcf3ab0c9870073c0904c41c449e9d0441efd29
d10sfan <sales@circuitcube.com>
2020-08-02 11:59:55 -0500

    Update build.sh

--- engines/common-qt5/build.sh
+++ engines/common-qt5/build.sh
@@ -15,7 +15,7 @@ sudo apt-get -y install flex gperf bison
 # BUILD PHASE
 mkdir -p qt5-build
 pushd qt5-build
-../source/configure -opensource -nomake examples -nomake tests -confirm-license -prefix "$pfx" -skip qtconnectivity -skip qtandroidextras -skip qtpurchasing -skip qtserialbus -skip qtserialport -skip qtcharts -skip qtcanvas3d -skip qt3d -skip qtwebview -skip qtvirtualkeyboard -skip qtwayland -skip qtcharts -skip qtsensors -skip qtdatavis3d -skip qtdocgallery -skip qtfeedback -skip qtlocation -skip qttools -skip qttranslations -skip qtwebsockets -qt-xcb
+../source/configure -opensource -nomake examples -nomake tests -confirm-license -prefix "$pfx" -skip qtconnectivity -skip qtandroidextras -skip qtpurchasing -skip qtquick1 -skip qtserialbus -skip qtserialport -skip qtcharts -skip qtcanvas3d -skip qt3d -skip qtwebview -skip qtvirtualkeyboard -skip qtwayland -skip qtcharts -skip qtsensors -skip qtdatavis3d -skip qtdocgallery -skip qtfeedback -skip qtlocation -skip qttools -skip qttranslations -skip qtwebsockets -qt-xcb
 make -j $(nproc)
 make install
 popd
