78606b68c95ee5f5309adfd3e0808a15e3ad4d5b
d10sfan <sales@circuitcube.com>
2020-08-01 23:04:59 -0500

    Common qt5 (#100)

---
+++ engines/common-qt5/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://code.qt.io/qt/qt5.git source
+pushd source
+git checkout -f 5.12
+git submodule update --init --recursive
+popd
+
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
+sudo apt-get -y install flex gperf bison
+
+# BUILD PHASE
+mkdir -p qt5-build
+pushd qt5-build
+../source/configure -opensource -nomake examples -nomake tests -confirm-license -prefix "$pfx" -skip qtconnectivity -skip qtandroidextras -skip qtpurchasing -skip qtserialbus -skip qtserialport -skip qtcharts -skip qtcanvas3d -skip qt3d -skip qtwebview -skip qtvirtualkeyboard -skip qtwayland -skip qtcharts -skip qtsensors -skip qtdatavis3d -skip qtdocgallery -skip qtfeedback -skip qtlocation -skip qttools -skip qtwebglplugin -skip qttranslations -skip qtwebsockets
+make -j $(nproc)
+make install
+popd
+
+# COPY PHASE
+cp -rfv "$pfx/"* "$diststart/common/dist"
---
+++ engines/common-qt5/env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST=""
+export LICENSE_PATH="./source/LICENSE.LGPLv3"
+export ADDITIONAL_LICENSES="./source/LICENSE.GPLv3 ./source/LICENSE.FDL"
+export GCC_9="1"
+export COMMON_PACKAGE="1"
+export ARCHIVE_WITHOUT_V7="1"
