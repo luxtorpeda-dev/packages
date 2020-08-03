128adfb451a6be9445c8486065895f644e370e22
d10sfan <d10sfan@gmail.com>
2020-08-02 23:56:29 -0500

    warzone2100

---
+++ engines/warzone2100/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/OpenMW/openmw source
+pushd source
+git checkout -f 0abcb54
+git submodule update --init --recursive
+popd
+
+git clone https://github.com/jedisct1/libsodium.git libsodium
+pushd libsodium
+git checkout -f 1.0.18
+popd
+
+# BUILD PHASE
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
+pushd "libsodium"
+make -j "$(nproc)"
+DESTDIR="$pfx" make install
+popd
+
+pushd "source"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_PREFIX_PATH="$pfx;$pfx/qt5" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    -GNinja \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    ..
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
