e23f75c1288f312943c5b5b3f50d6f204d81ce8b
d10sfan <d10sfan@gmail.com>
2020-08-07 00:01:13 -0500

    scummvm - Fix script to get path

--- engines/scummvm/assets/run-scummvm.sh
+++ engines/scummvm/assets/run-scummvm.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
-LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/scummvm --add --recursive
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/scummvm --add --path=../ --recursive
 LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/scummvm --fullscreen --gfx-mode=opengl --themepath=./share/scummvm
