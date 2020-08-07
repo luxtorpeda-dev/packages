1eceec5edfaec94def6ad45e61b3627147ce76d5
d10sfan <d10sfan@gmail.com>
2020-08-06 22:35:25 -0500

    licenses

--- engines/scummvm/env.sh
+++ engines/scummvm/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export STEAM_APP_ID_LIST=""
-export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./fluidsynth/LICENSE ./mad/COPYING ./mad/COPYRIGHT"
+export LICENSE_PATH="./source/COPYING"
+export ADDITIONAL_LICENSES="./fluidsynth/LICENSE ./mad/COPYING ./mad/COPYRIGHT ./source/COPYING.BSD ./source/COPYING.FREEFONT ./source/COPYING.LGPL ./source/COPYING.OFL ./source/COPYRIGHT"
 export COMMON_PACKAGE="1"
