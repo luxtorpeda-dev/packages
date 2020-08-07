01c5a1bb0779e7d745c8a6a374a889d45b767eed
d10sfan <sales@circuitcube.com>
2020-08-06 23:40:22 -0500

    Scummvm (#109)

---
+++ engines/scummvm/assets/run-scummvm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR"
+
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/scummvm --add --recursive
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/scummvm --fullscreen --gfx-mode=opengl --themepath=./share/scummvm
---
+++ engines/scummvm/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="1368340 default"
+export LICENSE_PATH="./source/COPYING"
+export ADDITIONAL_LICENSES="./fluidsynth/LICENSE ./mad/COPYING ./mad/COPYRIGHT ./source/COPYING.BSD ./source/COPYING.FREEFONT ./source/COPYING.LGPL ./source/COPYING.OFL ./source/COPYRIGHT"
+export COMMON_PACKAGE="1"
