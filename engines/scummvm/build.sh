fe1a9152f92c8dcc7a4726ef8bf880c06e8cf428
d10sfan <d10sfan@gmail.com>
2020-08-06 22:41:22 -0500

    fix

--- engines/scummvm/build.sh
+++ engines/scummvm/build.sh
@@ -14,6 +14,10 @@ popd
 git clone https://github.com/markjeee/libmad.git mad
 pushd mad
 git checkout -f c2f96fa
+# from http://www.linuxfromscratch.org/blfs/view/svn/multimedia/libmad.html
+patch -Np1 -i ../patches/libmad-0.15.1b-fixes-1.patch
+sed "s@AM_CONFIG_HEADER@AC_CONFIG_HEADERS@g" -i configure.ac
+autoreconf -fi
 popd
 
 readonly pfx="$PWD/local"
@@ -30,7 +34,7 @@ make -j "$(nproc)" install
 popd
 
 pushd "mad"
-./configure --prefix="$pfx"
+./configure --prefix="$pfx" --disable-static
 make -j "$(nproc)" install
 popd
 
