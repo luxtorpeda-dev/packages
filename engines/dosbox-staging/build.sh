d87592d4e07331686ae984bdc05640729e81b5d4
d10sfan <d10sfan@gmail.com>
2020-08-08 09:32:13 -0500

    Initial

---
+++ engines/dosbox-staging/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/dosbox-staging/dosbox-staging.git source
+pushd source
+git checkout -f 70999dc
+popd
+
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
+# BUILD PHASE
+pushd "source"
+./autogen.sh
+./configure --prefix="$pfx"
+make -j "$(nproc)"
+make install
+popd
+
+# COPY PHASE
+cp -rfv "$pfx/bin/dosbox" "$diststart/common/dist/dosbox-staging"
---
+++ engines/dosbox-staging/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="default"
+export LICENSE_PATH="./source/COPYING"
+export COMMON_PACKAGE="1"
