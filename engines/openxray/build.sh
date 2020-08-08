613694f536ba2d4e4b5ea07199593414fa32f581
d10sfan <d10sfan@gmail.com>
2020-08-07 23:15:22 -0500

    fixes

--- engines/openxray/build.sh
+++ engines/openxray/build.sh
@@ -288,13 +288,9 @@ cp -rfv "$pfx/bin/xr_3da" "$diststart/41700/dist"
 cp -rfv "$pfx/lib"/*.so* "$diststart/41700/dist/lib"
 cp -rfv "$pfx/Crypto++/lib"/*.so* "$diststart/41700/dist/lib"
 cp -rfv "$pstart/tbb/build"/libtbb*.so* "$diststart/41700/dist/lib"
-cp -rfv "$pstart/liblockfile/liblockfile.so" "$diststart/41700/dist/lib"
+cp -rfv "$pfx/LockFile/lib/"*.so* "$diststart/41700/dist/lib"
 cp -rfv glew/glew-2.1.0/lib/*.so* "$diststart/41700/dist/lib"
 
 cp assets/run-openxray.sh "$diststart/41700/dist"
 cp -rfv plus/res/gamedata/* "$diststart/41700/dist/gamedata"
 cp -rfv "$pfx/share/openxray"/* "$diststart/41700/dist/"
-
-pushd "$diststart/41700/dist/lib"
-ln -s "liblockfile.so" "liblockfile.so.1"
-popd
