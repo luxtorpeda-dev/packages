370eaae725aebb39747d18a2f16b094f74e40da6
d10sfan <d10sfan@gmail.com>
2020-08-07 22:46:16 -0500

    fix

--- common/lib.sh
+++ common/lib.sh
@@ -171,16 +171,18 @@ use_common_qt5 () {
     popd
 }
 
-setup_ubuntu() {
-    apt-get update
-    apt-get -y install build-essential cmake git g++-8 gcc-8 sudo wget unzip libx11-dev libgl1-mesa-dev automake libtool ncurses-dev pkg-config libpulse-dev freeglut3-dev
-    git config --global user.email "actions@github.com"
-    git config --global user.name "GitHub Action"
+setup_custom_container() {
+    if [ $CUSTOM_CONTAINER = "ubuntu1804" ]; then
+        apt-get update
+        apt-get -y install build-essential cmake git g++-8 gcc-8 sudo wget unzip libx11-dev libgl1-mesa-dev automake libtool ncurses-dev pkg-config libpulse-dev freeglut3-dev
+        git config --global user.email "actions@github.com"
+        git config --global user.name "GitHub Action"
 
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 8
-    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 8
-    sudo update-alternatives --set gcc "/usr/bin/gcc-8"
-    sudo update-alternatives --set g++ "/usr/bin/g++-8"
+        sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 8
+        sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 8
+        sudo update-alternatives --set gcc "/usr/bin/gcc-8"
+        sudo update-alternatives --set g++ "/usr/bin/g++-8"
+    fi
 }
 
 set -x
--- engines/openxray/env.sh
+++ engines/openxray/env.sh
@@ -3,4 +3,4 @@
 export STEAM_APP_ID_LIST="41700"
 export LICENSE_PATH="./source/License.txt"
 export ADDITIONAL_LICENSES="./freeimage/license-fi.txt ./liblockfile/COPYRIGHT ./readline/COPYING ./tbb/LICENSE ./glew/glew-2.1.0/LICENSE.txt ./openal/COPYING ./theora/COPYING ./ogg/COPYING ./SDL/COPYING.txt ./libjpeg-turbo/LICENSE.md ./pcre/LICENSE"
-export UBUNTU="1804"
+export CUSTOM_CONTAINER="ubuntu1804"
