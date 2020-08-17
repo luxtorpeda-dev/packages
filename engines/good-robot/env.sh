850cbdaaf235e7a18543d7b4c407139cbeb17ff0
d10sfan <d10sfan@gmail.com>
2020-08-17 16:53:29 -0500

    fix

--- engines/good-robot/env.sh
+++ engines/good-robot/env.sh
@@ -2,4 +2,4 @@
 
 export STEAM_APP_ID_LIST="358830"
 export LICENSE_PATH="./source/LICENSE.txt"
-export ADDITIONAL_LICENSES="/boost/LICENSE_1_0.txt ./devil/LICENSE"
+export ADDITIONAL_LICENSES="/boost/LICENSE_1_0.txt ./devil/LICENSE ./freealut/COPYING"
