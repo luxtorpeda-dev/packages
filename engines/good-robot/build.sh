15c6899b88f68c33dc6bc7d95bc868396ee2b48f
d10sfan <d10sfan@gmail.com>
2020-08-17 21:05:38 -0500

    progress

--- engines/good-robot/build.sh
+++ engines/good-robot/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get -y install mercurial
+
 # CLONE PHASE
 git clone https://github.com/arvindrajayadav/Good-Robot.git source
 pushd source
@@ -21,6 +23,27 @@ git clone https://github.com/vancegroup/freealut.git freealut
 pushd freealut
 git checkout -f 570dea5
 popd
+
+git clone https://github.com/kcat/openal-soft.git openal
+pushd openal
+git checkout -f f5e0eef
+popd
+
+hg clone https://hg.libsdl.org/SDL
+pushd SDL
+hg checkout release-2.0.12
+popd
+
+git clone https://github.com/aseprite/freetype2.git freetype2
+pushd freetype2
+git checkout -f fbbcf50
+popd
+
+wget https://github.com/nigels-com/glew/releases/download/glew-2.1.0/glew-2.1.0.zip
+unzip glew-2.1.0.zip -d glew
+
+export CXXFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas -w"
+export CFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas -w"
 
 readonly pfx="$PWD/local"
 mkdir -p "$pfx"
@@ -49,6 +72,53 @@ mkdir build
 cd build || exit 1
 cmake \
     -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+# build glew
+pushd glew/glew-2.1.0
+GLEW_DEST="$pfx" make -j "$(nproc)"
+GLEW_DEST="$pfx" make install
+make install
+popd
+
+pushd "openal"
+rm -rf build
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    -DBUILD_SHARED_LIBS=ON \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+pushd "SDL"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+pushd freetype2
+mkdir build
+cd build
+cmake \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    -DCMAKE_CXX_FLAGS="-fPIC" \
+    -DCMAKE_C_FLAGS="-fPIC" \
+    -DBUILD_SHARED_LIBS=ON \
     ..
 make -j "$(nproc)"
 make install
--- engines/good-robot/env.sh
+++ engines/good-robot/env.sh
@@ -2,5 +2,6 @@
 
 export STEAM_APP_ID_LIST="358830"
 export LICENSE_PATH="./source/LICENSE.txt"
-export ADDITIONAL_LICENSES="./boost/LICENSE_1_0.txt ./devil/LICENSE ./freealut/COPYING ./source/steamworks/Readme.txt"
-export GCC_9="1"
+export ADDITIONAL_LICENSES="./boost/LICENSE_1_0.txt ./devil/LICENSE ./freealut/COPYING ./source/steamworks/Readme.txt ./glew/glew-2.1.0/LICENSE.txt ./openal/COPYING ./SDL/COPYING.txt ./freetype2/README"
+export CUSTOM_CONTAINER="ubuntu:18.04"
+export COMMON_PACKAGE="1"
