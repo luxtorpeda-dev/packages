113a92150d1ff493a04f0e15ea1187fc2fb5caeb
d10sfan <d10sfan@gmail.com>
2020-08-17 23:19:01 -0500

    cleanup

--- engines/good-robot/build.sh
+++ engines/good-robot/build.sh
@@ -140,10 +140,22 @@ cmake \
 make
 popd
 
+rm -rf source/build
+pushd "source"
+mkdir build
+cd build || exit 1
+cmake \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DBOOST_ROOT="$boostlocation" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    -DSDL2_LIBRARIES="$pfx/lib/libSDL2-2.0.so.0.12.0" \
+    ..
+make
+popd
+
 # COPY PHASE
 mkdir -p "$diststart/358830/dist/lib"
 cp -rfv "$pfx/lib/"*.so* "$diststart/358830/dist/lib"
 cp -rfv source/build/good_robot "$diststart/358830/dist/"
 cp -rfv assets/run-good-robot.sh "$diststart/358830/dist/"
-cp -rfv boost/stage/lib/*.so* "$diststart/358830/dist/lib"
 cp -rfv source/steamworks/redistributable_bin/linux64/libsteam_api.so "$diststart/358830/dist/lib"
