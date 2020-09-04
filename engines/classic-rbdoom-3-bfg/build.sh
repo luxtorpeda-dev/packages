3e77f7c908c7d4494926f54df84410f486d72510
d10sfan <d10sfan@gmail.com>
2020-09-04 13:27:56 -0500

    Add classic-rbdoom-3-bfg

---
+++ engines/classic-rbdoom-3-bfg/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/MadDeCoDeR/Classic-RBDOOM-3-BFG.git source
+pushd source
+git checkout 99c9d86
+popd
+
+git clone https://github.com/FFmpeg/FFmpeg.git ffmpeg
+pushd ffmpeg
+git checkout -f 523da8ea
+git submodule update --init --recursive
+popd
+
+readonly pfx="$PWD/local"
+readonly tmp="$PWD/tmp"
+mkdir -p "$pfx"
+mkdir -p "$tmp"
+
+# BUILD PHASE
+./build-ffmpeg.sh
+
+pushd "source"
+mkdir build
+cd build
+cmake \
+    -G "Eclipse CDT4 - Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DSDL2=ON \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    ../neo
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
+cp "source/build/RBDoom3BFG" "$diststart/208200/dist/RBDoom3BFG"
+mkdir -p "$diststart/208200/dist/lib"
+cp -rfv "$pfx/"lib/*.so* "$diststart/208200/dist/lib/"
+cp -rfv ./assets/run-rbdoom-3-bfg.sh "$diststart/208200/dist/"
---
+++ engines/classic-rbdoom-3-bfg/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="208200"
+export LICENSE_PATH="./source/COPYING.txt"
+export ADDITIONAL_LICENSES="./source/README.md ./ffmpeg/COPYING.LGPLv2.1"
