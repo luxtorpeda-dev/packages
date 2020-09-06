d1cd8163efbfc3ea5dc4f9086414d0916528f3bc
d10sfan <d10sfan@gmail.com>
2020-09-05 20:44:53 -0500

    classic-rbdoom-3-bfg - Use Ubuntu 18.04

--- engines/classic-rbdoom-3-bfg/build.sh
+++ engines/classic-rbdoom-3-bfg/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get -y install mercurial
+
 # CLONE PHASE
 git clone https://github.com/MadDeCoDeR/Classic-RBDOOM-3-BFG.git source
 pushd source
@@ -12,13 +14,49 @@ git checkout -f 523da8ea
 git submodule update --init --recursive
 popd
 
+git clone https://github.com/kcat/openal-soft.git openal
+pushd openal
+git checkout -f f5e0eef
+popd
+
+hg clone https://hg.libsdl.org/SDL
+pushd SDL
+hg checkout release-2.0.12
+popd
+
 readonly pfx="$PWD/local"
 readonly tmp="$PWD/tmp"
 mkdir -p "$pfx"
 mkdir -p "$tmp"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$pfx/lib/pkgconfig"
 
 # BUILD PHASE
 ./build-ffmpeg.sh
+
+pushd "openal"
+rm -rf build
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+pushd "SDL"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
 
 pushd "source"
 mkdir build
