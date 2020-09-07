9a0641d8490ed23be8114971520264003703fdf4
d10sfan <d10sfan@gmail.com>
2020-09-06 23:07:34 -0500

    f?

--- engines/doomsday/build.sh
+++ engines/doomsday/build.sh
@@ -30,6 +30,15 @@ cmake \
 make -j "$(nproc)" install
 popd
 
+pushd "fluidsynth"
+rm -rf build
+mkdir -p build
+cd build
+cmake \
+    ..
+make -j "$(nproc)" install
+popd
+
 pushd "source"
 mkdir -p build
 cd build
