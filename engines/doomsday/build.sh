063253e592138f7834db1987573fce7795b14cc7
d10sfan <d10sfan@gmail.com>
2020-09-06 21:52:42 -0500

    fix

--- engines/doomsday/build.sh
+++ engines/doomsday/build.sh
@@ -22,6 +22,7 @@ cmake \
     -DCMAKE_PREFIX_PATH="$pfx;$pfx/qt5;$pfx/usr/local" \
     -DCMAKE_INSTALL_PREFIX="$pfx" \
     -DCMAKE_BUILD_TYPE=Release \
+    -DDENG_STATIC_LINK=ON \
     ..
 make -j "$(nproc)"
 DESTDIR="$pfx" make install
@@ -29,7 +30,7 @@ popd
 
 # COPY PHASE
 mkdir -p "$diststart/common/dist/lib"
-cp -rfv "$pfx/usr/bin/"* "$diststart/common/dist/"
+cp -rfv "$pfx/usr/bin/" "$diststart/common/dist/bin"
 cp -rfv "$pfx/usr/lib/x86_64-linux-gnu/"*.so* "$diststart/common/dist/lib"
 cp -rfv "$pfx/usr/lib/x86_64-linux-gnu/doomsday/"*.so* "$diststart/common/dist/lib"
-cp -rfv "$pfx/usr/share/doomsday/"* "$diststart/common/dist/"
+cp -rfv "$pfx/usr/share/doomsday/"* "$diststart/common/dist/bin"
