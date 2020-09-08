10309c784fd9ee7478041bd3a2fb763ff1d9616c
d10sfan <sales@circuitcube.com>
2020-09-07 22:18:24 -0500

    Reminiscence (#125)

---
+++ engines/reminiscence/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# CLONE PHASE
+wget http://cyxdown.free.fr/reminiscence/REminiscence-0.4.6.tar.bz2
+tar xvf REminiscence-0.4.6.tar.bz2
+
+git clone https://github.com/Konstanty/libmodplug.git
+pushd libmodplug
+git checkout -f d7ba5ef
+popd
+
+git clone https://gitlab.xiph.org/xiph/tremor.git
+pushd tremor
+git checkout -f 7c30a66346199f3f09017a09567c6c8a3a0eedc8
+popd
+
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
+# BUILD PHASE
+pushd "libmodplug"
+mkdir -p build
+cd build
+cmake \
+    ..
+make -j "$(nproc)" install
+popd
+
+pushd "tremor"
+./autogen.sh
+make
+make install
+popd
+
+# BUILD PHASE
+pushd REminiscence-0.4.6
+make
+popd
+
+# COPY PHASE
+cp -rfv REminiscence-0.4.6/rs "$diststart/961620/dist/rs"
+mkdir -p "$diststart/961620/dist/lib"
+cp -rfv /usr/local/lib/libvorbisidec.so.* "$diststart/961620/dist/lib"
+cp -rfv assets/* "$diststart/961620/dist/"
---
+++ engines/reminiscence/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="961620"
+export LICENSE_PATH="./REminiscence-0.4.6/README.txt"
+export ADDITIONAL_LICENSES="./libmodplug/COPYING ./tremor/COPYING"
