8f2616aea865cc742b88dd70dbd277885f83f4e0
d10sfan <sales@circuitcube.com>
2020-09-07 21:35:24 -0500

    Serious engine (#124)

---
+++ engines/serious-engine/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+apt-get -y install bison flex
+
+# CLONE PHASE
+git clone https://github.com/ptitSeb/Serious-Engine.git source
+pushd source
+git checkout 253d2b9
+popd
+
+# PRE-COPY PHASE
+mkdir -p "$diststart/41050/dist/Bin/"
+mkdir -p "$diststart/41060/dist/Bin/"
+cp -rfv source/SE1_10.gro "$diststart/41050/dist"
+cp -rfv source/SE1_10.gro "$diststart/41060/dist"
+
+# BUILD PHASE
+pushd source/Sources
+./build-linux64.sh
+popd
+
+# COPY PHASE
+cp -rfv source/Sources/cmake-build/ssam "$diststart/41060/dist/Bin/ssam"
+cp -rfv source/Sources/cmake-build/Debug/* "$diststart/41060/dist/Bin"
+
+# BUILD PHASE
+pushd source/Sources
+./build-linux64.sh -DTFE=TRUE
+popd
+
+# COPY PHASE
+cp -rfv source/Sources/cmake-build/ssam-tfe "$diststart/41050/dist/Bin/ssam-tfe"
+cp -rfv source/Sources/cmake-build/Debug/* "$diststart/41050/dist/Bin"
---
+++ engines/serious-engine/env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="41050 41060"
+export LICENSE_PATH="./source/LICENSE"
