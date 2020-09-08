ff6e7ba39c134f59893c6218bc4107f911794d1c
d10sfan <sales@circuitcube.com>
2020-09-07 20:50:58 -0500

    Chocolate doom (#123)

---
+++ engines/chocolate-doom/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="2280 2290 2300 2360 2390"
+export LICENSE_PATH="./source/COPYING.md"
+export ADDITIONAL_LICENSES="./fluidsynth/LICENSE"
+export COMMON_PACKAGE="1"
