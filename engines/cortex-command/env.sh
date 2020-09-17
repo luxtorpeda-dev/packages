1d9901774e850770b459f8e035f28c4fb04ce634
d10sfan <d10sfan@gmail.com>
2020-09-17 15:35:13 -0500

    fix

--- engines/cortex-command/env.sh
+++ engines/cortex-command/env.sh
@@ -3,4 +3,4 @@
 export STEAM_APP_ID_LIST="209670"
 export LICENSE_PATH="./source/LICENSE"
 export ADDITIONAL_LICENSES="./data/Credits.txt ./data/CreditsAssets.txt"
-export GCC_9 = "1"
+export GCC_9= "1"
