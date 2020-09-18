3e6eca055a5fdda6495f357b2e4df0924443be93
d10sfan <d10sfan@gmail.com>
2020-09-17 21:26:41 -0500

    fix

--- engines/cortex-command/build.sh
+++ engines/cortex-command/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt-get install -y mercurial meson ninja-build python3-pip
+sudo pip3 install meson
+
 # CLONE PHASE
 git clone https://github.com/cortex-command-community/Cortex-Command-Community-Project-Source.git source
 pushd source
@@ -22,9 +25,29 @@ git checkout -f afb333b7
 git submodule update --init --recursive
 popd
 
+git clone https://github.com/LuaJIT/LuaJIT.git luajit
+pushd luajit
+git checkout -f 570e758
+popd
+
+git clone https://github.com/nmoinvaz/minizip.git minizip
+pushd minizip
+git checkout -f 3ecff07
+popd
+
+git clone https://github.com/lz4/lz4.git lz4
+pushd lz4
+git checkout -f fdf2ef5
+popd
+
+hg clone https://hg.libsdl.org/SDL
+pushd SDL
+hg checkout release-2.0.12
+popd
+
 readonly pfx="$PWD/local"
 mkdir -p "$pfx"
-export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$pfx/lib/pkgconfig"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$pfx/lib/pkgconfig:$pfx/usr/local/lib/pkgconfig"
 
 # BUILD PHASE
 pushd allegro4
@@ -35,9 +58,50 @@ cmake .. \
     -DCMAKE_INSTALL_PREFIX="$pfx" \
     -DWANT_DOCS=OFF
 make -j "$(nproc)"
+make install
 popd
 
 ./build-boost.sh
 
+pushd luajit
+make -j "$(nproc)"
+DESTDIR="$pfx" make install
+popd
+
+pushd minizip
+mkdir -p build
+cd build
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="$pfx"
+make -j "$(nproc)"
+make install
+popd
+
+pushd lz4
+make -j "$(nproc)"
+DESTDIR="$pfx" make install
+popd
+
+pushd "SDL"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+pushd source
+BOOST_ROOT="$pfx" python3 ../meson/meson.py build
+cd build
+python3 ../meson/meson.py compile CCCP
+popd
 
 # COPY PHASE
+mkdir -p "$diststart/209670/dist/lib"
+cp -rfv "$pfx/"lib/*.so* "$diststart/209670/dist/lib/"
+cp -rfv "$pfx/"usr/local/lib/*.so* "$diststart/209670/dist/lib/"
--- engines/cortex-command/env.sh
+++ engines/cortex-command/env.sh
@@ -2,5 +2,5 @@
 
 export STEAM_APP_ID_LIST="209670"
 export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./data/Credits.txt ./data/CreditsAssets.txt"
-export GCC_9="1"
+export ADDITIONAL_LICENSES="./data/Credits.txt ./data/CreditsAssets.txt ./allegro4/docs/src/readme._tx ./boost/LICENSE_1_0.txt ./luajit/COPYRIGHT ./minizip/LICENSE ./lz4/LICENSE ./SDL/COPYING.txt"
+export CUSTOM_CONTAINER="ubuntu:18.04"
