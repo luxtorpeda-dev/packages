51be05ac2735fb007cdd10547b77e19f14924d8a
d10sfan <d10sfan@gmail.com>
2020-09-23 22:45:08 -0500

    Init

---
+++ engines/realrtcw/build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/wolfetplayer/RealRTCW.git source
+pushd source
+git checkout 7e6dc5d
+popd
+
+git clone https://github.com/dscharrer/innoextract.git innoextract
+pushd innoextract
+git checkout 81fd9b9
+popd
+
+git clone https://github.com/boostorg/boost boost
+pushd boost
+git checkout -f afb333b7
+git submodule update --init --recursive
+popd
+
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
+
+
+# BUILD PHASE
+pushd source || exit 1
+ARCH=x86_64 make
+popd || exit 1
+
+./build-boost.sh
+
+pushd innoextract
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+# COPY PHASE
+mkdir tmp || exit 1
+mkdir tmp/Main || exit 1
+
+cp -rfv source/build/release-linux-x86_64/*.so tmp/
+cp -rfv source/build/release-linux-x86_64/RealRTCW.x86_64 tmp/
+cp -rfv source/build/release-linux-x86_64/main/*.so tmp/Main/
+cp -rfv "$pfx/lib/"*.so tmp/
+cp -rfv "$pfx/bin/"* tmp/
+
+cp -rfv tmp/* "$diststart/9010/dist/"
---
+++ engines/realrtcw/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="9010"
+export LICENSE_PATH="./source/COPYING.txt"
+export ADDITIONAL_LICENSES="./source/README.txt ./source/README.md ./innoextract/LICENSE"
