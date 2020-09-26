e7772c5dd7cadae71b9f0013224fa919557308aa
d10sfan <d10sfan@gmail.com>
2020-09-26 17:05:45 -0500

    fix

--- engines/reone/build.sh
+++ engines/reone/build.sh
@@ -12,11 +12,26 @@ git checkout -f afb333b7
 git submodule update --init --recursive
 popd
 
+git clone https://github.com/markjeee/libmad.git mad
+pushd mad
+git checkout -f c2f96fa
+# from http://www.linuxfromscratch.org/blfs/view/svn/multimedia/libmad.html
+patch -Np1 -i ../patches/libmad-0.15.1b-fixes-1.patch
+sed "s@AM_CONFIG_HEADER@AC_CONFIG_HEADERS@g" -i configure.ac
+touch NEWS AUTHORS ChangeLog
+autoreconf -fi
+popd
+
 readonly pfx="$PWD/local"
 mkdir -p "$pfx"
 
 # BUILD PHASE
 ./build-boost.sh
+
+pushd "mad"
+./configure --prefix="$pfx" --disable-static
+make -j "$(nproc)" install
+popd
 
 pushd "source"
 mkdir -p build
--- engines/reone/env.sh
+++ engines/reone/env.sh
@@ -2,4 +2,4 @@
 
 export STEAM_APP_ID_LIST="32370"
 export LICENSE_PATH="./source/COPYING"
-export ADDITIONAL_LICENSES="./boost/LICENSE_1_0.txt"
+export ADDITIONAL_LICENSES="./boost/LICENSE_1_0.txt ./mad/COPYING ./mad/COPYRIGHT"
