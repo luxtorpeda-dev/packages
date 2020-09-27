9f0dea80e2785e3dd3f9a573464042fee86ab92a
d10sfan <d10sfan@gmail.com>
2020-09-26 22:03:20 -0500

    fix

--- engines/realrtcw/assets/setup-realrtcw.sh
+++ engines/realrtcw/assets/setup-realrtcw.sh
@@ -4,6 +4,6 @@ ln -rsf ./Main/*.pk3 ./realrtcw/Main
 
 cd realrtcw
 
-LD_LIBRARY_PATH=./lib ./innoextract realrtcwsetup.exe
+LD_LIBRARY_PATH=./lib ./innoextract realrtcwcompletesetup.exe
 cp -r ./app/* ./
 rm -rf ./app
