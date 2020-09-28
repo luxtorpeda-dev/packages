8a2762f38a8dffbef4b873c6935f624c65a6644e
d10sfan <d10sfan@gmail.com>
2020-09-27 21:07:10 -0500

    fix

--- engines/reone/build.sh
+++ engines/reone/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+apt-get -y install mercurial
+
 # CLONE PHASE
 git clone https://github.com/seedhartha/reone.git source
 pushd source
-git checkout -f 159d490
+git checkout -f 641e981
 popd
 
 git clone https://github.com/boostorg/boost boost
@@ -22,8 +24,22 @@ touch NEWS AUTHORS ChangeLog
 autoreconf -fi
 popd
 
+hg clone https://hg.libsdl.org/SDL
+pushd SDL
+hg checkout release-2.0.12
+popd
+
+git clone https://github.com/kcat/openal-soft.git openal
+pushd openal
+git checkout -f f5e0eef
+popd
+
+wget https://github.com/nigels-com/glew/releases/download/glew-2.1.0/glew-2.1.0.zip
+unzip glew-2.1.0.zip -d glew
+
 readonly pfx="$PWD/local"
 mkdir -p "$pfx"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$pfx/lib/pkgconfig"
 
 # BUILD PHASE
 ./build-boost.sh
@@ -31,6 +47,50 @@ mkdir -p "$pfx"
 pushd "mad"
 ./configure --prefix="$pfx" --disable-static
 make -j "$(nproc)" install
+popd
+
+pushd glew/glew-2.1.0
+GLEW_DEST="$pfx" make -j "$(nproc)"
+GLEW_DEST="$pfx" make install
+make install
+popd
+
+pushd "openal"
+rm -rf build
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    -DBUILD_SHARED_LIBS=ON \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+pushd "openal"
+rm -rf build
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DBUILD_SHARED_LIBS=ON \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+pushd "SDL"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
 popd
 
 pushd "source"
@@ -47,5 +107,6 @@ popd
 # COPY PHASE
 mkdir -p "$diststart/32370/dist/lib/"
 cp -rfv "local/lib"/*.so* "$diststart/32370/dist/lib/"
+cp -rfv glew/glew-2.1.0/lib/*.so* "$diststart/32370/dist/lib"
 cp -rfv source/build/reone* "$diststart/32370/dist/"
 cp -rfv assets/* "$diststart/32370/dist/"
