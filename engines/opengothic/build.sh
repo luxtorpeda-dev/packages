7f52c5a4493693832eb46ce8d3d8ee1551126c42
d10sfan <d10sfan@gmail.com>
2020-11-07 10:38:33 -0600

    opengothic - Update to 0.32

--- engines/opengothic/build.sh
+++ engines/opengothic/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/Try/OpenGothic source
 pushd source
-git checkout -f 90c7c24  
+git checkout -f 202f847  
 git submodule update --init --recursive
 popd
 curl -fsSL -o glslang-master-linux-Release.zip https://github.com/KhronosGroup/glslang/releases/download/master-tot/glslang-master-linux-Release.zip
