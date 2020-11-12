b1a77bff760cff14f0d08c54a2f016985940e7e0
d10sfan <d10sfan@gmail.com>
2020-11-11 22:50:39 -0600

    env

--- engines/reone/env.sh
+++ engines/reone/env.sh
@@ -2,5 +2,5 @@
 
 export STEAM_APP_ID_LIST="32370"
 export LICENSE_PATH="./source/COPYING"
-export ADDITIONAL_LICENSES="./boost/LICENSE_1_0.txt ./mad/COPYING ./mad/COPYRIGHT ./glew/glew-2.1.0/LICENSE.txt ./openal/COPYING ./SDL/COPYING.txt"
+export ADDITIONAL_LICENSES="./boost/LICENSE_1_0.txt ./mad/COPYING ./mad/COPYRIGHT ./glew/glew-2.1.0/LICENSE.txt ./openal/COPYING ./SDL/COPYING.txt ./ffmpeg/COPYING.LGPLv2.1"
 export CUSTOM_CONTAINER="ubuntu:18.04"
