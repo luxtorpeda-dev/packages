92b18a1e05c466c45dd4fd26f29753fcfc20dc96
d10sfan <d10sfan@gmail.com>
2020-11-26 17:11:13 -0600

    openloco

---
+++ engines/openloco/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/OpenLoco/OpenLoco.git source
+pushd source
+git checkout -f eb39c1a
+popd
+
+git clone https://github.com/glennrp/libpng.git libpng
+pushd libpng
+git checkout -f c17d164
+popd
+
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$pfx/lib/pkgconfig"
+
+# BUILD PHASE
+
+pushd libpng
+mkdir build
+cd build
+cmake \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+pushd source
+mkdir build
+cd build
+cmake \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DPNG_LIBRARIES="$pfx/lib/libpng16.so" \
+    ..
+make -j "$(nproc)"
+cp -rfv ../data .
+make g2
+popd
+
+# COPY PHASE
+mkdir -p "$diststart/356430/dist/lib"
+mkdir -p "$diststart/356430/dist/data"
+cp -rfv "$pfx/lib/"*.so* "$diststart/356430/dist/lib"
+
+cp -rfv "source/build/openloco" "$diststart/356430/dist/"
+cp -rfv "source/build/data/"* "$diststart/356430/dist/data"
+
+cp -rfv "assets/run-openloco.sh" "$diststart/356430/dist"
---
+++ engines/openloco/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="356430"
+export LICENSE_PATH="./source/LICENSE"
+export ADDITIONAL_LICENSES="./libpng/LICENSE"
+export GCC_9="1"
