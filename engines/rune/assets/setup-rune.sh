7bf11962a5098c8f7d3bc71ff8494ed3125b5491
d10sfan <d10sfan@gmail.com>
2020-12-26 22:59:52 -0600

    fixes

--- engines/rune/assets/setup-rune.sh
+++ engines/rune/assets/setup-rune.sh
@@ -2,9 +2,11 @@
 
 cd ../
 
+LD_LIBRARY_PATH=./System/7z ./System/7z/7z x -o"./" System/rune%2Bhon.7z
+mv ./"rune+hon" ./linuxdata
+
 rm linuxdata/System/Rune.ini
 cp System/Default.ini linuxdata/Default.ini
 cp linuxdata/System/Default.ini linuxdata/System/Rune.ini
 cp System/libSDL-1.2.so.0 linuxdata/System
 rm System/rune%2Bhon.7z
-rm linuxdata/System/rune%2Bhon.7z
