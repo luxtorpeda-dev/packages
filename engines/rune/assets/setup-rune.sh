b82e4a1c7b0ca98d5c8eedc5f7ff1c33c2bee119
d10sfan <d10sfan@gmail.com>
2020-12-26 22:16:49 -0600

    Fix

--- engines/rune/assets/setup-rune.sh
+++ engines/rune/assets/setup-rune.sh
@@ -4,5 +4,7 @@ cd ../
 
 rm linuxdata/System/Rune.ini
 cp System/Default.ini linuxdata/Default.ini
-cp linuxdata/System/Default.ini Rune.ini
+cp linuxdata/System/Default.ini linuxdata/System/Rune.ini
 cp System/libSDL-1.2.so.0 linuxdata/System
+rm System/rune%2Bhon.7z
+rm linuxdata/System/rune%2Bhon.7z
--- engines/rune/build.sh
+++ engines/rune/build.sh
@@ -10,6 +10,9 @@ popd
 wget https://downloads.sourceforge.net/project/p7zip/p7zip/16.02/p7zip_16.02_src_all.tar.bz2
 tar -xvjf p7zip_16.02_src_all.tar.bz2
 
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
 # BUILD PHASE
 pushd "sdlcl"
 make -j "$(nproc)"
@@ -18,9 +21,12 @@ popd
 pushd "p7zip_16.02"
 cp -rfv makefile.linux_amd64_asm makefile.machine
 make all3
-make install DEST_DIR="$diststart/210950/dist/"
+make install DEST_DIR="$pfx"
 popd
 
 # COPY PHASE
+mkdir -p "$diststart/210950/dist/7z/"
 cp -rfv assets/* "$diststart/210950/dist/"
+cp -rfv "$pfx/usr/local/bin/"* "$diststart/210950/dist/7z/"
+cp -rfv "$pfx/usr/local/lib/"* "$diststart/210950/dist/7z/"
 cp -rfv "sdlcl/libSDL-1.2.so.0" "$diststart/3970/dist/"
