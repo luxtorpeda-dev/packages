c898272a65dc69a3e91d212210377d45eaa8a302
d10sfan <4337981+d10sfan@users.noreply.github.com>
2020-12-27 09:28:50 -0600

    rune (#165)
    * rune
    
    * Fix
    
    * fix
    
    * fixes
    
    Co-authored-by: d10sfan <d10sfan@gmail.com>

---
+++ engines/rune/assets/run-rune.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ../ # game tries to start in system directory, so have to get out and back to the normal directory
+
+cd linuxdata
+
+pulseaudiolib=""
+
+if [[ -f "/usr/lib/i386-linux-gnu/pulseaudio/libpulsedsp.so" ]]; then
+    pulseaudiolib="/usr/lib/i386-linux-gnu/pulseaudio/libpulsedsp.so"
+elif [[ -f "/usr/lib32/pulseaudio/libpulsedsp.so" ]]; then
+    pulseaudiolib="/usr/lib32/pulseaudio/libpulsedsp.so"
+elif [[ -n $(cat /etc/os-release | grep NixOS) ]]; then
+    pulseaudiolib="$(nix-store --query -R $(which steam-run) | grep steam-run-fhs | tail -n1)/lib32/pulseaudio/libpulsedsp.so"
+fi
+
+if [ -z "$pulseaudiolib" ]; then
+    "$STEAM_ZENITY" --error --text="Could not find libpulsedsp.so"
+    exit 0
+fi
+
+LD_PRELOAD="$LD_PRELOAD $pulseaudiolib" LD_LIBRARY_PATH="./System" RUNE_DATA_PATH="./System" padsp ./rune
---
+++ engines/rune/assets/setup-rune.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ../
+
+LD_LIBRARY_PATH=./System/7z ./System/7z/7z x -o"./" System/rune%2Bhon.7z
+mv ./"rune+hon" ./linuxdata
+
+rm linuxdata/System/Rune.ini
+cp System/Default.ini linuxdata/Default.ini
+cp linuxdata/System/Default.ini linuxdata/System/Rune.ini
+cp System/libSDL-1.2.so.0 linuxdata/System
+rm System/rune%2Bhon.7z
