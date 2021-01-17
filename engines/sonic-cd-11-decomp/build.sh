4363c97d4ac1185c5ce768e9c799504f88f75d18
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-01-17 14:32:53 -0600

    Sonic cd 11 (#201)
    * init
    
    * gcc9
    
    * add metadata
    
    * Update packages.json
    
    Co-authored-by: d10sfan <d10sfan@gmail.com>

---
+++ engines/sonic-cd-11-decomp/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/Rubberduckycooly/Sonic-CD-11-Decompilation.git source
+pushd source
+git checkout -f 41c1397
+popd
+
+# BUILD PHASE
+
+pushd "source"
+make
+popd
+
+# COPY PHASE
+cp -rfv "source/soniccd" "$diststart/200940/dist/"
