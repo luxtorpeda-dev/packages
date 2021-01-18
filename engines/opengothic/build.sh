a87053cb2b14e6e689839ab06598f41d2bd96338
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-01-18 11:32:30 -0600

    opengothic - Update to 0.35 (#200)

--- engines/opengothic/build.sh
+++ engines/opengothic/build.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 
+sudo apt-get -y install curl libssl1.0-dev libasound2-dev
+
+wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
+sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-1.2.162-bionic.list https://packages.lunarg.com/vulkan/1.2.162/lunarg-vulkan-1.2.162-bionic.list
+sudo apt update
+sudo apt install -y vulkan-sdk
+
 # CLONE PHASE
 git clone https://github.com/Try/OpenGothic source
 pushd source
-git checkout -f 202f847  
+git checkout -f a144bbd3917fa51d067586fcde5a27daa8fe7bad  
 git submodule update --init --recursive
 popd
+
 curl -fsSL -o glslang-master-linux-Release.zip https://github.com/KhronosGroup/glslang/releases/download/master-tot/glslang-master-linux-Release.zip
 mkdir glslang
 unzip glslang-master-linux-Release.zip -d glslang
 
+git clone https://github.com/Kitware/CMake.git cmake
+pushd cmake
+git checkout -f 39c6ac5
+popd
+
 # BUILD PHASE
 readonly pstart="$PWD"
 export PATH="$PATH:$pstart/glslang/bin"
+
+pushd cmake
+./bootstrap
+make 
+sudo make install
+popd
+
+export CMAKE_ROOT=/usr/local/share/cmake-3.16/
+/usr/local/bin/cmake --version
 
 pushd "source"
 cmake -H. -Bbuild -DBUILD_EXTRAS:BOOL=OFF -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DBUILD_SHARED_MOLTEN_TEMPEST:BOOL=ON
