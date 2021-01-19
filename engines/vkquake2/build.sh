aeadf1620619721907c8fd9ac38b5d4b09372440
d10sfan <d10sfan@gmail.com>
2021-01-19 15:35:19 -0600

    fix paths to libraries

--- engines/vkquake2/build.sh
+++ engines/vkquake2/build.sh
@@ -22,4 +22,20 @@ make -j "$(nproc)" release
 popd
 
 # COPY PHASE
-cp -rfv "source/linux/releasex64/"* "$diststart/common/dist/"
+cp -rfv "source/linux/releasex64/quake2" "$diststart/common/dist/"
+cp -rfv "source/linux/releasex64/ref_*" "$diststart/common/dist/"
+cp -rfv "source/linux/releasex64/q2ded" "$diststart/common/dist/"
+
+cp -rfv "source/linux/releasex64/baseq2" "$diststart/common/dist/"
+
+mkdir -p "$diststart/common/dist/ctf"
+cp -rfv "source/linux/releasex64/ctf/gamex64.so" "$diststart/common/dist/ctf"
+
+mkdir -p "$diststart/common/dist/rogue"
+cp -rfv "source/linux/releasex64/rogue/gamex64.so" "$diststart/common/dist/rogue"
+
+mkdir -p "$diststart/common/dist/xatrix"
+cp -rfv "source/linux/releasex64/xatrix/gamex64.so" "$diststart/common/dist/xatrix"
+
+mkdir -p "$diststart/common/dist/zaero"
+cp -rfv "source/linux/releasex64/zaero/gamex64.so" "$diststart/common/dist/zaero"
