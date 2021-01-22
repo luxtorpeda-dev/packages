ddfe6387f5e8e660a99ed5e8b2995eb0f12ca5b3
d10sfan <d10sfan@gmail.com>
2021-01-21 22:47:50 -0600

    ?

--- engines/shockolate/build.sh
+++ engines/shockolate/build.sh
@@ -6,31 +6,18 @@ pushd source
 git checkout 7da4d1b
 popd
 
-git clone https://github.com/FluidSynth/fluidsynth.git fluidsynth
-pushd fluidsynth
-git checkout -f 19a20eb
-popd
-
 readonly pfx="$PWD/local"
 mkdir -p "$pfx"
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$pfx/lib/pkgconfig:$pfx/lib64/pkgconfig"
 
 # BUILD PHASE
-pushd "fluidsynth"
-mkdir -p build
-cd build
-cmake \
-    -DCMAKE_INSTALL_PREFIX="$pfx" \
-    ..
-make -j "$(nproc)" install
-popd
 
 pushd source
 mkdir build
 cd build
 cmake \
     -DENABLE_SDL2=ON \
-    -DENABLE_FLUIDSYNTH=ON \
+    -DENABLE_FLUIDSYNTH=OFF \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH="$pfx" \
     -DFLUIDSYNTH_INCLUDE_DIR="$pfx/include" \
@@ -43,7 +30,5 @@ popd
 mkdir -p "$diststart/410700/dist/lib"
 mkdir -p "$diststart/410700/dist/res"
 cp -rfv "source/build/systemshock" "$diststart/410700/dist/"
-cp -rfv "$pfx/lib64"/libfluidsynth.so* "$diststart/410700/dist/lib"
 cp -rfv "source/shaders" "$diststart/410700/dist"
-wget http://rancid.kapsi.fi/windows.sf2
-cp -rfv windows.sf2 "$diststart/410700/dist/res"
+cp -rfv assets/* "$diststart/410700/dist/"
--- engines/shockolate/env.sh
+++ engines/shockolate/env.sh
@@ -2,4 +2,3 @@
 
 export STEAM_APP_ID_LIST="410700"
 export LICENSE_PATH="./source/COPYING.txt"
-export ADDITIONAL_LICENSES="./fluidsynth/LICENSE"
