06e4d25b745a89373a5bf9884547ad8063de03bd
d10sfan <d10sfan@gmail.com>
2021-01-24 23:47:46 -0600

    script fix

--- engines/openloco/assets/run-openloco.sh
+++ engines/openloco/assets/run-openloco.sh
@@ -2,10 +2,8 @@
 
 if [ ! -f ~/.config/OpenLoco/openloco.yml ] 
 then
-    echo "ASDASD"
     mkdir -p ~/.config/OpenLoco
     echo "loco_install_path: ./" > ~/.config/OpenLoco/openloco.yml
 fi
-
 
 LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./openloco
