066e545026ff41515532d9655ae8aed3fdea233a
d10sfan <d10sfan@gmail.com>
2021-01-24 21:38:33 -0600

    force rebuild

--- engines/openloco/env.sh
+++ engines/openloco/env.sh
@@ -3,5 +3,5 @@
 export STEAM_APP_ID_LIST="356430"
 export LICENSE_PATH="./source/LICENSE"
 export ADDITIONAL_LICENSES="./libpng/LICENSE"
-export GCC_9="1"
 export CUSTOM_CONTAINER="luxtorpedadev/runtime-32:4"
+export GCC_9="1"
