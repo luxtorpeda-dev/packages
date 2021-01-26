6f73f5019aff15755f96fe296f101976861f5b92
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-01-25 22:46:04 -0600

    unreal-gold (#216)
    * unreal-gold
    
    * Fix
    
    Co-authored-by: d10sfan <d10sfan@gmail.com>

---
+++ engines/unreal-gold/assets/run-unreal.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! -f ./UnrealLinux.bin ] 
+then
+    if [ ! -f ./UnrealGoldPatch227i.7z ] 
+    then
+        "$STEAM_ZENITY" --error --text="UnrealGoldPatch227i.7z must be manually downloaded to System directory. Please see System/README.txt for instructions in game directory."
+        exit 1
+    fi
+    LD_LIBRARY_PATH=.7z ./7z/7z x -o"./" ./UnrealGoldPatch227i.7z
+    wine UnrealGoldPatch227i.exe
+    if [ ! -f ./UnrealLinux.bin ] 
+    then
+        "$STEAM_ZENITY" --error --text="Wine-based installation failed. Please see System/README.txt for instructions in game directory."
+        exit 1
+    fi
+fi
+
+chmod +x ./UnrealLinux.bin
+LD_LIBRARY_PATH="./libs" LD_PRELOAD="" ./UnrealLinux.bin -log
