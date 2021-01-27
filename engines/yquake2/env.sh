3f4a764d157fe922457903289d5d3c8be1df0db4
d10sfan <d10sfan@gmail.com>
2021-01-27 08:12:16 -0600

    add license

--- engines/yquake2/env.sh
+++ engines/yquake2/env.sh
@@ -2,5 +2,5 @@
 
 export STEAM_APP_ID_LIST="2320 2330 2340 9130"
 export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./LICENSE"
+export ADDITIONAL_LICENSES="./LICENSE ./source-ctf/LICENSE ./source-xatrix/LICENSE ./source-rogue/LICENSE"
 export COMMON_PACKAGE="1"
