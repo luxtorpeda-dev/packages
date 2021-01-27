52535ce55366282e1c5869e86010e78b0cbfa686
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-01-27 11:34:39 -0600

    prboom-plus (#223)

---
+++ engines/prboom-plus/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="2280 2300 2390"
+export LICENSE_PATH="./source/prboom2/COPYING"
+export ADDITIONAL_LICENSES="./fluidsynth/LICENSE"
+export COMMON_PACKAGE="1"
