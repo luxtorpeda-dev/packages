c39cf7f4d0bfe674b3532f33913c5a12b917c51b
d10sfan <d10sfan@gmail.com>
2021-01-28 20:52:11 -0600

    quakespasm - Fix for errors

--- engines/quakespasm-spiked/assets/quakespasm-hipnotic.sh
+++ engines/quakespasm-spiked/assets/quakespasm-hipnotic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f quakespasm/share/quake/hipnotic/config.cfg ] ; then
-	cp -f quakespasm/share/quake/default.lux.cfg quakespasm/share/quake/rogue/config.cfg
+	cp -f quakespasm/share/quake/default.lux.cfg quakespasm/share/quake/hipnotic/config.cfg
 	sed -i "s|%USER%|$USER|" quakespasm/share/quake/hipnotic/config.cfg
 fi
 
