5ca160108edf9310fabc64f06b41ee75c2f5f965
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-02-01 07:54:21 -0600

    solarus (#234)

---
+++ engines/solarus/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="1393750"
+export LICENSE_PATH="./source/license_gpl.txt"
+export ADDITIONAL_LICENSES="./physfs/LICENSE.txt ./libmodplug/COPYING ./luajit/COPYRIGHT ./glm/copying.txt"
