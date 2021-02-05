cbb8d1a0e74401bdfa313714d73d170105418625
d10sfan <d10sfan@gmail.com>
2021-02-05 08:14:49 -0600

    prey2006 - Fix for ubuntu not loading, custom launch script

--- engines/prey2006/assets/run-prey.sh
+++ engines/prey2006/assets/run-prey.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 cd linuxdata
-./prey
+
+PREY_DATA_PATH="."
+
+export ORIG_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
+
+LD_LIBRARY_PATH=.:${PREY_DATA_PATH}:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH
+
+exec "./prey.x86" "$@"
