6f25a0e49df30d9e1b005a79605c09b0ebea8bbb
d10sfan <d10sfan@gmail.com>
2021-02-11 22:36:53 -0600

    fixes

--- engines/hammerofthyrion/build.sh
+++ engines/hammerofthyrion/build.sh
@@ -6,7 +6,30 @@ pushd source
 git checkout fdf6f72
 popd
 
+git clone https://github.com/markjeee/libmad.git mad
+pushd mad
+git checkout -f c2f96fa
+# from http://www.linuxfromscratch.org/blfs/view/svn/multimedia/libmad.html
+patch -Np1 -i ../patches/libmad-0.15.1b-fixes-1.patch
+sed "s@AM_CONFIG_HEADER@AC_CONFIG_HEADERS@g" -i configure.ac
+touch NEWS AUTHORS ChangeLog
+autoreconf -fi
+popd
+
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
 # BUILD PHASE
+pushd "mad"
+./configure --prefix="$pfx" --disable-static
+make -j "$(nproc)" install
+popd
+
+pushd "mad"
+./configure --disable-static
+make -j "$(nproc)" install
+popd
+
 pushd "source/engine/hexen2"
 make -j "$(nproc)" h2
 popd
--- engines/hammerofthyrion/env.sh
+++ engines/hammerofthyrion/env.sh
@@ -2,3 +2,4 @@
 
 export STEAM_APP_ID_LIST="9060"
 export LICENSE_PATH="./source/docs/COPYING"
+export ADDITIONAL_LICENSES="./mad/COPYING ./mad/COPYRIGHT"
