c60579bb3614c892dda76a811b2d8af3064f4316
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-02-13 22:33:40 -0600

    Voidsw (#249)

---
+++ engines/voidsw/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://voidpoint.io/terminx/eduke32.git source
+pushd source
+git checkout b7d4ae3a561e0e6f26d999ce5dde7328c68725c0
+popd
+
+# BUILD PHASE
+pushd source
+make voidsw
+popd
+
+# COPY PHASE
+cp -rfv "source/voidsw" "$diststart/common/dist/"
+cp -rfv assets/* "$diststart/common/dist/"
