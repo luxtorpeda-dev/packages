a8181e351caa93dee39fe3b515c3faed7ddf9a32
d10sfan <d10sfan@gmail.com>
2021-02-14 21:28:01 -0600

    rednukem

---
+++ engines/rednukem/assets/run-rr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./rednukem -usecwd -nosetup
---
+++ engines/rednukem/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="565550 580940"
+export LICENSE_PATH="./source/source/rr/gpl-2.0.txt"
+export COMMON_PACKAGE="1"
