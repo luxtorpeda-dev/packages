42b3d4fd192678905a74313bb42347717be6aacf
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-02-14 23:03:16 -0600

    commander-genius (#254)

---
+++ engines/commander-genius/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://gitlab.com/Dringgstein/Commander-Genius.git source
+pushd source
+git checkout 0b56993f
+popd
+
+# BUILD PHASE
+pushd source
+mkdir -p build
+cd build
+cmake -DUSE_BOOST=0 ..
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
+cp -rfv "source/build/src/CGeniusExe" "$diststart/9180/dist/"
+cp -rfv assets/* "$diststart/9180/dist/"
