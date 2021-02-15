e9a29c3b36e1e46b6cef821179a750efdc6b4141
d10sfan <d10sfan@gmail.com>
2021-02-14 21:20:35 -0600

    eduke32

---
+++ engines/eduke32/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://voidpoint.io/terminx/eduke32.git source
+pushd source
+git checkout b7d4ae3a561e0e6f26d999ce5dde7328c68725c0
+popd
+
+# BUILD PHASE
+pushd source
+make duke3d
+popd
+
+# COPY PHASE
+cp -rfv "source/duke3d" "$diststart/common/dist/"
+cp -rfv assets/* "$diststart/common/dist/"
