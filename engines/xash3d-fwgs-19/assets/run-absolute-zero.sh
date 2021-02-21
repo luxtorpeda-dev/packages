8f75df0d144a65145373699c4b4ece40b755c468
d10sfan <d10sfan@gmail.com>
2021-02-21 09:34:39 -0600

    xash3d-fwgs-19 - Add arguments

--- engines/xash3d-fwgs-19/assets/run-absolute-zero.sh
+++ engines/xash3d-fwgs-19/assets/run-absolute-zero.sh
@@ -4,4 +4,4 @@ GAMEROOT="."
 GAMEEXE="xash3d"
 export LD_LIBRARY_PATH="$GAMEROOT:$LD_LIBRARY_PATH"
 
-LC_ALL=C "${GAMEROOT}"/"${GAMEEXE}" -game AbsoluteZero -dll dlls/az.dll -clientlib cl_dlls/client.so
+LC_ALL=C "${GAMEROOT}"/"${GAMEEXE}" -game AbsoluteZero -dll dlls/az.dll -clientlib cl_dlls/client.so "$@"
