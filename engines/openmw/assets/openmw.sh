9b353c5dbe7c5c211144fb80e1e9f4279c7eca6e
d10sfan <d10sfan@gmail.com>
2021-02-26 14:35:14 -0600

    openmw - Update script to check for morrowind.ini case

--- engines/openmw/assets/openmw.sh
+++ engines/openmw/assets/openmw.sh
@@ -8,6 +8,14 @@ if [ ! -f "openmw.cfg" ]; then
     cp openmw-template.cfg openmw.cfg
 fi
 
+if [ ! -f Morrowind.ini ]; then
+    if [ ! -f morrowind.ini ]; then
+        "$STEAM_ZENITY" --error --text="No morrowind ini file not found"
+    else
+        ln -rsf morrowind.ini Morrowind.ini
+    fi
+fi
+
 ./openmw-iniimporter Morrowind.ini openmw.cfg
 
 ./openmw --data-local "Data Files" "$@" 
