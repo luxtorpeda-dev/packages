1e37fb84dd20dc9a8bb9beff40d656ddf44990eb
d10sfan <d10sfan@gmail.com>
2021-02-26 14:37:04 -0600

    openmw-tes3mp - Update script to check for morrowind.ini case

--- engines/openmw-tes3mp/assets/tes3mp-launcher.sh
+++ engines/openmw-tes3mp/assets/tes3mp-launcher.sh
@@ -12,6 +12,14 @@ if [ ! -f "openmw.cfg" ]; then
     cp openmw-template.cfg openmw.cfg
 fi
 
+if [ ! -f ../Morrowind.ini ]; then
+    if [ ! -f ../morrowind.ini ]; then
+        "$STEAM_ZENITY" --error --text="No morrowind ini file not found"
+    else
+        ln -rsf ../morrowind.ini ../Morrowind.ini
+    fi
+fi
+
 ./openmw-iniimporter ../Morrowind.ini openmw.cfg
 
 LD_LIBRARY_PATH=../qt5/lib:$LD_LIBRARY_PATH QT_QPA_PLATFORM_PLUGIN_PATH=../qt5/plugins ./openmw-launcher "$@" 
