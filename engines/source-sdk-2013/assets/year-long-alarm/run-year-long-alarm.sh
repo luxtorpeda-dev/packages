1181b21cdccd8d6bd0b2864065889092ee07f980
d10sfan <d10sfan@gmail.com>
2021-02-27 12:10:31 -0600

    source-sdk-2013 - Add support for year-long-alarm

---
+++ engines/source-sdk-2013/assets/year-long-alarm/run-year-long-alarm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+if [ ! -f "sdkpath.txt" ]; then
+    HL_PATH=$("$STEAM_ZENITY" --file-selection --title="Browse to Source SDK Base 2013 Singleplayer Installation" --directory)
+
+    if [ -z "$HL_PATH" ]; then
+        "$STEAM_ZENITY" --error --title="Setup Error" --text="Path to Source SDK 2013 not given"
+        exit 1
+    fi
+
+    if [ ! -d "$HL_PATH/sdktools" ]; then
+        "$STEAM_ZENITY" --error --title="Setup Error" --text="Path to Source SDK 2013 incorrect"
+        exit 1
+    fi
+    
+    echo "$HL_PATH" >> sdkpath.txt
+    
+    pushd "yearlongalarm"
+        find ./ | sort -r | sed 's/\(.*\/\)\(.*\)/mv "\1\2" "\1\L\2"/' |sh
+    popd
+fi
+
+sdkpath=`cat sdkpath.txt`
+
+pushd "yearlongalarm"
+    cp -rfv ../gameinfo.txt gameinfo.txt
+popd
+
+"$sdkpath"/hl2.sh -game "$PWD/yearlongalarm" -steam
--- engines/source-sdk-2013/build.sh
+++ engines/source-sdk-2013/build.sh
@@ -10,3 +10,5 @@ popd
 cp -rfv assets/entropy-zero/* "$diststart/714070/dist/"
 
 cp -rfv assets/metastasis/* "$diststart/235780/dist/"
+
+cp -rfv assets/year-long-alarm/* "$diststart/747250/dist/"
