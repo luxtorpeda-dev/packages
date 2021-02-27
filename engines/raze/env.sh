d67cca8663291c3dd915d7a197a6c57c9406e059
d10sfan <d10sfan@gmail.com>
2021-02-26 22:27:32 -0600

    razeg

---
+++ engines/raze/env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="1010750 299030 225140 329650 376750 565550 580940"
+export LICENSE_PATH="./source/LICENSE"
+export ADDITIONAL_LICENSES="./fluidsynth/LICENSE ./zmusic/licenses/bsd.txt ./zmusic/licenses/dumb.txt ./zmusic/licenses/gplv3.txt ./zmusic/licenses/legal.txt ./zmusic/licenses/zmusic.txt"
+export COMMON_PACKAGE="1"
+export GCC_9="1"
