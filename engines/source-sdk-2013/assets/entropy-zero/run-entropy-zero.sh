9fbfcda3dd5978e1ef448242bcd8a02ddb4d344f
d10sfan <d10sfan@gmail.com>
2021-02-27 20:39:45 -0600

    source-sdk-2013 - entropy-zero fix

--- engines/source-sdk-2013/assets/entropy-zero/run-entropy-zero.sh
+++ engines/source-sdk-2013/assets/entropy-zero/run-entropy-zero.sh
@@ -21,11 +21,13 @@ if [ ! -f "sdkpath.txt" ]; then
     for d in sound materials resource; do
 		find "${d}/" -exec sh -eux -c "echo {} | grep -q '[A-Z]' && ln -vs \"\$(basename {})\" \"\$(echo {}|tr '[A-Z]' '[a-z]')\"" \;
 	done
-	
-	sed -Ei -e 's/("clip_size".*)".*"/\1"-1"/p;s/"clip_size"(.*)".*"/"default_clip"\1"1"' -e 's/("item_flags".*)".*"/\1"4" \/\/ ITEM_FLAG_EXHAUSTIBLE/' scripts/weapon_manhacktoss.txt
     popd
 fi
 
 sdkpath=`cat sdkpath.txt`
+
+pushd "Entropy Zero/EntropyZero"
+    cp -rfv ../../weapon_manhacktoss.txt scripts/
+popd
 
 "$sdkpath"/hl2.sh -game "$PWD/Entropy Zero/EntropyZero" -steam
