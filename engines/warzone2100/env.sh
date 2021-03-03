72560845f1783e48f0bdc6c9a51ca4ec3aafeac5
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-03-03 12:59:31 -0600

    warzone-2100 - Update to 4.0.0-beta1 (#282)

--- engines/warzone2100/env.sh
+++ engines/warzone2100/env.sh
@@ -2,6 +2,5 @@
 
 export STEAM_APP_ID_LIST="1241950"
 export LICENSE_PATH="./source/COPYING"
-export ADDITIONAL_LICENSES="./source/COPYING.NONGPL ./source/COPYING.README ./physfs/LICENSE.txt ./libsodium/LICENSE ./harfbuzz/COPYING ./freetype2/README ./curl/COPYING ./libbzip2/LICENSE"
-export COMMON_QT5="1"
-export GCC_9="1"
+export ADDITIONAL_LICENSES="./source/COPYING.NONGPL ./source/COPYING.README ./physfs/LICENSE.txt ./libsodium/LICENSE ./harfbuzz/COPYING ./freetype2/README ./curl/COPYING ./libbzip2/LICENSE ./sqlite/LICENSE.md ./openal/COPYING ./theora/COPYING ./ogg/COPYING ./SDL/COPYING.txt ./zlib-1.2.11/LICENSE ./libpng/LICENSE ./openssl/LICENSE"
+export CUSTOM_CONTAINER="ubuntu:18.04"
