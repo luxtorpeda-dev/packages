ce285b28fbe276a810d888e014c4a4d35601df8b
d10sfan <d10sfan@gmail.com>
2021-03-12 15:23:16 -0600

    ut99 - Add check for opensuse pulseaudio

--- engines/ut99/assets/run-ut99-436.sh
+++ engines/ut99/assets/run-ut99-436.sh
@@ -10,6 +10,8 @@ if [[ -f "/usr/lib/i386-linux-gnu/pulseaudio/libpulsedsp.so" ]]; then
     pulseaudiolib="/usr/lib/i386-linux-gnu/pulseaudio/libpulsedsp.so"
 elif [[ -f "/usr/lib32/pulseaudio/libpulsedsp.so" ]]; then
     pulseaudiolib="/usr/lib32/pulseaudio/libpulsedsp.so"
+elif [[ -f "/usr/lib/pulseaudio/libpulsedsp.so" ]]; then
+    pulseaudiolib="/usr/lib/pulseaudio/libpulsedsp.so"
 elif [[ -n $(cat /etc/os-release | grep NixOS) ]]; then
     pulseaudiolib="$(nix-store --query -R $(which steam-run) | grep steam-run-fhs | tail -n1)/lib32/pulseaudio/libpulsedsp.so"
 fi
