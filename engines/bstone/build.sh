281c662cd8b60e706a1316eee2eace981682a568
d10sfan <d10sfan@gmail.com>
2021-03-13 22:57:35 -0600

    bstone

---
+++ engines/bstone/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/bibendovsky/bstone.git source
+pushd source
+git checkout 2a0db92
+popd
+
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
+# BUILD PHASE
+pushd source
+mkdir build
+cd build
+cmake \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+# COPY PHASE
+cp -rfv "$pfx/bin/"* "$diststart/common/dist/"
