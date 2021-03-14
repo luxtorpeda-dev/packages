a9b0e2f6b50ed0df7326c166125086b488d829e2
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-03-14 00:10:51 -0600

    bstone (#296)

---
+++ engines/bstone/env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="238050 358190 358310"
+export LICENSE_PATH="./source/LICENSE"
+export ADDITIONAL_LICENSES="./SDL/COPYING.txt"
+export COMMON_PACKAGE="1"
+export CUSTOM_CONTAINER="ubuntu:18.04"
+
