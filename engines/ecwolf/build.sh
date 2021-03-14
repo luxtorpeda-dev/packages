51d820a7b5fd92db787da8f47a71506af0c64e88
d10sfan <d10sfan@gmail.com>
2021-03-13 23:14:44 -0600

    ecwolf

---
+++ engines/ecwolf/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://bitbucket.org/ecwolf/ecwolf.git source
+pushd source
+git checkout 3ce6e4d
+popd
+
+# BUILD PHASE
+pushd source
+mkdir build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release -DGPL=ON \
+    ..
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
+cp -rfv source/build/ecwolf "$diststart/common/dist/"
