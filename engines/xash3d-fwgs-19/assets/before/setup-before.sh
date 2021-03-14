512da645bd0a19197d4aed265ef755c8cd4001a8
d10sfan <d10sfan@gmail.com>
2021-03-13 22:31:03 -0600

    fixes

--- engines/xash3d-fwgs-19/assets/before/setup-before.sh
+++ engines/xash3d-fwgs-19/assets/before/setup-before.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-HL_PATH=$("$STEAM_ZENITY" --file-selection --title="Browse to Half Life installation" --directory)
-
-if [ -z "$HL_PATH" ]; then
-    "$STEAM_ZENITY" --error --title="Absolute Zero Setup Error" --text="Path to Half Life not given"
-    exit 1
-fi
-
-if [ ! -d "$HL_PATH/valve" ]; then
-    "$STEAM_ZENITY" --error --title="Absolute Zero Setup Error" --text="Path to Half Life incorrect"
-    exit 1
-fi
-
-cp -rfv "$HL_PATH/valve" ./
 cp -rfv ./Spirit/cl_dlls/client.so ./before/cl_dlls
-cp -rfv ./Spirit/cl_dlls/spirit.so ./before/dlls
-sed -i "s/gamedll \"dlls/spirit.dll\"/gamedll \"dlls/spirit.so\"/" before/liblist.gam
+cp -rfv ./Spirit/dlls/spirit.so ./before/dlls
+sed -i 's/gamedll "dlls\/spirit\.dll"/gamedll "dlls\/spirit\.so"/' before/liblist.gam
