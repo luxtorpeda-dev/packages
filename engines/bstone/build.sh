3d91255b93c805e2ee8e41b3abbe3e0e36c7e489
d10sfan <d10sfan@gmail.com>
2021-03-13 23:58:46 -0600

    fixes

--- engines/bstone/build.sh
+++ engines/bstone/build.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
+apt-get -y install mercurial
+
 # CLONE PHASE
 git clone https://github.com/bibendovsky/bstone.git source
 pushd source
 git checkout 2a0db92
 popd
 
+hg clone https://hg.libsdl.org/SDL
+pushd SDL
+hg checkout release-2.0.12
+popd
+
 readonly pfx="$PWD/local"
 mkdir -p "$pfx"
 
 # BUILD PHASE
+pushd "SDL"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
 pushd source/src
 mkdir build
 cd build
 cmake \
     -DCMAKE_PREFIX_PATH="$pfx" \
+    -DBSTONE_USE_PCH=OFF \
     ..
 make -j "$(nproc)"
 make install
@@ -22,3 +42,6 @@ popd
 
 # COPY PHASE
 cp -rfv "source/src/build/bstone" "$diststart/common/dist/"
+cp -rfv assets/* "$diststart/common/dist/"
+mkdir -p "$diststart/common/dist/lib"
+cp -rfv "$pfx/lib/"*.so* "$diststart/common/dist/lib"
