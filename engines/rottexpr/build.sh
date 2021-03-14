78499f55023fc394e4628de5dcda3a2967538278
d10sfan <d10sfan@gmail.com>
2021-03-13 22:42:24 -0600

    rottexpr

---
+++ engines/rottexpr/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/LTCHIPS/rottexpr.git source
+pushd source
+git checkout 407e3d8
+popd
+
+# BUILD PHASE
+pushd "source/src"
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
+cp -rfv source/src/rott "$diststart/238050/dist/"
