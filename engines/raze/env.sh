62fc84b2a109a74fd82cbd7a1943b7bbce1341b4
d10sfan <d10sfan@gmail.com>
2021-03-13 22:15:16 -0600

    raze - Fix missing package

--- engines/raze/env.sh
+++ engines/raze/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export STEAM_APP_ID_LIST="1010750 299030 225140 329650 376750 565550 580940"
+export STEAM_APP_ID_LIST="1010750 299030 225140 329650 376750 565550 580940 225160"
 export LICENSE_PATH="./source/README.md"
 export ADDITIONAL_LICENSES="./fluidsynth/LICENSE ./zmusic/licenses/bsd.txt ./zmusic/licenses/dumb.txt ./zmusic/licenses/gplv3.txt ./zmusic/licenses/legal.txt ./zmusic/licenses/zmusic.txt ./SDL/COPYING.txt ./libvpx/LICENSE ./openal/COPYING"
 export COMMON_PACKAGE="1"
