f184d00d53b50369e3290cbd90ef0e1810c05b08
d10sfan <d10sfan@gmail.com>
2021-03-15 09:17:55 -0500

    ecwolf - Spear of Destiny launch checks

--- engines/ecwolf/assets/run-ecwolf.sh
+++ engines/ecwolf/assets/run-ecwolf.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+gamearg="$1"
+
 if [ -d "base" ]; then
+    echo "gamearg $gamearg"
+
     cp -rfv ./ecwolf ./base
     cp -rfv ./ecwolf.pk3 ./base
     cd base
-    ./ecwolf --config ecwolf.cfg
+    if [ "$gamearg" = "base\\M1-SOD.exe" ]; then
+        ./ecwolf --config ecwolf.cfg --data SOD
+    elif [ "$gamearg" = "base\\M2-SOD.exe" ]; then
+        ./ecwolf --config ecwolf.cfg --data SD2
+    elif [ "$gamearg" = "base\\M3-SOD.exe" ]; then
+        ./ecwolf --config ecwolf.cfg --data SD3
+    else
+        ./ecwolf --config ecwolf.cfg
+    fi
+    
 else
     ./ecwolf --config ecwolf.cfg
 fi
