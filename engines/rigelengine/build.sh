301e508136e0c104842156ab84ae4e4c62fd85ac
d10sfan <d10sfan@gmail.com>
2021-03-16 09:47:58 -0500

    fix

--- engines/rigelengine/build.sh
+++ engines/rigelengine/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get -y install mercurial
+
 # CLONE PHASE
 git clone https://github.com/lethal-guitar/RigelEngine.git source
 pushd source
@@ -13,13 +15,48 @@ git checkout -f 68a24986
 git submodule update --init --recursive
 popd
 
-# BUILD PHASE
+hg clone https://hg.libsdl.org/SDL
+pushd SDL
+hg checkout release-2.0.12
+popd
 
-# build Boost
-readonly boostlocation="$PWD/boost"
+git clone https://github.com/SDL-mirror/SDL_mixer.git SDL_mixer
+pushd SDL_mixer
+git checkout -f release-2.0.4
+popd
+
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
+# BUILD PHASE
+pushd "SDL"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+pushd "SDL_mixer"
+./configure --prefix="$pfx"
+make -j "$(nproc)"
+make install
+popd
+
 pushd "boost"
 ./bootstrap.sh
 ./b2 headers
+./b2 \
+	--with-program_options \
+	--with-filesystem \
+	--with-system \
+	--with-iostreams \
+	--with-locale \
+./b2 install --prefix="$pfx"
 popd
 
 pushd "source"
@@ -27,7 +64,8 @@ mkdir build
 cd build
 cmake \
     -DBoost_DEBUG=1 \
-    -DBOOST_ROOT="$boostlocation" \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DBoost_LIBRARY_DIRS="$pfx/lib" \
     -DCMAKE_BUILD_TYPE=Release \
     ..
 make -j "$(nproc)"
@@ -36,4 +74,6 @@ popd
 
 
 # COPY PHASE
+mkdir -p "$diststart/240180/dist/lib"
+cp -rfv "$pfx/lib/"*.so* "$diststart/240180/dist/lib"
 cp -rfv source/build/RigelEngine "$diststart/240180/dist/"
--- engines/rigelengine/env.sh
+++ engines/rigelengine/env.sh
@@ -2,3 +2,5 @@
 
 export STEAM_APP_ID_LIST="240180"
 export LICENSE_PATH="./source/LICENSE.MD"
+export ADDITIONAL_LICENSES="./SDL/COPYING.txt ./SDL_mixer/COPYING.txt"
+export CUSTOM_CONTAINER="ubuntu:18.04"
