6f68c58906fd0ab60008c5f6b9c2a2830d72a871
d10sfan <d10sfan@gmail.com>
2021-03-16 09:49:01 -0500

    license

--- engines/rigelengine/env.sh
+++ engines/rigelengine/env.sh
@@ -2,5 +2,5 @@
 
 export STEAM_APP_ID_LIST="240180"
 export LICENSE_PATH="./source/LICENSE.MD"
-export ADDITIONAL_LICENSES="./SDL/COPYING.txt ./SDL_mixer/COPYING.txt"
+export ADDITIONAL_LICENSES="./SDL/COPYING.txt ./SDL_mixer/COPYING.txt ./boost/LICENSE_1_0.txt"
 export CUSTOM_CONTAINER="ubuntu:18.04"
