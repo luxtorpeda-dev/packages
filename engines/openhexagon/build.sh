c64cb4f1fef577181aa708d5ad7dbef30118b7e5
d10sfan <d10sfan@gmail.com>
2021-03-17 08:25:11 -0500

    ubuntu1804

--- engines/openhexagon/build.sh
+++ engines/openhexagon/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get -y install mercurial libopenssl-dev libgl1-mesa-glx
+
 # CLONE PHASE
 git clone https://github.com/SuperV1234/SSVOpenHexagon.git source
 pushd source
@@ -7,16 +9,44 @@ git checkout 4285d30de43f380199cee03fc446eb5910c3ec0c
 git submodule update --init --recursive
 popd
 
-git clone https://github.com/g-truc/glm glm
-pushd glm
-git checkout -f 947527d3
-git submodule update --init --recursive
-popd
-
 git clone https://github.com/Kitware/CMake.git cmake
 pushd cmake
 git checkout -f 39c6ac5
 popd
+
+git clone https://github.com/kcat/openal-soft.git openal
+pushd openal
+git checkout -f f5e0eef
+popd
+
+git clone https://github.com/xiph/theora.git theora
+pushd theora
+git checkout -f 7ffd8b2
+popd
+
+git clone https://github.com/xiph/ogg.git ogg
+pushd ogg
+git checkout -f bada457
+popd
+
+git clone https://github.com/xiph/vorbis.git vorbis
+pushd vorbis
+git checkout -f 0657aee
+popd
+
+git clone https://github.com/xiph/flac.git flac
+pushd flac
+git checkout -f f764434
+popd
+
+hg clone https://hg.libsdl.org/SDL
+pushd SDL
+hg checkout release-2.0.12
+popd
+
+readonly pstart="$PWD"
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
 
 # BUILD PHASE
 pushd cmake
@@ -27,6 +57,73 @@ popd
 
 export CMAKE_ROOT=/usr/local/share/cmake-3.16/
 /usr/local/bin/cmake --version
+
+pushd "openal"
+rm -rf build
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    -DBUILD_SHARED_LIBS=ON \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+pushd flac
+mkdir -p build
+cd build
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+make -j "$(nproc)"
+make install
+popd
+
+pushd "ogg"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_CXX_FLAGS="-fPIC" \
+    -DCMAKE_C_FLAGS="-fPIC" \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+pushd "vorbis"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_CXX_FLAGS="-fPIC" \
+    -DCMAKE_C_FLAGS="-fPIC" \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+pushd "SDL"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    ..
+make -j "$(nproc)"
+make install
+popd
 
 pushd "source"
 mkdir -p build
--- engines/openhexagon/env.sh
+++ engines/openhexagon/env.sh
@@ -2,5 +2,5 @@
 
 export STEAM_APP_ID_LIST="1358090"
 export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./glm/copying.txt"
-export GCC_9="1"
+export ADDITIONAL_LICENSES="./openal/COPYING ./theora/COPYING ./ogg/COPYING ./SDL/COPYING.txt ./flac/COPYING.FDL ./flac/COPYING.GPL ./flac/COPYING.LGPL ./flac/COPYING.Xiph"
+export CUSTOM_CONTAINER="ubuntu:18.04"
