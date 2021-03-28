c0d4a25db20bdf70520dc2ea412f09b9679e9cc8
d10sfan <d10sfan@gmail.com>
2021-03-27 20:47:54 -0500

    test

---
+++ libraries/steam-runtime/boost-headers/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+readonly pfx="$PWD/local"
+mkdir -p "$pfx"
+
+# CLONE PHASE
+git clone https://github.com/boostorg/boost boost
+pushd boost
+git checkout -f 68a24986
+git submodule update --init --recursive
+popd
+
+# BUILD PHASE
+pushd "boost"
+./bootstrap.sh
+./b2 headers --prefix="$pfx"
+popd
+
+
+# COPY PHASE
+cp -rfv "$pfx"/* "$diststart/common/dist/"
---
+++ libraries/steam-runtime/boost-headers/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST=""
+export LICENSE_PATH="./boost/LICENSE_1_0.txt"
+export COMMON_PACKAGE="1"
