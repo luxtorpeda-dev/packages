9225867be3aaac0909b5b4283f066fd9e9c5f5f7
d10sfan <d10sfan@gmail.com>
2021-03-29 23:00:45 -0500

    xash3d-fwgs-19 - before support given command line args

--- engines/xash3d-fwgs-19/assets/before/run-before.sh
+++ engines/xash3d-fwgs-19/assets/before/run-before.sh
@@ -4,4 +4,4 @@ GAMEROOT="."
 GAMEEXE="xash3d"
 export LD_LIBRARY_PATH="$GAMEROOT:$LD_LIBRARY_PATH"
 
-LC_ALL=C "${GAMEROOT}"/"${GAMEEXE}" -game before -dll dlls/spirit.so -clientlib cl_dlls/client.so
+LC_ALL=C "${GAMEROOT}"/"${GAMEEXE}" -game before -dll dlls/spirit.so -clientlib cl_dlls/client.so "$@"
