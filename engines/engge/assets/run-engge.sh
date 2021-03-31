5a9dec71ad0fa4a861a76e97e1b45c0e518a6f81
d10sfan <d10sfan@gmail.com>
2021-03-31 00:31:44 -0500

    fix

--- engines/engge/assets/run-engge.sh
+++ engines/engge/assets/run-engge.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-originalpwd="$PWD"
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$DIR"
-
-LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH" ./engge
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./engge
