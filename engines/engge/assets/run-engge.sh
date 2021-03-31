a49822c0d9aed4235f9a7e230529c305e30597f7
d10sfan <d10sfan@gmail.com>
2021-03-31 00:34:37 -0500

    fix

--- engines/engge/assets/run-engge.sh
+++ engines/engge/assets/run-engge.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+ln -rsf ./Resources/ThimbleweedPark.ggpack1 ./ThimbleweedPark.ggpack1
+ln -rsf ./Resources/ThimbleweedPark.ggpack2 ./ThimbleweedPark.ggpack2
+
 LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./engge
