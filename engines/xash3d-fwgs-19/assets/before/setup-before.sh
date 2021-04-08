5f7430e8482b5b74b0084340fcc57be0e5e6f554
d10sfan <d10sfan@gmail.com>
2021-04-08 07:43:26 -0500

    xash3d-fwgs-19 - before change for steam library list

--- engines/xash3d-fwgs-19/assets/before/setup-before.sh
+++ engines/xash3d-fwgs-19/assets/before/setup-before.sh
@@ -3,3 +3,5 @@
 cp -rfv ./Spirit/cl_dlls/client.so ./before/cl_dlls
 cp -rfv ./Spirit/dlls/spirit.so ./before/dlls
 sed -i 's/gamedll "dlls\/spirit\.dll"/gamedll "dlls\/spirit\.so"/' before/liblist.gam
+mv ./Spirit/liblist.gam ./Spirit/liblist.gam.bak
+mv ./before/liblist.gam ./before/liblist.gam.bak
