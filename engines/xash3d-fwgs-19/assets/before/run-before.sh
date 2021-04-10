14987929925f96c2a3f8c157f06cc7e786b193da
d10sfan <d10sfan@gmail.com>
2021-04-09 21:10:41 -0500

    xash3d-fwgs-19 - Before change

--- engines/xash3d-fwgs-19/assets/before/run-before.sh
+++ engines/xash3d-fwgs-19/assets/before/run-before.sh
@@ -4,4 +4,6 @@ GAMEROOT="."
 GAMEEXE="xash3d"
 export LD_LIBRARY_PATH="$GAMEROOT:$LD_LIBRARY_PATH"
 
+mv ./before/liblist.gam ./before/liblist.gam.bak
+
 LC_ALL=C "${GAMEROOT}"/"${GAMEEXE}" -game before -dll dlls/spirit.so -clientlib cl_dlls/client.so "$@"
