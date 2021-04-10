0c945424692cffb88799da8594946fa30f2b6f4b
d10sfan <d10sfan@gmail.com>
2021-04-09 21:43:03 -0500

    source-sdk-2013 - resistance element crash fix

--- engines/source-sdk-2013/assets/resistance-element/run-resistance-element.sh
+++ engines/source-sdk-2013/assets/resistance-element/run-resistance-element.sh
@@ -17,14 +17,30 @@ if [ ! -f "sdkpath.txt" ]; then
     echo "$HL_PATH" >> sdkpath.txt
     
     pushd "ResistanceElement"
-        mkdir bin
-        ln -rsf "$HL_PATH/sourcetest/bin/client.so" bin/client.so
-        ln -rsf "$HL_PATH/sourcetest/bin/server.so" bin/server.so
-    popd
-    
-    pushd "ResistanceElement"
         # from https://steamcommunity.com/sharedfiles/filedetails/?id=754991349&insideModal=0
         find ./ | sort -r | sed 's/\(.*\/\)\(.*\)/mv "\1\2" "\1\L\2"/' |sh
+    popd
+fi
+
+if [ ! -f "hlpath.txt" ]; then
+    EPISODE_PATH=$("$STEAM_ZENITY" --file-selection --title="Browse to Half Life 2 Installation" --directory)
+
+    if [ -z "EPISODE_PATH" ]; then
+        "$STEAM_ZENITY" --error --title="Setup Error" --text="Path to Half Life 2 not given"
+        exit 1
+    fi
+
+    if [ ! -d "$EPISODE_PATH/episodic" ]; then
+        "$STEAM_ZENITY" --error --title="Setup Error" --text="Path to Half Life 2 incorrect"
+        exit 1
+    fi
+    
+    echo "$EPISODE_PATH" >> hlpath.txt
+    
+    pushd "ResistanceElement"
+        mkdir bin
+        ln -rsf "$EPISODE_PATH/episodic/bin/client.so" bin/client.so
+        ln -rsf "$EPISODE_PATH/episodic/bin/server.so" bin/server.so
     popd
 fi
 
