143dfe04e96c98b76fec44cbc0f52d215983a5ca
d10sfan <d10sfan@gmail.com>
2021-04-09 21:12:55 -0500

    fs2open - Update to Release 21.2.0 RC2

--- engines/fs2open/build.sh
+++ engines/fs2open/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/scp-fs2open/fs2open.github.com source
 pushd source
-git checkout -f 1eeb7de 
+git checkout -f 4b5df61 
 git submodule update --init --recursive
 popd
 git clone https://github.com/FFmpeg/FFmpeg.git ffmpeg
@@ -37,5 +37,5 @@ popd
 # COPY PHASE
 mkdir -p "$diststart/273620/dist/lib/"
 cp -rfv "local/"lib/*.so* "$diststart/273620/dist/lib/"
-cp -rfv "source/build/bin/fs2_open_21_2_0_RC1_x64" "$diststart/273620/dist/fs2_open_x64"
+cp -rfv "source/build/bin/fs2_open_21_2_0_RC2_x64" "$diststart/273620/dist/fs2_open_x64"
 cp -rfv "assets/run-freespace2.sh" "$diststart/273620/dist/run-freespace2.sh"
