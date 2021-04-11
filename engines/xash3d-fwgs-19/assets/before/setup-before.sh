2c75912a0e16186af4521193a57ee3209d7e7e22
d10sfan <d10sfan@gmail.com>
2021-04-10 22:28:06 -0500

    xash3d-fwgs-19 - before fixes

--- engines/xash3d-fwgs-19/assets/before/setup-before.sh
+++ engines/xash3d-fwgs-19/assets/before/setup-before.sh
@@ -4,4 +4,3 @@ cp -rfv ./Spirit/cl_dlls/client.so ./before/cl_dlls
 cp -rfv ./Spirit/dlls/spirit.so ./before/dlls
 sed -i 's/gamedll "dlls\/spirit\.dll"/gamedll "dlls\/spirit\.so"/' before/liblist.gam
 mv ./Spirit/liblist.gam ./Spirit/liblist.gam.bak
-mv ./before/liblist.gam ./before/liblist.gam.bak
