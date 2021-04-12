271a2b6ec4620ac9ca838020c4fdc05b3fca922a
d10sfan <d10sfan@gmail.com>
2021-04-11 21:18:53 -0500

    gcc 10

--- engines/openloco/env.sh
+++ engines/openloco/env.sh
@@ -5,3 +5,4 @@ export LICENSE_PATH="./source/LICENSE"
 export ADDITIONAL_LICENSES="./libpng/LICENSE ./SDL/COPYING.txt ./SDL_mixer/COPYING.txt"
 export CUSTOM_CONTAINER="ubuntu@sha256:cba704e6616274262b2be5116bbcb5df171ec6bac0956b895b16b277c612edf1"
 # ubuntu 18.04 32-bit
+export GCC_10="1"
