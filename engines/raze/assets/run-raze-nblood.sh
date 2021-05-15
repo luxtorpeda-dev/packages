c1ce6f620c6373632077db4566b33f64d37ca530
d10sfan <d10sfan@gmail.com>
2021-05-15 10:57:13 -0500

    raze - Update to 1.1.2 and support blood better

--- engines/raze/assets/run-raze-nblood.sh
+++ engines/raze/assets/run-raze-nblood.sh
@@ -3,11 +3,11 @@
 gamearg="$1"
 
 if [ -z $1 ]; then
-    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze
+    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -gamegrp BLOOD.RFF
 elif [ "$gamearg" = "-addon" ]; then
     ln -rsf "addons/Cryptic Passage/tiles007.ART" "addons/Cryptic Passage/TILES007.ART"
     ln -rsf "addons/Cryptic Passage/tiles015.ART" "addons/Cryptic Passage/TILES015.ART"
-    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze
+    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -con "addons/Cryptic Passage/CRYPTIC.INI" -gamegrp BLOOD.RFF -j "addons/Cryptic Passage"
 else
-    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze
+    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -gamegrp BLOOD.RFF
 fi
