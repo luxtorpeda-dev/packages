d7118e93bd6c8e8f387df857bc1b00cd61b29c76
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-05-17 08:51:05 -0500

    augustus - Update to 3.0 (#363)

--- engines/augustus/build.sh
+++ engines/augustus/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/Keriew/augustus.git source
 pushd source
-git checkout 51c54ba
+git checkout 9475899
 popd
 
 # BUILD PHASE
@@ -16,7 +16,7 @@ popd
 
 # COPY PHASE
 cp -rfv "source/build/augustus" "$diststart/517790/dist/"
-cp -rfv "source/mods" "$diststart/517790/dist/"
+cp -rfv "source/assets" "$diststart/517790/dist/"
 cp -rfv "source/res/maps" "$diststart/517790/dist/"
 
 cp -rfv assets/* "$diststart/517790/dist/"
