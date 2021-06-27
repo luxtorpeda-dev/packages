9154a090880cb65f05b18d4bb2af87290ca9bcd8
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-06-26 20:23:05 -0500

    Re-add re3 (#370)
    * Re-add re3
    
    * fix

---
+++ engines/re3/env.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash
+
+export STEAM_APP_ID_LIST="12100"
+export LICENSE_PATH="./source/README.md"
+export ADDITIONAL_LICENSES="./glew/glew-2.1.0/LICENSE.txt ./openal/COPYING ./glfw/LICENSE.md ./libsndfile/COPYING ./libmpg123/COPYING"
+export CUSTOM_CONTAINER="ubuntu:18.04"
