f378c68140bdacaa0f47b1204cedc818d3c66b18
d10sfan <d10sfan@gmail.com>
2021-07-05 20:30:33 -0500

    warzone2100 - Update to 4.1.0

--- engines/warzone2100/build.sh
+++ engines/warzone2100/build.sh
@@ -6,7 +6,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -y install mercurial curl tcl gettext
 # CLONE PHASE
 git clone https://github.com/Warzone2100/warzone2100.git source
 pushd source
-git checkout -f 3a3c652
+git checkout -f 046c2f9
 git submodule update --init --recursive
 popd
 
