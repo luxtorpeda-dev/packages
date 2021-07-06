c3d2754deb68bce09d53d4caaee113d4c3bf2b62
d10sfan <d10sfan@gmail.com>
2021-07-05 21:07:17 -0500

    dosbox-staging - Update to 0.77 (ubuntu 12.04)

--- engines/dosbox-staging/build.sh
+++ engines/dosbox-staging/build.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init --path)"' >> ~/.profile
+
+pyenv install 3.6.0
+pyenv local 3.6.0
+pip3 install meson
+
 # CLONE PHASE
 git clone https://github.com/dosbox-staging/dosbox-staging.git source
 pushd source
 git checkout -f cf72ec3
 popd
 
-git clone https://github.com/FluidSynth/fluidsynth.git fluidsynth
-pushd fluidsynth
-git checkout -f 19a20eb
-popd
-
-readonly pfx="$PWD/local"
-mkdir -p "$pfx"
-
 # BUILD PHASE
-pushd "fluidsynth"
-mkdir -p build
-cd build
-cmake \
-    -DCMAKE_INSTALL_PREFIX="$pfx" \
-    ..
-make -j "$(nproc)" install
-popd
-
-export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$pfx/lib64/pkgconfig"
-
 pushd "source"
-./autogen.sh
-./configure CPPFLAGS="-DNDEBUG" CFLAGS="-O3" CXXFLAGS="-O3" --prefix="$pfx"
-make -j "$(nproc)"
-make install
+meson setup -Dbuildtype=release \
+    -Dc_args=-Ofast \
+    -Dcpp_args=-Ofast \
+    -Db_asneeded=true -Dstrip=true \
+    -Ddefault_library=static \
+    -Dfluidsynth:enable-floats=true \
+    -Dfluidsynth:try-static-deps=true \
+    build
 popd
 
 # COPY PHASE
