b2b3e7046d4d2a26dca064c941c3608a15eb2c4e
d10sfan <d10sfan@gmail.com>
2021-08-07 21:13:21 -0500

    arxlibertatis - Fix for too many file descriptors

--- engines/arxlibertatis/assets/arx-launcher.sh
+++ engines/arxlibertatis/assets/arx-launcher.sh
@@ -7,13 +7,13 @@ lowercase () {
 create_relative_symlink () {
 	local -r target=$1
 	local -r symlink="share/arx/data/$(lowercase "$target")"
-	mkdir -p "$(dirname "$symlink")"
-	ln -rsf "$target" "$symlink"
+	LD_PRELOAD="" mkdir -p "$(dirname "$symlink")"
+	LD_PRELOAD="" ln -rsf "$target" "$symlink"
 }
 
 # http://wiki.arx-libertatis.org/Required_data_files_and_checksums
 
-find {misc,Graph} -type f  | while read -r file_name ; do
+LD_PRELOAD="" find {misc,Graph} -type f  | while read -r file_name ; do
 	create_relative_symlink "$file_name"
 done
 for pak in *.pak ; do
