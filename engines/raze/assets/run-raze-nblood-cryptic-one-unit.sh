9660297de58a1307e483b54886fa1a2a87e428d0
d10sfan <d10sfan@gmail.com>
2021-08-09 16:09:41 -0500

    raze - Fix for blood issue on first launch

--- engines/raze/assets/run-raze-nblood-cryptic-one-unit.sh
+++ engines/raze/assets/run-raze-nblood-cryptic-one-unit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -f raze-template.ini ]; then
+    LD_PRELOAD="" cp -rfv ./raze-template.ini ./raze.ini
+fi
+
 mkdir ./ini
 cp -rfv ./BLOOD.INI ./ini/BLOOD.INI
-LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -cryptic
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -cryptic -config raze.ini
