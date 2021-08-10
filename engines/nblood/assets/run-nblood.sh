8c34e167de19c2c39951217230c2e707ff864d0e
d10sfan <d10sfan@gmail.com>
2021-08-10 12:37:34 -0500

    nblood - Fix for cryptic

--- engines/nblood/assets/run-nblood.sh
+++ engines/nblood/assets/run-nblood.sh
@@ -7,7 +7,7 @@ if [ -z $1 ]; then
 elif [ "$gamearg" = "-addon" ]; then
     ln -rsf "addons/Cryptic Passage/tiles007.ART" "addons/Cryptic Passage/TILES007.ART"
     ln -rsf "addons/Cryptic Passage/tiles015.ART" "addons/Cryptic Passage/TILES015.ART"
-    ./nblood -usecwd -nosetup -ini "addons/Cryptic Passage/CRYPTIC.INI" -j "addons/Cryptic Passage"
+    ./nblood -usecwd -nosetup -ini CRYPTIC.INI -j "addons/Cryptic Passage"
 else
     ./nblood -usecwd -nosetup
 fi
