e0741d7cb4d29281de04ea2f21429fcda9cfdb9f
d10sfan <d10sfan@gmail.com>
2021-08-11 09:49:15 -0500

    missing dxvk

--- engines/perimeter/build.sh
+++ engines/perimeter/build.sh
@@ -163,10 +163,12 @@ cd build
 /usr/local/bin/cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT="$boostlocation" -DCMAKE_PREFIX_PATH="$pfx" -DCMAKE_INSTALL_PREFIX="$pfx"
 ninja dependencies
 ninja
+cd Source/dxvk-native-prefix/src/dxvk-native-build
+DESTDIR="$pfx" ninja install
 popd
 
 # COPY PHASE
 mkdir -p "$diststart/common/dist/lib"
 cp -rfv "$pfx/lib"/*.so* "$diststart/common/dist/lib"
-
+cp -rfv "$pfx/usr/local/lib/x86_64-linux-gnu"/*.so* "$diststart/common/dist/lib"
 cp -rfv source/build/Source/perimeter "$diststart/common/dist/"
