c8244a5cbed43e3ca09cd10b69cc0b3dd5f302a1
d10sfan <d10sfan@gmail.com>
2021-08-11 11:39:40 -0500

    perimeter - Fix app id

--- engines/perimeter/env.sh
+++ engines/perimeter/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export STEAM_APP_ID_LIST="289440 289240"
+export STEAM_APP_ID_LIST="289440"
 export LICENSE_PATH="./source/LICENSE"
 export ADDITIONAL_LICENSES="./openal/COPYING ./theora/COPYING ./ogg/COPYING ./SDL/COPYING.txt ./boost/LICENSE_1_0.txt"
 export CUSTOM_CONTAINER="ubuntu:18.04"
