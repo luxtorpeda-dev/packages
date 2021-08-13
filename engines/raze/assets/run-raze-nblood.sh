9288a7068b8b754b0d85b93400c3511d9a7f2a9b
McMarius11 <McMarius@gmx.net>
2021-08-13 03:03:43 +0200

    Update run-raze-nblood.sh (#397)
    * Update run-raze-nblood.sh
    
    with these changes i can play both, the main game and the cryptic passage.
    
    these commands work
    
    ./raze -iwad blood.rff
    ./raze -gamegrp blood.rff
    
    ./raze -iwad blood.rff -con cryptic.ini -file "addons/Cryptic Passage/"
    ./raze -gamegrp blood.rff -con cryptic.ini -file "addons/Cryptic Passage/"
    
    * Remove raze.ini need
    
    Co-authored-by: d10sfan <d10sfan@gmail.com>

--- engines/raze/assets/run-raze-nblood.sh
+++ engines/raze/assets/run-raze-nblood.sh
@@ -2,16 +2,12 @@
 
 gamearg="$1"
 
-if [ ! -f raze-template.ini ]; then
-    LD_PRELOAD="" cp -rfv ./raze-template.ini ./raze.ini
-fi
-
 if [ -z $1 ]; then
-    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -gamegrp BLOOD.RFF -config raze.ini
+    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -gamegrp blood.rff
 elif [ "$gamearg" = "-addon" ]; then
     ln -rsf "addons/Cryptic Passage/tiles007.ART" "addons/Cryptic Passage/TILES007.ART"
     ln -rsf "addons/Cryptic Passage/tiles015.ART" "addons/Cryptic Passage/TILES015.ART"
-    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -con "addons/Cryptic Passage/CRYPTIC.INI" -gamegrp BLOOD.RFF -j "addons/Cryptic Passage" -config raze.ini
+    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -gamegrp blood.rff -con cryptic.ini -file "addons/Cryptic Passage"
 else
-    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -gamegrp BLOOD.RFF -config raze.ini
+    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -gamegrp blood.rff
 fi
