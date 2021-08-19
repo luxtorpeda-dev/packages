55f394e4288c8a2b185b5bb64c531d00e84f034f
d10sfan <d10sfan@gmail.com>
2021-08-19 13:52:57 -0500

    quakespasm-spiked - Support for new update

--- engines/quakespasm-spiked/assets/quakespasm-hipnotic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f quakespasm/share/quake/hipnotic/config.cfg ] ; then
-	cp -f quakespasm/share/quake/default.lux.cfg quakespasm/share/quake/hipnotic/config.cfg
-	sed -i "s|%USER%|$USER|" quakespasm/share/quake/hipnotic/config.cfg
-fi
-
-LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./quakespasm/lib" ./quakespasm/quakespasm -fitz -basedir quakespasm/share/quake -game hipnotic "$@"
--- engines/quakespasm-spiked/assets/quakespasm.sh
+++ engines/quakespasm-spiked/assets/quakespasm.sh
@@ -5,4 +5,26 @@ if [ ! -f quakespasm/share/quake/id1/config.cfg ] ; then
 	sed -i "s|%USER%|$USER|" quakespasm/share/quake/id1/config.cfg
 fi
 
+if [ ! -f quakespasm/share/quake/id1/music ] ; then
+    LD_PRELOAD="" ln -rsf ./rerelease/id1/music ./quakespasm/share/quake/id1/music
+fi
+
+if [ ! -f quakespasm/share/quake/rogue/config.cfg ] ; then
+	cp -f quakespasm/share/quake/default.lux.cfg quakespasm/share/quake/rogue/config.cfg
+	sed -i "s|%USER%|$USER|" quakespasm/share/quake/rogue/config.cfg
+fi
+
+if [ ! -f quakespasm/share/quake/rogue/music ] ; then
+    LD_PRELOAD="" ln -rsf ./rerelease/rogue/music ./quakespasm/share/quake/rogue/music
+fi
+
+if [ ! -f quakespasm/share/quake/hipnotic/config.cfg ] ; then
+	cp -f quakespasm/share/quake/default.lux.cfg quakespasm/share/quake/hipnotic/config.cfg
+	sed -i "s|%USER%|$USER|" quakespasm/share/quake/hipnotic/config.cfg
+fi
+
+if [ ! -f quakespasm/share/quake/hipnotic/music ] ; then
+    LD_PRELOAD="" ln -rsf ./rerelease/hipnotic/music ./quakespasm/share/quake/hipnotic/music
+fi
+
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./quakespasm/lib" ./quakespasm/quakespasm -fitz -basedir quakespasm/share/quake "$@"
