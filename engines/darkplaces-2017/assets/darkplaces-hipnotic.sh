c891691f70dd177e6decb9dcb41190186c185d42
d10sfan <d10sfan@gmail.com>
2021-08-19 13:50:44 -0500

    darkplaces-2017 - Support for new update

--- engines/darkplaces-2017/assets/darkplaces-hipnotic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f darkplaces-2017/share/quake/hipnotic/config.cfg ] ; then
-	cp -f darkplaces-2017/share/quake/default.lux.cfg darkplaces-2017/share/quake/rogue/config.cfg
-	sed -i "s|%USER%|$USER|" darkplaces-2017/share/quake/hipnotic/config.cfg
-fi
-
-./darkplaces-2017/darkplaces-sdl -basedir darkplaces-2017/share/quake -game hipnotic "$@"
