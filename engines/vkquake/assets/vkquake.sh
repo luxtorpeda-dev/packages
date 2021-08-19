3698e8ffcc47a00973850aa82d52fce8c8f76e3c
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-08-19 13:48:42 -0500

    vkquake - Support for new update (#401)

--- engines/vkquake/assets/vkquake.sh
+++ engines/vkquake/assets/vkquake.sh
@@ -11,4 +11,26 @@ if [ ! -f share/quake/id1/config.cfg ] ; then
 	sed -i "s|%USER%|$USER|" share/quake/id1/config.cfg
 fi
 
+if [ ! -f share/quake/id1/music ] ; then
+    LD_PRELOAD="" ln -rsf ./rerelease/id1/music ./share/quake/id1/music
+fi
+
+if [ ! -f share/quake/rogue/config.cfg ] ; then
+	cp -f share/quake/default.lux.cfg share/quake/rogue/config.cfg
+	sed -i "s|%USER%|$USER|" share/quake/rogue/config.cfg
+fi
+
+if [ ! -f share/quake/rogue/music ] ; then
+    LD_PRELOAD="" ln -rsf ./rerelease/rogue/music ./share/quake/rogue/music
+fi
+
+if [ ! -f share/quake/hipnotic/config.cfg ] ; then
+	cp -f share/quake/default.lux.cfg share/quake/hipnotic/config.cfg
+	sed -i "s|%USER%|$USER|" share/quake/hipnotic/config.cfg
+fi
+
+if [ ! -f share/quake/hipnotic/music ] ; then
+    LD_PRELOAD="" ln -rsf ./rerelease/hipnotic/music ./share/quake/hipnotic/music
+fi
+
 ./vkquake -fitz -basedir share/quake "$@"
