3d8ef96ebc97c7dcbd8fdcce57a6f04f118749ac
d10sfan <d10sfan@gmail.com>
2021-08-20 11:50:05 -0500

    ftequakeworld - Support new missions

---
+++ engines/ftequakeworld/assets/ftequakeworld-quake1-mg1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! -d id1/music ] ; then
+    LD_PRELOAD="" ln -rsf ./rerelease/id1/music ./id1/music
+fi
+
+if [ ! -d rogue/music ] ; then
+    LD_PRELOAD="" ln -rsf ./rerelease/rogue/music ./rogue/music
+fi
+
+if [ ! -d hipnotic/music ] ; then
+    LD_PRELOAD="" ln -rsf ./rerelease/hipnotic/music ./hipnotic/music
+fi
+
+if [ ! -d mg1 ] ; then
+    LD_PRELOAD="" ln -rsf ./rerelease/mg1 ./mg1
+fi
+
+chmod +x fteqw64
+./fteqw64 "$@"
--- engines/ftequakeworld/build.sh
+++ engines/ftequakeworld/build.sh
@@ -3,3 +3,4 @@
 # COPY PHASE
 cp -v assets/ftequakeworld.sh "$diststart/common/dist/"
 cp -v assets/ftequakeworld-quake1.sh "$diststart/common/dist/"
+cp -v assets/ftequakeworld-quake1-mg1.sh "$diststart/common/dist/"
