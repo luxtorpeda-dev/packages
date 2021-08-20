3442642e3b630c7ee0e787242f08e5aaf82f378c
d10sfan <d10sfan@gmail.com>
2021-08-19 21:24:22 -0500

    darkplaces2017 - Add new mission support

--- engines/darkplaces-2017/assets/darkplaces.sh
+++ engines/darkplaces-2017/assets/darkplaces.sh
@@ -5,7 +5,7 @@ if [ ! -f darkplaces-2017/share/quake/id1/config.cfg ] ; then
 	sed -i "s|%USER%|$USER|" darkplaces-2017/share/quake/id1/config.cfg
 fi
 
-if [ ! -f darkplaces-2017/share/quake/id1/music ] ; then
+if [ ! -d darkplaces-2017/share/quake/id1/music ] ; then
     LD_PRELOAD="" ln -rsf ./rerelease/id1/music ./darkplaces-2017/share/quake/id1/music
 fi
 
@@ -14,7 +14,7 @@ if [ ! -f darkplaces-2017/share/quake/rogue/config.cfg ] ; then
 	sed -i "s|%USER%|$USER|" darkplaces-2017/share/quake/rogue/config.cfg
 fi
 
-if [ ! -f darkplaces-2017/share/quake/rogue/music ] ; then
+if [ ! -d darkplaces-2017/share/quake/rogue/music ] ; then
     LD_PRELOAD="" ln -rsf ./rerelease/rogue/music ./darkplaces-2017/share/quake/rogue/music
 fi
 
@@ -23,8 +23,18 @@ if [ ! -f darkplaces-2017/share/quake/hipnotic/config.cfg ] ; then
 	sed -i "s|%USER%|$USER|" darkplaces-2017/share/quake/hipnotic/config.cfg
 fi
 
-if [ ! -f darkplaces-2017/share/quake/hipnotic/music ] ; then
+if [ ! -d darkplaces-2017/share/quake/hipnotic/music ] ; then
     LD_PRELOAD="" ln -rsf ./rerelease/hipnotic/music ./darkplaces-2017/share/quake/hipnotic/music
+fi
+
+if [ ! -f darkplaces-2017/share/quake/dopa/config.cfg ] ; then
+	cp -f darkplaces-2017/share/quake/default.lux.cfg darkplaces-2017/share/quake/dopa/config.cfg
+	sed -i "s|%USER%|$USER|" darkplaces-2017/share/quake/dopa/config.cfg
+fi
+
+if [ ! -f darkplaces-2017/share/quake/mg1/config.cfg ] ; then
+	cp -f darkplaces-2017/share/quake/default.lux.cfg darkplaces-2017/share/quake/mg1/config.cfg
+	sed -i "s|%USER%|$USER|" darkplaces-2017/share/quake/mg1/config.cfg
 fi
 
 ./darkplaces-2017/darkplaces-sdl -basedir darkplaces-2017/share/quake "$@"
