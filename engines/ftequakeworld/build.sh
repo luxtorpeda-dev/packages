c9d0cb51aeb3d0dc8a3086e0637ec3b62029f213
d10sfan <d10sfan@gmail.com>
2021-08-20 11:04:35 -0500

    fix

--- engines/ftequakeworld/build.sh
+++ engines/ftequakeworld/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sudo apt-get -y install svn
+
 # CLONE PHASE
-svn checkout https://svn.code.sf.net/p/fteqw/code/trunk fteqw-code
+svn checkout https://svn.code.sf.net/p/fteqw/code/trunk source
 
 # COPY PHASE
 cp -v assets/ftequakeworld.sh "$diststart/common/dist/"
