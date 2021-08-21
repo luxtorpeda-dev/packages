5af5855411af03ef8f789c2fd588f50d035896cb
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-08-20 23:04:47 -0500

    Classic-rbdoom-3-bfg - Fix language issue (#408)

--- engines/classic-rbdoom-3-bfg/assets/run-classic-rbdoom-3-bfg.sh
+++ engines/classic-rbdoom-3-bfg/assets/run-classic-rbdoom-3-bfg.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+create_relative_symlink () {
+    local -r target=$1
+    local -r symlink="classic/$target"
+    LD_PRELOAD="" mkdir -p "$(LD_PRELOAD="" dirname "$symlink")"
+    LD_PRELOAD="" ln -rsf "$target" "$symlink"
+}
+
+LD_PRELOAD="" find base -type f  | while LD_PRELOAD="" read -r file_name ; do
+    create_relative_symlink "$file_name"
+done
+
+LD_PRELOAD="" cp -rfv classic/updatedbase/* classic/base
+
 cd classic
 
 LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./DoomBFA
