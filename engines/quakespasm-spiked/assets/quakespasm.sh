8f01720b7c8b701bc547edb9610a4a230a2ddcf4
d10sfan <d10sfan@gmail.com>
2021-08-23 12:38:38 -0500

    quakespasm-spiked - Support Id1 path

--- engines/quakespasm-spiked/assets/quakespasm.sh
+++ engines/quakespasm-spiked/assets/quakespasm.sh
@@ -9,6 +9,15 @@ if [ ! -d quakespasm/share/quake/id1/music ] ; then
     LD_PRELOAD="" ln -rsf ./rerelease/id1/music ./quakespasm/share/quake/id1/music
 fi
 
+if [ ! -e quakespasm/share/quake/id1/pak0.pak ] ; then
+    echo "id1 pak0.pak link broken"
+    if [ -f Id1/PAK0.PAK ] ; then
+        echo "Found Id1/PAK0.PAK"
+        LD_PRELOAD="" ln -rsf ./Id1/PAK0.PAK ./quakespasm/share/quake/id1/pak0.pak
+        LD_PRELOAD="" ln -rsf ./Id1/PAK1.PAK ./quakespasm/share/quake/id1/pak1.pak
+    fi
+fi
+
 if [ ! -f quakespasm/share/quake/rogue/config.cfg ] ; then
 	cp -f quakespasm/share/quake/default.lux.cfg quakespasm/share/quake/rogue/config.cfg
 	sed -i "s|%USER%|$USER|" quakespasm/share/quake/rogue/config.cfg
