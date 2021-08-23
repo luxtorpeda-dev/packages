d23e11389d19cd8a0be9f8a027bc332cfd79e044
d10sfan <d10sfan@gmail.com>
2021-08-23 12:42:13 -0500

    ftequakeworld - Support Id1 path

--- engines/ftequakeworld/assets/ftequakeworld-quake1.sh
+++ engines/ftequakeworld/assets/ftequakeworld-quake1.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ ! -e id1/pak0.pak ] ; then
+    echo "id1 pak0.pak not found"
+    if [ -f Id1/PAK0.PAK ] ; then
+        echo "Found Id1/PAK0.PAK"
+        LD_PRELOAD="" mkdir ./id1
+        LD_PRELOAD="" ln -rsf ./Id1/PAK0.PAK ./id1/pak0.pak
+        LD_PRELOAD="" ln -rsf ./Id1/PAK1.PAK ./id1/pak1.pak
+    fi
+fi
+
 if [ ! -d id1/music ] ; then
     LD_PRELOAD="" ln -rsf ./rerelease/id1/music ./id1/music
 fi
