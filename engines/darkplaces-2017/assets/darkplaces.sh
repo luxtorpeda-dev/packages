f9333b6e5deae97a9769f372dbd11ffa38cf56e8
d10sfan <d10sfan@gmail.com>
2021-08-23 12:39:24 -0500

    darkplaces-2017 - Support Id1 path

--- engines/darkplaces-2017/assets/darkplaces.sh
+++ engines/darkplaces-2017/assets/darkplaces.sh
@@ -9,6 +9,15 @@ if [ ! -d darkplaces-2017/share/quake/id1/music ] ; then
     LD_PRELOAD="" ln -rsf ./rerelease/id1/music ./darkplaces-2017/share/quake/id1/music
 fi
 
+if [ ! -e darkplaces-2017/share/quake/id1/pak0.pak ] ; then
+    echo "id1 pak0.pak link broken"
+    if [ -f Id1/PAK0.PAK ] ; then
+        echo "Found Id1/PAK0.PAK"
+        LD_PRELOAD="" ln -rsf ./Id1/PAK0.PAK ./darkplaces-2017/share/quake/id1/pak0.pak
+        LD_PRELOAD="" ln -rsf ./Id1/PAK1.PAK ./darkplaces-2017/share/quake/id1/pak1.pak
+    fi
+fi
+
 if [ ! -f darkplaces-2017/share/quake/rogue/config.cfg ] ; then
 	cp -f darkplaces-2017/share/quake/default.lux.cfg darkplaces-2017/share/quake/rogue/config.cfg
 	sed -i "s|%USER%|$USER|" darkplaces-2017/share/quake/rogue/config.cfg
