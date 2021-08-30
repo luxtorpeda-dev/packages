67e38077fbe68cf8b6e798657426057d7be7d158
d10sfan <d10sfan@gmail.com>
2021-08-29 22:18:21 -0500

    vkquake - commit was removed

--- engines/vkquake/build.sh
+++ engines/vkquake/build.sh
@@ -5,7 +5,7 @@
 # CLONE PHASE
 git clone https://github.com/Novum/vkQuake source
 pushd source
-git checkout aa13c83
+git checkout 33bf2dc
 popd
 
 wget https://downloads.sourceforge.net/project/p7zip/p7zip/16.02/p7zip_16.02_src_all.tar.bz2
