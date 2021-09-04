2f0d921e8fa0aba5ffd38a60a19f08b337c17b75
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-03 21:57:05 -0500

    perimeter - Update to 0b355d5 (#419)

--- engines/perimeter/env.sh
+++ engines/perimeter/env.sh
@@ -2,7 +2,7 @@
 
 export STEAM_APP_ID_LIST="289440"
 export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./openal/COPYING ./theora/COPYING ./ogg/COPYING ./SDL/COPYING.txt ./boost/LICENSE_1_0.txt"
+export ADDITIONAL_LICENSES="./openal/COPYING ./theora/COPYING ./ogg/COPYING ./SDL/COPYING.txt ./boost/LICENSE_1_0.txt ./sdlimage/LICENSE.txt ./ffmpeg/COPYING.LGPLv2.1"
 export CUSTOM_CONTAINER="ubuntu:18.04"
 export COMMON_PACKAGE="1"
 export GCC_10="1"
