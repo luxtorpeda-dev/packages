a359b087bd65a40ba03a02687afe0c210d88b982
d10sfan <d10sfan@gmail.com>
2021-09-11 21:53:26 -0500

    gzdoom - Add Relentless Frontier Demo

--- engines/gzdoom/env.sh
+++ engines/gzdoom/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export STEAM_APP_ID_LIST="2280 2290 2300 2360 2390 2370 1118550 817220 1072150 1546440 1271050 1417010 9160"
+export STEAM_APP_ID_LIST="2280 2290 2300 2360 2390 2370 1118550 817220 1072150 1546440 1271050 1417010 9160 1731320"
 export LICENSE_PATH="./source/LICENSE"
 export ADDITIONAL_LICENSES="./LICENSE ./fluidsynth/LICENSE ./zmusic/licenses/bsd.txt ./zmusic/licenses/dumb.txt ./zmusic/licenses/gplv3.txt ./zmusic/licenses/legal.txt ./zmusic/licenses/zmusic.txt"
 export COMMON_PACKAGE="1"
