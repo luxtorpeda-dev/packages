320961c206f27b7028960a981e93809fb737480f
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-11 22:25:37 -0500

    wigzdoom (#426)

---
+++ engines/wigzdoom/env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="1679920"
+export LICENSE_PATH="./source/LICENSE"
+export ADDITIONAL_LICENSES="./LICENSE ./fluidsynth/LICENSE ./zmusic/licenses/bsd.txt ./zmusic/licenses/dumb.txt ./zmusic/licenses/gplv3.txt ./zmusic/licenses/legal.txt ./zmusic/licenses/zmusic.txt"
+export COMMON_PACKAGE="1"
+export GCC_9="1"
