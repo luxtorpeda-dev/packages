25998630e33cc28b44208d9f17d9363d13bd6a4c
d10sfan <d10sfan@gmail.com>
2021-09-14 08:42:08 -0500

    dhewm3 - Add demo support

--- engines/dhewm3/env.sh
+++ engines/dhewm3/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export STEAM_APP_ID_LIST="9050 9070"
+export STEAM_APP_ID_LIST="9050 9070 9100"
 export LICENSE_PATH="./source/COPYING.txt"
 export ADDITIONAL_LICENSES="./source/README.md ./openal/COPYING"
 export COMMON_PACKAGE="1"
