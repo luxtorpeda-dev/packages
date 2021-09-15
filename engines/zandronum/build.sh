69b0593c758130e1207fb8d3663aa7b05ae80e8c
d10sfan <d10sfan@gmail.com>
2021-09-14 19:40:06 -0500

    fmod

--- engines/zandronum/build.sh
+++ engines/zandronum/build.sh
@@ -2,6 +2,8 @@
 
 # From https://gitlab.com/luxtorpeda/packages/gzdoom - See LICENSE file for more information
 
+apt-get install -y wget
+
 # CLONE PHASE
 git clone https://github.com/TorrSamaho/zandronum.git source
 pushd source
@@ -13,11 +15,18 @@ pushd fluidsynth
 git checkout -f 19a20eb
 popd
 
+wget https://zandronum.com/essentials/fmod/fmodapi42416linux64.tar.gz
+tar -xvf fmodapi42416linux64.tar.gz
+
 readonly pfx="$PWD/local"
 mkdir -p "$pfx"
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$pfx/lib64/pkgconfig:$pfx/lib/pkgconfig"
 
 # BUILD PHASE
+pushd "fmodapi42416linux64"
+make install
+popd
+
 pushd "fluidsynth"
 mkdir -p build
 cd build
@@ -39,7 +48,8 @@ popd
 
 # COPY PHASE
 mkdir -p "$diststart/common/dist/lib"
-
+cp -rfv fmodapi42416linux64/api/lib/* "$diststart/common/dist/lib"
+cp -rfv fmodapi42416linux64/api/plugins "$diststart/common/dist/lib"
 cp -rfv "source/build"/{zandronum,soundfonts,*.pk3} "$diststart/common/dist/"
 cp -rfv "$pfx/lib64"/libfluidsynth.so* "$diststart/common/dist/lib"
 cp -rfv assets/* "$diststart/common/dist/"
