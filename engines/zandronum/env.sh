d39ec02a0c6368dfcaba4b2020970725186dc8c8
d10sfan <d10sfan@gmail.com>
2021-09-14 19:50:00 -0500

    ?

--- engines/zandronum/env.sh
+++ engines/zandronum/env.sh
@@ -2,6 +2,6 @@
 
 export STEAM_APP_ID_LIST="1566570"
 export LICENSE_PATH="./source/LICENSE.txt"
-export ADDITIONAL_LICENSES="./LICENSE"
+export ADDITIONAL_LICENSES="./LICENSE ./fmodapi42416linux64/documentation/LICENSE.TXT"
 export COMMON_PACKAGE="1"
 export GCC_9="1"
