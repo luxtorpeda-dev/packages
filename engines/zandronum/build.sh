8bdd8f3d37426f312d54858f45b20e308e9369d0
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-14 19:56:15 -0500

    zandronum (#428)

---
+++ engines/zandronum/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get install -y wget
+
+# CLONE PHASE
+git clone https://github.com/TorrSamaho/zandronum.git source
+pushd source
+git checkout 99a19d7
+popd
+
+wget https://zandronum.com/essentials/fmod/fmodapi42416linux64.tar.gz
+tar -xvf fmodapi42416linux64.tar.gz
---
+++ engines/zandronum/env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="1566570"
+export LICENSE_PATH="./source/LICENSE.txt"
+export ADDITIONAL_LICENSES="./LICENSE ./fmodapi42416linux64/documentation/LICENSE.TXT"
+export COMMON_PACKAGE="1"
+export GCC_9="1"
