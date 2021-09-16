0f3b627c9e3eadc06b13e516d9bfea63bbe3e729
d10sfan <d10sfan@gmail.com>
2021-09-16 18:38:33 -0500

    eventually default to steamrt

--- engines/dosbox-staging/env.sh
+++ engines/dosbox-staging/env.sh
@@ -2,6 +2,4 @@
 
 export STEAM_APP_ID_LIST="default 410970"
 export LICENSE_PATH="./source/COPYING"
-export ADDITIONAL_LICENSES=""
 export COMMON_PACKAGE="1"
-export CUSTOM_CONTAINER="registry.gitlab.steamos.cloud/steamrt/soldier/sdk:0.20210906.1"
