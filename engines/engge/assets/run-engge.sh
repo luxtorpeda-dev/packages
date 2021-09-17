5cde13acbd12b87bbe84324d4b5fcdcd899dfa8c
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-16 22:30:18 -0500

    engge - Build for steam runtime (#437)

--- engines/engge/assets/run-engge.sh
+++ engines/engge/assets/run-engge.sh
@@ -3,4 +3,4 @@
 ln -rsf ./Resources/ThimbleweedPark.ggpack1 ./ThimbleweedPark.ggpack1
 ln -rsf ./Resources/ThimbleweedPark.ggpack2 ./ThimbleweedPark.ggpack2
 
-LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./engge
+./engge
