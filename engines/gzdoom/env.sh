0566ce4494948ceb4a69ac65b02f638d9e4040bf
d10sfan <d10sfan@gmail.com>
2021-09-16 21:07:08 -0500

    gzdoom - Build for steamrt

--- engines/gzdoom/env.sh
+++ engines/gzdoom/env.sh
@@ -4,4 +4,3 @@ export STEAM_APP_ID_LIST="2280 2290 2300 2360 2390 2370 1118550 817220 1072150 1
 export LICENSE_PATH="./source/LICENSE"
 export ADDITIONAL_LICENSES="./LICENSE ./fluidsynth/LICENSE ./zmusic/licenses/bsd.txt ./zmusic/licenses/dumb.txt ./zmusic/licenses/gplv3.txt ./zmusic/licenses/legal.txt ./zmusic/licenses/zmusic.txt"
 export COMMON_PACKAGE="1"
-export GCC_9="1"
