77a412ef46f3427f43456b0b83ab6669e25f6eb0
d10sfan <d10sfan@gmail.com>
2021-09-16 21:24:44 -0500

    opengothic - Build for steamrt

--- engines/opengothic/env.sh
+++ engines/opengothic/env.sh
@@ -2,6 +2,3 @@
 
 export STEAM_APP_ID_LIST="39510"
 export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./assets/LICENSE.glslang"
-export CUSTOM_CONTAINER="ubuntu:18.04"
-export GCC_10="1"
