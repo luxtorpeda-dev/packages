fec46e01ff01116206fe941632a9c959d031527f
d10sfan <d10sfan@gmail.com>
2021-09-16 21:38:24 -0500

    vkquake2 - Build for steamrt

--- engines/vkquake2/assets/vkQuake2.sh
+++ engines/vkquake2/assets/vkQuake2.sh
@@ -2,4 +2,4 @@
 
 cd vkQuake2
 
-LD_LIBRARY_PATH="./:$LD_LIBRARY_PATH" ./quake2 "$@"
+./quake2 "$@"
--- engines/vkquake2/build.sh
+++ engines/vkquake2/build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install -y libxxf86vm-dev libasound2-dev libx11-dev libxcb1-dev curl
-
-curl -L -v -o vulkansdk-linux-x86_64-1.2.148.1.tar.gz -O https://sdk.lunarg.com/sdk/download/1.2.148.1/linux/vulkan_sdk.tar.gz?Human=true
-tar zxf vulkansdk-linux-x86_64-1.2.148.1.tar.gz
-
-curl -L -v -o libXxf86dga-1.1.5.tar.bz2 http://www.x.org/releases/individual/lib/libXxf86dga-1.1.5.tar.bz2
-tar xvf libXxf86dga-1.1.5.tar.bz2
-
 export CXXFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas"
 export CFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas"
 
@@ -17,13 +9,6 @@ pushd source
 git checkout 429f23b
 popd
 
-pushd "libXxf86dga-1.1.5"
-./configure
-make
-make install
-popd
-
-export VULKAN_SDK="$PWD/1.2.148.1/x86_64"
 # BUILD PHASE
 pushd "source"
 cd linux
@@ -48,9 +33,5 @@ cp -rfv "source/linux/releasex64/xatrix/gamex64.so" "$diststart/common/dist/xatr
 
 mkdir -p "$diststart/common/dist/zaero"
 cp -rfv "source/linux/releasex64/zaero/gamex64.so" "$diststart/common/dist/zaero"
-
-cp -rfv /usr/local/lib/libXxf86dga.so.1.0.0 "$diststart/common/dist"
-cp -rfv /usr/local/lib/libXxf86dga.so "$diststart/common/dist"
-cp -rfv /usr/local/lib/libXxf86dga.so.1 "$diststart/common/dist"
 
 cp -rfv assets/* "$diststart/common/dist/"
