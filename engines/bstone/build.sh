43d38685f871d46a93e8db4a048a2091fd8c2610
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-16 22:44:05 -0500

    bstone - Build for steamrt (#435)

--- engines/bstone/build.sh
+++ engines/bstone/build.sh
@@ -1,47 +1,21 @@
 #!/bin/bash
 
-apt-get -y install mercurial
-
 # CLONE PHASE
 git clone https://github.com/bibendovsky/bstone.git source
 pushd source
 git checkout d329e75
 popd
 
-hg clone https://hg.libsdl.org/SDL
-pushd SDL
-hg checkout release-2.0.12
-popd
-
-readonly pfx="$PWD/local"
-mkdir -p "$pfx"
-
 # BUILD PHASE
-pushd "SDL"
-mkdir -p build
-cd build
-cmake \
-    -DCMAKE_BUILD_TYPE=MinSizeRel \
-    -DCMAKE_PREFIX_PATH="$pfx" \
-    -DCMAKE_INSTALL_PREFIX="$pfx" \
-    ..
-make -j "$(nproc)"
-make install
-popd
-
 pushd source
 cd build
 cmake \
-    -DCMAKE_PREFIX_PATH="$pfx" \
     -DCMAKE_BUILD_TYPE=Release \
     -DBSTONE_USE_PCH=OFF \
     ..
 make -j "$(nproc)"
-make install
 popd
 
 # COPY PHASE
-cp -rfv "source/build/src/bstone" "$diststart/common/dist/"
-cp -rfv assets/* "$diststart/common/dist/"
-mkdir -p "$diststart/common/dist/lib"
-cp -rfv "$pfx/lib/"*.so* "$diststart/common/dist/lib"
+cp -rfv "source/build/src/bstone" "$diststart/common/dist"
+cp -rfv assets/* "$diststart/common/dist"
