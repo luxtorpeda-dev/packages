57ee1e57566d97f72b81625f1006185fd3a5e0d1
d10sfan <d10sfan@gmail.com>
2021-09-17 22:29:50 -0500

    eternaljk - Build for steamrt

--- engines/eternaljk/build.sh
+++ engines/eternaljk/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/eternalcodes/EternalJK.git source
 pushd source
-git checkout 0396e4a
+git checkout a40e793
 git am < ../patches/0001-Revert-use-of-7za-from-commit-c2dfd02ebd73b06ba2326c.patch
 popd
 
