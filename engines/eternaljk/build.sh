2fdea4daefae28743b8f24b425740302d8249334
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-17 22:42:27 -0500

    eternaljk - Build for steamrt (#450)

--- engines/eternaljk/build.sh
+++ engines/eternaljk/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/eternalcodes/EternalJK.git source
 pushd source
-git checkout 0396e4a
+git checkout a40e793
 git am < ../patches/0001-Revert-use-of-7za-from-commit-c2dfd02ebd73b06ba2326c.patch
 popd
 
@@ -12,9 +12,11 @@ mkdir -p tmp
 # BUILD PHASE
 pushd source
 mkdir build
+
 cd build
 cmake \
     -DCMAKE_INSTALL_PREFIX=../../tmp \
+    -DBuildDiscordRichPresence=OFF \
     ..
 make -j "$(nproc)"
 make install
