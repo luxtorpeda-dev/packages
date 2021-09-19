f47b7eb80a1b87ccfe9cfc4e4a6bb9f3debe423f
d10sfan <d10sfan@gmail.com>
2021-09-19 01:14:02 -0500

    doomsday - Build for steamrt

--- engines/doomsday/env.sh
+++ engines/doomsday/env.sh
@@ -2,7 +2,6 @@
 
 export STEAM_APP_ID_LIST="2280 2290 2300 2360 2390 2370"
 export LICENSE_PATH="./source/doomsday/gpl-3.0.txt"
-export ADDITIONAL_LICENSES="./source/doomsday/lgpl-3.0.txt ./fluidsynth/LICENSE ./assimp/LICENSE"
+export ADDITIONAL_LICENSES="./source/doomsday/lgpl-3.0.txt"
 export COMMON_PACKAGE="1"
-export COMMON_QT5="1"
-export GCC_9="1"
+export LIBRARIES="fluidsynth assimp qt5"
