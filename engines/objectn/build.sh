d92a1af0f74243df49b9d7cc20fa96bdc6f3d917
d10sfan <d10sfan@gmail.com>
2021-09-18 20:40:21 -0500

    fix

--- engines/objectn/build.sh
+++ engines/objectn/build.sh
@@ -4,6 +4,7 @@
 git clone https://github.com/KleskBY/ObjectN-DarkPlaces-engine.git source
 pushd source
 git checkout -f de3b618
+git am < ../patches/0001-Remove-nuklear-menu.patch
 popd
 
 # BUILD PHASE
