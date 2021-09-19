ff557697ab1e45fb9623f2569f262225a17718aa
d10sfan <d10sfan@gmail.com>
2021-09-19 00:57:34 -0500

    chocolate-doom - Fix fluidsynth

--- engines/chocolate-doom/env.sh
+++ engines/chocolate-doom/env.sh
@@ -3,4 +3,4 @@
 export STEAM_APP_ID_LIST="2280 2290 2300 2360 2390"
 export LICENSE_PATH="./source/COPYING.md"
 export COMMON_PACKAGE="1"
-export LICENSES="fluidsynth"
+export LIBRARIES="fluidsynth"
