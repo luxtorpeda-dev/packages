ccb9887c3a0316d9cc59cfcdf0c0a3208b95f791
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-19 00:10:31 -0500

    quakespasm-spiked - Build for steamrt (#470)

--- engines/quakespasm-spiked/build.sh
+++ engines/quakespasm-spiked/build.sh
@@ -6,37 +6,12 @@ pushd source
 git checkout 9ffca1d
 popd
 
-git clone https://github.com/markjeee/libmad.git mad
-pushd mad
-git checkout -f c2f96fa
-# from http://www.linuxfromscratch.org/blfs/view/svn/multimedia/libmad.html
-patch -Np1 -i ../patches/libmad-0.15.1b-fixes-1.patch
-sed "s@AM_CONFIG_HEADER@AC_CONFIG_HEADERS@g" -i configure.ac
-touch NEWS AUTHORS ChangeLog
-autoreconf -fi
-popd
-
-readonly pfx="$PWD/local"
-mkdir -p "$pfx"
-
 # BUILD PHASE
-pushd "mad"
-./configure --prefix="$pfx" --disable-static
-make -j "$(nproc)" install
-popd
-
-pushd "mad"
-./configure --disable-static
-make -j "$(nproc)" install
-popd
-
 pushd "source/quakespasm/Quake"
 make -j "$(nproc)" USE_SDL2=1
 popd
 
 # COPY PHASE
-mkdir -p "$diststart/common/dist/quakespasm/lib"
-cp -rfv "$pfx/lib/"*.so* "$diststart/common/dist/quakespasm/lib"
 mkdir -p "$diststart/common/dist/quakespasm/share/quake/id1"
 mkdir -p "$diststart/common/dist/quakespasm/share/quake/rogue"
 mkdir -p "$diststart/common/dist/quakespasm/share/quake/hipnotic"
