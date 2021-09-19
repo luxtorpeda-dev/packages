73bd97b545d68b280b66fe46931c0ce03d1ddcab
d10sfan <d10sfan@gmail.com>
2021-09-18 20:37:28 -0500

    objectn - Build for steamrt

--- engines/objectn/build.sh
+++ engines/objectn/build.sh
@@ -4,7 +4,6 @@
 git clone https://github.com/KleskBY/ObjectN-DarkPlaces-engine.git source
 pushd source
 git checkout -f de3b618
-git am < ../patches/0001-Remove-nuklear-menu.patch
 popd
 
 # BUILD PHASE
