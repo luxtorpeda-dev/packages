85f6486f4b8d07f2ac85caad749f49b2400702e6
d10sfan <d10sfan@gmail.com>
2021-09-19 10:45:21 -0500

    switch repo

--- engines/ctp2/build.sh
+++ engines/ctp2/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # CLONE PHASE
-git clone https://github.com/ptitSeb/ctp2.git source
+git clone https://github.com/civctp2/civctp2.git source
 pushd source
-git checkout ac46e601b94fad8162677b964ff87a2e07923c9e
+git checkout ae8b555
 popd
-
-apt-get install -y byacc flex
 
 # BUILD PHASE
 pushd source
@@ -14,7 +12,7 @@ cp -rfv "EULA - Source Code for CTP2.rtf" "EULA.rtf"
 cp -rfv "Activision CTP2 Source Code_Readme.txt" "Activision_CTP2_Source_Code_Readme.txt"
 cp -rfv "Apolyton CTP2 Source Code_Readme.txt" "Apolyton_CTP2_Source_Code_Readme.txt"
 ./autogen.sh
-CFLAGS="$CFLAGS -w -fuse-ld=gold" CXXFLAGS="$CXXFLAGS -w -fuse-ld=gold" ./configure --enable-silent-rules
+CFLAGS="$CFLAGS -O3 -fuse-ld=gold" CXXFLAGS="$CXXFLAGS -fpermissive -O3 -fuse-ld=gold" ./configure --enable-silent-rules
 make
 popd
 
