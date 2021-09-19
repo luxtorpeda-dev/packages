811ce8a57b8faff55137684f8318b7b452eed305
d10sfan <d10sfan@gmail.com>
2021-09-18 22:36:27 -0500

    raze - Build for steamrt

--- engines/raze/env.sh
+++ engines/raze/env.sh
@@ -2,6 +2,6 @@
 
 export STEAM_APP_ID_LIST="1010750 299030 225140 329650 376750 565550 580940 225160 434050 1260020"
 export LICENSE_PATH="./source/README.md"
-export ADDITIONAL_LICENSES="./fluidsynth/LICENSE ./zmusic/licenses/bsd.txt ./zmusic/licenses/dumb.txt ./zmusic/licenses/gplv3.txt ./zmusic/licenses/legal.txt ./zmusic/licenses/zmusic.txt ./SDL/COPYING.txt ./libvpx/LICENSE ./openal/COPYING"
+export ADDITIONAL_LICENSES="./libvpx/LICENSE"
 export COMMON_PACKAGE="1"
-export CUSTOM_CONTAINER="ubuntu:18.04"
+export LIBRARIES="fluidsynth zmusic libvpx"
