0b13d6c89813adc6abb2c00c8ed9072a16a5a204
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-18 22:36:46 -0500

    prboom - Build for steamrt (#465)

--- engines/prboom-plus/build.sh
+++ engines/prboom-plus/build.sh
@@ -3,27 +3,10 @@
 # CLONE PHASE
 git clone https://github.com/coelckers/prboom-plus.git source
 pushd source
-git checkout 9b8a632
+git checkout edb0a01
 popd
-
-git clone https://github.com/FluidSynth/fluidsynth.git fluidsynth
-pushd fluidsynth
-git checkout -f 19a20eb
-popd
-
-readonly pfx="$PWD/local"
-mkdir -p "$pfx"
 
 # BUILD PHASE
-pushd "fluidsynth"
-mkdir -p build
-cd build
-cmake \
-    -DCMAKE_INSTALL_PREFIX="$pfx" \
-    ..
-make -j "$(nproc)" install
-popd
-
 pushd "source/prboom2"
 mkdir -p build
 cd build
@@ -36,7 +19,6 @@ popd
 
 # COPY PHASE
 mkdir -p "$diststart/common/dist/lib"
-cp -rfv "$pfx/lib64"/libfluidsynth.so* "$diststart/common/dist/lib"
 cp -rfv "source/prboom2/build/prboom-plus" "$diststart/common/dist/"
 cp -rfv "source/prboom2/build/prboom-plus.wad" "$diststart/common/dist/"
 cp -rfv "source/prboom2/build/prboom-plus-game-server" "$diststart/common/dist/"
