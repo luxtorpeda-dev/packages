291f045a36205d19fdc2b7f9b29455fe342d5c22
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-19 00:11:52 -0500

    classic-rbdoom-3-bfg - Build for steamrt (#431)

--- engines/classic-rbdoom-3-bfg/env.sh
+++ engines/classic-rbdoom-3-bfg/env.sh
@@ -2,5 +2,5 @@
 
 export STEAM_APP_ID_LIST="208200"
 export LICENSE_PATH="./source/COPYING.txt"
-export ADDITIONAL_LICENSES="./source/README.md ./ffmpeg/COPYING.LGPLv2.1 ./openal/COPYING ./SDL/COPYING.txt"
-export CUSTOM_CONTAINER="ubuntu:18.04"
+export ADDITIONAL_LICENSES="./source/README.md"
+export LIBRARIES="yasm ffmpeg"
