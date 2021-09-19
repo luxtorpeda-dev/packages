f1bc644f9a9259f2127bb584ec878bd602828c4d
d10sfan <d10sfan@gmail.com>
2021-09-18 21:56:14 -0500

    force build

--- engines/gzdoom/build.sh
+++ engines/gzdoom/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # From https://gitlab.com/luxtorpeda/packages/gzdoom - See LICENSE file for more information
-
 # CLONE PHASE
 git clone https://github.com/coelckers/gzdoom.git source
 pushd source
