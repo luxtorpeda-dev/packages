f7b57a8d5d6ac2455020da104857c8993edc1691
d10sfan <d10sfan@gmail.com>
2021-09-18 23:51:07 -0500

    vkquake - Build for steamrt

--- engines/vkquake/build.sh
+++ engines/vkquake/build.sh
@@ -26,9 +26,11 @@ mkdir -p "$diststart/common/dist/share/quake/rerelease/dopa"
 mkdir -p "$diststart/common/dist/share/quake/rerelease/id1"
 mkdir -p "$diststart/common/dist/share/quake/rerelease/rogue"
 mkdir -p "$diststart/common/dist/share/quake/rerelease/hipnotic"
+
 cp -v source/Quake/vkquake "$diststart/common/dist/"
 cp -v assets/vkquake.sh "$diststart/common/dist/"
 cp -v assets/default.lux.cfg "$diststart/common/dist/share/quake"
+
 ln -s "../../../id1/PAK0.PAK" "$diststart/common/dist/share/quake/id1/pak0.pak"
 ln -s "../../../id1/PAK1.PAK" "$diststart/common/dist/share/quake/id1/pak1.pak"
 ln -s "../../../rogue/pak0.pak" "$diststart/common/dist/share/quake/rogue/pak0.pak"
