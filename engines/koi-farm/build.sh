d923488fa242465bff2e329a5afad438a771375b
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-20 14:45:34 -0500

    koi-farm - Build for steamrt (#440)

--- engines/koi-farm/build.sh
+++ engines/koi-farm/build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 # SETUP PHASE
-sudo apt-get -y install curl
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install -y nodejs
+KEYRING=/usr/share/keyrings/nodesource.gpg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | sudo tee "$KEYRING" >/dev/null
+gpg --no-default-keyring --keyring "$KEYRING" --list-keys
+
+VERSION=node_16.x
+DISTRO="buster"
+
+echo "deb [signed-by=$KEYRING] https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb-src [signed-by=$KEYRING] https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update
+sudo apt-get install nodejs
 
 # CLONE PHASE
 git clone https://github.com/jobtalle/Koi source
