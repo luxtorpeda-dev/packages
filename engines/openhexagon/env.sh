1c246db90acccecc70bae5232297efc1f676bc46
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-20 14:33:52 -0500

    openhexagon - Build for steamrt (#438)

--- engines/openhexagon/env.sh
+++ engines/openhexagon/env.sh
@@ -2,6 +2,4 @@
 
 export STEAM_APP_ID_LIST="1358090"
 export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./openal/COPYING ./theora/COPYING ./ogg/COPYING ./SDL/COPYING.txt ./flac/COPYING.FDL ./flac/COPYING.GPL ./flac/COPYING.LGPL ./flac/COPYING.Xiph ./freetype2/README"
-export CUSTOM_CONTAINER="ubuntu:18.04"
-export GCC_10="1"
+export GCC_9="1"
