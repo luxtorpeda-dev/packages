89651bee269bf3d7d111b94d3e301d1fa55fa595
d10sfan <d10sfan@gmail.com>
2021-09-21 09:58:43 -0500

    ?????

--- engines/cortex-command/env.sh
+++ engines/cortex-command/env.sh
@@ -2,6 +2,6 @@
 
 export STEAM_APP_ID_LIST="209670"
 export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./data/Credits.txt ./data/CreditsAssets.txt ./source/Licences/FMOD.TXT ./source/Licenses/RakNet.txt"
+export ADDITIONAL_LICENSES="./data/Credits.txt ./data/CreditsAssets.txt ./source/Licences/FMOD.TXT ./source/Licences/RakNet.txt"
 export LIBRARIES="boost luajit zlib liblz4 allegro4"
 export GCC_9="1"
