eae01d773aac0b510e61b266972e17cd576ee53e
d10sfan <d10sfan@gmail.com>
2021-09-22 21:33:53 -0500

    fix

--- libraries/harfbuzz/env.sh
+++ libraries/harfbuzz/env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export LIBRARY_LICENSES="./harfbuzz/COPYINGE"
-export LIBRARY_COPY_TO_LIB="$pfx/lib/*.so*"
+export LIBRARY_LICENSES="./harfbuzz/COPYING"
+export LIBRARY_COPY_TO_LIB="$pfx/lib/libharfbuzz*.so*"
