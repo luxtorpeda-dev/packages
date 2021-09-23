3123b5643cdd7fba3bf6ecfe3b44f8ea235f242f
d10sfan <d10sfan@gmail.com>
2021-09-23 09:39:10 -0500

    scummvm - Fix packages

--- engines/scummvm/env.sh
+++ engines/scummvm/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export STEAM_APP_ID_LIST="1368340 default"
+export STEAM_APP_ID_LIST="1368340 6310 default"
 export LICENSE_PATH="./source/COPYING"
 export ADDITIONAL_LICENSES="./source/COPYING.BSD ./source/COPYING.FREEFONT ./source/COPYING.LGPL ./source/COPYING.OFL ./source/COPYRIGHT"
 export COMMON_PACKAGE="1"
