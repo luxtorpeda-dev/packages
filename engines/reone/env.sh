276c7d77bceb84c3ab06cbe56f0dfb720c8ac9ee
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-23 13:01:06 -0500

    reone - Build for steamrt (#485)

--- engines/reone/env.sh
+++ engines/reone/env.sh
@@ -2,5 +2,4 @@
 
 export STEAM_APP_ID_LIST="32370"
 export LICENSE_PATH="./source/COPYING"
-export ADDITIONAL_LICENSES="./boost/LICENSE_1_0.txt ./mad/COPYING ./mad/COPYRIGHT ./glew/glew-2.1.0/LICENSE.txt ./openal/COPYING ./SDL/COPYING.txt ./ffmpeg/COPYING.LGPLv2.1 ./wxWidgets/docs/licence.txt"
-export CUSTOM_CONTAINER="ubuntu:18.04"
+export LIBRARIES="yasm boost mad ffmpeg"
