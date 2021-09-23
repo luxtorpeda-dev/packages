f626245cbfef92ee9e60f4457fdab916240c3d04
d10sfan <d10sfan@gmail.com>
2021-09-23 16:03:53 -0500

    openxray - Build for steamrt

--- engines/openxray/env.sh
+++ engines/openxray/env.sh
@@ -2,6 +2,5 @@
 
 export STEAM_APP_ID_LIST="41700 20510"
 export LICENSE_PATH="./source/License.txt"
-export ADDITIONAL_LICENSES="./freeimage/license-fi.txt ./liblockfile/COPYRIGHT ./readline/COPYING ./tbb/LICENSE ./glew/glew-2.1.0/LICENSE.txt ./openal/COPYING ./theora/COPYING ./ogg/COPYING ./SDL/COPYING.txt ./libjpeg-turbo/LICENSE.md ./pcre/LICENCE"
-export CUSTOM_CONTAINER="ubuntu:18.04"
+export ADDITIONAL_LICENSES="./freeimage/license-fi.txt ./libjpeg-turbo/LICENSE.md"
 export COMMON_PACKAGE="1"
