b93ac7e35749575a715eecd50d49d34f78bd3ac1
d10sfan <d10sfan@gmail.com>
2021-09-23 13:07:35 -0500

    solarus - Build for steamrt

--- engines/solarus/build.sh
+++ engines/solarus/build.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-apt-get -y install mercurial
-
 # CLONE PHASE
 git clone https://gitlab.com/solarus-games/solarus.git source
 pushd source
-git checkout 6d2a11dd
-popd
-
-hg clone https://hg.icculus.org/icculus/physfs
-pushd physfs
-hg checkout release-3.0.2
+git checkout 3aec70b0
 popd
 
 git clone https://gitlab.com/solarus-games/libmodplug.git libmodplug
@@ -18,45 +11,12 @@ pushd libmodplug
 git checkout a35e253001b1bc9046b7955d0871f841f88f993b
 popd
 
-git clone https://github.com/LuaJIT/LuaJIT.git luajit
-pushd luajit
-git checkout -f 570e758
-popd
-
-git clone https://github.com/g-truc/glm glm
-pushd glm
-git checkout -f 947527d3
-git submodule update --init --recursive
-sudo cp -rfv ./glm /usr/include
-popd
-
-readonly pfx="$PWD/local"
-enginepath="$PWD"
-mkdir -p "$pfx"
-
 # BUILD PHASE
-pushd physfs
-mkdir build
-cd build
-cmake \
-    -DCMAKE_INSTALL_PREFIX="$pfx" \
-    ..
-make -j "$(nproc)"
-make install
-popd
-
-export PKG_CONFIG_PATH="$pfx/lib/pkgconfig"
-
 pushd "libmodplug"
 autoreconf --install
 ./configure --prefix="$pfx"
 make -j "$(nproc)"
 make install
-popd
-
-pushd luajit
-make -j "$(nproc)"
-DESTDIR="$pfx" make install
 popd
 
 pushd "source"
@@ -67,8 +27,6 @@ cmake \
     -DSOLARUS_TESTS=OFF \
     -DCMAKE_INSTALL_PREFIX="$pfx" \
     -DSOLARUS_GUI=OFF \
-    -DLUA_INCLUDE_DIR="$pfx/usr/local/include/luajit-2.1/" \
-    -DLUA_LIBRARY="$pfx/usr/local/lib/libluajit-5.1.so.2.1.0" \
     -DCMAKE_BUILD_TYPE=Release \
     ..
 make -j "$(nproc)"
--- engines/solarus/env.sh
+++ engines/solarus/env.sh
@@ -2,4 +2,5 @@
 
 export STEAM_APP_ID_LIST="1393750"
 export LICENSE_PATH="./source/license_gpl.txt"
-export ADDITIONAL_LICENSES="./physfs/LICENSE.txt ./libmodplug/COPYING ./luajit/COPYRIGHT ./glm/copying.txt"
+export ADDITIONAL_LICENSES="./libmodplug/COPYING"
+export LIBRARIES="physfs luajit"
