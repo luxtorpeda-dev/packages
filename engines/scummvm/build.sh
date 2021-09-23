fa37082772349636f3cf9af8e77a9f197830f543
d10sfan <d10sfan@gmail.com>
2021-09-23 08:19:06 -0500

    build

--- engines/scummvm/build.sh
+++ engines/scummvm/build.sh
@@ -3,42 +3,10 @@
 # CLONE PHASE
 git clone https://github.com/scummvm/scummvm.git source
 pushd source
-git checkout -f v2.2.0
+git checkout -f 62c4cea
 popd
-
-git clone https://github.com/FluidSynth/fluidsynth.git fluidsynth
-pushd fluidsynth
-git checkout -f 19a20eb
-popd
-
-git clone https://github.com/markjeee/libmad.git mad
-pushd mad
-git checkout -f c2f96fa
-# from http://www.linuxfromscratch.org/blfs/view/svn/multimedia/libmad.html
-patch -Np1 -i ../patches/libmad-0.15.1b-fixes-1.patch
-sed "s@AM_CONFIG_HEADER@AC_CONFIG_HEADERS@g" -i configure.ac
-touch NEWS AUTHORS ChangeLog
-autoreconf -fi
-popd
-
-readonly pfx="$PWD/local"
-mkdir -p "$pfx"
 
 # BUILD PHASE
-pushd "fluidsynth"
-mkdir -p build
-cd build
-cmake \
-    -DCMAKE_INSTALL_PREFIX="$pfx" \
-    ..
-make -j "$(nproc)" install
-popd
-
-pushd "mad"
-./configure --prefix="$pfx" --disable-static
-make -j "$(nproc)" install
-popd
-
 pushd "source"
 ./configure --prefix="$pfx"
 make -j "$(nproc)"
@@ -46,9 +14,6 @@ make install
 popd
 
 # COPY PHASE
-mkdir -p "$diststart/common/dist/lib"
 cp -rfv "$pfx/bin/" "$diststart/common/dist/"
 cp -rfv "$pfx/share" "$diststart/common/dist/"
-cp -rfv "$pfx/lib64/"*.so* "$diststart/common/dist/lib"
-cp -rfv "$pfx/lib/"*.so* "$diststart/common/dist/lib"
 cp -rfv "assets/run-scummvm.sh" "$diststart/common/dist/"
