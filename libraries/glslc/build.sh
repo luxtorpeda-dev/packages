946563528a29a7e54cddd7e24b2caf565a89cb71
d10sfan <d10sfan@gmail.com>
2021-09-22 22:27:32 -0500

    fix

--- libraries/glslc/build.sh
+++ libraries/glslc/build.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
 # CLONE PHASE
-git clone https://github.com/KhronosGroup/SPIRV-Tools.git spirv-tools
-pushd spirv-tools
-git checkout -f 1fbed83
-popd
-
 git clone https://github.com/google/shaderc.git
 pushd shaderc
 git checkout -f d0d8d7e
 git submodule update --init --recursive
+./utils/git-sync-deps
 popd
 
 # BUILD PHASE
-pushd spirv-tools
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j "$(nproc)"
-make install
-popd
-
 pushd shaderc
 mkdir build
 cd build
