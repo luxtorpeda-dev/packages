2c171dc2ad665e063e06b57ccc0ca6037a79e9da
d10sfan <d10sfan@gmail.com>
2021-09-23 13:15:28 -0500

    ?

--- engines/solarus/env.sh
+++ engines/solarus/env.sh
@@ -3,4 +3,4 @@
 export STEAM_APP_ID_LIST="1393750"
 export LICENSE_PATH="./source/license_gpl.txt"
 export ADDITIONAL_LICENSES="./libmodplug/COPYING"
-export LIBRARIES="physfs luajit"
+export LIBRARIES="glm physfs luajit"
