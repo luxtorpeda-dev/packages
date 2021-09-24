78d4b165247ccc3d4349e77d124c3c0d771fe1da
d10sfan <d10sfan@gmail.com>
2021-09-23 22:09:04 -0500

    unreal-gold - Build for steamrt

--- engines/unreal-gold/env.sh
+++ engines/unreal-gold/env.sh
@@ -2,4 +2,5 @@
 
 export STEAM_APP_ID_LIST="13250"
 export LICENSE_PATH="./p7zip_16.02/DOC/License.txt"
-export ADDITIONAL_LICENSES="./p7zip_16.02/DOC/License.txt ./p7zip_16.02/DOC/unRarLicense.txt ./sdlcl/COPYING"
+export ADDITIONAL_LICENSES="./p7zip_16.02/DOC/License.txt ./p7zip_16.02/DOC/unRarLicense.txt"
+export LIBRARIES="sdlcl32"
