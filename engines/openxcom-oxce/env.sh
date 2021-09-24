3725202b5976c4616d819af1038724a65261602a
d10sfan <d10sfan@gmail.com>
2021-09-23 21:56:59 -0500

    openxcom-oxce - Build for steamrt

--- engines/openxcom-oxce/env.sh
+++ engines/openxcom-oxce/env.sh
@@ -2,7 +2,6 @@
 
 export STEAM_APP_ID_LIST="7650 7760"
 export LICENSE_PATH="./source/LICENSE.txt"
-export ADDITIONAL_LICENSES="./sdl_gfx/LICENSE ./yaml-cpp/LICENSE"
 export COMMON_PACKAGE="1"
 export ARCHIVE_WITHOUT_V7="1"
-export GCC_9="1"
+export LIBRARIES="sdl12compat sdl12gfx sdl12mixer sdl12image yaml-cpp"
