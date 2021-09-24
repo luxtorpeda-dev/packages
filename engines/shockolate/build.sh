9add9c80b42c85aa9d42d9ca8f25352b3b6d61bd
d10sfan <d10sfan@gmail.com>
2021-09-24 14:39:39 -0500

    shockolate - Build for steamrt

--- engines/shockolate/build.sh
+++ engines/shockolate/build.sh
@@ -3,19 +3,13 @@
 # CLONE PHASE
 git clone https://github.com/Interrupt/systemshock.git source
 pushd source
-git checkout 9de43cc
+git checkout bcdb119
+rm CMakeLists.txt
+cp -rfv CMakeLists.32bit.txt CMakeLists.txt
 popd
 
-readonly pfx="$PWD/local"
-mkdir -p "$pfx"
-
 # BUILD PHASE
-
-sudo apt-get -y remove libsdl2-2.0-0 libsdl2-dev
-
 pushd source
-sudo ./osx-linux/install_32bit_sdl.sh
-
 mkdir build
 cd build
 cmake \
@@ -26,9 +20,6 @@ make -j "$(nproc)" systemshock
 popd
 
 # COPY PHASE
-mkdir -p "$diststart/410700/dist/lib"
 cp -rfv "source/build/systemshock" "$diststart/410700/dist/"
 cp -rfv "source/shaders" "$diststart/410700/dist"
 cp -rfv assets/* "$diststart/410700/dist/"
-cp -rfv "source/build_ext/built_sdl/lib/"libSDL2*.so* "$diststart/410700/dist/lib"
-cp -rfv "source/build_ext/built_sdl_mixer/lib/"libSDL2*.so* "$diststart/410700/dist/lib"
