48130ff95ad53ae4598b386452df06e3769ca2da
d10sfan <d10sfan@gmail.com>
2021-09-23 23:17:54 -0500

    unreal-gold - Fix

--- engines/unreal-gold/env.sh
+++ engines/unreal-gold/env.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 export STEAM_APP_ID_LIST="13250"
-export LICENSE_PATH="./p7zip_16.02/DOC/License.txt"
-export ADDITIONAL_LICENSES="./p7zip_16.02/DOC/License.txt ./p7zip_16.02/DOC/unRarLicense.txt"
-export LIBRARIES="yasm sdlcl32"
+export LIBRARIES="sdlcl32"
