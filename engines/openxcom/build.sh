07a8e471027f3dd6515926a4cf37eebe3c3a0471
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-23 21:43:43 -0500

    openxcom - Build for steamrt (#495)

--- engines/openxcom/build.sh
+++ engines/openxcom/build.sh
@@ -3,39 +3,10 @@
 # CLONE PHASE
 git clone https://github.com/OpenXcom/OpenXcom.git source
 pushd source
-git checkout bd342df
+git checkout -f 6ef7970
 popd
-
-git clone https://github.com/ferzkopp/SDL_gfx.git sdl_gfx
-pushd sdl_gfx
-git checkout -f 0df23ee
-popd
-
-git clone https://github.com/jbeder/yaml-cpp.git yaml-cpp
-pushd yaml-cpp
-git checkout -f 98acc5a
-popd
-
-readonly pfx="$PWD/local"
-mkdir -p "$pfx"
 
 # BUILD PHASE
-pushd "sdl_gfx"
-./configure --prefix="$pfx"
-make -j "$(nproc)"
-make install
-popd
-
-pushd yaml-cpp
-mkdir build
-cd build
-cmake \
-    -DCMAKE_INSTALL_PREFIX="$pfx" \
-    ..
-make -j "$(nproc)"
-make install
-popd
-
 pushd source
 mkdir build
 cd build
@@ -48,10 +19,6 @@ make install
 popd
 
 # COPY PHASE
-mkdir -p "$diststart/common/dist/lib"
-
-cp -rfv "$pfx/lib/"*.so* "$diststart/common/dist/lib"
 cp -rfv "$pfx/share/" "$diststart/common/dist"
 cp -rfv "$pfx/bin/openxcom" "$diststart/common/dist"
-
 cp -rfv ./assets/*.sh "$diststart/common/dist"
