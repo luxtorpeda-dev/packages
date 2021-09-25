0125d9327526946f03e7d3c8bec8d87314a4300a
d10sfan <d10sfan@gmail.com>
2021-09-24 21:42:36 -0500

    ?

--- engines/shockolate/build.sh
+++ engines/shockolate/build.sh
@@ -39,6 +39,8 @@ make -j "$(nproc)" systemshock
 popd
 
 # COPY PHASE
+mkdir -p "$diststart/410700/dist/lib"
 cp -rfv "source/build/systemshock" "$diststart/410700/dist/"
 cp -rfv "source/shaders" "$diststart/410700/dist"
+cp -rfv /usr/local/lib/libfluidsynth*.so* "$diststart/410700/dist/lib"
 cp -rfv assets/* "$diststart/410700/dist/"
--- engines/shockolate/env.sh
+++ engines/shockolate/env.sh
@@ -2,3 +2,4 @@
 
 export STEAM_APP_ID_LIST="410700"
 export LICENSE_PATH="./source/COPYING.txt"
+export ADDITIONAL_LICENSES="./fluidsynth-lite/COPYING"
