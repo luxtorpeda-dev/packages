433dc44b1cc2eaa15e989f273fdc5cbeed0cbbd7
d10sfan <d10sfan@gmail.com>
2021-09-24 22:12:57 -0500

    ?

--- engines/shockolate/env.sh
+++ engines/shockolate/env.sh
@@ -2,4 +2,4 @@
 
 export STEAM_APP_ID_LIST="410700"
 export LICENSE_PATH="./source/COPYING.txt"
-export ADDITIONAL_LICENSES="./fluidsynth-lite/COPYING"
+export ADDITIONAL_LICENSES="./build_ext/fluidsynth-lite/COPYING"
