bf973030aa41a957ca25dbdf621cfa355558293c
d10sfan <4337981+d10sfan@users.noreply.github.com>
2021-09-29 12:47:01 -0500

    avp - Fix for other languages (#515)

--- engines/avp/build.sh
+++ engines/avp/build.sh
@@ -4,6 +4,7 @@
 git clone https://github.com/neuromancer/avp.git source
 pushd source
 git checkout 2d57747
+git am < ../patches/0001-Remove-RenderSmallFontString_Wrapped-call-in-RenderH.patch
 popd
 
 # BUILD PHASE
