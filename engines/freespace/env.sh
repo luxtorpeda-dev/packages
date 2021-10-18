2a60c58ee528d03aa95d5fb2235b8ce27b3bc360
d10sfan <d10sfan@gmail.com>
2021-10-18 09:56:46 -0500

    fix

--- engines/freespace/env.sh
+++ engines/freespace/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export STEAM_APP_ID_LIST="273600"
-export LICENSE_PATH="./source/README.md ./p7zip_16.02/DOC/License.txt"
+export LICENSE_PATH="./source/README.md"
 export ADDITIONAL_LICENSES="./p7zip_16.02/DOC/License.txt ./p7zip_16.02/DOC/unRarLicense.txt"
 export LIBRARIES="yasm sdl12compat"
