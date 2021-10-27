6a814897042860023a81e5201298a5206906559d
d10sfan <d10sfan@gmail.com>
2021-10-27 16:25:39 -0500

    perimeter - Add expansion

--- engines/perimeter/env.sh
+++ engines/perimeter/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export STEAM_APP_ID_LIST="289440"
+export STEAM_APP_ID_LIST="289440 289240"
 export LICENSE_PATH="./source/LICENSE"
 export LIBRARIES="yasm boostheaders ffmpeg"
 export GCC_9="1"
