61e5b067c4fc832015ac1b9b86b5a706a4f45740
d10sfan <d10sfan@gmail.com>
2021-10-29 14:00:51 -0500

    freespace - Support new errors

--- engines/freespace/assets/run-fsport.sh
+++ engines/freespace/assets/run-fsport.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+if [ -f "last_file.txt" ]; then
+    rm last_error.txt
+fi
+
 if [[ ! -z "${DEPPATH_273620}" ]]; then
     echo "Automatic path for freespace 2 found at $DEPPATH_273620"
 
     if [ ! -f "$DEPPATH_273620/fs2_open_x64" ]; then
-        "$STEAM_ZENITY" --error --title="Error" --text="Freespace 2 not found or not launched."
-        exit 1
+        error_message="Freespace 2 not found or not launched."
+        if [[ -z "${LUX_ERRORS_SUPPORTED}" ]]; then
+            "$STEAM_ZENITY" --error --title="Error" --text="$error_message"
+        else
+            echo "$error_message" > last_error.txt
+        fi
+        exit 10
     fi
 else
-    "$STEAM_ZENITY" --error --title="Error" --text="Freespace 2 path not found."
-    exit 1
+    error_message="Freespace 2 path not found."
+    if [[ -z "${LUX_ERRORS_SUPPORTED}" ]]; then
+        "$STEAM_ZENITY" --error --title="Error" --text="$error_message"
+    else
+        echo "$error_message" > last_error.txt
+    fi
+    exit 10
 fi
 
 
