254fc6dd0d6c263e56c67c3c0069d11d7d136e14
d10sfan <d10sfan@gmail.com>
2021-10-29 15:33:32 -0500

    easyrpg-player - Fix build

--- engines/easyrpg-player/env.sh
+++ engines/easyrpg-player/env.sh
@@ -2,6 +2,6 @@
 
 export STEAM_APP_ID_LIST="default"
 export LICENSE_PATH="./source/COPYING"
-export ADDITIONAL_LICENSES="./liblcf/COPYING"
+export ADDITIONAL_LICENSES="./liblcf/COPYING ./fmt/LICENSE.rst"
 export COMMON_PACKAGE="1"
 export LIBRARIES="harfbuzz wildmidi libxmp"
