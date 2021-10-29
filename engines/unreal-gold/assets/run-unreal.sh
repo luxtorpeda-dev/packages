345d29e0b3d6a73b44307855f2c1d932bd6e65f9
d10sfan <d10sfan@gmail.com>
2021-10-29 15:04:26 -0500

    unreal-gold - Support new errors

--- engines/unreal-gold/assets/run-unreal.sh
+++ engines/unreal-gold/assets/run-unreal.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+if [ -f "last_error.txt" ]; then
+    rm last_error.txt
+fi
+
 if [ ! -f ./UnrealLinux.bin ] 
 then
-    "$STEAM_ZENITY" --error --text="Unreal Linux not found. Please see System/README.txt for instructions in game directory."
-    exit 1
+    error_message="Unreal Linux not found. Please see System/README.txt for instructions in game directory."
+    if [[ -z "${LUX_ERRORS_SUPPORTED}" ]]; then
+        "$STEAM_ZENITY" --error --title="Error" --text="$error_message"
+    else
+        echo "$error_message" > last_error.txt
+    fi
+    exit 10
 fi
 
 chmod +x ./UnrealLinux.bin
