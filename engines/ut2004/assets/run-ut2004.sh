1badf596523a1c2ebf77ccef16a3fa8dcd248375
d10sfan <d10sfan@gmail.com>
2021-10-29 16:24:21 -0500

    ut2004 - Possible fix for input issues

--- engines/ut2004/assets/run-ut2004.sh
+++ engines/ut2004/assets/run-ut2004.sh
@@ -2,5 +2,15 @@
 
 cd ../ # game tries to start in system directory, so have to get out and back to the normal directory
 
-cd linuxdata/System
-./ut2004-bin-linux-amd64
+if [[ ! -z "${DEPPATH_1070560}" ]]; then
+    runtimepath="$DEPPATH_1070560"
+    echo "Automatically detected runtimepath at $runtimepath"
+
+    cd linuxdata/System
+    "$runtimepath/scout-on-soldier-entry-point-v2" --verbose -- ./ut2004-bin-linux-amd64
+else
+    cd linuxdata/System
+    ./ut2004-bin-linux-amd64
+fi
+
+
