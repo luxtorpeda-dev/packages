de57f1e813f9e0f63b9551272ac26b24442e98aa
d10sfan <d10sfan@gmail.com>
2021-10-30 02:10:09 -0500

    ui progress

--- webui/src/app/packages/packages.component.ts
+++ webui/src/app/packages/packages.component.ts
@@ -10,6 +10,7 @@ export class PackagesComponent implements OnInit {
   constructor() { }
 
   titles: any = [];
+  titleEnginePicked: any = {};
 
   async ngOnInit() {
     const response = await fetch(`/packagesruntime.json`);
@@ -19,14 +20,52 @@ export class PackagesComponent implements OnInit {
 
   sortTitles() {
       const finalTitles: any = [];
+      let defaultRecord;
       for (const titleId in this.titles) {
-        if(this.titles[titleId].information) {
-          if(!Array.isArray(this.titles[titleId].information)) {
-            this.titles[titleId].information = [this.titles[titleId].information];
-          }
-          this.titles[titleId].titleId = titleId;
-          finalTitles.push(this.titles[titleId]);
+        this.titles[titleId].titleId = titleId;
+        if(!this.titles[titleId].engines && this.titles[titleId].information) {
+            if(!Array.isArray(this.titles[titleId].information)) {
+              this.titles[titleId].information = [this.titles[titleId].information];
+            }
+            this.titles[titleId].engines = {};
+
+            for(let informationItem of this.titles[titleId].information) {
+                this.titles[titleId].engines[informationItem.engine_name] = informationItem;
+
+                if(!this.titles[titleId].engines[informationItem.engine_name].notices) {
+                  this.titles[titleId].engines[informationItem.engine_name].notices = [];
+                }
+
+                if(informationItem.non_free) {
+                    this.titles[titleId].engines[informationItem.engine_name].notices.push({"key": "non_free", "label": "Non-free license"});
+                }
+
+                if(informationItem["32-bit"]) {
+                    this.titles[titleId].engines[informationItem.engine_name].notices.push({"key": "32-bit", "label": "32-bit libraries"});
+                }
+
+                if(informationItem.comments) {
+                    this.titles[titleId].engines[informationItem.engine_name].notices.push({"label": informationItem.comments});
+                }
+            }
         }
+
+        if(!this.titles[titleId].information) {
+          continue;
+        }
+
+        const engineKeysSorted = Object.keys(this.titles[titleId].engines);
+        engineKeysSorted.sort();
+
+        this.titleEnginePicked[titleId] = engineKeysSorted[0];
+
+        if(titleId === 'default') {
+          this.titles[titleId].isDefault = true;
+          defaultRecord = this.titles[titleId];
+          continue;
+        }
+
+        finalTitles.push(this.titles[titleId]);
      }
 
      finalTitles.sort(function (a: any, b: any) {
@@ -39,6 +78,16 @@ export class PackagesComponent implements OnInit {
       return 0;
     });
 
+    if(defaultRecord) {
+      finalTitles.unshift(defaultRecord);
+    }
+
     this.titles = finalTitles;
+
+    console.log("ASDADS", this.titles);
+  }
+
+  onEngineClicked($event: any, title: any, engineKey: any) {
+    this.titleEnginePicked[title.titleId] = engineKey;
   }
 }
