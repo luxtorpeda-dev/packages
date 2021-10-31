b13047abca2ba3cdea59f0ba83c0575cc01ccc75
d10sfan <d10sfan@gmail.com>
2021-10-30 22:14:41 -0500

    prey2006 - Possible fix, use steam runtime

--- engines/prey2006/assets/run-prey.sh
+++ engines/prey2006/assets/run-prey.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -f "last_error.txt" ]; then
+    rm last_error.txt
+fi
+
 cd linuxdata
 
 PREY_DATA_PATH="."
@@ -9,4 +13,17 @@ export ORIG_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
 LD_LIBRARY_PATH=.:${PREY_DATA_PATH}:${LD_LIBRARY_PATH}
 export LD_LIBRARY_PATH
 
-exec "./prey.x86" "$@"
+if [[ ! -z "${DEPPATH_1070560}" ]]; then
+    runtimepath="$DEPPATH_1070560"
+    echo "Automatically detected runtimepath at $runtimepath"
+else
+    error_message="Steam Linux Runtime Not Found."
+    if [[ -z "${LUX_ERRORS_SUPPORTED}" ]]; then
+        "$STEAM_ZENITY" --error --title="Error" --text="$error_message"
+    else
+        echo "$error_message" > ../last_error.txt
+    fi
+    exit 10
+fi
+
+"$runtimepath/scout-on-soldier-entry-point-v2" --verbose -- "./prey.x86" "$@"
