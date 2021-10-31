6bcd5fc2ac2028598a4cf4f22476820a17792f6f
d10sfan <d10sfan@gmail.com>
2021-10-30 21:25:10 -0500

    scummvm - Check for Original folder

--- engines/scummvm/assets/run-scummvm.sh
+++ engines/scummvm/assets/run-scummvm.sh
@@ -3,5 +3,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
-LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/scummvm --add --path=../ --recursive
+if [[ -d "../Original" ]]; then
+    echo "Assuming original path for scummvm"
+    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/scummvm --add --path=../Original --recursive
+else
+    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/scummvm --add --path=../ --recursive
+fi
+
+
 LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/scummvm --fullscreen --gfx-mode=opengl --themepath=./share/scummvm
