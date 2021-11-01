c2578bddb278216d4f695ff3dd63b6023b1c3d9c
d10sfan <d10sfan@gmail.com>
2021-10-31 21:05:14 -0500

    Revert "prey2006 - Possible fix, use steam runtime"
    This reverts commit b13047abca2ba3cdea59f0ba83c0575cc01ccc75.

--- engines/prey2006/assets/run-prey.sh
+++ engines/prey2006/assets/run-prey.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ -f "last_error.txt" ]; then
-    rm last_error.txt
-fi
-
 cd linuxdata
 
 PREY_DATA_PATH="."
@@ -13,17 +9,4 @@ export ORIG_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
 LD_LIBRARY_PATH=.:${PREY_DATA_PATH}:${LD_LIBRARY_PATH}
 export LD_LIBRARY_PATH
 
-if [[ ! -z "${DEPPATH_1070560}" ]]; then
-    runtimepath="$DEPPATH_1070560"
-    echo "Automatically detected runtimepath at $runtimepath"
-else
-    error_message="Steam Linux Runtime Not Found."
-    if [[ -z "${LUX_ERRORS_SUPPORTED}" ]]; then
-        "$STEAM_ZENITY" --error --title="Error" --text="$error_message"
-    else
-        echo "$error_message" > ../last_error.txt
-    fi
-    exit 10
-fi
-
-"$runtimepath/scout-on-soldier-entry-point-v2" --verbose -- "./prey.x86" "$@"
+exec "./prey.x86" "$@"
