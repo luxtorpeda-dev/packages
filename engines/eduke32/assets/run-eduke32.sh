c98967f7577441f1937b1a5b35f68d065659ac9c
Vortex Acherontic <vortex@z-ray.de>
2021-11-04 20:09:41 +0100

    Revert "#560 Fixed addon selection for eduke32 and raze"
    This reverts commit 0baec14a91f09f491b661562d9513b3d6a9f4de0.

--- engines/eduke32/assets/run-eduke32.sh
+++ engines/eduke32/assets/run-eduke32.sh
@@ -6,11 +6,11 @@ gamenum="$2"
 if [ -z $1 ]; then
     ./eduke32 -usecwd -nosetup
 elif [ "$gamenum" = "1" ]; then
-    ./eduke32 -j gameroot -j addons/dc -usecwd -nosetup -addon 1
+    ./eduke32 -j gameroot -j gameroot/addons/dc -usecwd -nosetup -addon 1
 elif [ "$gamenum" = "2" ]; then
-    ./eduke32 -j gameroot -j addons/nw -usecwd -nosetup -addon 2
+    ./eduke32 -j gameroot -j gameroot/addons/nw -usecwd -nosetup -addon 2
 elif [ "$gamenum" = "3" ]; then
-    ./eduke32 -j gameroot -j addons/vacation -usecwd -nosetup -addon 3
+    ./eduke32 -j gameroot -j gameroot/addons/vacation -usecwd -nosetup -addon 3
 else
     ./eduke32 -usecwd -nosetup
 fi
--- engines/raze/assets/run-raze.sh
+++ engines/raze/assets/run-raze.sh
@@ -6,11 +6,11 @@ gamenum="$2"
 if [ -z $1 ]; then
     LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze
 elif [ "$gamenum" = "1" ]; then
-    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -gamegrp addons/dc/dukedc.grp
+    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -gamegrp gameroot/addons/dc/dukedc.grp
 elif [ "$gamenum" = "2" ]; then
-    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -gamegrp addons/nw/nwinter.grp
+    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -gamegrp gameroot/addons/nw/nwinter.grp
 elif [ "$gamenum" = "3" ]; then
-    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -gamegrp addons/vacation/vacation.grp
+    LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze -gamegrp gameroot/addons/vacation/vacation.grp
 else
     LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./raze
 fi
