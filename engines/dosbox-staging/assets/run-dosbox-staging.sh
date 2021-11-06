a60561b5a20f4acfd6653c8343cff1295d642039
Joshua Fern <JoshuaFern@ProtonMail.com>
2021-11-06 13:16:00 -0700

    dosbox-staging: Enhance run script

--- engines/dosbox-staging/assets/run-dosbox-staging.sh
+++ engines/dosbox-staging/assets/run-dosbox-staging.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$DIR"
-
-new_args=(./dosbox)
+cd "$DIR" && cd ..
+shopt -s nocasematch
+cur_args=($@)
+new_args=(./dosbox-staging/dosbox)
 new_args+=("-fullscreen")
+new_args+=("-exit")
 
-for var in "$@"
+if [[ -z $cur_args && $LUX_ORIGINAL_EXE_FILE == *"bat" ]]; then
+    cur_args=$(cat "$LUX_ORIGINAL_EXE" | grep dosbox)
+fi
+
+for var in ${cur_args[@]}
 do
-    tmp_arg="${var/\\//}"
-    if [[ $tmp_arg != *"-"* ]]; then
+    tmp_arg="${var//\\//}"
+    if [[ ! -f "$tmp_arg" && $tmp_arg == *"conf" && $tmp_arg != "-"* ]]; then
         tmp_arg=$(basename $tmp_arg | xargs -I% find .. -type f -iname "%")
+    elif [[ $tmp_arg == *"bat" ]]; then
+        tmp_arg=$(basename $tmp_arg | xargs -I% find .. -type f -iname "%")
+    elif [[ $tmp_arg == "-fullscreen" || $tmp_arg == "-noconsole" || $tmp_arg == "-exit" ]]; then
+        tmp_arg=""
     fi
-    echo "$tmp_arg"
-    new_args+=("$tmp_arg")
+    if [[ -n $tmp_arg ]]; then
+        new_args+=("$tmp_arg")
+    fi
 done
 
-LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" "${new_args[@]}"
+echo "run-dosbox-staging.sh: Launching with, ${new_args[@]}"
+LD_LIBRARY_PATH="dosbox-staging/lib:$LD_LIBRARY_PATH" "${new_args[@]}"
