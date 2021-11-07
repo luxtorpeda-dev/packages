b6a3076e79b6792d5237fc94cad875c839fd3201
Joshua Fern <JoshuaFern@protonmail.com>
2021-11-06 20:29:37 -0700

    dosbox-staging - Add XCom Games (#568)
    * Add dosbox-staging to the xcoms
    
    * add appids to dosbox env

--- engines/dosbox-staging/env.sh
+++ engines/dosbox-staging/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export STEAM_APP_ID_LIST="default 410970"
+export STEAM_APP_ID_LIST="default 7650 7660 7760 410970"
 export LICENSE_PATH="./source/COPYING"
 export COMMON_PACKAGE="1"
 export ADDITIONAL_LICENSES="./source/subprojects/fluidsynth-2.2.0/LICENSE ./source/subprojects/munt-libmt32emu_2_5_0/LICENSE.build"
