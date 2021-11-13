92b919598859a6cc453f235f82a23811fd8bb7eb
d10sfan <d10sfan@gmail.com>
2021-11-12 23:32:27 -0600

    ruffle - Add game

--- engines/ruffle/assets/run-ruffle.sh
+++ engines/ruffle/assets/run-ruffle.sh
@@ -7,6 +7,10 @@ set -e
 
 if [ ! -f run.swf ]; then
     export PATH="$PATH:./jdk-11.0.12/bin/"
-    java -jar ffdec/ffdec.jar -extract $LUX_ORIGINAL_EXE_FILE -o run.swf biggest
+    if [[ $LUX_ORIGINAL_EXE_FILE == "Samorost2.exe" ]]; then
+        java -jar ffdec/ffdec.jar -extract "Samorost 2 Original (2005)/Samorost2.exe" -o run.swf biggest
+    else
+        java -jar ffdec/ffdec.jar -extract $LUX_ORIGINAL_EXE_FILE -o run.swf biggest
+    fi
 fi
 ./ruffle_desktop run.swf
