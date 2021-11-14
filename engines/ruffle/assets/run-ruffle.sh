b8d3a8dd7ee82f2023ebf06b8de370f4f46e4f0f
Kunio <12818876+ToughGuyKunio@users.noreply.github.com>
2021-11-14 11:42:13 +0100

    Update run-ruffle.sh
    The swf needs to be launched from Samarost 2 Orginal folder

--- engines/ruffle/assets/run-ruffle.sh
+++ engines/ruffle/assets/run-ruffle.sh
@@ -8,7 +8,8 @@ set -e
 if [ ! -f run.swf ]; then
     export PATH="$PATH:./jdk-11.0.12/bin/"
     if [[ $LUX_ORIGINAL_EXE_FILE == "Samorost2.exe" ]]; then
-        java -jar ffdec/ffdec.jar -extract "Samorost 2 Original (2005)/Samorost2.exe" -o run.swf biggest
+        cd "Samorost 2 Original (2005)"
+        java -jar "../ffdec/ffdec.jar" -extract "Samorost2.exe" -o run.swf biggest
     else
         java -jar ffdec/ffdec.jar -extract "$LUX_ORIGINAL_EXE_FILE" -o run.swf biggest
     fi
