1f40dd8896f7445ba232182f06b44b2f2d2b107f
Kunio <ToughGuyKunio@users.noreply.github.com>
2021-11-20 12:04:21 +0100

    updating env.sh

--- engines/blastem/env.sh
+++ engines/blastem/env.sh
@@ -2,4 +2,3 @@
 
 export STEAM_APP_ID_LIST="34270"
 export LICENSE_PATH="./source/COPYING"
-export ADDITIONAL_LICENSES="./source/LICENSE.DejaVu ./boost/LICENSE_1_0.txt ./glm/copying.txt ./data/LICENSE"
