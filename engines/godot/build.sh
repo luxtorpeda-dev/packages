c126b9d948fd150a466838f66e7e693b4c2056c7
Kunio <ToughGuyKunio@users.noreply.github.com>
2021-12-11 18:15:22 +0100

    build and env fixes

--- engines/godot/build.sh
+++ engines/godot/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt-get install build-essential scons pkg-config libx11-dev libxcursor-dev libxinerama-dev \
-    libgl1-mesa-dev libglu-dev libasound2-dev libpulse-dev libudev-dev libxi-dev libxrandr-dev yasm
-
 # CLONE PHASE
 git clone --depth 1 https://github.com/godotengine/godot.git -b 3.4-stable source
 
@@ -13,6 +9,5 @@ scons -j$(nproc) platform=x11
 popd
 
 # COPY PHASE
-mkdir -p "$dirstart/34270/dist/godot/"
-cp -rfv "source/bin/godot.x11.tools.64" "$diststart/34270/dist/"
-cp -rfv "assets/run-godot.sh" "$diststart/34270/dist/"
+cp -rfv source/bin/godot.x11.tools.64 "$diststart/common/dist/"
+cp -rfv assets/*.sh "$diststart/common/dist/"
