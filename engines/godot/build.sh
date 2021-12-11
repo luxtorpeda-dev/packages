7dda909ba612c56607ea9c23e2260bd473a1581a
Kunio <ToughGuyKunio@users.noreply.github.com>
2021-12-11 17:23:11 +0100

    modfied build.sh

--- engines/godot/build.sh
+++ engines/godot/build.sh
@@ -5,10 +5,7 @@ sudo apt-get install build-essential scons pkg-config libx11-dev libxcursor-dev 
     libgl1-mesa-dev libglu-dev libasound2-dev libpulse-dev libudev-dev libxi-dev libxrandr-dev yasm
 
 # CLONE PHASE
-git clone --depth 1 https://github.com/godotengine/godot.git source
-pushd source
-git checkout 3.4-stable
-popd
+git clone --depth 1 https://github.com/godotengine/godot.git -b 3.2.3-stable source
 
 # BUILD PHASE
 pushd source
