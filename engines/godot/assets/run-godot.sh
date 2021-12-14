a8109a2b03d5bebb67b0e7a410e4ed067b788c26
Kunio <ToughGuyKunio@users.noreply.github.com>
2021-12-14 14:57:31 +0100

    Fixes finding pck files in subfolders and also stops editor from open if it doesn't find a pck file

--- engines/godot/assets/run-godot.sh
+++ engines/godot/assets/run-godot.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "launching game in Godot"
-./godot.x11.tools.64 --fullscreen --main-pack *.pck
+# Find pck file
+pckfile=$(find . -name \*.pck)
+
+# Check if pck file exists and launch game if it does.
+if [ -n "$pckfile" ]; then
+    echo "Launching .pck file from $pckfile"
+    ./godot.x11.tools.64 --fullscreen --main-pack "$pckfile"
+else
+    echo "Couldn't find .pck file"
+fi
