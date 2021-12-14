73728dc798c404c03ccb9ed21cbc3a54b7dbcba0
Kunio <ToughGuyKunio@users.noreply.github.com>
2021-12-14 16:03:52 +0100

    changing error msg to steam zenity

--- engines/godot/assets/run-godot.sh
+++ engines/godot/assets/run-godot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Find pck file
+# Find the package file
 pckfile=$(find . -name \*.pck)
 
 # Check if pck file exists and launch game if it does.
@@ -8,5 +8,5 @@ if [ -n "$pckfile" ]; then
     echo "Launching .pck file from $pckfile"
     ./godot.x11.tools.64 --fullscreen --main-pack "$pckfile"
 else
-    echo "Couldn't find .pck file"
+    "$STEAM_ZENITY" --error --title="Error" --text="Couldn't find .pck file"
 fi
