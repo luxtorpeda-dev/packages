f1d004ff58afad16d58e1d15314f2942ad7a0233
Kunio <ToughGuyKunio@users.noreply.github.com>
2021-12-14 16:30:04 +0100

    adding egui error msg as default, zenity as fallback

--- engines/godot/assets/run-godot.sh
+++ engines/godot/assets/run-godot.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Find the package file
+if [ -f "last_error.txt" ]; then
+    rm last_error.txt
+fi
+
 pckfile=$(find . -name \*.pck)
 
 # Check if pck file exists and launch game if it does.
@@ -8,5 +11,12 @@ if [ -n "$pckfile" ]; then
     echo "Launching .pck file from $pckfile"
     ./godot.x11.tools.64 --fullscreen --main-pack "$pckfile"
 else
-    "$STEAM_ZENITY" --error --title="Error" --text="Couldn't find .pck file"
+    error_message="Couldn't find a .pck file."
+
+    if [[ -z "${LUX_ERRORS_SUPPORTED}" ]]; then
+        "$STEAM_ZENITY" --error --title="Error" --text="$error_message"
+    else
+        echo "$error_message" > last_error.txt
+    fi
+    exit 10
 fi
