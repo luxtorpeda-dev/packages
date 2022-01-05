d1ccf4ef540ff7cd5816202c3baa83c72b47d38c
d10sfan <4337981+d10sfan@users.noreply.github.com>
2022-01-04 22:23:26 -0600

    onelife (#593)

---
+++ engines/onelife/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get -y install imagemagick
+
+# CLONE PHASE
+git clone https://github.com/jasonrohrer/OneLife.git source
+pushd source
+git checkout ad2a7aa
+popd
+
+# BUILD PHASE
+cp -rfv "$pfx/include/"* /usr/include/
+
+pushd "source"
+cd build/source/OneLife_Live_UnixSource
+./pullAndBuildLatest
+popd
+
+# COPY PHASE
+cp -rfv source/build/source/OneLife_Live_UnixSource/* "$diststart/595690/dist/"
+cp -rfv "assets/"* "$diststart/595690/dist/"
