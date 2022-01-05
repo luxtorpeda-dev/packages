bc3eb1b69e3f342fba359a77e673c2d1faedfc9c
d10sfan <4337981+d10sfan@users.noreply.github.com>
2022-01-04 22:27:49 -0600

    dosbox-staging - Update to 0.78.1 (#602)

--- engines/dosbox-staging/build.sh
+++ engines/dosbox-staging/build.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-wget https://bootstrap.pypa.io/get-pip.py
-
-python3 get-pip.py.1
-pip3 install meson --upgrade
-
 # CLONE PHASE
 git clone https://github.com/dosbox-staging/dosbox-staging.git source
 pushd source
-git checkout -f 656e98e
+git checkout -f 7b744e4
 popd
 
 # BUILD PHASE
@@ -25,11 +20,5 @@ ninja -C build
 popd
 
 # COPY PHASE
-mkdir -p "$diststart/common/dist/lib"
-
-cp -rfv "source/build/subprojects/fluidsynth-2.2.0/libfluidsynth.so" "$diststart/common/dist/lib"
-cp -rfv "source/build/subprojects/munt-libmt32emu_2_5_0/libmt32emu.so" "$diststart/common/dist/lib"
-
 cp -rfv "source/build/dosbox" "$diststart/common/dist/"
-
 cp -rfv assets/*.sh "$diststart/common/dist/"
