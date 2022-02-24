bc50bd901cabedc11c4c8d3f4c3073fbd8441546
d10sfan <d10sfan@gmail.com>
2022-02-24 10:30:33 -0600

    fix

--- libraries/sdl2mixer/build.sh
+++ libraries/sdl2mixer/build.sh
@@ -10,6 +10,7 @@ popd
 pushd "sdl2_mixer"
 ./configure --disable-static --prefix="$pfx" --enable-music-midi-fluidsynth --enable-music-midi-fluidsynth-shared
 make -j "$(nproc)"
+make install
 popd
 
 cp -rfv "$pfx/lib/"* /usr/lib
