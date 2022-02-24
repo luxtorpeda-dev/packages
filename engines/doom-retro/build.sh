d6b110b93d79a03e5b2e657231a8e7fb80ad1919
d10sfan <d10sfan@gmail.com>
2022-02-24 09:20:15 -0600

    doom-retro

---
+++ engines/doom-retro/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/bradharding/doomretro.git source
+pushd source
+git checkout aec56d2
+popd
+
+# BUILD PHASE
+pushd "source"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    ..
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
+cp -rfv "source/build/doomretro" "$diststart/common/dist/"
+cp -rfv "source/build/doomretro.wad" "$diststart/common/dist/"
+cp -rfv "assets/run-doom-retro.sh" "$diststart/common/dist/"
---
+++ engines/doom-retro/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="2280 2290 2300"
+export LICENSE_PATH="./source/LICENSE"
+export COMMON_PACKAGE="1"
