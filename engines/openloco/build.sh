fabf5b58120e453478aaadd4bff8f7b3ac377618
d10sfan <d10sfan@gmail.com>
2022-03-08 08:19:10 -0600

    openloco - Update to 22.03

--- engines/openloco/build.sh
+++ engines/openloco/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/OpenLoco/OpenLoco.git source
 pushd source
-git checkout -f 3bec790
+git checkout -f 433f3bd
 popd
 
 git clone https://github.com/jbeder/yaml-cpp.git yaml-cpp
