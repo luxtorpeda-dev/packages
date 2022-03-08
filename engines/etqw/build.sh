62d004e46c6b6e26efb39ba404e3da7a16324370
d10sfan <d10sfan@gmail.com>
2022-03-08 10:44:11 -0600

    etqw - Fix

--- engines/etqw/build.sh
+++ engines/etqw/build.sh
@@ -2,5 +2,5 @@
 
 # COPY PHASE
 
-cp -rfv "$pfx/lib/libSDL-1.2.so.1.2.52" "$diststart/13250/dist/libSDL-1.2.so.0"
+cp -rfv "$pfx/lib/libSDL-1.2.so.1.2.52" "$diststart/10050/dist/libSDL-1.2.so.0"
 cp -rfv assets/* "$diststart/10050/dist/"
