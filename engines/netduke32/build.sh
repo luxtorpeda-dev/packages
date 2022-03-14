3ed4ccb7cb52bf33738312ff4bf11d2a08d575cd
d10sfan <4337981+d10sfan@users.noreply.github.com>
2022-03-14 09:37:34 -0500

    netduke32 (#632)

---
+++ engines/netduke32/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://voidpoint.io/StrikerTheHedgefox/eduke32-csrefactor.git source
+pushd source
+git checkout c4afe166
+popd
+
+# BUILD PHASE
+pushd source
+make oldmp
+popd
+
+# COPY PHASE
+cp -rfv "source/netduke32" "$diststart/common/dist/"
+cp -rfv "source/eduke32.dat" "$diststart/common/dist/"
+cp -rfv assets/* "$diststart/common/dist/"
