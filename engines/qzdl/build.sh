fa251fee38292094d2b9a51875f8de8d6910aef7
d10sfan <d10sfan@gmail.com>
2022-03-19 12:57:47 -0500

    qzdl

---
+++ engines/qzdl/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/qbasicer/qzdl.git source
+pushd source
+git checkout -f 9c76408
+git am < ../patches/0001-Compile-error-fix.patch
+popd
+
+export CXXFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas"
+export CFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas"
+
+# BUILD PHASE
+pushd "source"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    ..
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
+cp -rfv source/build/zdl "$diststart/common/dist"
+cp -rfv "assets"/* "$diststart/common/dist"
