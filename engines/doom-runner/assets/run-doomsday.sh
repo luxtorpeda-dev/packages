c6bdd6ff3a265f0a4e34352157255e942019d8ee
d10sfan <d10sfan@gmail.com>
2022-03-18 21:32:10 -0500

    doom-runner

---
+++ engines/doom-runner/assets/run-doomsday.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export LD_LIBRARY_PATH="$DIR/dlib:$DIR/lib:$LD_LIBRARY_PATH"
+QT_QPA_PLATFORM_PLUGIN_PATH="$DIR/plugins" "$DIR/bin/doomsday" -fullscreen -imusic fluidsynth -isfx openal "$@"
---
+++ engines/doom-runner/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/Youda008/DoomRunner.git source
+pushd source
+git checkout -f 9161b70
+popd
+
+export CXXFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas"
+export CFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas"
+
+# BUILD PHASE
+pushd "source"
+mkdir -p build
+cd build
+qmake ../DoomRunner.pro -spec linux-g++ "CONFIG+=release"
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
