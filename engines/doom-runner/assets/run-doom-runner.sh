6b925d95890b0e569ecae8310bd4f6d2e98daab6
d10sfan <d10sfan@gmail.com>
2022-03-18 22:53:04 -0500

    cleanup

--- engines/doom-runner/assets/run-doom-runner.sh
+++ engines/doom-runner/assets/run-doom-runner.sh
@@ -3,4 +3,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export LD_LIBRARY_PATH="$DIR/lib:$LD_LIBRARY_PATH"
-QT_QPA_PLATFORM_PLUGIN_PATH="$DIR/plugins" "$DIR/bin/doomsday" "$@"
+
+if [ ! -f ~/.config/DoomRunner/options.json ]; then
+    if [ ! -d ~/.config/DoomRunner ]; then
+        mkdir -p ~/.config/DoomRunner
+    fi
+    echo "options.json file detected, so creating"
+    cp -rfv "$DIR/options-template.json" ~/.config/DoomRunner/options.json
+fi
+
+QT_QPA_PLATFORM_PLUGIN_PATH="$DIR/plugins" "$DIR/DoomRunner" "$@"
