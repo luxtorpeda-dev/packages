5085b46dd0b57668ec286bc8c3c00994a0eedae8
d10sfan <4337981+d10sfan@users.noreply.github.com>
2022-03-19 16:19:37 -0500

    qzdl (#636)

---
+++ engines/qzdl/assets/run-qzdl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ORIGINALDIR="$PWD"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export LD_LIBRARY_PATH="$DIR/lib:$LD_LIBRARY_PATH"
+
+if [ ! -f "$DIR/qZDL.ini" ]; then
+    echo -e "[zdl.ports]\n" > "$DIR/qZDL.ini"
+    echo -e "p0n=GZDoom\n" >> "$DIR/qZDL.ini"
+    echo -e "p0f=$ORIGINALDIR/run-gzdoom.sh\n" >> "$DIR/qZDL.ini"
+fi
+
+QT_QPA_PLATFORM_PLUGIN_PATH="$DIR/plugins" "$DIR/zdl" "$@"
