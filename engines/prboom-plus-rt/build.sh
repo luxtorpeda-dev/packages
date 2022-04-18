b62a69a05c94acc651b781d11d8d5137186fb89d
d10sfan <d10sfan@gmail.com>
2022-04-18 09:09:58 -0500

    fix

--- engines/prboom-plus-rt/build.sh
+++ engines/prboom-plus-rt/build.sh
@@ -43,4 +43,5 @@ popd
 cp -rfv "source/prboom2/build/prboom-plus" "$diststart/common/dist/"
 cp -rfv "source/prboom2/build/prboom-plus.wad" "$diststart/common/dist/"
 cp -rfv "source/prboom2/build/prboom-plus-game-server" "$diststart/common/dist/"
+cp -rfv RayTracedGL1/libRayTracedGL1.so "$diststart/common/dist/"
 cp -rfv "assets/run-prboom-plus-rt.sh" "$diststart/common/dist/"
