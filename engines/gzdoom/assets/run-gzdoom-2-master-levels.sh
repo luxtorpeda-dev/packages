3fb9f0d0be7992f5d53f37ec3b4e9a5805da3e02
d10sfan <d10sfan@gmail.com>
2022-04-23 23:34:25 -0500

    gzdoom - Add controller support

--- engines/gzdoom/assets/run-gzdoom-2-master-levels.sh
+++ engines/gzdoom/assets/run-gzdoom-2-master-levels.sh
@@ -12,4 +12,11 @@ if [ ! -f doom_complete.pk3 ]; then
     cd ../
 fi
 
+if [ ! -f ~/.config/gzdoom/gzdoom.ini ]; then
+    if [ ! -d ~/.config/gzdoom ]; then
+        mkdir -p ~/.config/gzdoom
+    fi
+    cp -rfv ./gzdoom_template.ini ~/.config/gzdoom/gzdoom.ini
+fi
+
 LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./gzdoom "$@"
