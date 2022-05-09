6c12e26b1f660f2e6b41f976417e0211d2da04a2
d10sfan <4337981+d10sfan@users.noreply.github.com>
2022-05-08 23:19:47 -0500

    scummvm-latest (#662)

---
+++ engines/scummvm-latest/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/scummvm/scummvm.git source
+pushd source
+git checkout -f 1607b25
+popd
+
+# BUILD PHASE
+pushd "source"
+./configure --prefix="$pfx"
+make -j "$(nproc)"
+make install
+popd
+
+# COPY PHASE
+cp -rfv "$pfx/bin/" "$diststart/common/dist/"
+cp -rfv "$pfx/share" "$diststart/common/dist/"
+cp -rfv "assets/run-scummvm.sh" "$diststart/common/dist/"
