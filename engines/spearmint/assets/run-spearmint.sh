290bf21ba988b8cc83210abd2259fe7db037d8b8
d10sfan <4337981+d10sfan@users.noreply.github.com>
2022-05-09 14:54:39 -0500

    Spearmint (#663)

---
+++ engines/spearmint/assets/run-spearmint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd ./spearmint
+
+ln -rsf ../baseq3/*.pk3 ./spearmint-patch-data/baseq3
+ln -rsf ../missionpack/*.pk3 ./spearmint-patch-data/missionpack
+
+if [ ! -f ~/.local/share/spearmint/baseq3 ]; then
+    if [ ! -d ~/.local/share/spearmint/baseq3 ]; then
+        mkdir -p ~/.local/share/spearmint/baseq3
+    fi
+
+    cp -rfv ./config.cfg ~/.local/share/spearmint/baseq3/config.cfg
+fi
+
+if [ ! -f ~/.local/share/spearmint/missionpack ]; then
+    if [ ! -d ~/.local/share/spearmint/missionpack ]; then
+        mkdir -p ~/.local/share/spearmint/missionpack
+    fi
+
+    cp -rfv ./config.cfg ~/.local/share/spearmint/missionpack/config.cfg
+fi
+
+./spearmint/spearmint_x86_64 +set fs_cdpath "./spearmint-patch-data" +set fs_basepath "./mint-arena" "$@"
---
+++ engines/spearmint/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/zturtleman/spearmint.git source
+pushd source
+git checkout 1ce702f
+popd
+
+git clone https://github.com/zturtleman/mint-arena.git mint-arena
+pushd mint-arena
+git checkout 2620bce
+pushd
+
+git clone https://github.com/zturtleman/spearmint-patch-data.git spearmint-patch-data
+pushd spearmint-patch-data
+git checkout 3efbbca
+pushd
+
+wget https://raw.githubusercontent.com/gabomdq/SDL_GameControllerDB/master/gamecontrollerdb.txt
+
+# BUILD PHASE
+pushd "source"
+make -j "$(nproc)"
+popd
+
+pushd "mint-arena"
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
+mkdir -p "$diststart/common/dist/spearmint"
+cp -rfv source/build/release-linux-x86_64/* "$diststart/common/dist/spearmint"
+cp -rfv ./gamecontrollerdb.txt "$diststart/common/dist/spearmint"
+
+mkdir -p "$diststart/common/dist/mint-arena"
+cp -rfv mint-arena/build/release-linux-x86_64/* "$diststart/common/dist/mint-arena"
+
+mkdir -p "$diststart/common/dist/spearmint-patch-data"
+cp -rfv spearmint-patch-data/* "$diststart/common/dist/spearmint-patch-data"
+
+cp -rfv assets/* "$diststart/common/dist/"
