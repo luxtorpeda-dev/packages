2caf6012193dd4969bf4fc4ee56263d0f30e069f
d10sfan <4337981+d10sfan@users.noreply.github.com>
2022-05-08 22:10:06 -0500

    scummvm - Support iso games (#661)

--- engines/scummvm/assets/run-scummvm.sh
+++ engines/scummvm/assets/run-scummvm.sh
@@ -3,6 +3,16 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
+if ! [[ -d "../iso_data" ]]; then
+    iso_find=$(find ../ -type f -name "*.iso")
+    if ! [ -z "$iso_find" ]; then
+        echo "Found iso - $iso_find"
+
+        ./xorriso -osirrox on -indev "$iso_find" -extract / ../iso_data
+        LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/scummvm -c scummvm.ini --add --path=../iso_data --recursive
+    fi
+fi
+
 if [[ -d "../Original" ]]; then
     echo "Assuming original path for scummvm"
     LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/scummvm -c scummvm.ini --add --path=../Original --recursive
