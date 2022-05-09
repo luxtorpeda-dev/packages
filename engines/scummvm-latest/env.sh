c4a9216b55ff1654e801f51fcf134ecfda5fa6a1
d10sfan <d10sfan@gmail.com>
2022-05-08 22:37:39 -0500

    fixg

--- engines/scummvm-latest/env.sh
+++ engines/scummvm-latest/env.sh
@@ -2,6 +2,6 @@
 
 export STEAM_APP_ID_LIST="1367160 default"
 export LICENSE_PATH="./source/COPYING"
-export ADDITIONAL_LICENSES="./source/COPYING.BSD ./source/COPYING.FREEFONT ./source/COPYING.LGPL ./source/COPYING.OFL ./source/COPYRIGHT"
+export ADDITIONAL_LICENSES="./source/COPYING ./source/COPYRIGHT"
 export COMMON_PACKAGE="1"
 export LIBRARIES="fluidsynth mad xorriso"
