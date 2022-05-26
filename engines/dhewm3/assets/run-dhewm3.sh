46698206cffc55dae1ac4d9e6f02c0de8c977ecd
d10sfan <d10sfan@gmail.com>
2022-05-25 21:42:42 -0500

    dhewm3 - Add defaults for steam deck

---
+++ engines/dhewm3/assets/run-dhewm3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if ! [[ -z "${LUX_STEAM_DECK}" ]]; then
+    if [ ! -f ~/.config/dhewm3/base/dhewm.cfg ]; then
+        if [ ! -d ~/.config/dhewm3/base ]; then
+            mkdir -p ~/.config/dhewm3/base
+        fi
+
+        echo -e "seta r_fullscreen \"1\"" >> ~/.config/dhewm3/base/dhewm.cfg
+        echo -e "seta r_mode \"-1\"" >> ~/.config/dhewm3/base/dhewm.cfg
+        echo -e "seta r_customHeight \"1080\"" >> ~/.config/dhewm3/base/dhewm.cfg
+        echo -e "seta r_customWidth \"2560\"" >> ~/.config/dhewm3/base/dhewm.cfg
+    fi
+
+    if [ ! -f ~/.config/dhewm3/d3xp/dhewm.cfg ]; then
+        if [ ! -d ~/.config/dhewm3/d3xp ]; then
+            mkdir -p ~/.config/dhewm3/d3xp
+        fi
+
+        echo -e "seta r_fullscreen \"1\"" >> ~/.config/dhewm3/d3xp/dhewm.cfg
+        echo -e "seta r_mode \"-1\"" >> ~/.config/dhewm3/d3xp/dhewm.cfg
+        echo -e "seta r_customHeight \"1080\"" >> ~/.config/dhewm3/d3xp/dhewm.cfg
+        echo -e "seta r_customWidth \"2560\"" >> ~/.config/dhewm3/d3xp/dhewm.cfg
+    fi
+fi
+
+./dhewm3 "$@"
