113f5e3bf8040173167250b2d52c5f77c47ed7a2
d10sfan <d10sfan@gmail.com>
2022-05-25 22:16:05 -0500

    openjk - Fix

--- engines/openjk/assets/run-openjo.sh
+++ engines/openjk/assets/run-openjo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if ! [[ -z "${LUX_STEAM_DECK}" ]]; then
-    if [ ! -f ~/.local/share/openjk/base/openjk_sp.cfg ]; then
-        if [ ! -d ~/.local/share/openjk/base ]; then
-            mkdir -p ~/.local/share/openjk/base
+    if [ ! -f ~/.local/share/openjo/base/openjo_sp.cfg ]; then
+        if [ ! -d ~/.local/share/openjo/base ]; then
+            mkdir -p ~/.local/share/openjo/base
         fi
 
-        echo -e "seta r_fullscreen \"1\"" >> ~/.local/share/openjk/base/openjk_sp.cfg
-        echo -e "seta r_mode \"-1\"" >> ~/.local/share/openjk/base/openjk_sp.cfg
-        echo -e "seta r_customHeight \"800\"" >> ~/.local/share/openjk/base/openjk_sp.cfg
-        echo -e "seta r_customWidth \"1280\"" >> ~/.local/share/openjk/base/openjk_sp.cfg
+        echo -e "seta r_fullscreen \"1\"" >> ~/.local/share/openjo/base/openjo_sp.cfg
+        echo -e "seta r_mode \"-1\"" >> ~/.local/share/openjo/base/openjo_sp.cfg
+        echo -e "seta r_customHeight \"800\"" >> ~/.local/share/openjo/base/openjo_sp.cfg
+        echo -e "seta r_customWidth \"1280\"" >> ~/.local/share/openjo/base/openjo_sp.cfg
     fi
 fi
 
