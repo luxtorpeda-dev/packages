991b7cc25ae0ee4d9ff3b09db0785663ec76f0aa
d10sfan <d10sfan@gmail.com>
2022-05-27 23:40:18 -0500

    yaml

--- engines/openmw-latest/env.sh
+++ engines/openmw-latest/env.sh
@@ -3,5 +3,5 @@
 export STEAM_APP_ID_LIST="22320"
 export LICENSE_PATH="./source/LICENSE"
 export ADDITIONAL_LICENSES="./openmw-shaders/README.md"
-export LIBRARIES="yasm unshield bullet3 mygui openmwosg boost liblz4 ffmpeg qt5"
+export LIBRARIES="yasm unshield bullet3 mygui openmwosg boost liblz4 ffmpeg qt5 yaml-cpp"
 export ARCHIVE_WITHOUT_V7="1"
