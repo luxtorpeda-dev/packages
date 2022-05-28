09084938ed64755d1a9b7c24cf05be183e7bcab3
d10sfan <d10sfan@gmail.com>
2022-05-27 22:48:17 -0500

    classic-rbdoom-3-bfg - Update to latest

--- engines/classic-rbdoom-3-bfg/build.sh
+++ engines/classic-rbdoom-3-bfg/build.sh
@@ -3,24 +3,10 @@
 # CLONE PHASE
 git clone https://github.com/MadDeCoDeR/Classic-RBDOOM-3-BFG.git source
 pushd source
-git checkout cf9c2fa
-popd
-
-git clone https://github.com/kcat/openal-soft.git openal
-pushd openal
-git checkout ae4eacf
+git checkout d33be88
 popd
 
 # BUILD PHASE
-pushd "openal"
-cd build
-cmake \
-    -DCMAKE_BUILD_TYPE=MinSizeRel \
-    ..
-make -j "$(nproc)"
-make install
-popd
-
 pushd "source"
 mkdir build
 cd build
@@ -38,5 +24,3 @@ popd
 cp "source/build/DoomBFA" "$diststart/208200/dist/DoomBFA"
 cp -rfv ./assets/* "$diststart/208200/dist/"
 cp -rfv ./source/base "$diststart/208200/dist/updatedbase"
-cp -rfv "/usr/local/lib/libopenal.so.1.21.1" "$diststart/208200/dist/lib/libopenal.so"
-cp -rfv "/usr/local/lib/libopenal.so.1.21.1" "$diststart/208200/dist/lib/libopenal.so.1"
