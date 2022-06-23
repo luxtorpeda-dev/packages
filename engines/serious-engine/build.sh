f8535ac27c9a02dcf953503b5f259e48625dedad
d10sfan <d10sfan@gmail.com>
2022-06-23 09:53:52 -0500

    fix

--- engines/serious-engine/build.sh
+++ engines/serious-engine/build.sh
@@ -6,18 +6,13 @@ pushd source
 git checkout 3620b73
 popd
 
-# PRE-COPY PHASE
-mkdir -p "$diststart/41050/dist/Bin/"
-mkdir -p "$diststart/41060/dist/Bin/"
-
 # BUILD PHASE
 pushd source/SamTSE/Sources
 ./build-linux64.sh
 popd
 
 # COPY PHASE
-cp -rfv source/SamTSE/Sources/cmake-build/ssam "$diststart/41060/dist/Bin/ssam"
-cp -rfv source/SamTSE/Sources/cmake-build/RelWithDebInfo/* "$diststart/41060/dist/Bin"
+cp -rfv source/SamTSE/Sources/Bin "$diststart/41060/dist/"
 
 # BUILD PHASE
 pushd source/SamTFE/Sources
@@ -25,5 +20,4 @@ pushd source/SamTFE/Sources
 popd
 
 # COPY PHASE
-cp -rfv source/SamTFE/Sources/cmake-build/ssam-tfe "$diststart/41050/dist/Bin/ssam-tfe"
-cp -rfv source/SamTFE/Sources/cmake-build/RelWithDebInfo/* "$diststart/41050/dist/Bin"
+cp -rfv source/SamTFE/Sources/Bin "$diststart/41050/dist/"
