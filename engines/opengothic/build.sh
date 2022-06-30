8d84728a948cd86e4a493337f4dafe5d463fe383
d10sfan <4337981+d10sfan@users.noreply.github.com>
2022-06-29 21:02:38 -0500

    OpenGothic - Update to v1.0.1527 (#697)

--- engines/opengothic/build.sh
+++ engines/opengothic/build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+wget https://sdk.lunarg.com/sdk/download/1.3.216.0/linux/vulkansdk-linux-x86_64-1.3.216.0.tar.gz
+tar xvf vulkansdk-linux-x86_64-1.3.216.0.tar.gz
+source 1.3.216.0/setup-env.sh
+
+sudo cp -r $VULKAN_SDK/include/vulkan/ /usr/local/include/
+sudo cp -P $VULKAN_SDK/lib/libvulkan.so* /usr/local/lib/
+sudo cp $VULKAN_SDK/lib/libVkLayer_*.so /usr/local/lib/
+sudo mkdir -p /usr/local/share/vulkan/explicit_layer.d
+sudo cp $VULKAN_SDK/etc/vulkan/explicit_layer.d/VkLayer_*.json /usr/local/share/vulkan/explicit_layer.d
+sudo ldconfig
+
 # CLONE PHASE
 git clone https://github.com/Try/OpenGothic source
 pushd source
-git checkout -f c9c8079
+git checkout -f 2de4fd8
 git submodule update --init --recursive
 popd
 
