1e4491a83a2d3ae707e34a97fd0518793afeffb1
d10sfan <d10sfan@gmail.com>
2022-07-04 21:21:31 -0500

    Add HomeworldSDL

---
+++ engines/homeworldsdl/assets/run-homeworldsdl.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+mkdir -p ./dataconv/
+
+lowercase () {
+        echo "$@" | tr '[:upper:]' '[:lower:]'
+}
+
+create_relative_symlink () {
+        local -r target=$1
+        local -r symlink="dataconv/$(lowercase "$target")"
+        mkdir -p "$(dirname "$symlink")"
+        ln -rsf "$target" "$symlink"
+}
+
+create_relative_symlink_fmvs () {
+        local -r target="$(basename "$1")"
+        local -r baselower="$(lowercase "$target")"
+        if [ "$target" != "$baselower" ]; then
+			local -r symlink="FMVs/$(lowercase "$target")"
+			ln -rsf "$1" "$symlink"
+        fi
+}
+
+if [ ! -f ready ]; then
+	find {avp_huds,avp_rifs,fastfile,Text} -type f  | while read -r file_name ; do
+        	create_relative_symlink "$file_name"
+	done
+
+	ln -rsf default.cfg dataconv/default.cfg
+	ln -rsf language.txt dataconv/language.txt
+	ln -rsf "CD Tracks.txt" dataconv/"cd tracks.txt"
+
+	touch ready
+fi
+
+if [ ! -f fmv_ready ]; then
+	find FMVs -type f  | while read -r file_name ; do
+		create_relative_symlink "$file_name"
+	done
+
+	find FMVs -type f  | while read -r file_name ; do
+		create_relative_symlink_fmvs "$file_name"
+	done
+
+	touch fmv_ready
+fi
+
+AVP_DATA=dataconv LD_LIBRARY_PATH=./lib ./avp --fullscreen 
---
+++ engines/homeworldsdl/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/HomeworldSDL/HomeworldSDL.git source
+pushd source
+git checkout 36ff7fb
+git am < ../patches/0001-Fix-compile-error.patch
+popd
+
+# BUILD PHASE
+pushd "source"
+cd Linux
+./bootstrap
+../configure
+make
+popd
+
+pushd "source"
+cd tools/biggie
+./biggie-Linux-compile.sh
+cd ../../HomeworldSDL_big
+./convert_directory_to_big_file
+popd
+
+# COPY PHASE
+cp -rfv "source/Linux/src/homeworld" "$diststart/244160/dist/"
+cp -rfv "source/HomeworldSDL.big" "$diststart/244160/dist/"
+cp -rfv assets/* "$diststart/244160/dist/"
