3c8574e8c4fece63f63a1ae0e6bad1073eb5b116
d10sfan <d10sfan@gmail.com>
2022-07-07 21:52:45 -0500

    Remove buildgdx

--- engines/buildgdx/assets/run-buildgdx-witchaven2-silent.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gamepath="$PWD/GAME/WHAVEN2"
-export PATH="$PATH:./jdk-11.0.12/bin/:./"
-java -DLWJGL_DISABLE_XRANDR=true -jar BuildGDX.jar -path "$gamepath" -silent
--- engines/buildgdx/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export STEAM_APP_ID_LIST="1655410 1655430 299030 329650 1010750 1260020 434050"
-export LICENSE_PATH="./source/apache.txt"
-export ADDITIONAL_LICENSES="./source/buildlic.txt ./source/gpl3.txt"
-export LIBRARIES="openjdk11"
-export COMMON_PACKAGE="1"
