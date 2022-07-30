279b9440ea19c3ca265c8f8a99c61ae637ec45e0
d10sfan <d10sfan@gmail.com>
2022-07-30 11:13:24 -0500

    rvgl - Steam deck controls

--- engines/rvgl/assets/run-rvgl.sh
+++ engines/rvgl/assets/run-rvgl.sh
@@ -28,18 +28,32 @@ if [ ! -f ./rvgl ]; then
 fi
 
 if ! [[ -z "${LUX_STEAM_DECK}" ]]; then
-    if [ ! -f profiles/player/profile.ini ]; then
+    if [ ! -f profiles/deck/profile.ini ]; then
         echo "Creating profile"
-        if [ ! -d profiles/player ]; then
-            mkdir -p profiles/player
+        if [ ! -d profiles/deck ]; then
+            mkdir -p profiles/deck
         fi
 
         echo -e "[Misc]" >> profiles/rvgl.ini
         echo -e "UseProfiles = 1" >> profiles/rvgl.ini
-        echo -e "DefaultProfile = \"player\"" >> profiles/rvgl.ini
-
-        echo -e "[Controller1]" >> profiles/player/profile.ini
-        echo -e "Joystick = 0" >> profiles/player/profile.ini
+        echo -e "DefaultProfile = \"deck\"" >> profiles/rvgl.ini
+        echo -e "[Controller1]" >> profiles/deck/profile.ini
+        echo -e "Joystick = 0" >> profiles/deck/profile.ini
+        echo -e "NonLinearSteering = 1" >> profiles/deck/profile.ini
+        echo -e "SteeringDeadzone = 10" >> profiles/deck/profile.ini
+        echo -e "SteeringRange = 90" >> profiles/deck/profile.ini
+        echo -e "ButtonOpacity = 50" >> profiles/deck/profile.ini
+        echo -e "KeyLeft = 0x02000000" >> profiles/deck/profile.ini
+        echo -e "KeyRight = 0x03000000" >> profiles/deck/profile.ini
+        echo -e "KeyFwd = 0x02000001" >> profiles/deck/profile.ini
+        echo -e "KeyBack = 0x03000001" >> profiles/deck/profile.ini
+        echo -e "KeyFire = 0x01000000" >> profiles/deck/profile.ini
+        echo -e "KeyReset = 0x01000001" >> profiles/deck/profile.ini
+        echo -e "KeyReposition = 0x01000002" >> profiles/deck/profile.ini
+        echo -e "KeyHonka = 0x01000005" >> profiles/deck/profile.ini
+        echo -e "KeyChangeCamera = 0x0000003a" >> profiles/deck/profile.ini
+        echo -e "KeyRearView = 0x01000004" >> profiles/deck/profile.ini
+        echo -e "KeyPause = 0x01000003" >> profiles/deck/profile.ini
     fi
 fi
 
