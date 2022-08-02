fca01bc143a1bfa5eda2a65379e685ac756b6c77
d10sfan <4337981+d10sfan@users.noreply.github.com>
2022-08-01 21:20:26 -0500

    scummvm - Update to 2.6.0 (#709)

--- engines/scummvm-latest/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export STEAM_APP_ID_LIST="255940 284050 1044340 1367160 default"
-export LICENSE_PATH="./source/COPYING"
-export ADDITIONAL_LICENSES="./source/COPYING ./source/COPYRIGHT"
-export COMMON_PACKAGE="1"
-export LIBRARIES="fluidsynth mad xorriso"
