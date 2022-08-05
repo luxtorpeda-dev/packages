d73d3702eddf1a8550ccbf3e72f377d682516efb
d10sfan <4337981+d10sfan@users.noreply.github.com>
2022-08-05 08:50:16 -0500

    dxx-rebirth - Update to d1ed53e (#712)
    * dxx-rebirth - Update to d1ed53e
    
    * fix

--- engines/dxx-rebirth/build.sh
+++ engines/dxx-rebirth/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/dxx-rebirth/dxx-rebirth.git source
 pushd source
-git checkout -f 49dee72
+git checkout -f d1ed53e
 popd
 
 # BUILD PHASE
@@ -12,7 +12,7 @@ scons sdl2=1
 popd
 
 # COPY PHASE
-cp -rfv source/d1x-rebirth/d1x-rebirth "$diststart/273570/dist/"
-cp -rfv source/d2x-rebirth/d2x-rebirth "$diststart/273580/dist/"
+cp -rfv source/build/d1x-rebirth/d1x-rebirth "$diststart/273570/dist/"
+cp -rfv source/build/d2x-rebirth/d2x-rebirth "$diststart/273580/dist/"
 cp -rfv assets/run-d1x.sh "$diststart/273570/dist/"
 cp -rfv assets/run-d2x.sh "$diststart/273580/dist/"
