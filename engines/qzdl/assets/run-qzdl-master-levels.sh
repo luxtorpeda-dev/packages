480aecd8980a0d6927594e7b5a107ac463e38a3a
d10sfan <d10sfan@gmail.com>
2022-08-10 21:09:52 -0500

    qzdl - Changes to handle Doom II updates

--- engines/qzdl/assets/run-qzdl-master-levels.sh
+++ engines/qzdl/assets/run-qzdl-master-levels.sh
@@ -4,8 +4,8 @@ if [ ! -f doom_complete.pk3 ]; then
     cd wadsmoosh-branch-default
     cp -rfv ../wadsmoosh.py ./
 
-    cp -rfv ../master/wads/*.WAD "source_wads/"
-    cp -rfv ../doom2/*.WAD "source_wads/"
+    cp -rfv ../masterbase/master/wads/*.WAD "source_wads/"
+    cp -rfv ../base/*.WAD "source_wads/"
     python3.7 wadsmoosh.py
     cp -rfv ./doom_complete.pk3 ../
 
