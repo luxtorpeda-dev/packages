5984f862900ced460873cecacb00e63b6a0b79ef
d10sfan <d10sfan@gmail.com>
2022-08-10 21:11:03 -0500

    misc - env fixes to handle Doom II updates

--- engines/chocolate-doom/env.sh
+++ engines/chocolate-doom/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export STEAM_APP_ID_LIST="2280 2290 2300 2360 2390"
+export STEAM_APP_ID_LIST="2280 2300 2360 2390"
 export LICENSE_PATH="./source/COPYING.md"
 export COMMON_PACKAGE="1"
 export LIBRARIES="fluidsynth"
--- engines/doomsday/env.sh
+++ engines/doomsday/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export STEAM_APP_ID_LIST="2280 2290 2300 2360 2390 2370"
+export STEAM_APP_ID_LIST="2280 2300 2360 2390 2370"
 export LICENSE_PATH="./source/doomsday/gpl-3.0.txt"
 export ADDITIONAL_LICENSES="./source/doomsday/lgpl-3.0.txt"
 export COMMON_PACKAGE="1"
