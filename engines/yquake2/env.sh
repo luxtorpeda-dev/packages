5ce5631b940b946cf9cae7094b7519ad8c3bb20b
d10sfan <d10sfan@gmail.com>
2022-08-11 15:20:55 -0500

    metadata - Support for Quake II Update

--- engines/yquake2/env.sh
+++ engines/yquake2/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export STEAM_APP_ID_LIST="2320 2330 2340 9130"
+export STEAM_APP_ID_LIST="2320 9130"
 export LICENSE_PATH="./source/LICENSE"
 export ADDITIONAL_LICENSES="./LICENSE ./source-ctf/LICENSE ./source-xatrix/LICENSE ./source-rogue/LICENSE ./sdl_gamecontrollerdb/LICENSE"
 export COMMON_PACKAGE="1"
