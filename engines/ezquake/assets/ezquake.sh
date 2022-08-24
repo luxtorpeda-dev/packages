afbd3940ac91be80805d0639ef09585bc3410fde
d10sfan <d10sfan@gmail.com>
2022-08-23 21:50:31 -0500

    progress

--- engines/ezquake/assets/ezquake.sh
+++ engines/ezquake/assets/ezquake.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ ! -f ezquake/share/quake/id1/config.cfg ] ; then
-	cp -f ezquake/share/quake/default.lux.cfg ezquake/share/quake/id1/config.cfg
-	sed -i "s|%USER%|$USER|" ezquake/share/quake/id1/config.cfg
-fi
-
 if [ ! -d ezquake/share/quake/id1/music ] ; then
     LD_PRELOAD="" ln -rsf ./rerelease/id1/music ./ezquake/share/quake/id1/music
 fi
@@ -18,32 +13,15 @@ if [ ! -e ezquake/share/quake/id1/pak0.pak ] ; then
     fi
 fi
 
-if [ ! -f ezquake/share/quake/rogue/config.cfg ] ; then
-	cp -f ezquake/share/quake/default.lux.cfg ezquake/share/quake/rogue/config.cfg
-	sed -i "s|%USER%|$USER|" ezquake/share/quake/rogue/config.cfg
-fi
-
 if [ ! -d ezquake/share/quake/rogue/music ] ; then
     LD_PRELOAD="" ln -rsf ./rerelease/rogue/music ./ezquake/share/quake/rogue/music
-fi
-
-if [ ! -f ezquake/share/quake/hipnotic/config.cfg ] ; then
-	cp -f ezquake/share/quake/default.lux.cfg ezquake/share/quake/hipnotic/config.cfg
-	sed -i "s|%USER%|$USER|" ezquake/share/quake/hipnotic/config.cfg
 fi
 
 if [ ! -d ezquake/share/quake/hipnotic/music ] ; then
     LD_PRELOAD="" ln -rsf ./rerelease/hipnotic/music ./ezquake/share/quake/hipnotic/music
 fi
 
-if [ ! -f ezquake/share/quake/dopa/config.cfg ] ; then
-	cp -f ezquake/share/quake/default.lux.cfg ezquake/share/quake/dopa/config.cfg
-	sed -i "s|%USER%|$USER|" ezquake/share/quake/dopa/config.cfg
-fi
-
-if [ ! -f ezquake/share/quake/mg1/config.cfg ] ; then
-	cp -f ezquake/share/quake/default.lux.cfg ezquake/share/quake/mg1/config.cfg
-	sed -i "s|%USER%|$USER|" ezquake/share/quake/mg1/config.cfg
-fi
+mkdir -p ./ezquake/share/quake/ezquake/share/
+ln -rsf ./ezquake/share/quake/ ./ezquake/share/quake/ezquake/share
 
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./ezquake/lib:./lib" ./ezquake/ezquake-linux-x86_64 -basedir ezquake/share/quake "$@"
