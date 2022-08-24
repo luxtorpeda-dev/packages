e6ae7745e011c6b33f59fb60f50e3a27bdbabc6f
d10sfan <d10sfan@gmail.com>
2022-08-23 20:35:41 -0500

    runescape - Update to 2.2.10

--- engines/runescape/assets/run-runescape.sh
+++ engines/runescape/assets/run-runescape.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [ ! -d ./usr ]; then
-    LD_LIBRARY_PATH=.7z ./7z/7z x ./runescape-launcher_2.2.9_amd64.deb
+if [ ! -f rl2210 ]; then
+    LD_LIBRARY_PATH=.7z ./7z/7z x ./runescape-launcher_2.2.10_amd64.deb
     tar xvf data.tar
+    touch rl2210
 fi
 
 ./usr/share/games/runescape-launcher/runescape --configURI http://www.runescape.com/k=5/l=\$\(Language:0\)/jav_config.ws
