ff0c232ee601033b6efdfd95a9b6b41f6cb92678
d10sfan <d10sfan@gmail.com>
2022-08-24 22:41:45 -0500

    fix

--- engines/ezquake/build.sh
+++ engines/ezquake/build.sh
@@ -20,7 +20,6 @@ mkdir -p "$diststart/common/dist/ezquake/share/quake/mg1"
 
 cp -v source/ezquake-linux-x86_64 "$diststart/common/dist/ezquake/"
 cp -v assets/ezquake.sh "$diststart/common/dist/ezquake/"
-cp -v assets/default.lux.cfg "$diststart/common/dist/ezquake/share/quake"
 
 ln -s "../../../../id1/PAK0.PAK" "$diststart/common/dist/ezquake/share/quake/id1/pak0.pak"
 ln -s "../../../../id1/PAK1.PAK" "$diststart/common/dist/ezquake/share/quake/id1/pak1.pak"
