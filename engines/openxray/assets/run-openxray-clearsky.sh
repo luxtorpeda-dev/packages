b64bf7d2c04db489b1ad4895ef5716ccc102350a
d10sfan <d10sfan@gmail.com>
2022-08-29 20:37:02 -0500

    openxray - Add LD_AUDIT reset

--- engines/openxray/assets/run-openxray-clearsky.sh
+++ engines/openxray/assets/run-openxray-clearsky.sh
@@ -8,4 +8,4 @@ cp -r ./gamedata-mods/* ./gamedata
 
 ln -rsf /lib/x86_64-linux-gnu/libncurses.so.5.9 ./lib/libncurses.so.6
 
-LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./xr_3da -fsltx fsgame.ltx "$@"
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" LD_AUDIT="" ./xr_3da -fsltx fsgame.ltx "$@"
--- engines/openxray/assets/run-openxray.sh
+++ engines/openxray/assets/run-openxray.sh
@@ -2,4 +2,4 @@
 
 ln -rsf /lib/x86_64-linux-gnu/libncurses.so.5.9 ./lib/libncurses.so.6
 
-LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./xr_3da -fsltx fsgame.ltx "$@"
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" LD_AUDIT="" ./xr_3da -fsltx fsgame.ltx "$@"
