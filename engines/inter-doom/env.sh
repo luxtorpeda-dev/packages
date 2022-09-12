dd1c9d1c36eecca080bba3ac6d8845f4f37ec7e1
d10sfan <d10sfan@gmail.com>
2022-09-11 21:23:34 -0500

    ?

--- engines/inter-doom/env.sh
+++ engines/inter-doom/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export STEAM_APP_ID_LIST="2390 2360 2370"
-export LICENSE_PATH="./source/COPYING.md"
+export LICENSE_PATH="./source/LICENSE.txt"
 export COMMON_PACKAGE="1"
 export LIBRARIES="fluidsynth"
