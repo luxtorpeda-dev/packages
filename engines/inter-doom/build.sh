e024494ef2b1f3d613b435798d60f05a0d875e72
d10sfan <4337981+d10sfan@users.noreply.github.com>
2022-09-11 21:49:50 -0500

    Added International Doom (#743)

---
+++ engines/inter-doom/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/JNechaevsky/inter-doom.git source
+pushd source
+git checkout 092cc7a
+popd
+
+# BUILD PHASE
+pushd "source"
+mkdir build
+cd build
+cmake \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DCOMPILE_DOOM=OFF \
+    -DBUILD_PORTABLE=ON \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+# COPY PHASE
+mkdir -p "$diststart/common/dist/base"
+cp -rfv source/build/src/inter-* "$diststart/common/dist/"
+cp -rfv "$pfx/base/"* "$diststart/common/dist/base/"
+cp -rfv assets/* "$diststart/common/dist/"
