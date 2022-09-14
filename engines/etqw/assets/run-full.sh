7c51816ff5e30dbd7c8180df8de0a1bab3ca2fb4
John-Michael Mulesa <thesaxophonist@gmail.com>
2022-09-14 23:47:44 +1000

    etqw - Add auto download/install support for full game (#745)
    * Add auto download/install/uninstall support for ETQW.
    
    * Fix relative path.
    
    * Fix SDL library name.
    
    * Fix url path.
    
    * Use built-in zip override.
    
    * Better cleanup of setup files.
    
    * Chmod the right files.
    
    Co-authored-by: GitHub Action <actions@github.com>

--- engines/etqw/assets/run-full.sh
+++ engines/etqw/assets/run-full.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-if [ ! -f etqw-rthread ]; then
-    error_message="Linux version not in correct location."
-    if [[ -z "${LUX_ERRORS_SUPPORTED}" ]]; then
-        "$STEAM_ZENITY" --error --title="Error" --text="$error_message"
-    else
-        echo "$error_message" > last_error.txt
-    fi
-    exit 10
-fi
+cp -rfv ./libSDL-1.2.so.0 ./linuxdata/libSDL-1.2.id.so.0
 
-cp -rfv ./libSDL-1.2.so.0 ./libSDL-1.2.id.so.0
+cd linuxdata
 ./etqw-rthread
---
+++ engines/etqw/assets/setup-full.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+create_relative_symlink () {
+        local -r target=$1
+        local -r symlink="linuxdata/$target"
+        mkdir -p "$(dirname "$symlink")"
+        ln -rsf "$target" "$symlink"
+}
+
+mkdir -p ./linuxdata
+
+mv ./etqwsetup/data/* ./linuxdata/
+
+rm -rf ./etqwsetup
+rm ./linuxdata/libgcc_s.so.1
+rm ./linuxdata/libstdc++.so.6
+
+find base -type f  | while read -r file_name ; do
+    create_relative_symlink "$file_name"
+done
+    
+chmod +x ./linuxdata/{etqw-dedicated,etqwded.x86,etqw,etqw.x86,etqw-rthread,etqw-rthread.x86,*.so,*.so.*,pb/*.so}
+
+chmod 755 ./linuxdata/base/bots
