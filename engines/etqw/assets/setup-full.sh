2f662ba166d58984c6e0d69bdcb027e441bd3277
GitHub Action <actions@github.com>
2022-09-14 15:00:47 +1000

    Use built-in zip override.

--- engines/etqw/assets/setup-full.sh
+++ engines/etqw/assets/setup-full.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 create_relative_symlink () {
         local -r target=$1
@@ -7,18 +7,13 @@ create_relative_symlink () {
         ln -rsf "$target" "$symlink"
 }
 
-mkdir -p ./linuxdata
-
-sh -c "unzip -o ./ETQW-client-1.5-full.x86.run -d ./ linuxdata/*; true"
-    
 find base -type f  | while read -r file_name ; do
     create_relative_symlink "$file_name"
 done
     
 rm ./linuxdata/libgcc_s.so.1
 rm ./linuxdata/libstdc++.so.6
+
 chmod +x ./linuxdata/{etqw,etqw-dedicated,etqw.x86,*.so,*.so.*,pb/*.so}
 
 chmod 755 ./linuxdata/base/bots
-    
-rm ETQW-client-1.5-full.x86.run
