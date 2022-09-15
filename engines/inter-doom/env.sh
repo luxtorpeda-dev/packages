e2e00cedbb146af55540fc489a479b1f0ba465d2
d10sfan <d10sfan@gmail.com>
2022-09-15 15:28:28 -0500

    inter-doom - Add support for doom

--- engines/inter-doom/env.sh
+++ engines/inter-doom/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export STEAM_APP_ID_LIST="2390 2360 2370"
+export STEAM_APP_ID_LIST="2390 2360 2370 2300 2280 2290"
 export LICENSE_PATH="./source/LICENSE.txt"
 export COMMON_PACKAGE="1"
 export LIBRARIES="fluidsynth sdl2mixer"
