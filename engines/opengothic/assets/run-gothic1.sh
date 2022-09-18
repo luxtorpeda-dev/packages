9d536e918485d517dc39847bfd4c2cf7eca0f9ae
d10sfan <d10sfan@gmail.com>
2022-09-17 22:21:39 -0500

    OpenGothic - Fix 1 launch

--- engines/opengothic/assets/run-gothic1.sh
+++ engines/opengothic/assets/run-gothic1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd ..
+
 if [ ! -f Gothic.ini ]; then
     cp system/GOTHIC.INI Gothic.ini
 fi
 
-LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/Gothic2Notr -g1 -g . -nofrate
+LD_LIBRARY_PATH="system/lib:$LD_LIBRARY_PATH" ./system/bin/Gothic2Notr -g1 -g . -nofrate
