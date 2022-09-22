80ca1a5a5e0529ca452865c308fca9dfe9291884
d10sfan <d10sfan@gmail.com>
2022-09-22 09:21:15 -0500

    augustus - Fix assets

--- engines/augustus/build.sh
+++ engines/augustus/build.sh
@@ -14,8 +14,18 @@ cmake ..
 make -j "$(nproc)"
 popd
 
+pushd "source"
+mkdir -p res/asset_packer/build
+pushd res/asset_packer/build
+cmake -DCMAKE_BUILD_TYPE=Release $DISABLE_SYSTEM_LIBS  ..
+make -j "$(nproc)"
+./asset_packer ../../
+popd
+popd
+
 # COPY PHASE
+mkdir -p "$diststart/517790/dist/assets"
 cp -rfv "source/build/augustus" "$diststart/517790/dist/"
-cp -rfv "source/assets" "$diststart/517790/dist/"
+cp -rfv "source/res/packed_assets/"* "$diststart/517790/dist/assets"
 cp -rfv "source/res/maps" "$diststart/517790/dist/"
 cp -rfv assets/* "$diststart/517790/dist/"
