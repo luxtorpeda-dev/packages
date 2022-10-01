991c0f81c35a9e9ec413fb639ee64b21afb71bf2
d10sfan <d10sfan@gmail.com>
2022-10-01 12:08:27 -0500

    quakespasm-spiked - Fix build

--- engines/quakespasm-spiked/build.sh
+++ engines/quakespasm-spiked/build.sh
@@ -7,7 +7,7 @@ git checkout e9822ae
 popd
 
 # BUILD PHASE
-pushd "source/quakespasm/Quake"
+pushd "source/Quake"
 make -j "$(nproc)" USE_SDL2=1
 popd
 
@@ -18,7 +18,7 @@ mkdir -p "$diststart/common/dist/quakespasm/share/quake/hipnotic"
 mkdir -p "$diststart/common/dist/quakespasm/share/quake/dopa"
 mkdir -p "$diststart/common/dist/quakespasm/share/quake/mg1"
 
-cp -v source/quakespasm/Quake/quakespasm "$diststart/common/dist/quakespasm/"
+cp -v source/Quake/quakespasm "$diststart/common/dist/quakespasm/"
 cp -v assets/quakespasm.sh "$diststart/common/dist/quakespasm/"
 cp -v assets/default.lux.cfg "$diststart/common/dist/quakespasm/share/quake"
 
