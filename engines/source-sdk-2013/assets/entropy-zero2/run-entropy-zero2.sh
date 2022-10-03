6e906311b2a961ed31f37ccf6619a9a7c5b90932
d10sfan <4337981+d10sfan@users.noreply.github.com>
2022-10-03 15:35:58 -0500

    source-sdk-2013 - Add entropy-zero2 (#760)

---
+++ engines/source-sdk-2013/assets/entropy-zero2/run-entropy-zero2.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+if [ -f "last_error.txt" ]; then
+    rm last_error.txt
+fi
+
+if [ ! -f "sdkpath.txt" ]; then
+    if [[ ! -z "${DEPPATH_243730}" ]]; then
+        echo "Automatic path for sdk found at $DEPPATH_243730"
+        HL_PATH="$DEPPATH_243730"
+    else
+        "$STEAM_ZENITY" --info --text="Browse to Source SDK Base 2013 Singleplayer Installation" --title="Information"
+        HL_PATH=$("$STEAM_ZENITY" --file-selection --title="Browse to Source SDK Base 2013 Singleplayer Installation" --directory)
+
+        if [ -z "$HL_PATH" ]; then
+            error_message="Path to Source SDK 2013 not given."
+            if [[ -z "${LUX_ERRORS_SUPPORTED}" ]]; then
+                "$STEAM_ZENITY" --error --title="Error" --text="$error_message"
+            else
+                echo "$error_message" > last_error.txt
+            fi
+            exit 10
+        fi
+
+        if [ ! -d "$HL_PATH/sdktools" ]; then
+            error_message="Path to Source SDK 2013 incorrect."
+            if [[ -z "${LUX_ERRORS_SUPPORTED}" ]]; then
+                "$STEAM_ZENITY" --error --title="Error" --text="$error_message"
+            else
+                echo "$error_message" > last_error.txt
+            fi
+            exit 10
+        fi
+    fi
+
+    echo "$HL_PATH" >> sdkpath.txt
+    
+    pushd "entropyzero2"
+    # from z33ky script
+    for d in sound materials resource; do
+		LD_PRELOAD="" find "${d}/" -exec sh -eux -c "echo {} | grep -q '[A-Z]' && ln -vs \"\$(basename {})\" \"\$(echo {}|tr '[A-Z]' '[a-z]')\"" \;
+	done
+    popd
+fi
+
+if [ ! -f "runtimepath.txt" ]; then
+    if [[ ! -z "${DEPPATH_1070560}" ]]; then
+        echo "Automatic path for runtime found at $DEPPATH_1070560"
+        RUNTIME_PATH="$DEPPATH_1070560"
+    else
+        "$STEAM_ZENITY" --info --text="Browse to Steam Linux Runtime Installation. You should see a scout-on-soldier-entry-point-v2 file in the proper directory." --title="Information"
+        RUNTIME_PATH=$("$STEAM_ZENITY" --file-selection --title="Browse to Steam Linux Runtime Installation." --directory)
+
+        if [ -z "$RUNTIME_PATH" ]; then
+            error_message="Path to Steam Linux Runtime not given."
+            if [[ -z "${LUX_ERRORS_SUPPORTED}" ]]; then
+                "$STEAM_ZENITY" --error --title="Error" --text="$error_message"
+            else
+                echo "$error_message" > last_error.txt
+            fi
+            exit 10
+        fi
+
+        if [ ! -f "$RUNTIME_PATH/scout-on-soldier-entry-point-v2" ]; then
+            error_message="Path to Steam Linux Runtime incorrect."
+            if [[ -z "${LUX_ERRORS_SUPPORTED}" ]]; then
+                "$STEAM_ZENITY" --error --title="Error" --text="$error_message"
+            else
+                echo "$error_message" > last_error.txt
+            fi
+            exit 10
+        fi
+    fi
+
+    echo "$RUNTIME_PATH" >> runtimepath.txt
+fi
+
+if [[ ! -z "${DEPPATH_243730}" ]]; then
+    sdkpath="$DEPPATH_243730"
+    echo "Automatically detected sdkpath at $sdkpath"
+else
+    sdkpath=$(cat sdkpath.txt)
+fi
+
+if [[ ! -z "${DEPPATH_1070560}" ]]; then
+    runtimepath="$DEPPATH_1070560"
+    echo "Automatically detected runtimepath at $runtimepath"
+else
+    runtimepath=$(cat runtimepath.txt)
+fi
+
+pushd "entropyzero2"
+    LD_PRELOAD="" cp -rfv ../gameinfo-override.txt gameinfo.txt
+popd
+
+cp -rfv ./ez2-binaries/entropyzero2/bin/* ./entropyzero2/bin
+
+if [[ ! -d "ez2extracted" ]]; then
+    LD_PRELOAD="" echo "ez2extracted not found, extracting vpks"
+
+    export PYTHONUSERBASE="$PWD/vpk/vpkinstall"
+    if [[ ! -d "vpk/vpkinstall" ]]; then
+        LD_PRELOAD="" tar xvf setuptools-65.4.1.tar.gz
+        pushd setuptools-65.4.1
+        LD_PRELOAD="" python3.7 setup.py install --user
+        popd
+        pushd vpk
+        LD_PRELOAD="" python3.7 setup.py install --user
+        popd
+    fi
+
+    LD_PRELOAD="" ./vpk/vpkinstall/bin/vpk entropyzero2/ez2/ez2_dir.vpk -x ./ez2extracted
+fi
+
+"$runtimepath/scout-on-soldier-entry-point-v2" --verbose -- "$sdkpath"/hl2.sh -game "$PWD/entropyzero2" -steam
