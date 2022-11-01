3470e06a26ecfb7660de3f0ce1cb503c3f18189e
d10sfan <d10sfan@gmail.com>
2022-11-01 10:01:52 -0500

    notblood

---
+++ engines/notblood/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/clipmove/NotBlood.git source
+pushd source
+git checkout 09c193f
+popd
+
+# BUILD PHASE
+pushd source
+make blood
+popd
+
+# COPY PHASE
+cp -rfv "source/notblood" "$diststart/common/dist/"
+cp -rfv "source/notblood.pk3" "$diststart/common/dist/"
+cp -rfv assets/* "$diststart/common/dist/"
