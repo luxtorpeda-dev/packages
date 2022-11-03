817f99293c1a54eeb70ea76a74d8a6fad266c7dd
d10sfan <d10sfan@gmail.com>
2022-11-02 23:08:22 -0500

    warzone2100 - Update to 4.3.1

--- engines/warzone2100/build.sh
+++ engines/warzone2100/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/Warzone2100/warzone2100.git source
 pushd source
-git checkout -f 84a35f5
+git checkout -f b8549e8
 git submodule update --init --recursive
 popd
 
