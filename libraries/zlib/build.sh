6f859c15bd82a4cc14fbb82d872cc642103a46fc
d10sfan <d10sfan@gmail.com>
2022-11-02 23:18:54 -0500

    Revert "warzone2100 - Fix"
    This reverts commit 78ea14c6b3c8c0ed20911ad40b6078492d5e2210.

--- libraries/zlib/build.sh
+++ libraries/zlib/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # CLONE PHASE
-wget https://zlib.net/zlib-1.2.13.tar.gz
-tar xvf zlib-1.2.13.tar.gz
+wget https://zlib.net/zlib-1.2.11.tar.gz
+tar xvf zlib-1.2.11.tar.gz
 
 # BUILD PHASE
-pushd zlib-1.2.13
+pushd zlib-1.2.11
 ./configure --prefix="$pfx"
 make
 make install DESTDIR="$pfx"
