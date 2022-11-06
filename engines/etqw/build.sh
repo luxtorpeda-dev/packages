2ac9933d7a2442eeec4a14392745e6144fa309e0
d10sfan <d10sfan@gmail.com>
2022-11-05 23:20:25 -0500

    etqw - Update sdl12-compat

--- engines/etqw/build.sh
+++ engines/etqw/build.sh
@@ -2,8 +2,7 @@
 
 # COPY PHASE
 
-cp -rfv "$pfx/lib/libSDL-1.2.so.1.2.52" "$diststart/10050/dist/libSDL-1.2.so.0"
+cp -rfv "$pfx/lib/libSDL-1.2.so.1.2.60" "$diststart/10050/dist/libSDL-1.2.so.0"
 cp -rfv assets/* "$diststart/10050/dist/"
-
-cp -rfv "$pfx/lib/libSDL-1.2.so.1.2.52" "$diststart/10000/dist/libSDL-1.2.so.0"
+cp -rfv "$pfx/lib/libSDL-1.2.so.1.2.60" "$diststart/10000/dist/libSDL-1.2.so.0"
 cp -rfv assets/* "$diststart/10000/dist/"
