43091d57e234b734e8ecd34533f13c251d1a8488
d10sfan <d10sfan@gmail.com>
2022-12-13 13:41:45 -0600

    unreal-gold - Update to 227j

--- engines/unreal-gold/assets/run-unreal.sh
+++ engines/unreal-gold/assets/run-unreal.sh
@@ -4,9 +4,9 @@ if [ -f "last_error.txt" ]; then
     rm last_error.txt
 fi
 
-if [ ! -f ./UnrealLinux.bin ] 
+if [ ! -f ../System64/UnrealLinux.bin ]
 then
-    error_message="Unreal Linux not found. Please see System/README.txt for instructions in game directory."
+    error_message="Unreal Linux (64-bit) not found. Please see System/README.txt for instructions in game directory."
     if [[ -z "${LUX_ERRORS_SUPPORTED}" ]]; then
         "$STEAM_ZENITY" --error --title="Error" --text="$error_message"
     else
@@ -15,6 +15,10 @@ then
     exit 10
 fi
 
-chmod +x ./UnrealLinux.bin
+chmod +x ../System64/UnrealLinux.bin
+cd ../System64
+mkdir -p libs
+tar -xvf ../Help/convenience_libs.tar.bz2
 cp -rfv libSDL-1.2.so.0 ./libs
+cp -rfv convenience_libs/System64/* ./libs
 LD_LIBRARY_PATH="./libs" LD_PRELOAD="" ./UnrealLinux.bin -log
