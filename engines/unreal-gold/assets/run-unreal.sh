bbdd371a3a320e372836b5a1d018f98ba1cc0139
d10sfan <4337981+d10sfan@users.noreply.github.com>
2022-12-15 20:38:55 -0600

    unreal-gold - Switch to 32-bit (#803)

--- engines/unreal-gold/assets/run-unreal.sh
+++ engines/unreal-gold/assets/run-unreal.sh
@@ -4,9 +4,9 @@ if [ -f "last_error.txt" ]; then
     rm last_error.txt
 fi
 
-if [ ! -f ../System64/UnrealLinux.bin ]
+if [ ! -f ../System/UnrealLinux.bin ]
 then
-    error_message="Unreal Linux (64-bit) not found. Please see System/README.txt for instructions in game directory."
+    error_message="Unreal Linux not found. Please see System/README.txt for instructions in game directory."
     if [[ -z "${LUX_ERRORS_SUPPORTED}" ]]; then
         "$STEAM_ZENITY" --error --title="Error" --text="$error_message"
     else
@@ -15,6 +15,6 @@ then
     exit 10
 fi
 
-chmod +x ../System64/UnrealLinux.bin
-cd ../System64
+chmod +x ../System/UnrealLinux.bin
+cd ../System
 LD_LIBRARY_PATH="../System/lib:$LD_LIBRARY_PATH" ./UnrealLinux.bin -log
