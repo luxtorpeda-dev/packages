0cf6e8b7cd6cca5b43e3b8db6e82aabb2575e3c4
d10sfan <d10sfan@gmail.com>
2023-01-15 20:46:17 -0600

    ?

--- engines/serious-engine/build.sh
+++ engines/serious-engine/build.sh
@@ -4,13 +4,11 @@
 git clone https://github.com/tx00100xt/SeriousSamClassic-VK.git source
 pushd source
 git checkout 590a0fb
-patch -p1 < ../patches/tfe-vk-last-update.patch || return 1
-patch -p1 < ../patches/tse-vk-last-update.patch || return 1
 popd
 
 # BUILD PHASE
 pushd source/SamTSE/Sources
-./build-linux64.sh
+./build-linux32.sh
 popd
 
 # COPY PHASE
@@ -19,7 +17,7 @@ cp -rfv assets/* "$diststart/41060/dist/"
 
 # BUILD PHASE
 pushd source/SamTFE/Sources
-./build-linux64.sh -DTFE=TRUE
+./build-linux32.sh -DTFE=TRUE
 popd
 
 # COPY PHASE
