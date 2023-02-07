2d8a91ebc743e943bd92651538632919afb14463
d10sfan <d10sfan@gmail.com>
2023-02-06 21:58:30 -0600

    openrct2 - Added classic support

---
+++ engines/openrct2/assets/run-openrct2-classic.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ ! -d "Data" ]; then
+    LD_PRELOAD="" echo "Data directory not found, assuming steam installed it as 'Assets'"
+    LD_PRELOAD="" ln -rsf ./Assets ./Data
+fi
+
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./openrct2
