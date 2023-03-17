20dfacfb719482e754541c2c616f0cb391a4060d
d10sfan <d10sfan@gmail.com>
2023-03-17 14:15:55 -0500

    ironwail - Fix

---
+++ libraries/curl/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/curl/curl.git curl
+pushd curl
+git checkout -f curl-7_88_1
+popd
+
+# BUILD PHASE
+pushd "curl"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    ..
+make -j "$(nproc)"
+DESTDIR="$pfx" make install
+popd
+
+cp -rfv "$pfx/lib/"* /usr/lib
+cp -rfv "$pfx/include/"* /usr/include
