443298b964c8a8a6bc5babb83845b39abe090b4d
d10sfan <d10sfan@gmail.com>
2023-03-29 09:33:37 -0500

    doom64ex-plus

---
+++ engines/doom64ex-plus/assets/run-doom64ex-plus.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -f ~/.config/gzdoom/gzdoom.ini ]; then
+    if [ ! -d ~/.config/gzdoom ]; then
+        mkdir -p ~/.config/gzdoom
+    fi
+    cp -rfv ./gzdoom_template.ini ~/.config/gzdoom/gzdoom.ini
+fi
+
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./gzdoom "$@"
