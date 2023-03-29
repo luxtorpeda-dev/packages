1b5887dffcc1db28eebeaceef86550e2f50b85e2
d10sfan <4337981+d10sfan@users.noreply.github.com>
2023-03-29 11:11:38 -0500

    doom64ex-plus (#858)

---
+++ engines/doom64ex-plus/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# From https://gitlab.com/luxtorpeda/packages/gzdoom - See LICENSE file for more information
+# CLONE PHASE
+git clone https://github.com/atsb/Doom64EX-Plus.git source
+pushd source
+git checkout aa6fcb9
+popd
+
+# BUILD PHASE
+pushd "source/src/engine"
+./build.sh
+popd
+
+# COPY PHASE
+cp -rfv source/src/engine/DOOM64EX-Plus "$diststart/1148590/dist/"
+cp -rfv assets/* "$diststart/1148590/dist/"
+cp -rfv source/doomsnd.sf2 "$diststart/1148590/dist/"
+cp -rfv source/doom64ex-plus.wad "$diststart/1148590/dist/"
