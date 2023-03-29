323d109d865fc97465957ee1e3dd2a453bccdfc2
d10sfan <d10sfan@gmail.com>
2023-03-29 09:49:13 -0500

    ?

--- engines/doom64ex-plus/assets/run-doom64ex-plus.sh
+++ engines/doom64ex-plus/assets/run-doom64ex-plus.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-if [ ! -f ~/.config/gzdoom/gzdoom.ini ]; then
-    if [ ! -d ~/.config/gzdoom ]; then
-        mkdir -p ~/.config/gzdoom
-    fi
-    cp -rfv ./gzdoom_template.ini ~/.config/gzdoom/gzdoom.ini
-fi
-
-LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./gzdoom "$@"
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./DOOM64EX-Plus "$@"
--- engines/doom64ex-plus/build.sh
+++ engines/doom64ex-plus/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-apt-get update
-
 # From https://gitlab.com/luxtorpeda/packages/gzdoom - See LICENSE file for more information
 # CLONE PHASE
 git clone https://github.com/atsb/Doom64EX-Plus.git source
@@ -15,7 +13,7 @@ pushd "source/src/engine"
 popd
 
 # COPY PHASE
-cp -rfv source/src/engine/doom64ex-plus "$diststart/1148590/dist/"
+cp -rfv source/src/engine/DOOM64EX-Plus "$diststart/1148590/dist/"
 cp -rfv assets/* "$diststart/1148590/dist/"
 cp -rfv source/doomsnd.sf2 "$diststart/1148590/dist/"
 cp -rfv source/doom64ex-plus.wad "$diststart/1148590/dist/"
