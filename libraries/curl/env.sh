1d358f9f0c01e422dd66dac70f7b8d00ccd22fed
d10sfan <d10sfan@gmail.com>
2023-04-14 13:05:04 -0500

    library cleanup

--- libraries/curl/env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export LIBRARY_LICENSES="./curl/COPYING"
-export LIBRARY_COPY_TO_LIB="$pfx/usr/local/lib/*.so*"
--- libraries/sdl2/env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export LIBRARY_LICENSES="./sdl2/LICENSE.txt"
-export LIBRARY_COPY_TO_LIB="$pfx/usr/local/lib/*.so*"
