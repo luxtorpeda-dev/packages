a133be7b080f7dff736af6bf18e3e178402ab37c
d10sfan <d10sfan@gmail.com>
2023-04-13 22:29:48 -0500

    gzdoom - Build for sniper

--- engines/gzdoom/build.sh
+++ engines/gzdoom/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y mercurial
-
 # From https://gitlab.com/luxtorpeda/packages/gzdoom - See LICENSE file for more information
 # CLONE PHASE
 git clone https://github.com/coelckers/gzdoom.git source
