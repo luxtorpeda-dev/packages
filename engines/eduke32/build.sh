cf01e82d1698f95ac7ba342628d5048b5ac2e1ea
d10sfan <d10sfan@gmail.com>
2023-04-13 22:11:54 -0500

    eduke32 - Hack workaround

--- engines/eduke32/build.sh
+++ engines/eduke32/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # CLONE PHASE
+git config --global http.sslverify false
+export GIT_SSL_NO_VERIFY=1
 git clone https://voidpoint.io/terminx/eduke32.git source
 pushd source
 git checkout 54177821
