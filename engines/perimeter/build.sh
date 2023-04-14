030f03d200b4222d4b50bb98fbd03309b4a7ae64
d10sfan <d10sfan@gmail.com>
2023-04-14 16:38:34 -0500

    perimeter - Build for sniper

--- engines/perimeter/build.sh
+++ engines/perimeter/build.sh
@@ -11,7 +11,7 @@ popd
 pushd "source"
 mkdir build
 cd build
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT="$boostlocation" -DOPTION_LINKER_LLD=OFF -DCMAKE_PREFIX_PATH="$pfx" -DCMAKE_INSTALL_PREFIX="$pfx"
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DOPTION_LINKER_LLD=OFF -DCMAKE_PREFIX_PATH="$pfx" -DCMAKE_INSTALL_PREFIX="$pfx"
 ninja dependencies
 ninja
 cd Source/dxvk-native-prefix/src/dxvk-native-build
