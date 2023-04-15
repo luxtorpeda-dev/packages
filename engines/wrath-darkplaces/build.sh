6ff80e2e99babf6347ee6cdfc26e6a1de93e0193
d10sfan <d10sfan@gmail.com>
2023-04-14 22:07:45 -0500

    wrath-darkplaces - Build for sniper

--- engines/wrath-darkplaces/build.sh
+++ engines/wrath-darkplaces/build.sh
@@ -8,6 +8,7 @@ git submodule update --init --recursive
 popd
 
 export CXXFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas"
+
 export CFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas"
 
 # BUILD PHASE
