b3402fb80259014caf478f43c8b936c6edddf57b
d10sfan <d10sfan@gmail.com>
2023-04-15 16:54:41 -0500

    ?????

--- libraries/qt5/build.sh
+++ libraries/qt5/build.sh
@@ -1,6 +1,71 @@
 #!/bin/bash
 
-apt-get install -y libxcb-util-dev libxkbcommon-x11-dev libxcb-icccm4 libxcb-icccm4-dev libxcb-image0 libxcb-image0-dev libxcb-keysyms1 libxcb-keysyms1-dev libxcb-render-util0 libxcb-render-util0-dev
+# ===================== Xcb deps phase     =====================
+
+# CLONE PHASE
+wget https://xcb.freedesktop.org/dist/xcb-util-0.4.1.tar.xz
+tar xvf xcb-util-0.4.1.tar.xz
+
+git clone https://github.com/xkbcommon/libxkbcommon.git
+pushd libxkbcommon
+git checkout -f cecaa01
+popd
+
+wget https://xorg.freedesktop.org/archive/individual/lib/xcb-util-wm-0.4.2.tar.xz
+tar xvf xcb-util-wm-0.4.2.tar.xz
+
+wget https://xorg.freedesktop.org/archive/individual/lib/xcb-util-image-0.4.1.tar.xz
+tar xvf xcb-util-image-0.4.1.tar.xz
+
+wget https://xorg.freedesktop.org/archive/individual/lib/xcb-util-keysyms-0.4.1.tar.xz
+tar xvf xcb-util-keysyms-0.4.1.tar.xz
+
+wget https://xorg.freedesktop.org/archive/individual/lib/xcb-util-renderutil-0.3.10.tar.xz
+tar xvf xcb-util-renderutil-0.3.10.tar.xz
+
+# BUILD PHASE
+pushd xcb-util-0.4.1
+./configure --disable-static
+make
+make DESTDIR="$pfx" install
+make install
+popd
+
+pushd libxkbcommon
+meson setup build -Denable-docs=false
+meson install -C build --destdir "$pfx"
+meson install -C build
+popd
+
+pushd xcb-util-wm-0.4.2
+./configure --disable-static
+make
+make DESTDIR="$pfx" install
+make install
+popd
+
+pushd xcb-util-image-0.4.1
+./configure --disable-static
+make
+make DESTDIR="$pfx" install
+make install
+popd
+
+pushd xcb-util-keysyms-0.4.1
+./configure --disable-static
+make
+make DESTDIR="$pfx" install
+make install
+popd
+
+pushd xcb-util-renderutil-0.3.10
+./configure --disable-static
+make
+make DESTDIR="$pfx" install
+make install
+popd
+
+# ===================== Xcb deps phase end =====================
 
 # CLONE PHASE
 git clone https://github.com/qt/qt5.git source
--- libraries/qt5/env.sh
+++ libraries/qt5/env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export LIBRARY_LICENSES="./source/LICENSE.LGPLv3 ./source/LICENSE.GPLv3 ./source/LICENSE.FDL"
-export LIBRARY_COPY_TO_DIST="$pfx/qt5/*"
+export LIBRARY_LICENSES="./source/LICENSE.LGPLv3 ./source/LICENSE.GPLv3 ./source/LICENSE.FDL xcb-util-0.4.1/COPYING libxkbcommon/LICENSE xcb-util-wm-0.4.2/COPYING xcb-util-image-0.4.1/COPYING xcb-util-keysyms-0.4.1/COPYING xcb-util-renderutil-0.3.10/COPYING"
+export LIBRARY_COPY_TO_DIST="$pfx/qt5/* $pfx/usr/local/lib/*.so* $pfx/user/local/lib/x86_64-linux-gnu/*.so*"
