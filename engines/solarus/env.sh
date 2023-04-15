2164925476b43814a72c73d011eed7b75a3a1ecb
d10sfan <d10sfan@gmail.com>
2023-04-14 21:47:21 -0500

    solarus - Build for sniper

--- engines/solarus/env.sh
+++ engines/solarus/env.sh
@@ -3,4 +3,5 @@
 export STEAM_APP_ID_LIST="1393750"
 export LICENSE_PATH="./source/license_gpl.txt"
 export ADDITIONAL_LICENSES="./libmodplug/COPYING"
-export LIBRARIES="glm physfs luajit"
+export LIBRARIES="physfs luajit"
+export APT_LIBRARIES="libglm-dev"
