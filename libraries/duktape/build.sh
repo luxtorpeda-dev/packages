2794cca4fb2cecab643206bd190428e30ce15f2b
d10sfan <d10sfan@gmail.com>
2023-04-14 23:21:19 -0500

    openrct2 - Fix

--- libraries/duktape/build.sh
+++ libraries/duktape/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/svaarala/duktape.git duktape
 pushd duktape
-git checkout -f 5e169de
+git checkout -f 03d4d72
 popd
 
 # BUILD PHASE
@@ -12,7 +12,6 @@ make dist -j "$(nproc)"
 popd
 
 pushd "duktape/dist"
-mv Makefile.sharedlibrary Makefile
 sed 's/-Wall -Wextra/$(CFLAGS)/g' -i Makefile
 CFLAGS="$CFLAGS -D DUK_USE_FASTINT -w" make -j "$(nproc)"
 setconf Makefile INSTALL_PREFIX="$pfx"
