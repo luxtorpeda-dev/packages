ca818e710d27ec657a087c17e85b7ac9595b8997
d10sfan <d10sfan@gmail.com>
2023-04-14 23:42:54 -0500

    ?

--- engines/ctp2/env.sh
+++ engines/ctp2/env.sh
@@ -3,5 +3,5 @@
 export STEAM_APP_ID_LIST="572050"
 export LICENSE_PATH="./source/EULA.rtf"
 export ADDITIONAL_LICENSES="./source/Activision_CTP2_Source_Code_Readme.txt ./source/Apolyton_CTP2_Source_Code_Readme.txt"
-export APT_LIBRARIES="byacc"
+export APT_LIBRARIES="byacc libgtk2.0-dev"
 export LIBRARIES="sdl12compat sdl12image sdl12mixer"
