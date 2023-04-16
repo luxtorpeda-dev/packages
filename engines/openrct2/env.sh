5415be22518184e7f1a53370570c06cdf9719b06
d10sfan <d10sfan@gmail.com>
2023-04-15 23:17:39 -0500

    openrct2 - Fix

--- engines/openrct2/env.sh
+++ engines/openrct2/env.sh
@@ -3,4 +3,4 @@
 export STEAM_APP_ID_LIST="285330 285310"
 export LICENSE_PATH="./source/licence.txt"
 export COMMON_PACKAGE="1"
-export LIBRARIES="duktape jansson libzip json"
+export LIBRARIES="jansson libzip json"
