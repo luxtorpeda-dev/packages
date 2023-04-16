4393e6865a21259795957aa076962706a3408440
d10sfan <d10sfan@gmail.com>
2023-04-15 21:06:54 -0500

    openmw - Build for sniper

--- engines/openmw/build.sh
+++ engines/openmw/build.sh
@@ -35,6 +35,7 @@ cp -rfv "$pfx/"lib/*.so* "$diststart/22320/dist/lib/"
 cp -rfv "$pfx/lib/"osgPlugins-* "$diststart/22320/dist/lib/"
 cp -rfv "$tmp/usr/local/"{etc,share} "$diststart/22320/dist/"
 cp -rfv "$tmp/usr/local/bin/"* "$diststart/22320/dist/"
+
 cp assets/* "$diststart/22320/dist/"
 cp "source/files/settings-default.cfg" "$diststart/22320/dist/"
 cp -rfv source/build/defaults.bin "$diststart/22320/dist/"
