de5c125ddb55a625425217071abd25d8a284b416
d10sfan <d10sfan@gmail.com>
2023-04-15 22:52:18 -0500

    classic-rbdoom-3-bfg - Update to latest

--- engines/classic-rbdoom-3-bfg/build.sh
+++ engines/classic-rbdoom-3-bfg/build.sh
@@ -3,23 +3,8 @@
 # CLONE PHASE
 git clone https://github.com/MadDeCoDeR/Classic-RBDOOM-3-BFG.git source
 pushd source
-git checkout cf9c2fa
+git checkout 9200ffd
 git am < ../patches/e90b31dcc2901f4d8390f8089a8bd43f88c575ea.patch
-popd
-
-git clone https://github.com/kcat/openal-soft.git openal
-pushd openal
-git checkout ae4eacf
-popd
-
-# BUILD PHASE
-pushd "openal"
-cd build
-cmake \
-    -DCMAKE_BUILD_TYPE=MinSizeRel \
-    ..
-make -j "$(nproc)"
-make install
 popd
 
 pushd "source"
@@ -39,5 +24,3 @@ popd
 cp "source/build/DoomBFA" "$diststart/208200/dist/DoomBFA"
 cp -rfv ./assets/* "$diststart/208200/dist/"
 cp -rfv ./source/base "$diststart/208200/dist/updatedbase"
-cp -rfv "/usr/local/lib/libopenal.so.1.21.1" "$diststart/208200/dist/lib/libopenal.so"
-cp -rfv "/usr/local/lib/libopenal.so.1.21.1" "$diststart/208200/dist/lib/libopenal.so.1"
--- engines/classic-rbdoom-3-bfg/env.sh
+++ engines/classic-rbdoom-3-bfg/env.sh
@@ -2,5 +2,5 @@
 
 export STEAM_APP_ID_LIST="208200 1898710"
 export LICENSE_PATH="./source/COPYING.txt"
-export ADDITIONAL_LICENSES="./source/README.md ./source/neo/libs/platform/COPYING.txt ./openal/COPYING"
+export ADDITIONAL_LICENSES="./source/README.md ./source/neo/libs/platform/COPYING.txt"
 export LIBRARIES="yasm ffmpeg"
