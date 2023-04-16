c062a5bce9af4975b133dcacffd3d47531848a42
d10sfan <d10sfan@gmail.com>
2023-04-15 21:16:11 -0500

    ???

--- libraries/libunwind/build.sh
+++ libraries/libunwind/build.sh
@@ -3,8 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/libunwind/libunwind.git libunwind
 pushd libunwind
-git checkout -f 1847559
-git am < ../patches/0001-Workaround-for-force-link-of-lgcc.patch
+git checkout -f 5010beb
 popd
 
 # BUILD PHASE
