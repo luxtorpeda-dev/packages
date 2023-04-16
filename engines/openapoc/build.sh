0bd873bc6773cf65d73d9b6d7a07c2c9e6f8cb91
d10sfan <4337981+d10sfan@users.noreply.github.com>
2023-04-15 22:44:07 -0500

    OpenApoc - Update to a224501 (#891)

--- engines/openapoc/build.sh
+++ engines/openapoc/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/OpenApoc/OpenApoc.git source
 pushd source
-git checkout -f ffc66ce
+git checkout -f a224501
 git submodule update --init --recursive
 popd
 
@@ -24,6 +24,5 @@ popd
 # COPY PHASE
 mkdir -p "$diststart/7660/dist/bin/"
 cp -rfv "source/build/bin/"* "$diststart/7660/dist/bin/"
-
 cp -rfv "source/data" "$diststart/7660/dist/"
 cp -rfv "assets/"* "$diststart/7660/dist/"
