9f6d0e914686c92dc737b02f9e03e27339ccd85e
GitHub Action <actions@github.com>
2023-04-17 14:18:11 -0500

    ??

--- engines/classic-rbdoom-3-bfg/build.sh
+++ engines/classic-rbdoom-3-bfg/build.sh
@@ -16,7 +16,6 @@ cmake \
     -DCMAKE_PREFIX_PATH="$pfx" \
     --preset=linux-retail \
     ..
-
 cd ../../buildRetail
 make -j "$(nproc)"
 popd
@@ -25,3 +24,15 @@ popd
 cp "source/buildRetail/DoomBFA" "$diststart/208200/dist/DoomBFA"
 cp -rfv ./assets/* "$diststart/208200/dist/"
 cp -rfv ./source/base "$diststart/208200/dist/updatedbase"
+
+mkdir -p licenses
+licensepath="$PWD/licenses"
+pushd ./source/buildRetail/vcpkg_installed/x64-linux/share
+for d in */ ; do
+    directory=${d::-1}
+    echo "$directory"
+    if [ -f "$directory/copyright" ]; then
+        cp -rfv "$d/copyright" "$licensepath/$directory.copyright"
+    fi
+done
+popd
