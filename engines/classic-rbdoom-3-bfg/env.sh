b980b8eb80ee05235b6255b50f31b6a0c35e9714
d10sfan <d10sfan@gmail.com>
2023-04-16 20:37:51 -0500

    ???

--- engines/classic-rbdoom-3-bfg/env.sh
+++ engines/classic-rbdoom-3-bfg/env.sh
@@ -3,4 +3,4 @@
 export STEAM_APP_ID_LIST="208200 1898710"
 export LICENSE_PATH="./source/COPYING.txt"
 export ADDITIONAL_LICENSES="./source/README.md ./source/neo/libs/platform/COPYING.txt"
-export APT_LIBRARIES="yasm"
+export APT_LIBRARIES="yasm nasm"
