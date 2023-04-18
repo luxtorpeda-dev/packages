1e818524a1ab23f3ca388002d8a724ab11597498
d10sfan <d10sfan@gmail.com>
2023-04-17 22:38:18 -0500

    ???

--- engines/cortex-command/env.sh
+++ engines/cortex-command/env.sh
@@ -3,4 +3,4 @@
 export STEAM_APP_ID_LIST="209670"
 export LICENSE_PATH="./source/LICENSE"
 export ADDITIONAL_LICENSES="./data/Credits.txt ./data/CreditsAssets.txt ./source/Licences/FMOD.TXT ./source/Licences/RakNet.txt"
-export LIBRARIES="boost lua minizip liblz4 allegro4"
+export LIBRARIES="boost luajit lua minizip liblz4 allegro4"
