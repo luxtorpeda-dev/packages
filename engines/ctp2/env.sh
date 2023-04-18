fcd227397986734a63e6cdd5c9aa7762bb3d53ec
d10sfan <d10sfan@gmail.com>
2023-04-17 21:40:23 -0500

    fix

--- engines/ctp2/env.sh
+++ engines/ctp2/env.sh
@@ -3,4 +3,5 @@
 export STEAM_APP_ID_LIST="572050"
 export LICENSE_PATH="./source/EULA.rtf"
 export ADDITIONAL_LICENSES="./source/Activision_CTP2_Source_Code_Readme.txt ./source/Apolyton_CTP2_Source_Code_Readme.txt"
-export APT_LIBRARIES="byacc yasm ffmpeg"
+export APT_LIBRARIES="byacc yasm"
+export LIBRARIES="ffmpeg"
