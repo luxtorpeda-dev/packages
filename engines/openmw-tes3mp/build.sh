16a233c9cff70ed6fc401c2b850ac38fb5df2545
d10sfan <d10sfan@gmail.com>
2023-04-19 09:26:29 -0500

    ??

--- engines/openmw-tes3mp/build.sh
+++ engines/openmw-tes3mp/build.sh
@@ -12,5 +12,10 @@ git submodule update --init --recursive
 popd
 
 # COPY PHASE
+wget https://github.com/TES3MP/TES3MP/releases/download/tes3mp-0.8.1/tes3mp-GNU+Linux-x86_64-release-0.8.1-68954091c5-6da3fdea59.tar.gz
+tar xvf tes3mp-GNU+Linux-x86_64-release-0.8.1-68954091c5-6da3fdea59.tar.gz
+rm -rf TES3MP/_deps
+cp -rfv TES3MP/* "$diststart/22320/dist/"
+
 cp "assets/tes3mp-launcher.sh" "$diststart/22320/dist/"
 cp -rfv source/tes3mp-credits.md "$diststart/22320/dist"
