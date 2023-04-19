a680f04db5c3ea522b4df09135bcf131d18d2cae
d10sfan <d10sfan@gmail.com>
2023-04-18 22:23:26 -0500

    ?

--- engines/ctp2/assets/run-ctp2.sh
+++ engines/ctp2/assets/run-ctp2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -rsf /overrides/lib/x86_64-linux-gnu/libgtk-3.so.0 ./lib/libgtk-x11-2.0.so.0
+ln -rsf /lib/x86_64-linux-gnu/libgtk-3.so.0 ./lib/libgtk-x11-2.0.so.0
 
 cd ctp2_program/ctp
 LD_LIBRARY_PATH="../../lib:$LD_LIBRARY_PATH" LD_PRELOAD="" ./ctp2 -fullscreen
