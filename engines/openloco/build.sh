f89a57352971b96ce201063ad2e48b3ec464a215
d10sfan <d10sfan@gmail.com>
2023-04-19 14:48:13 -0500

    ??

--- engines/openloco/build.sh
+++ engines/openloco/build.sh
@@ -28,12 +28,10 @@ cmake --preset linux -DSTRICT=OFF \
     -DOPENLOCO_BUILD_TESTS=OFF \
     -DCMAKE_PREFIX_PATH="$pfx"
 cmake --build --preset linux-release
-cp -rfv data Release
 popd
 
 # COPY PHASE
 mkdir -p "$diststart/356430/dist/data"
-
-cp -rfv "source/Release/OpenLoco" "$diststart/356430/dist/openloco"
-cp -rfv "source/Release/data/"* "$diststart/356430/dist/data"
+cp -rfv "source/build/linux/Release/OpenLoco" "$diststart/356430/dist/openloco"
+cp -rfv "source/data/"* "$diststart/356430/dist/data"
 cp -rfv "assets/run-openloco.sh" "$diststart/356430/dist"
