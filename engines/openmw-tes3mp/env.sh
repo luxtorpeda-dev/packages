0587ac5e58a349a3db7acca296a6df01f82f1a92
d10sfan <d10sfan@gmail.com>
2023-04-18 22:30:49 -0500

    openmw-tes3mp - Sniper fix, use upstream build

--- engines/openmw-tes3mp/env.sh
+++ engines/openmw-tes3mp/env.sh
@@ -2,6 +2,5 @@
 
 export STEAM_APP_ID_LIST="22320"
 export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./crabnet/LICENSE ./callff/LICENSE ./terra-Linux-x86_64-332a506/share/terra/LICENSE.txt"
-export LIBRARIES="yasm unshield bullet3 mygui openmwosg boost ffmpeg luajit qt5 liblz4"
+export LIBRARIES="qt5"
 export ARCHIVE_WITHOUT_V7="1"
