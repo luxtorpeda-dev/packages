06ad1921ebfa8b97f4addc5e7d19a098eda1f87d
d10sfan <d10sfan@gmail.com>
2023-04-20 14:02:08 -0500

    fixes

--- common/vcpkg_support/custom-triplets/x64-linux-dynamic.cmake
+++ common/vcpkg_support/custom-triplets/x64-linux-dynamic.cmake
@@ -5,3 +5,5 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic) # This changed from static to dynamic
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_BUILD_TYPE release)
--- engines/fs2open/env.sh
+++ engines/fs2open/env.sh
@@ -2,4 +2,4 @@
 
 export STEAM_APP_ID_LIST="273620"
 export LICENSE_PATH="./source/Copying.md"
-export APT_LIBRARIES="yasm"
+export APT_LIBRARIES="nasm"
