b2159685419688020cc1c6bbc69d72c37cef863d
d10sfan <d10sfan@gmail.com>
2023-04-21 15:03:55 -0500

    cortex-command - Build with vcpkg

--- engines/cortex-command/env.sh
+++ engines/cortex-command/env.sh
@@ -3,4 +3,3 @@
 export STEAM_APP_ID_LIST="209670"
 export LICENSE_PATH="./source/LICENSE"
 export ADDITIONAL_LICENSES="./data/Credits.txt ./source/Licences/FMOD.TXT ./source/Licences/RakNet.txt"
-export LIBRARIES="boost luajit lua minizip liblz4 allegro4"
