dc413c3089a040cfab7c94819d4f33d73335c250
d10sfan <d10sfan@gmail.com>
2023-04-23 13:53:14 -0500

    ??

--- engines/cortex-command/env.sh
+++ engines/cortex-command/env.sh
@@ -3,4 +3,4 @@
 export STEAM_APP_ID_LIST="209670"
 export LICENSE_PATH="./source/LICENSE"
 export ADDITIONAL_LICENSES="./data/Credits.txt ./source/Licences/FMOD.TXT ./source/Licences/RakNet.txt"
-export LIBRARIES="minizip"
+export LIBRARIES="lua minizip"
