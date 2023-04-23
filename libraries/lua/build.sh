ee9277447b8138205747126e5f4ba6edb171348a
d10sfan <d10sfan@gmail.com>
2023-04-23 13:53:18 -0500

    ??

---
+++ libraries/lua/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+apt-get -y install libreadline-dev
+
+# CLONE PHASE
+wget https://www.lua.org/ftp/lua-5.4.4.tar.gz
+tar xvf lua-5.4.4.tar.gz
+mv lua-5.4.4 lua
+pushd lua
+patch -p1 -i ../patches/liblua.so.patch
+sed "s/%VER%/5/g;s/%REL%/5.4.4/g" ../lua.pc > lua.pc
+popd
+
+# BUILD PHASE
+pushd lua
+make MYCFLAGS="$CFLAGS -fPIC" MYLDFLAGS="$LDFLAGS" linux-readline
+make \
+    TO_LIB="liblua.a liblua.so liblua.so.5.4 liblua.so.5.4.4" \
+    INSTALL_DATA='cp -d' \
+    INSTALL_TOP="$pfx"/usr \
+    install
+
+ln -rsf lua.pc /usr/lib/pkgconfig/lua.pc
+ln -rsf lua.pc /usr/lib/pkgconfig/lua5.4.pc
+ln -rsf lua.pc /usr/lib/pkgconfig/lua-5.4.pc
+cp -rfv /usr/lib/pkgconfig/* /usr/lib/x86_64-linux-gnu/pkgconfig/
+cp -rfv $pfx/usr/lib/*.so* /usr/lib
+popd
