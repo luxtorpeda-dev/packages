292f07ad9e871e512e8f0c67a420be1724306ceb
d10sfan <d10sfan@gmail.com>
2023-04-29 09:10:41 -0500

    fixes

--- engines/rbdoom-3-bfg/build.sh
+++ engines/rbdoom-3-bfg/build.sh
@@ -4,6 +4,7 @@
 git clone https://github.com/RobertBeckebans/RBDOOM-3-BFG.git source
 pushd source
 git checkout ab7fefc
+git submodule update --init --recursive
 popd
 
 pushd "source"
@@ -14,11 +15,12 @@ cmake \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DSDL2=ON \
     -DCMAKE_PREFIX_PATH="$pfx" \
+    -DFFMPEG=OFF \
+    -DBINKDEC=ON \
     ../neo
 make -j "$(nproc)"
 popd
 
 # COPY PHASE
 cp "source/build/RBDoom3BFG" "$diststart/208200/dist/RBDoom3BFG"
-
 cp -rfv ./assets/run-rbdoom-3-bfg.sh "$diststart/208200/dist/"
--- engines/rbdoom-3-bfg/env.sh
+++ engines/rbdoom-3-bfg/env.sh
@@ -2,5 +2,4 @@
 
 export STEAM_APP_ID_LIST="208200"
 export LICENSE_PATH="./source/LICENSE.md"
-export ADDITIONAL_LICENSES="./source/LICENSE_EXCEPTIONS.md"
-export APT_LIBRARIES="nasm"
+export ADDITIONAL_LICENSES="./source/LICENSE_EXCEPTIONS.md neo/extern/nvrhi/LICENSE.txt"
