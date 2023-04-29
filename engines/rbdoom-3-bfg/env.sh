a953183a2570a7d685c23e8492b8db5266f8f2d4
d10sfan <d10sfan@gmail.com>
2023-04-29 13:32:51 -0500

    ??

--- engines/rbdoom-3-bfg/env.sh
+++ engines/rbdoom-3-bfg/env.sh
@@ -2,4 +2,4 @@
 
 export STEAM_APP_ID_LIST="208200"
 export LICENSE_PATH="./source/LICENSE.md"
-export ADDITIONAL_LICENSES="./source/LICENSE_EXCEPTIONS.md neo/extern/nvrhi/LICENSE.txt"
+export ADDITIONAL_LICENSES="./source/LICENSE_EXCEPTIONS.md ./source/neo/extern/nvrhi/LICENSE.txt"
