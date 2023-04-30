22c6df27e302f37537adc4defe0184196891d21c
d10sfan <4337981+d10sfan@users.noreply.github.com>
2023-04-29 20:46:51 -0500

    RBDOOM-3-BFG - Update to 1.5.0 (#914)

--- engines/rbdoom-3-bfg/env.sh
+++ engines/rbdoom-3-bfg/env.sh
@@ -2,5 +2,4 @@
 
 export STEAM_APP_ID_LIST="208200"
 export LICENSE_PATH="./source/LICENSE.md"
-export ADDITIONAL_LICENSES="./source/LICENSE_EXCEPTIONS.md"
-export LIBRARIES="yasm ffmpeg"
+export ADDITIONAL_LICENSES="./source/LICENSE_EXCEPTIONS.md ./source/neo/extern/nvrhi/LICENSE.txt"
