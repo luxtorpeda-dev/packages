6904f3e445144a193194620e3b8f35ac8cd814a1
d10sfan <d10sfan@gmail.com>
2023-05-25 16:44:51 -0500

    openjk - Add support for steam cloud

--- engines/openjk/assets/run-openjo.sh
+++ engines/openjk/assets/run-openjo.sh
@@ -13,4 +13,29 @@ if ! [[ -z "${LUX_STEAM_DECK}" ]]; then
     fi
 fi
 
+if ! [[ -z "${LUX_STEAM_CLOUD}" ]]; then
+    engine_config_dir="$HOME/.local/share/openjo/base"
+    engine_save_dir="$engine_config_dir/saves"
+    cloud_save_dir="$PWD/base/saves"
+    cloud_transfer_run_file="$PWD/luxsteamcloud"
+
+    if [ ! -f "${cloud_transfer_run_file}" ]; then
+        echo "$engine_save_dir"
+        if [ ! -d "${engine_save_dir}" ]; then
+            mkdir -p "$engine_save_dir"
+        fi
+
+        if [ -d "${engine_save_dir}" ]; then
+            cp -rfv "$engine_save_dir"/* "$cloud_save_dir"
+            mv "$engine_save_dir" "$engine_save_dir"-beforelux
+        fi
+
+        ln -s "$cloud_save_dir" "$engine_save_dir"
+        touch "$cloud_transfer_run_file"
+    fi
+
+    mv "$engine_save_dir" "$engine_save_dir"-oldlink
+    ln -s "$cloud_save_dir" "$engine_save_dir"
+fi
+
 ./openjo_sp.x86_64 "$@"
