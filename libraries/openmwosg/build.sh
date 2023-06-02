91238e06068c5c669f629a243189580cf88fbece
d10sfan <d10sfan@gmail.com>
2023-06-02 16:23:17 -0500

    openmw - Updates

--- libraries/openmwosg/build.sh
+++ libraries/openmwosg/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone --recursive https://github.com/OpenMW/osg osg
 pushd osg
-git checkout -f dd803bc
+git checkout -f 76e061739610bc9a3420a59e7c9395e742ce2f97
 git submodule update --init --recursive
 popd
 
@@ -14,9 +14,17 @@ cd build
 cmake \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="$pfx" \
-	-DOSG_USE_QT=OFF \
 	-DBUILD_OSG_APPLICATIONS=OFF \
 	-DBUILD_OSG_EXAMPLES=OFF \
+	-DBUILD_OSG_PLUGINS_BY_DEFAULT=0 \
+	-DBUILD_OSG_PLUGIN_OSG=1 \
+	-DBUILD_OSG_PLUGIN_DDS=1 \
+	-DBUILD_OSG_PLUGIN_DAE=1 \
+	-DBUILD_OSG_PLUGIN_TGA=1 \
+	-DBUILD_OSG_PLUGIN_BMP=1 \
+	-DBUILD_OSG_PLUGIN_JPEG=1 \
+	-DBUILD_OSG_PLUGIN_PNG=1 \
+	-DBUILD_OSG_DEPRECATED_SERIALIZERS=0 \
 	..
 make -j "$(nproc)"
 make install
