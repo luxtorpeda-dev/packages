e27c3c81b17bab1bc8d5ff4f7f0e9d4b5d1e2ac3
d10sfan <d10sfan@gmail.com>
2023-06-04 09:45:23 -0500

    ??

--- engines/openmw/env.sh
+++ engines/openmw/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CFLAGS="$CFLAGS -O2 -g"
-export CXXFLAGS="$CXXFLAGS -O2 -g"
+export CFLAGS="$CFLAGS -O2"
+export CXXFLAGS="$CXXFLAGS -O2"
 export STEAM_APP_ID_LIST="22320"
 export LICENSE_PATH="./source/LICENSE"
 export LIBRARIES="yasm unshield bullet3 mygui openmwosg boost liblz4 ffmpeg qt5"
