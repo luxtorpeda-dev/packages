167986cb170d0dcec8db03c89dd565dbc531c52b
d10sfan <d10sfan@gmail.com>
2023-06-05 15:41:21 -0500

    fsport - Remove copy_only with client 7z support being implemented

--- engines/freespace/build.sh
+++ engines/freespace/build.sh
@@ -7,26 +7,12 @@ git checkout -f 500ee24
 git am < ../patches/0001-narrowing-fix.patch
 popd
 
-git clone https://github.com/p7zip-project/p7zip.git
-pushd p7zip
-git checkout -f a45b883
-popd
-
 # BUILD PHASE
 pushd "source"
 make -j "$(nproc)" FS1=true DEBUG=true PANDORA=false
-popd
-
-pushd "p7zip"
-cp -rfv makefile.linux_amd64_asm makefile.machine
-make all3
-make install DEST_DIR="$pfx"
 popd
 
 # COPY PHASE
 cp -rfv "source/freespace" "$diststart/273600/dist/freespace"
 cp -rfv "assets/run-freespace.sh" "$diststart/273600/dist/"
 cp -rfv "assets/run-fsport.sh" "$diststart/273600/dist/"
-mkdir -p "$diststart/273600/dist/7z/"
-cp -rfv "$pfx/usr/local/bin/"* "$diststart/273600/dist/7z/"
-cp -rfv "$pfx/usr/local/lib/p7zip/"* "$diststart/273600/dist/7z/"
--- engines/freespace/env.sh
+++ engines/freespace/env.sh
@@ -2,5 +2,4 @@
 
 export STEAM_APP_ID_LIST="273600"
 export LICENSE_PATH="./source/README.md"
-export ADDITIONAL_LICENSES="./p7zip/DOC/License.txt ./p7zip/DOC/unRarLicense.txt"
 export LIBRARIES="yasm sdl12compat"
