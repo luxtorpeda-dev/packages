1d9b4924d74a5808fbf2927cd0e56cea516549e8
d10sfan <d10sfan@gmail.com>
2023-06-15 23:29:51 -0500

    runescape - Fix

--- engines/dosbox-staging/env.sh
+++ engines/dosbox-staging/env.sh
@@ -4,3 +4,4 @@ export STEAM_APP_ID_LIST="default 7650 7660 7760 410970 1146370 1812390 1812410 
 export LICENSE_PATH="./source/COPYING"
 export COMMON_PACKAGE="1"
 export ADDITIONAL_LICENSES="./source/subprojects/fluidsynth-2.3.0/LICENSE ./source/subprojects/munt-libmt32emu_2_7_0/LICENSE.build"
+export GCC_9="1"
