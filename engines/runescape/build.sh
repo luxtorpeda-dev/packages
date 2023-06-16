afbc04ef8dbd456673a896a8f9ca913660ca1c90
d10sfan <d10sfan@gmail.com>
2023-06-16 08:21:45 -0500

    runescape - Fix

--- engines/runescape/build.sh
+++ engines/runescape/build.sh
@@ -14,5 +14,6 @@ popd
 # COPY PHASE
 cp -rfv assets/* "$diststart/1343400/dist/"
 mkdir -p "$diststart/1343400/dist/7z/"
+
 cp -rfv "$pfx/usr/local/bin/"* "$diststart/1343400/dist/7z/"
 cp -rfv "$pfx/usr/local/lib/p7zip/"* "$diststart/1343400/dist/7z/"
