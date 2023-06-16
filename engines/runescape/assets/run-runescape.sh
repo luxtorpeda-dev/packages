ac40fd852ad8f9720867fdfe7bc88d7c66c55311
d10sfan <d10sfan@gmail.com>
2023-06-15 23:23:41 -0500

    runescape - Run under steam linux runtime

--- engines/runescape/assets/run-runescape.sh
+++ engines/runescape/assets/run-runescape.sh
@@ -6,4 +6,12 @@ if [ ! -f rl2211 ]; then
     touch rl2211
 fi
 
-./usr/share/games/runescape-launcher/runescape --configURI http://www.runescape.com/k=5/l=\$\(Language:0\)/jav_config.ws
+if [[ ! -z "${DEPPATH_1070560}" ]]; then
+    runtimepath="$DEPPATH_1070560"
+    LD_PRELOAD="" echo "Automatically detected runtimepath at $runtimepath"
+else
+    echo "Steam Linux Runtime is not installed. Please ensure that it is installed and try again." > last_error.txt
+    exit 10
+fi
+
+"$runtimepath/scout-on-soldier-entry-point-v2" --verbose -- ./usr/share/games/runescape-launcher/runescape --configURI http://www.runescape.com/k=5/l=\$\(Language:0\)/jav_config.ws
