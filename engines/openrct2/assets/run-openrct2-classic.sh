9baa89e36b6d33d8ce474f3973f81f8e65dea132
d10sfan <d10sfan@gmail.com>
2023-06-16 10:01:23 -0500

    openrct2 - Fix for classic

--- engines/openrct2/assets/run-openrct2-classic.sh
+++ engines/openrct2/assets/run-openrct2-classic.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [ ! -d "Data" ]; then
-    LD_PRELOAD="" echo "Data directory not found, assuming steam installed it as 'Assets'"
-    LD_PRELOAD="" ln -rsf ./Assets ./Data
-fi
-
 LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./openrct2
