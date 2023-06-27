e085af86196ab4c3e9f73a48f0ef795cda3d4b58
d10sfan <d10sfan@gmail.com>
2023-06-27 15:41:58 -0500

    OpenGothic - Update to v1.0.2207

--- engines/opengothic/build.sh
+++ engines/opengothic/build.sh
@@ -14,7 +14,7 @@ sudo ldconfig
 # CLONE PHASE
 git clone https://github.com/Try/OpenGothic source
 pushd source
-git checkout -f c6dfb5e
+git checkout -f 16fff0e
 git submodule update --init --recursive
 popd
 
