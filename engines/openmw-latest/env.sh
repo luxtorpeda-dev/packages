5b873f6fc059c656dfe4b4be0f7262a8d927dcba
d10sfan <d10sfan@gmail.com>
2023-07-22 16:48:09 -0500

    misc - Remove openmw-latest

--- engines/openmw-latest/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export STEAM_APP_ID_LIST="22320"
-export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./openmw-shaders/README.md"
-export LIBRARIES="yasm unshield bullet3 mygui openmwosg boost liblz4 ffmpeg qt5 yaml-cpp luajit"
-export ARCHIVE_WITHOUT_V7="1"
