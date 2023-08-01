705a0af6ffdd0e1eec1f5e12801038d01a6d87c3
d10sfan <d10sfan@gmail.com>
2023-08-01 15:03:51 -0500

    good-robot - Switch to vcpkg

--- engines/good-robot/build.sh
+++ engines/good-robot/build.sh
@@ -12,7 +12,6 @@ mkdir build
 cd build || exit 1
 cmake \
     -DCMAKE_PREFIX_PATH="$pfx" \
-    -DBOOST_ROOT="$boostlocation" \
     -DCMAKE_INSTALL_PREFIX="$pfx" \
     ..
 make -j "$(nproc)"
@@ -21,5 +20,4 @@ popd
 # COPY PHASE
 cp -rfv source/build/good_robot "$diststart/358830/dist/"
 cp -rfv assets/run-good-robot.sh "$diststart/358830/dist/"
-
 cp -rfv source/steamworks/redistributable_bin/linux64/libsteam_api.so "$diststart/358830/dist/lib"
