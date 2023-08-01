07850369e82f41986339a1269e0ba01aea85ce3e
d10sfan <d10sfan@gmail.com>
2023-08-01 15:18:11 -0500

    ?

--- engines/ironwail/build.sh
+++ engines/ironwail/build.sh
@@ -2,7 +2,7 @@
 
 export CXXFLAGS=-I"$VCPKG_INSTALLED_PATH"/include
 export CFLAGS=-I"$VCPKG_INSTALLED_PATH"/include
-export LDFLAGS=-L"$VCPKG_INSTALLED_PATH"/lib
+export LDFLAGS=-L"$VCPKG_INSTALLED_PATH"/lib/libmad.so
 
 # CLONE PHASE
 git clone https://github.com/andrei-drexler/ironwail.git source
