ede42ec5f30c0a68860156913af6821fedb62f3c
d10sfan <d10sfan@gmail.com>
2023-08-01 16:06:11 -0500

    Lib cleanup

--- libraries/devil/env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export LIBRARY_LICENSES="./devil/LICENSE"
-export LIBRARY_COPY_TO_LIB="$pfx/lib/libIL*.so*"
--- libraries/freealut/env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export LIBRARY_LICENSES="./freealut/COPYING"
-export LIBRARY_COPY_TO_LIB="$pfx/lib/libalut*.so*"
