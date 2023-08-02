eb617ed7c477c6a11d4cefcab93337da6cfb1676
d10sfan <4337981+d10sfan@users.noreply.github.com>
2023-08-01 21:57:24 -0500

    solarus - Switch to vcpkg (#964)

--- engines/solarus/env.sh
+++ engines/solarus/env.sh
@@ -2,6 +2,4 @@
 
 export STEAM_APP_ID_LIST="1393750"
 export LICENSE_PATH="./source/license_gpl.txt"
-export ADDITIONAL_LICENSES="./libmodplug/COPYING"
-export LIBRARIES="physfs luajit"
 export APT_LIBRARIES="libglm-dev"
