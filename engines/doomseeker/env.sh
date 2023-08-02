933ef1436bf69d4f3beaa373b87f3ad6af787d05
d10sfan <d10sfan@gmail.com>
2023-08-01 22:45:21 -0500

    misc - Add xcb for qt5 libs
    [ci skip]

--- engines/doomseeker/env.sh
+++ engines/doomseeker/env.sh
@@ -3,5 +3,5 @@
 export STEAM_APP_ID_LIST="2280 2300"
 export LICENSE_PATH="./source/LICENSE"
 export COMMON_PACKAGE="1"
-export LIBRARIES="qt5 qt5tools"
+export LIBRARIES="xcb qt5 qt5tools"
 export ARCHIVE_WITHOUT_V7="1"
