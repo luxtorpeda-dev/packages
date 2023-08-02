9e74d455cbc1c0fcf913b644f0bd02c0bbc59cd8
d10sfan <4337981+d10sfan@users.noreply.github.com>
2023-08-02 10:52:07 -0500

    perimeter - Switch to vcpkg (#956)

--- engines/perimeter/build.sh
+++ engines/perimeter/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/IonAgorria/Perimeter.git source
 pushd source
-git checkout -f 48e3d63
+git checkout -f 3b06a6f
 git submodule update --init --recursive
 popd
 
@@ -11,7 +11,7 @@ popd
 pushd "source"
 mkdir build
 cd build
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DOPTION_LINKER_LLD=OFF -DCMAKE_PREFIX_PATH="$pfx" -DCMAKE_INSTALL_PREFIX="$pfx"
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DOPTION_LINKER_LLD=OFF -DCMAKE_PREFIX_PATH="$pfx;$VCPKG_INSTALLED_PATH" -DCMAKE_INSTALL_PREFIX="$pfx"
 ninja dependencies
 ninja
 cd Source/dxvk-prefix/src/dxvk-build
@@ -23,5 +23,6 @@ mkdir -p "$diststart/common/dist/lib"
 
 cp -rfv "$pfx/lib"/*.so* "$diststart/common/dist/lib"
 cp -rfv "$pfx/usr/local/lib/x86_64-linux-gnu"/*.so* "$diststart/common/dist/lib"
+
 cp -rfv source/build/Source/perimeter "$diststart/common/dist/"
 cp -rfv assets/* "$diststart/common/dist/"
