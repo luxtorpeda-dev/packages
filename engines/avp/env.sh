b0d8e0accd57ce75a87d2476e264e202615f5b9d
d10sfan <d10sfan@gmail.com>
2023-08-01 22:50:50 -0500

    avp - Switch to yasm from apt

--- engines/avp/env.sh
+++ engines/avp/env.sh
@@ -2,4 +2,5 @@
 
 export STEAM_APP_ID_LIST="3730"
 export LICENSE_PATH="./source/LICENSE"
-export LIBRARIES="sdl12compat yasm libav"
+export LIBRARIES="sdl12compat libav"
+export APT_LIBRARIES="yasm"
