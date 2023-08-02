0b4c240595d8990f4519a2cce6e4244de1777383
d10sfan <d10sfan@gmail.com>
2023-08-01 22:20:58 -0500

    misc - Cleanup

--- engines/wigzdoom/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export STEAM_APP_ID_LIST="1679920"
-export LICENSE_PATH="./source/LICENSE"
-export ADDITIONAL_LICENSES="./LICENSE"
-export COMMON_PACKAGE="1"
-export LIBRARIES="fluidsynth zmusic"
