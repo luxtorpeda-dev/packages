550fb38b8b815f8026a8a597b81e6e3ed22b8413
d10sfan <d10sfan@gmail.com>
2023-08-02 08:56:20 -0500

    ??

--- engines/openmw-tes3mp/build.sh
+++ engines/openmw-tes3mp/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# CLONE PHASE
+git clone https://github.com/TES3MP/openmw-tes3mp.git source
+pushd source
+git checkout -f 6895409
+popd
+
 # COPY PHASE
 cp "assets/tes3mp-launcher.sh" "$diststart/22320/dist/"
