8b2548535f70949f7dba773b6d49b86531e11541
d10sfan <4337981+d10sfan@users.noreply.github.com>
2023-08-02 21:15:34 -0500

    openmw-tes3mp - Switch to vcpkg (#973)
    [ci skip]

--- libraries/luajit/env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export LIBRARY_LICENSES="./luajit/COPYRIGHT"
-export LIBRARY_COPY_TO_LIB="$pfx/usr/local/lib/libluajit-5.1.so*"
--- libraries/unshield/env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export LIBRARY_LICENSES="./unshield/LICENSE"
-export LIBRARY_COPY_TO_LIB="$pfx/lib/*unshield*.so*"
