bb63326c2aab2ee030adbf6d6f49c1d4128a44ef
d10sfan <d10sfan@gmail.com>
2023-08-08 12:34:49 -0500

    good-robot - Add steam cloud support

--- engines/good-robot/assets/run-good-robot.sh
+++ engines/good-robot/assets/run-good-robot.sh
@@ -14,4 +14,32 @@ if ! [[ -z "${LUX_STEAM_DECK}" ]]; then
     fi
 fi
 
+if ! [[ -z "${LUX_STEAM_CLOUD}" ]]; then
+    engine_save_dir="$HOME/.config/Pyrodactyl/Good Robot/"
+    cloud_save_dir="$PWD/save"
+    cloud_transfer_run_file="$PWD/luxsteamcloud"
+
+    if [ ! -f "${cloud_transfer_run_file}" ]; then
+        echo "$engine_save_dir"
+        if [ ! -d "${engine_save_dir}" ]; then
+            mkdir -p "$engine_save_dir"
+        fi
+
+        if [ -d "${cloud_save_dir}" ]; then
+            mkdir -p "$cloud_save_dir"
+        fi
+
+        if [ -d "${engine_save_dir}" ]; then
+            cp -rfv "$engine_save_dir"/* "$cloud_save_dir"
+            mv "$engine_save_dir" "$engine_save_dir"-beforelux
+        fi
+
+        ln -s "$cloud_save_dir" "$engine_save_dir"
+        touch "$cloud_transfer_run_file"
+    fi
+
+    mv "$engine_save_dir" "$engine_save_dir"-oldlink
+    ln -s "$cloud_save_dir" "$engine_save_dir"
+fi
+
 LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./good_robot
