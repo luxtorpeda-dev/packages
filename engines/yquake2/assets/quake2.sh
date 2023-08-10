e402e38c91b8f2b72cfb0b32bc75ff361bd07549
d10sfan <d10sfan@gmail.com>
2023-08-10 13:17:43 -0500

    yquake2 - Support remaster music

--- engines/yquake2/assets/quake2.sh
+++ engines/yquake2/assets/quake2.sh
@@ -23,14 +23,48 @@ rm_default_config () {
 	fi
 }
 
-rm_default_config b69548e2817943cb007f1dd147574900 baseq2/config.cfg
-rm_default_config 11bf27caf2595b540c5b2700e73dc545 rogue/config.cfg
-rm_default_config b5cdf60549ac832497899f739dc89182 xatrix/config.cfg
+rm_default_config edee610dd81f6bf8290a8f93fc80c387 baseq2/config.cfg
 
 ARGS=()
 
 for var in "$@"; do
     [ "$var" != 'D:\data\max' ] && ARGS+=("$var")
 done
+
+mkdir -p baseq2/music
+ln -rsf rerelease/baseq2/music/track02.ogg baseq2/music/track02.ogg
+ln -rsf rerelease/baseq2/music/track03.ogg baseq2/music/track03.ogg
+ln -rsf rerelease/baseq2/music/track04.ogg baseq2/music/track04.ogg
+ln -rsf rerelease/baseq2/music/track05.ogg baseq2/music/track05.ogg
+ln -rsf rerelease/baseq2/music/track06.ogg baseq2/music/track06.ogg
+ln -rsf rerelease/baseq2/music/track07.ogg baseq2/music/track07.ogg
+ln -rsf rerelease/baseq2/music/track08.ogg baseq2/music/track08.ogg
+ln -rsf rerelease/baseq2/music/track09.ogg baseq2/music/track09.ogg
+ln -rsf rerelease/baseq2/music/track10.ogg baseq2/music/track10.ogg
+ln -rsf rerelease/baseq2/music/track11.ogg baseq2/music/track11.ogg
+
+mkdir -p rogue/music
+ln -rsf rerelease/baseq2/music/track17.ogg rogue/music/track02.ogg
+ln -rsf rerelease/baseq2/music/track12.ogg rogue/music/track03.ogg
+ln -rsf rerelease/baseq2/music/track13.ogg rogue/music/track04.ogg
+ln -rsf rerelease/baseq2/music/track14.ogg rogue/music/track05.ogg
+ln -rsf rerelease/baseq2/music/track14.ogg rogue/music/track06.ogg
+ln -rsf rerelease/baseq2/music/track18.ogg rogue/music/track07.ogg
+ln -rsf rerelease/baseq2/music/track15.ogg rogue/music/track08.ogg
+ln -rsf rerelease/baseq2/music/track19.ogg rogue/music/track09.ogg
+ln -rsf rerelease/baseq2/music/track20.ogg rogue/music/track10.ogg
+ln -rsf rerelease/baseq2/music/track21.ogg rogue/music/track11.ogg
+
+mkdir -p xatrix/music
+ln -rsf rerelease/baseq2/music/track09.ogg xatrix/music/track02.ogg
+ln -rsf rerelease/baseq2/music/track12.ogg xatrix/music/track03.ogg
+ln -rsf rerelease/baseq2/music/track13.ogg xatrix/music/track04.ogg
+ln -rsf rerelease/baseq2/music/track07.ogg xatrix/music/track05.ogg
+ln -rsf rerelease/baseq2/music/track14.ogg xatrix/music/track06.ogg
+ln -rsf rerelease/baseq2/music/track02.ogg xatrix/music/track07.ogg
+ln -rsf rerelease/baseq2/music/track15.ogg xatrix/music/track08.ogg
+ln -rsf rerelease/baseq2/music/track03.ogg xatrix/music/track09.ogg
+ln -rsf rerelease/baseq2/music/track04.ogg xatrix/music/track10.ogg
+ln -rsf rerelease/baseq2/music/track16.ogg xatrix/music/track11.ogg
 
 ./quake2 -portable "${ARGS[@]}"
