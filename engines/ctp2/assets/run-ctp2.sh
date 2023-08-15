ca7e24544b9a7712bdc6909675e2b99b11242b71
d10sfan <d10sfan@gmail.com>
2023-08-14 20:40:04 -0500

    civctp2 - Update to latest

--- engines/ctp2/assets/run-ctp2.sh
+++ engines/ctp2/assets/run-ctp2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-ln -rsf /lib/x86_64-linux-gnu/libgtk-3.so.0 ./lib/libgtk-x11-2.0.so.0
-
 cd ctp2_program/ctp
-LD_LIBRARY_PATH="../../lib:$LD_LIBRARY_PATH" LD_PRELOAD="" ./ctp2 -fullscreen
+LD_LIBRARY_PATH="../../lib:$LD_LIBRARY_PATH" ./ctp2 -fullscreen
--- engines/ctp2/env.sh
+++ engines/ctp2/env.sh
@@ -2,6 +2,5 @@
 
 export STEAM_APP_ID_LIST="572050"
 export LICENSE_PATH="./source/EULA.rtf"
-export ADDITIONAL_LICENSES="./source/Activision_CTP2_Source_Code_Readme.txt ./source/Apolyton_CTP2_Source_Code_Readme.txt ./gtk2-compat/COPYING"
-export APT_LIBRARIES="byacc libgtk2.0-dev"
-export LIBRARIES="sdl12compat sdl12image sdl12mixer"
+export ADDITIONAL_LICENSES="./source/Activision_CTP2_Source_Code_Readme.txt ./source/Apolyton_CTP2_Source_Code_Readme.txt"
+export APT_LIBRARIES="byacc"
