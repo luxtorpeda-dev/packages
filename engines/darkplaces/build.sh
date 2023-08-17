3e7acf93153100a6ff041049d8395a20e40a0cfd
d10sfan <d10sfan@gmail.com>
2023-08-17 09:28:24 -0500

    darkplaces - Force re-build

--- engines/darkplaces/build.sh
+++ engines/darkplaces/build.sh
@@ -20,7 +20,6 @@ mkdir -p "$diststart/common/dist/darkplaces/share/quake/mg1"
 
 cp -v source/darkplaces-sdl "$diststart/common/dist/darkplaces/"
 cp -v assets/darkplaces.sh "$diststart/common/dist/darkplaces/"
-
 cp -v assets/default.lux.cfg "$diststart/common/dist/darkplaces/share/quake"
 
 ln -s "../../../../id1/PAK0.PAK" "$diststart/common/dist/darkplaces/share/quake/id1/pak0.pak"
