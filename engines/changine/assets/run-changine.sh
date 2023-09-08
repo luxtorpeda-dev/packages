e95bfc3409f24601c387a4bea072c3ead2823c67
Jpxe <Jpxe@users.noreply.github.com>
2023-09-09 01:30:51 +0200

    change engine to official or other

---
+++ engines/changine/assets/run-changine.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define the app ID
+app_id="32310"
+
+# Check if a command argument was provided
+if [ $# -eq 1 ]; then
+    command_args="$1"
+
+    # Check if the command argument is "official"
+    if [ "$command_args" == "official" ]; then
+        compat_tool="\"\""
+    else
+        compat_tool="$command_args"
+    fi
+else
+    echo "Usage: $0 <compat_tool>"
+    exit 1
+fi
+
+# Construct the Steam command
+steam_command="steam -applaunch $app_id \"steam://nav/console/ +app_change_compat_tool $app_id $compat_tool \\\"\\\" 250\""
+
+# Execute the Steam command
+echo "Launching $app_id with $compat_tool"
+eval "$steam_command"
---
+++ engines/changine/env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="default"
+export COMMON_PACKAGE="1"
