a1b0a439a7eb80304d9deb68a62839fb658c8b2b
d10sfan <d10sfan@gmail.com>
2023-09-08 18:15:36 -0500

    Add support for OSS-Cosmic Amnesia

---
+++ engines/oss-cosmic-amnesia/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/OSS-Cosmic/AmnesiaTheDarkDescent.git source
+pushd source
+git checkout -f 6b387d7
+git submodule update --init --recursive
+popd
+
+# BUILD PHASE
+pushd "source"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=MinSizeRel \
+    ..
+make -j "$(nproc)"
+DESTDIR="$tmp" make install
+popd
+
+# COPY PHASE
+cp -rfv "source/build/"* "$diststart/57300/dist"
+cp -rfv "assets/"* "$diststart/57300/dist"
---
+++ engines/oss-cosmic-amnesia/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="57300"
+export LICENSE_PATH="./source/LICENSE"
+export APT_LIBRARIES="nasm"
