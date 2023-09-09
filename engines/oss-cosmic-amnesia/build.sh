81ed7124493c6fefec79b90e520826be9426466a
d10sfan <d10sfan@gmail.com>
2023-09-08 20:44:30 -0500

    ??

--- engines/oss-cosmic-amnesia/build.sh
+++ engines/oss-cosmic-amnesia/build.sh
@@ -4,17 +4,21 @@
 git clone https://github.com/OSS-Cosmic/AmnesiaTheDarkDescent.git source
 pushd source
 git checkout -f 6b387d7
-ln -rsf "$VCPKG_SRC_PATH" ./vcpkg
 
 git clone https://github.com/OSS-Cosmic/The-Forge.git external/The-Forge
 pushd external/The-Forge
 git checkout -f 80ddbfe
 popd
+
+git clone https://github.com/microsoft/vcpkg.git vcpkg
+pushd vcpkg
+git checkout -f 4a600e9
+popd
 popd
 
 # BUILD PHASE
 pushd "source"
-ls -l
+ls -l vcpkg
 mkdir -p build
 cd build
 cmake \
@@ -28,3 +32,15 @@ popd
 # COPY PHASE
 cp -rfv "source/build/"* "$diststart/57300/dist"
 cp -rfv "assets/"* "$diststart/57300/dist"
+
+mkdir -p licenses
+licensepath="$PWD/licenses"
+pushd ./source/vcpkg_installed/x64-linux/share
+for d in */ ; do
+    directory=${d::-1}
+    echo "$directory"
+    if [ -f "$directory/copyright" ]; then
+        cp -rfv "$d/copyright" "$licensepath/$directory.copyright"
+    fi
+done
+popd
--- engines/oss-cosmic-amnesia/env.sh
+++ engines/oss-cosmic-amnesia/env.sh
@@ -3,3 +3,4 @@
 export STEAM_APP_ID_LIST="57300"
 export LICENSE_PATH="./source/LICENSE"
 export APT_LIBRARIES="nasm"
+export ADDITIONAL_LICENSES="./licenses/*"
