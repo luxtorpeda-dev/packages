90ef5a10d6b27104b4867e045c7d2cd42a8ddd3f
Jpxe <Jpxe@users.noreply.github.com>
2023-09-09 23:29:41 +0200

    autodl

--- engines/daggerfall-unity/assets/run-daggerfall-unity.sh
+++ engines/daggerfall-unity/assets/run-daggerfall-unity.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Constants
 ORIGINALPWD="$PWD"
+MAX_WAIT_SECONDS=300  # Maximum time to wait for the Steam Linux Runtime (adjust as needed)
+STEAM_RUNTIME_PATH="$DEPPATH_1070560"
+IS_TESTING="true"
+TESTING_PATH="$HOME/.local/share/Steam/steamapps/common/SteamLinuxRuntime/"
 
 cd ../
 
@@ -12,46 +17,53 @@ if [ ! -d DFUPDATED ]; then
     LD_PRELOAD="" ln -rsf DF/DFCD/DAGGER/ARENA2/*.VID DFUPDATED/DAGGER/ARENA2
 fi
 
-# Maximum time to wait for installation in seconds (adjust as needed)
-timeout=600  # Change this to your preferred timeout value
+# Function to check if the Steam Linux Runtime is installed
+is_runtime_installed() {
+    if [[ ! -z "${STEAM_RUNTIME_PATH}" && -d "${STEAM_RUNTIME_PATH}" ]]; then
+        return 0  # Runtime found
+    else
+        return 1  # Runtime not found
+    fi
+}
 
-# Check if Steam Linux Runtime 1.0 (scout) is installed
-if [[ ! -z "${DEPPATH_1070560}" ]]; then
-    runtimepath="$DEPPATH_1070560"
-    LD_PRELOAD="" echo "Automatically detected runtimepath at $runtimepath"
-else
-    # Steam Linux Runtime 1.0 (scout) is not installed, so let's install it
-    LD_PRELOAD="" echo "Steam Linux Runtime 1.0 (scout) is not installed. Installing..."
+# Function to install the Steam Linux Runtime
+install_runtime() {
+    echo "Installing the Steam Linux Runtime..."
+    steam "steam://dev/console/ +app_install 1070560"
+}
 
-    # Start the installation in the background
-    steam "steam://dev/console/ +app_install 1070560" &
+# Check if in testing mode and set the runtime path accordingly
+if [[ "$IS_TESTING" == "true" ]]; then
+    STEAM_RUNTIME_PATH="$TESTING_PATH"
+fi
 
-    # Initialize a timer
+# Install the Steam Linux Runtime if not already installed
+if ! is_runtime_installed; then
+    echo "Steam Linux Runtime is not installed. Installing now..."
+    install_runtime
+
+    # Check for the Steam Linux Runtime in a loop
     start_time=$(date +%s)
-
-    # Check for the runtimepath in a loop until the installation is complete or times out
     while true; do
-        # Check if the installation is complete
-        if [[ ! -z "${DEPPATH_1070560}" ]]; then
-            runtimepath="$DEPPATH_1070560"
-            LD_PRELOAD="" echo "Installed Steam Linux Runtime 1.0 (scout) at $runtimepath"
-            break
+        if is_runtime_installed; then
+            break  # Runtime found, exit the loop
+        else
+            current_time=$(date +%s)
+            elapsed_time=$((current_time - start_time))
+
+            if [ "$elapsed_time" -ge "$MAX_WAIT_SECONDS" ]; then
+                echo "Timeout: Steam Linux Runtime installation took too long."
+                exit 1
+            else
+                echo "Waiting for Steam Linux Runtime installation..."
+                sleep 5  # Adjust the sleep interval as needed
+            fi
         fi
-
-        # Check if the timeout has been reached
-        current_time=$(date +%s)
-        elapsed_time=$((current_time - start_time))
-
-        if [[ $elapsed_time -ge $timeout ]]; then
-            echo "Timeout: Steam Linux Runtime 1.0 (scout) installation took too long."
-            echo "Please ensure that it is installed manually and try again." > last_error.txt
-            exit 10
-        fi
-
-        # Sleep for a short duration before checking again
-        sleep 2
     done
 fi
+
+# The Steam Linux Runtime is successfully installed
+echo "Steam Linux Runtime is installed and ready."
 
 if [ ! -f ~/.config/unity3d/Daggerfall\ Workshop/Daggerfall\ Unity/settings.ini ]; then
     if [ ! -d ~/.config/unity3d/Daggerfall\ Workshop/Daggerfall\ Unity ]; then
