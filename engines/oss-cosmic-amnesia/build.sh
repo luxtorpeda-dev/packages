1266e5c803b8d2d90d8d2306dc62bb151a620c8c
d10sfan <d10sfan@gmail.com>
2023-09-08 21:56:52 -0500

    ?

--- engines/oss-cosmic-amnesia/build.sh
+++ engines/oss-cosmic-amnesia/build.sh
@@ -16,6 +16,11 @@ git checkout -f 4a600e9
 popd
 popd
 
+export CXXFLAGS="-I"$VCPKG_INSTALLED_PATH"/include"
+export CFLAGS="-I"$VCPKG_INSTALLED_PATH"/include"
+export LDFLAGS=-L"$VCPKG_INSTALLED_PATH/lib"
+export LIBRARY_PATH="$VCPKG_INSTALLED_PATH/lib"
+
 # BUILD PHASE
 pushd "source"
 ls -l vcpkg
