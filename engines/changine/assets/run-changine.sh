a5472c6de7c68a0f1661ecf7122317ff552d1c39
Jpxe <Jpxe@users.noreply.github.com>
2023-09-09 11:14:46 +0200

    Chaned AppId to SteamAppID env variable

--- engines/changine/assets/run-changine.sh
+++ engines/changine/assets/run-changine.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Define the app ID
-app_id="32310"
-
 # Check if a command argument was provided
 if [ $# -eq 1 ]; then
     command_args="$1"
@@ -20,8 +17,8 @@ else
 fi
 
 # Construct the Steam command
-steam_command="steam -applaunch $app_id \"steam://nav/console/ +app_change_compat_tool $app_id $compat_tool \\\"\\\" 250\""
+steam_command="steam -applaunch $SteamAppId \"steam://nav/console/ +app_change_compat_tool $SteamAppId $compat_tool \\\"\\\" 250\""
 
 # Execute the Steam command
-echo "Launching $app_id with $compat_tool"
+echo "Launching $SteamAppId with $compat_tool"
 eval "$steam_command"
