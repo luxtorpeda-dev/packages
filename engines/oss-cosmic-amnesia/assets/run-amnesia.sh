d1ee1d23179e4a3f69b2c5af9448018f4667f114
d10sfan <d10sfan@gmail.com>
2023-09-08 21:14:10 -0500

    ?

--- engines/oss-cosmic-amnesia/assets/run-amnesia.sh
+++ engines/oss-cosmic-amnesia/assets/run-amnesia.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-rm ./fsport3_6.vp
-rm ./fsport-missions.vp
-rm ./sparky_hi_fs1.vp
-
-LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./fs2_open_x64 "$@"
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./Amnesia "$@"
