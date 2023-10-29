8f819e08b7ceee2fb9ef4af942adfe7ac0d0513f
d10sfan <d10sfan@gmail.com>
2023-10-29 10:27:29 -0500

    ??

--- engines/inter-doom/build.sh
+++ engines/inter-doom/build.sh
@@ -22,5 +22,4 @@ popd
 # COPY PHASE
 mkdir -p "$diststart/common/dist/base"
 cp -rfv source/build/src/inter-* "$diststart/common/dist/"
-cp -rfv "$pfx/base/"* "$diststart/common/dist/base/"
 cp -rfv assets/* "$diststart/common/dist/"
