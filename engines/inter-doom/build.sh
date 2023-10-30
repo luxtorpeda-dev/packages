ec39cc34a964eb6f5bce90b4fe3e9620b5233585
d10sfan <4337981+d10sfan@users.noreply.github.com>
2023-10-29 21:20:41 -0500

    International Doom - Update to 7.2.1 (#1008)

--- engines/inter-doom/build.sh
+++ engines/inter-doom/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/JNechaevsky/inter-doom.git source
 pushd source
-git checkout ed06d70
+git checkout c0e163e
 popd
 
 # BUILD PHASE
@@ -16,11 +16,9 @@ cmake \
     -DBUILD_PORTABLE=ON \
     ..
 make -j "$(nproc)"
-make install
+make
 popd
 
 # COPY PHASE
-mkdir -p "$diststart/common/dist/base"
 cp -rfv source/build/src/inter-* "$diststart/common/dist/"
-cp -rfv "$pfx/base/"* "$diststart/common/dist/base/"
 cp -rfv assets/* "$diststart/common/dist/"
