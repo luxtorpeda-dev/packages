b25b20129fb806e5ef1b0daa9f276e96d3c44fb8
d10sfan <d10sfan@gmail.com>
2023-11-05 20:32:18 -0600

    Add doom

--- engines/eternity/assets/run-eternity.sh
+++ engines/eternity/assets/run-eternity.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+cd eternity
+
+# workaround for libopengl
+mkdir lib
+cp /overrides/lib/i386-linux-gnu/libXext.so.6 lib/libOpenGL.so.0
+
 LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./eternity "$@"
