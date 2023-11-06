0548f51b2e257cb93f5896ec653e7249a010fe04
d10sfan <d10sfan@gmail.com>
2023-11-05 20:33:56 -0600

    ?

--- engines/eternity/assets/run-eternity.sh
+++ engines/eternity/assets/run-eternity.sh
@@ -4,6 +4,6 @@ cd eternity
 
 # workaround for libopengl
 mkdir lib
-cp /overrides/lib/i386-linux-gnu/libXext.so.6 lib/libOpenGL.so.0
+cp /overrides/lib/x86_64-linux-gnu/libXext.so.6 lib/libOpenGL.so.0
 
 LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./eternity "$@"
