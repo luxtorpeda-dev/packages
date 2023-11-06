20e58d0e1d08a6dc7f60f748b0c215344d9660e3
d10sfan <d10sfan@gmail.com>
2023-11-05 20:49:22 -0600

    ?

--- engines/eternity/assets/run-eternity.sh
+++ engines/eternity/assets/run-eternity.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ -z "${SDL_SOUNDFONTS}" ]]; then
+    echo "Setting default soundfont"
+    export SDL_SOUNDFONTS="fluid-soundfont-3.1/FluidR3_GM.sf2"
+fi
+
+
 cd eternity
 
 # workaround for libopengl
