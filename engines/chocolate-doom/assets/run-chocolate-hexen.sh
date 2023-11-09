4e261c8a74964f484981290a20493b7ef16e9261
d10sfan <d10sfan@gmail.com>
2023-11-08 20:43:09 -0600

    chocolate-doom - Add support for steam cloud

--- engines/chocolate-doom/assets/run-chocolate-hexen.sh
+++ engines/chocolate-doom/assets/run-chocolate-hexen.sh
@@ -3,4 +3,33 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export LD_LIBRARY_PATH="$DIR/lib:$LD_LIBRARY_PATH"
-"$DIR/chocolate-hexen" "$@"
+
+cur_args=($@)
+new_args=()
+
+one_after=""
+
+for var in ${cur_args[@]}
+do
+    tmp_arg="${var//\\//}"
+
+    if [ "$one_after" = "1" ]; then
+        tmp_arg=""
+        one_after=""
+    fi
+
+    if [[ $tmp_arg == *"-savedir"* ]]; then
+        if [[ -z "${LUX_STEAM_CLOUD}" ]]; then
+            # if steam cloud is disabled
+            tmp_arg=""
+            one_after="1"
+        fi
+    fi
+
+    if [[ -n $tmp_arg ]]; then
+        new_args+=("$tmp_arg")
+    fi
+done
+
+echo "Launching with, ${new_args[@]}"
+"$DIR/chocolate-hexen" "${new_args[@]}"
