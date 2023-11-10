c4ab677aadc1765cc3d0909235c203231dbf6227
d10sfan <d10sfan@gmail.com>
2023-11-10 15:06:04 -0600

    gzdoom - Add steam cloud support for hedon

--- engines/gzdoom/assets/run-gzdoom-hedon.sh
+++ engines/gzdoom/assets/run-gzdoom-hedon.sh
@@ -4,4 +4,14 @@ if [ ! -f gzdoom.ini ]; then
     cp -rfv ./hedon_template.ini ./gzdoom.ini
 fi
 
+if ! [[ -z "${LUX_STEAM_CLOUD}" ]]; then
+    cloud_transfer_run_file="$PWD/luxsteamcloud"
+
+    if [ ! -f "${cloud_transfer_run_file}" ]; then
+        cp -rfv ./saves/* Save
+        mv ./saves ./saves-presteamcloud
+        ln -rsf ./Save ./saves
+    fi
+fi
+
 LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./gzdoom "$@"
