1dfa02d6d165bf0416fcd19042dc4b97819048ad
d10sfan <d10sfan@gmail.com>
2023-11-10 22:01:14 -0600

    quake4 - Fix

--- engines/quake4/build.sh
+++ engines/quake4/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # COPY PHASE
-cp -rfv "$pfx/lib/libSDL-1.2.so.1.2.52" "$diststart/2210/dist/libSDL-1.2.id.so.0"
+cp -rfv "$pfx/lib/libSDL-1.2.so.1.2.60" "$diststart/2210/dist/libSDL-1.2.id.so.0"
 cp -rfv "assets/run-quake4.sh" "$diststart/2210/dist/"
 cp -rfv "assets/setup-quake4.sh" "$diststart/2210/dist/"
 cp -rfv "assets/uninstall-quake4.sh" "$diststart/2210/dist/"
