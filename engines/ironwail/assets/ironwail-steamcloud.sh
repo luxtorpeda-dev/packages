6edf4c6fbad52456600ee898205af6a1ae4edc27
d10sfan <d10sfan@gmail.com>
2023-11-10 21:59:46 -0600

    ironwail - Fix

--- engines/ironwail/assets/ironwail-steamcloud.sh
+++ engines/ironwail/assets/ironwail-steamcloud.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-echo "Running non re-release"
-LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./ironwail/lib:./lib" ./ironwail/ironwail -basedir . "$@"
+echo "Running steamcloud"
+
+ln -rsf id1/PAK0.PAK id1/pak0.pak
+ln -rsf id1/PAK1.PAK id1/pak1.pak
+
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./ironwail/lib:./lib" ./ironwail/ironwail "$@"
