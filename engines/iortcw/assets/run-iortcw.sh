2ec513656335fcbafe046c5fea3a67eebfcfcd2a
d10sfan <d10sfan@gmail.com>
2023-11-10 20:38:16 -0600

    iortcw - Add support for steam cloud

--- engines/iortcw/assets/run-iortcw.sh
+++ engines/iortcw/assets/run-iortcw.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-if ! [[ -z "${LUX_STEAM_DECK}" ]]; then
-    if [ ! -f ~/.wolf/Main/wolfconfig.cfg ]; then
-        if [ ! -d ~/.wolf/Main ]; then
-            mkdir -p ~/.wolf/Main
+if ! [[ -z "${LUX_STEAM_CLOUD}" ]]; then
+    if ! [[ -z "${LUX_STEAM_DECK}" ]]; then
+        if [ ! -f Main/wolfconfig.cfg ]; then
+            cp -rfv ./wolfconfig-template.cfg Main/wolfconfig.cfg
         fi
-
-        cp -rfv ./wolfconfig-template.cfg ~/.wolf/Main/wolfconfig.cfg
     fi
-fi
 
-./iowolfsp.x86_64 "$@"
+    ./iowolfsp.x86_64 "$@" +set fs_homepath "$PWD"
+else
+    if ! [[ -z "${LUX_STEAM_DECK}" ]]; then
+        if [ ! -f ~/.wolf/Main/wolfconfig.cfg ]; then
+            if [ ! -d ~/.wolf/Main ]; then
+                mkdir -p ~/.wolf/Main
+            fi
+
+            cp -rfv ./wolfconfig-template.cfg ~/.wolf/Main/wolfconfig.cfg
+        fi
+    fi
+
+    ./iowolfsp.x86_64 "$@"
+fi
