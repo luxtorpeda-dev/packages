cf9aa8b853a1b9e803bc91a97330059d4e619948
d10sfan <d10sfan@gmail.com>
2023-11-15 20:46:17 -0600

    inter-doom - Remove hexen & heretic, no longer supported

--- engines/inter-doom/assets/run-inter-heretic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-export LD_LIBRARY_PATH="$DIR/lib:$LD_LIBRARY_PATH"
-"$DIR/inter-heretic" -config "$DIR/inter-heretic.ini" "$@"
