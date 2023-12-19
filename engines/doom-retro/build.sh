d5293e8672287b02a5a504e21302556132233450
d10sfan <d10sfan@gmail.com>
2023-12-18 21:49:53 -0600

    doom-retro - Force re-build

--- engines/doom-retro/build.sh
+++ engines/doom-retro/build.sh
@@ -19,4 +19,5 @@ popd
 # COPY PHASE
 cp -rfv "source/build/doomretro" "$diststart/common/dist/"
 cp -rfv "source/build/doomretro.wad" "$diststart/common/dist/"
+
 cp -rfv assets/* "$diststart/common/dist/"
