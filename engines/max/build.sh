1cda69fe560be420b9ef9d086ed3d6adea600a37
d10sfan <d10sfan@gmail.com>
2024-01-24 19:29:28 -0600

    Add max

---
+++ engines/max/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export LDFLAGS=-L"$VCPKG_INSTALLED_PATH/lib"
+export LIBRARY_PATH="$VCPKG_INSTALLED_PATH/lib"
+
+# CLONE PHASE
+git clone https://github.com/klei1984/max.git source
+pushd source
+git checkout 0317272
+popd
+
+# BUILD PHASE
+pushd "source"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DCMAKE_PREFIX_PATH="$VCPKG_INSTALLED_PATH" \
+    ..
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
+cp -rfv source/build/max "$diststart/615250/dist/"
+cp -rfv assets/* "$diststart/615250/dist/"
---
+++ engines/max/env.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="615250"
+export LICENSE_PATH="./source/LICENSE"
