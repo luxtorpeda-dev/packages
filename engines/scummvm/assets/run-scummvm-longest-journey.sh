0adabbcbd602efa92fe923c69100ca9f5b15fa53
d10sfan <d10sfan@gmail.com>
2024-02-05 22:41:19 -0600

    scummvm - Add longest journey version

---
+++ engines/scummvm/assets/run-scummvm-longest-journey.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+GAME="$1"
+OPTIONS="$2"
+INIPATH="scummvm.ini"
+
+cd "$DIR"
+
+if [ ! -f "$INIPATH" ]; then
+    echo "Creating $INIPATH"
+    echo -e "[scummvm]\ngfx_mode=opengl" > "$INIPATH"
+fi
+
+if [[ -d "../Original" ]]; then
+    echo "Assuming original path for ScummVM"
+    PATH_ARG="--path=../Original"
+elif [[ $DIR == *"ScummVM_Windows"* ]]; then
+    echo "Running parent path"
+    PATH_ARG="--path=../../"
+else
+    echo "Running normal path"
+    PATH_ARG="--path=../"
+fi
+
+ln -rsf ./share/scummvm/shaders ../shaders
+
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/scummvm -c "$INIPATH" --add $PATH_ARG
+
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./bin/scummvm -c "$INIPATH" --fullscreen --themepath=./share/scummvm $OPTIONS $GAME
