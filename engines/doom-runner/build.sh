d2288d24e7a0999dc171abdcb36cb5b94b18c1ec
d10sfan <d10sfan@gmail.com>
2024-02-05 22:37:04 -0600

    Doom Runner - Update to v1.8.2

--- engines/doom-runner/build.sh
+++ engines/doom-runner/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/Youda008/DoomRunner.git source
 pushd source
-git checkout -f e402d23
+git checkout -f cad2dcf
 popd
 
 export CXXFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas"
