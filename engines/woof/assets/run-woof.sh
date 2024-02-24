2fc44f7466340af13121cd9bc312737887c819d0
d10sfan <d10sfan@gmail.com>
2024-02-24 17:11:22 -0600

    wolf - Fix

--- engines/woof/assets/run-woof.sh
+++ engines/woof/assets/run-woof.sh
@@ -4,7 +4,7 @@ cd ./woof
 
 if [[ -z "${SDL_SOUNDFONTS}" ]]; then
     echo "Setting default soundfont"
-    export SDL_SOUNDFONTS="fluid-soundfont-3.1/FluidR3_GM.sf2"
+    export SDL_SOUNDFONTS="soundfonts/TimGM6mb.sf2"
 fi
 
 export LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH"
