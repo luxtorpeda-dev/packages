98c9b8530941cbae20c5d78b95b90591fab9463b
d10sfan <4337981+d10sfan@users.noreply.github.com>
2024-02-24 21:58:33 -0600

    Add sc2 (#1096)

---
+++ engines/sc2/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://git.code.sf.net/p/sc2/uqm source
+pushd source
+git checkout 102099
+popd
+
+# BUILD PHASE
+cp -rfv ./config.state source/sc2
+pushd "source/sc2"
+./build.sh uqm reprocess_config
+./build.sh uqm
+./build.sh uqm install
+popd
+
+# COPY PHASE
+cp -v /usr/local/games/lib/uqm/uqm "$diststart/2645580/dist/"
+cp -v assets/sc2.sh "$diststart/2645580/dist/"
---
+++ engines/sc2/env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export STEAM_APP_ID_LIST="2645580"
+export LICENSE_PATH="./source/sc2/COPYING"
