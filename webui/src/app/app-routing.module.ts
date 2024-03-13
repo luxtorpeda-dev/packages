abe4d95fb60898fa301c3c1e1aed031220834717
d10sfan <d10sfan@gmail.com>
2024-03-13 14:19:17 -0500

    webui - Add client page

--- webui/src/app/app-routing.module.ts
+++ webui/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PackagesComponent} from './packages/packages.component'
+import {HomeComponent} from './home/home.component'
 
 const routes: Routes = [
-  { path: '**', component: PackagesComponent },
+  { path: 'packages', component: PackagesComponent },
+  { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PackagesComponent } from './packages/packages.component';
+import { HomeComponent } from './home/home.component';
 
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatIconModule} from '@angular/material/icon';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import { MarkdownModule } from 'ngx-markdown';
+import {MatButtonModule} from '@angular/material/button';
+import { HttpClientModule, HttpClient } from  '@angular/common/http';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PackagesComponent
+    PackagesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +31,11 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     MatIconModule,
     MatChipsModule,
     MatSidenavModule,
-    MatTooltipModule
+    MatTooltipModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({ loader: HttpClient }),
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
