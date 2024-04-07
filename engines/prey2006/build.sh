aceeeb23d36b9656ae8f444db138a2d84cd65bcb
d10sfan <d10sfan@gmail.com>
2024-04-07 13:34:48 -0500

    Remove prey2006 & quake4 native, since proton is better for those (for ut games the native versions in lux are better than proton); will be replaced with the wip dii4a

--- engines/prey2006/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# COPY PHASE
-cp -rfv "$pfx/lib/libSDL-1.2.so.1.2.52" "$diststart/3970/dist/libSDL-1.2.so.0"
-cp -rfv "assets/run-prey.sh" "$diststart/3970/dist/"
-cp -rfv "assets/setup-prey.sh" "$diststart/3970/dist/"
-cp -rfv "assets/uninstall-prey.sh" "$diststart/3970/dist/"
