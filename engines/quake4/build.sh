2bf70d2e40947f5511c3bcb9310e02d5ef084aab
d10sfan <4337981+d10sfan@users.noreply.github.com>
2024-04-08 10:27:45 -0500

    Add dii4a for quake 4 & prey 2006 (#1175)
    * Remove prey2006 & quake4 native, since proton is better for those (for ut games the native versions in lux are better than proton); will be replaced with the wip dii4a
    
    * Add support for dii4a
    
    * ?
    
    * Fix

--- engines/quake4/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# COPY PHASE
-cp -rfv "$pfx/lib/libSDL-1.2.so.1.2.60" "$diststart/2210/dist/libSDL-1.2.id.so.0"
-cp -rfv "assets/run-quake4.sh" "$diststart/2210/dist/"
-cp -rfv "assets/setup-quake4.sh" "$diststart/2210/dist/"
-cp -rfv "assets/uninstall-quake4.sh" "$diststart/2210/dist/"
-cp -rfv "assets/Quake4Config.cfg" "$diststart/2210/dist/"
