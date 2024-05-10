f52739bed4c612a7d833baf1d683acb0d46b3ade
d10sfan <4337981+d10sfan@users.noreply.github.com>
2024-05-10 13:24:39 -0500

    Fixes

--- engines/alephone/build.sh
+++ engines/alephone/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+sudo apt-get -y install autoconf-archive
+
+export CXXFLAGS=-I"$VCPKG_INSTALLED_PATH"/include
+export CFLAGS=-I"$VCPKG_INSTALLED_PATH"/include
+export LDFLAGS=-L"$VCPKG_INSTALLED_PATH/lib"
+export LIBRARY_PATH="$VCPKG_INSTALLED_PATH/lib"
+
 # CLONE PHASE
 git clone https://github.com/Aleph-One-Marathon/alephone.git source
 pushd source
@@ -8,10 +15,12 @@ git submodule update --init --recursive
 popd
 
 # BUILD PHASE
+pushd source
 autoreconf -i
-./configure
-make dist
+./configure --with-boost-libdir="$VCPKG_INSTALLED_PATH/lib"
+make
 popd
 
 # COPY PHASE
 cp -rfv assets/* "$diststart/2398450/dist/"
+cp -rfv source/Source_files/alpehone "$diststart/2398450/dist/"
