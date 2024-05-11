0bb6e21a43cfc68bbccb3fb1d5d74c9702597cdf
d10sfan <4337981+d10sfan@users.noreply.github.com>
2024-05-10 21:16:56 -0500

    Add Descent3 (#1208)
    * Add descent3
    
    * Update build.sh
    
    * Update packagessniper_v2.json
    
    * Update run-descent3.sh
    
    * Update run-descent3.sh

---
+++ engines/descent3/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/DescentDevelopers/Descent3.git source
+pushd source
+git checkout -f "$COMMIT_HASH"
+git submodule update --init --recursive
+popd
+
+export CXXFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas -I"$VCPKG_INSTALLED_PATH"/include"
+export CFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas -I"$VCPKG_INSTALLED_PATH"/include"
+export LDFLAGS=-L"$VCPKG_INSTALLED_PATH/lib"
+export LIBRARY_PATH="$VCPKG_INSTALLED_PATH/lib"
+
+# BUILD PHASE
+pushd "source"
+cmake --preset linux -D ENABLE_LOGGER=OFF -DCMAKE_PREFIX_PATH="$VCPKG_INSTALLED_PATH"
+cmake --build --preset linux --config Release
+popd
+
+# COPY PHASE
+cp -rfv "assets"/* "$diststart/273590/dist"
+cp -rfv source/builds/linux/Descent3/Release/Descent3 "$diststart/273590/dist/Descent3-lux"
