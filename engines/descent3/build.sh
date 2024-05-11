4e77df063885082669141457c8c443d9a961b666
d10sfan <4337981+d10sfan@users.noreply.github.com>
2024-05-10 21:01:44 -0500

    Update build.sh

--- engines/descent3/build.sh
+++ engines/descent3/build.sh
@@ -10,13 +10,14 @@ popd
 export CXXFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas -I"$VCPKG_INSTALLED_PATH"/include"
 export CFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas -I"$VCPKG_INSTALLED_PATH"/include"
 export LDFLAGS=-L"$VCPKG_INSTALLED_PATH/lib"
-export LIBRARY_PATH="$VCPKG_INSTALLED_PATH/lib;$pfx/lib"
+export LIBRARY_PATH="$VCPKG_INSTALLED_PATH/lib"
 
 # BUILD PHASE
 pushd "source"
-cmake --preset linux -D ENABLE_LOGGER=OFF
+cmake --preset linux -D ENABLE_LOGGER=OFF -DCMAKE_PREFIX_PATH="$VCPKG_INSTALLED_PATH"
 cmake --build --preset linux --config Release
 popd
 
 # COPY PHASE
 cp -rfv "assets"/* "$diststart/273590/dist"
+cp -rfv source/builds/linux/Descent3/Release/Descent3 "$diststart/273590/dist/Descent3-lux"
