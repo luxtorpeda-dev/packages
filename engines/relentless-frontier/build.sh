eb7f90dc07997b12747ee391630b27ab1f373eca
d10sfan <d10sfan@gmail.com>
2024-08-01 15:39:06 -0500

    Add RFGZDoom

---
+++ engines/relentless-frontier/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# From https://gitlab.com/luxtorpeda/packages/gzdoom - See LICENSE file for more information
+# CLONE PHASE
+git clone git@github.com:Waffle-Iron-Studios/Griddle.git source
+pushd source
+git checkout "$COMMIT_HASH"
+popd
+
+# BUILD PHASE
+pushd "source"
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    ..
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
+cp -rfv "source/build"/{rfgzdoom,soundfonts,*.pk3} "$diststart/common/dist/"
+cp -rfv assets/* "$diststart/common/dist/"
+
+ln -rsf "$diststart/common/dist/lib/libfluidsynth.so.3" "$diststart/common/dist/lib/libfluidsynth.so.2"
