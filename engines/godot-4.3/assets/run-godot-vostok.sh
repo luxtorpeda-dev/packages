6ef6bc9ec1e1b20ceabbe267898f488a59afbfab
Ryhon <ryhon@ryhn.link>
2024-10-13 02:48:17 +0200

    Godot 4.3 (#1353)
    * Godot 4.3
    
    * Fix logo license path
    
    * Update packagessniper_v2.json
    
    * updates
    
    * Update run-godot-vostok.sh
    
    ---------
    
    Co-authored-by: d10sfan <d10sfan@gmail.com>

---
+++ engines/godot-4.3/assets/run-godot-vostok.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -f "last_error.txt" ]; then
+    rm last_error.txt
+fi
+
+pckname="Public_Demo_2_v2"
+
+# Check if pck file exists and launch game if it does.
+if [ -n "$pckname.pck" ]; then
+    ./godot.linuxbsd.template_release.x86_64 --main-pack "$pckname.pck" $@
+else
+    error_message="Couldn't find a .pck file."
+    echo "$error_message" > last_error.txt
+    exit 10
+fi
