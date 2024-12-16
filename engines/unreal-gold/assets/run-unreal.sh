b2a9feddb9b0d86722d202c4c1af9340c063241f
d10sfan <4337981+d10sfan@users.noreply.github.com>
2024-12-15 20:08:11 -0600

    unreal-gold - Switch to 227k_12 (#1436)

--- engines/unreal-gold/assets/run-unreal.sh
+++ engines/unreal-gold/assets/run-unreal.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-if [ -f "last_error.txt" ]; then
-    rm last_error.txt
-fi
-
-if [ ! -f ../System/UnrealLinux.bin ]
-then
-    error_message="Unreal Linux not found. Please see System/README.txt for instructions in game directory."
-    echo "$error_message" > last_error.txt
-    exit 10
-fi
-
 chmod +x ../System/UnrealLinux.bin
 cd ../System
 LD_LIBRARY_PATH="../System/lib:$LD_LIBRARY_PATH" ./UnrealLinux.bin -log
