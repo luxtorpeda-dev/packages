825e243afedc3ece4e1cd56062ca7ef45735222e
d10sfan <4337981+d10sfan@users.noreply.github.com>
2024-12-15 20:13:42 -0600

    unreal-gold - Fix

--- engines/unreal-gold-64/assets/run-unreal-64.sh
+++ engines/unreal-gold-64/assets/run-unreal-64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-chmod +x ../System64/UnrealLinux.bin
+chmod +x ../System64/unreal-bin-amd64
 cd ../System64
-LD_LIBRARY_PATH="../System/lib:$LD_LIBRARY_PATH" ./UnrealLinux.bin -log
+LD_LIBRARY_PATH="../System/lib:$LD_LIBRARY_PATH" ./unreal-bin-amd64 -log
--- engines/unreal-gold/assets/run-unreal.sh
+++ engines/unreal-gold/assets/run-unreal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-chmod +x ../System/UnrealLinux.bin
+chmod +x ../System/unreal-bin-x86
 cd ../System
-LD_LIBRARY_PATH="../System/lib:$LD_LIBRARY_PATH" ./UnrealLinux.bin -log
+LD_LIBRARY_PATH="../System/lib:$LD_LIBRARY_PATH" ./unreal-bin-x86 -log
