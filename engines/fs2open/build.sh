3cd1a5956f2a63e9e6f8a2be7417d948bf140479
d10sfan <d10sfan@gmail.com>
2025-03-14 21:48:29 -0500

    fs2open - Update to release_24_2_0

--- engines/fs2open/build.sh
+++ engines/fs2open/build.sh
@@ -3,7 +3,7 @@
 # CLONE PHASE
 git clone https://github.com/scp-fs2open/fs2open.github.com source
 pushd source
-git checkout -f "$COMMIT_HASH"
+git checkout -f "$COMMIT_TAG"
 git submodule update --init --recursive
 popd
 
@@ -22,5 +22,5 @@ DESTDIR="$tmp" make install
 popd
 
 # COPY PHASE
-cp -rfv "source/build/bin/fs2_open_24_0_0_x64" "$diststart/273620/dist/fs2_open_x64"
+cp -rfv "source/build/bin/fs2_open_*_x64" "$diststart/273620/dist/fs2_open_x64"
 cp -rfv "assets/run-freespace2.sh" "$diststart/273620/dist/run-freespace2.sh"
