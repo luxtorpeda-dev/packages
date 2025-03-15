96541e5dcf581976cbddc08f8934b4a5ab759a26
d10sfan <d10sfan@gmail.com>
2025-03-14 21:57:00 -0500

    nwjs - Avoid cloning repo just for license

--- engines/nwjs/build.sh
+++ engines/nwjs/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # CLONE PHASE
-git clone https://github.com/nwjs/nw.js.git source
-pushd source
-git checkout "$COMMIT_HASH"
-popd
+wget https://raw.githubusercontent.com/nwjs/nw.js/refs/heads/nw97/LICENSE
 
 # COPY PHASE
 cp -rfv assets/* "$diststart/common/dist/"
