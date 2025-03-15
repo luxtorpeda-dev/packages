a7e27a108639541b92cbdb909f96b49460fbee02
d10sfan <d10sfan@gmail.com>
2025-03-15 12:17:38 -0500

    Update build.sh

--- engines/gardensofkadesh/build.sh
+++ engines/gardensofkadesh/build.sh
@@ -12,17 +12,6 @@ meson setup build
 meson compile -C build
 popd
 
-pushd "source"
-cp -rfv build/src/ThirdParty/CRC/libhw_crc.a src/ThirdParty/CRC/libhw_CRC.a
-cp -rfv build/src/ThirdParty/LZSS/libhw_lzss.a src/ThirdParty/LZSS/libhw_LZSS.a
-
-cd tools/biggie
-./biggie-Linux-compile.sh
-cd ../../HomeworldSDL_big
-./convert_directory_to_big_file
-popd
-
 # COPY PHASE
 cp -rfv "source/build/homeworld" "$diststart/244160/dist/"
-cp -rfv "source/HomeworldSDL.big" "$diststart/244160/dist/"
 cp -rfv assets/* "$diststart/244160/dist/"
