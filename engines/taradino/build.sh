679f98f373ca2a3f1ab6c02152825f68994e90f6
d10sfan <d10sfan@gmail.com>
2025-03-15 19:48:25 -0500

    Switch to taradino for rott

---
+++ engines/taradino/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/fabiangreffrath/taradino.git source
+pushd source
+git checkout "$COMMIT_HASH"
+popd
+
+# BUILD PHASE
+pushd "source"
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    ..
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
+cp -rfv source/build/rott "$diststart/238050/dist/"
+cp -rfv assets/* "$diststart/238050/dist/"
+
+cp -rfv source/build/rott "$diststart/358410/dist/"
+cp -rfv assets/* "$diststart/358410/dist/"
