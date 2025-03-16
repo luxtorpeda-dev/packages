dc965d6f95f7f87b00563ce2c63cf55e22d1cdbf
d10sfan <4337981+d10sfan@users.noreply.github.com>
2025-03-15 20:02:09 -0500

    Switch to taradino for rott (#1576)
    * Switch to taradino for rott
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh

---
+++ engines/taradino/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/fabiangreffrath/taradino.git source
+pushd source
+git checkout "$COMMIT_HASH"
+popd
+
+# BUILD PHASE
+pushd "source"
+mkdir build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    ..
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
+cp -rfv source/build/taradino "$diststart/238050/dist/rott"
+cp -rfv assets/* "$diststart/238050/dist/"
+
+
+cp -rfv source/build/taradino "$diststart/358410/dist/rott"
+cp -rfv assets/* "$diststart/358410/dist/"
