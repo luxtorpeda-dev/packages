08b81056cf7212b22322cd4b7e72f62c5e86b45d
d10sfan <d10sfan@gmail.com>
2025-03-31 20:00:27 -0500

    ?

--- engines/project-alice/build.sh
+++ engines/project-alice/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export LIBRARY_PATH="$VCPKG_INSTALLED_PATH/lib"
+export LD_LIBRARY_PATH="$VCPKG_INSTALLED_PATH/lib:$LD_LIBRARY_PATH"
+
 # CLONE PHASE
 git clone https://github.com/schombert/Project-Alice.git source
 pushd source
@@ -8,8 +11,9 @@ popd
 
 # BUILD PHASE
 pushd source
-cmake -DCMAKE_BUILD_TYPE=Release -B build
+cmake -DCMAKE_PREFIX_PATH="$VCPKG_INSTALLED_PATH" -DCMAKE_BUILD_TYPE=Release -B build
 cmake --build build --parallel --target AliceIncremental
 popd
 
 # COPY PHASE
+cp -rfv source/assets "$diststart/42960/dist/"
