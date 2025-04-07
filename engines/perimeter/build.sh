4c31346b6b29eb6e33e7a30c324f6db2c6407c27
David <d10sfan@gmail.com>
2025-04-07 13:56:59 -0500

    ?

--- engines/perimeter/build.sh
+++ engines/perimeter/build.sh
@@ -4,6 +4,7 @@ export CXXFLAGS=-I"$VCPKG_INSTALLED_PATH"/include
 export CFLAGS=-I"$VCPKG_INSTALLED_PATH"/include
 export LDFLAGS=-L"$VCPKG_INSTALLED_PATH/lib"
 export LIBRARY_PATH="$VCPKG_INSTALLED_PATH/lib"
+export LDFLAGS="-ldl"
 
 # CLONE PHASE
 git clone https://github.com/IonAgorria/Perimeter.git source
@@ -20,14 +21,14 @@ cmake .. \
     -G Ninja -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH="$pfx;$VCPKG_INSTALLED_PATH" \
     -DCMAKE_INSTALL_PREFIX="$pfx" \
-    -DOPTION_LINKER=lld \
     -DOPTION_LINK_LIBS="stdc++fs;pthread;dl" \
     -DOPTION_STATIC_LIB_STD=ON \
     -DOPTION_DEBUG_ASSERT=OFF \
     -DOPTION_DXVK_1=ON \
     -DCMAKE_INSTALL_DO_STRIP=OFF \
     -DCMAKE_INSTALL_RPATH="\$ORIGIN" \
-    -DCMAKE_EXE_LINKER_FLAGS="-ldl"
+    -DCMAKE_EXE_LINKER_FLAGS="-Wl,--start-group -Wl,--end-group" \
+    -DCMAKE_CXX_LINK_EXECUTABLE="<CMAKE_CXX_COMPILER> <CMAKE_CXX_LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES> -ldl" \
 ninja dependencies
 ninja
 cd Source/dxvk-prefix/src/dxvk-build
