15f2f5f6f619a398b002ce8944b2ca9046a2b575
David <d10sfan@gmail.com>
2025-04-07 13:40:47 -0500

    perimeter - Update to 3.1.9

--- engines/perimeter/build.sh
+++ engines/perimeter/build.sh
@@ -3,15 +3,17 @@
 # CLONE PHASE
 git clone https://github.com/IonAgorria/Perimeter.git source
 pushd source
-git checkout -f "$COMMIT_HASH"
+git checkout -f "$COMMIT_TAG"
 git submodule update --init --recursive
 popd
+
+export LDFLAGS="-ldl"
 
 # BUILD PHASE
 pushd "source"
 mkdir build
 cd build
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DOPTION_LINKER_LLD=OFF -DCMAKE_PREFIX_PATH="$pfx;$VCPKG_INSTALLED_PATH" -DCMAKE_INSTALL_PREFIX="$pfx"
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DOPTION_LINKER_LLD=OFF -DCMAKE_PREFIX_PATH="$pfx;$VCPKG_INSTALLED_PATH" -DCMAKE_INSTALL_PREFIX="$pfx" -DCMAKE_EXE_LINKER_FLAGS="-Wl,--start-group -Wl,--end-group" -DCMAKE_CXX_LINK_EXECUTABLE="<CMAKE_CXX_COMPILER> <CMAKE_CXX_LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES> -ldl"
 ninja dependencies
 ninja
 cd Source/dxvk-prefix/src/dxvk-build
