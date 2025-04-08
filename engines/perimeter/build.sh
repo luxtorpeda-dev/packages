c1f51292256aa4e3af54a84784a49591f54e3b32
David <d10sfan@gmail.com>
2025-04-07 19:13:56 -0500

    Update build.sh

--- engines/perimeter/build.sh
+++ engines/perimeter/build.sh
@@ -31,8 +31,7 @@ cmake .. \
     -DCMAKE_CXX_LINK_EXECUTABLE="<CMAKE_CXX_COMPILER> <CMAKE_CXX_LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES> -ldl" \
 ninja dependencies
 ninja
-cd Source/dxvk-prefix/src/dxvk-build
-DESTDIR="$pfx" ninja install
+cmake --install . --prefix "$pfx"
 popd
 
 # COPY PHASE
@@ -41,5 +40,5 @@ mkdir -p "$diststart/common/dist/lib"
 cp -rfv "$pfx/lib"/*.so* "$diststart/common/dist/lib"
 cp -rfv "$pfx/usr/local/lib/x86_64-linux-gnu"/*.so* "$diststart/common/dist/lib"
 
-cp -rfv source/build/Source/perimeter "$diststart/common/dist/"
+cp -rfv "$pfx/perimeter" "$diststart/common/dist/"
 cp -rfv assets/* "$diststart/common/dist/"
