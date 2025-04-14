1411c9830ecb01c6bc535b9e607f75fcf36a1790
David <d10sfan@gmail.com>
2025-04-13 21:00:36 -0500

    openra - Update tiberan hd to latest

--- engines/openra/assets/run-openra-tiberian.sh
+++ engines/openra/assets/run-openra-tiberian.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-chmod +x TiberianDawnHD-release-20230225-x86_64.AppImage
-LD_LIBRARY_PATH="" ./TiberianDawnHD-release-20230225-x86_64.AppImage --appimage-extract
+rm -rf ./squashfs-root
+
+chmod +x TiberianDawnHD-release-20250330-x86_64.AppImage
+LD_LIBRARY_PATH="" ./TiberianDawnHD-release-20250330-x86_64.AppImage --appimage-extract
 ./squashfs-root/AppRun
