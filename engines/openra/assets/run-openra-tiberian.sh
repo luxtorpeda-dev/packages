4f1f4e21823f926049fa904f232e96bba4df9b93
David <d10sfan@gmail.com>
2025-04-13 21:05:12 -0500

    Update run-openra-tiberian.sh

--- engines/openra/assets/run-openra-tiberian.sh
+++ engines/openra/assets/run-openra-tiberian.sh
@@ -2,6 +2,7 @@
 
 rm -rf ./squashfs-root
 
+
 chmod +x TiberianDawnHD-release-20250330-x86_64.AppImage
 LD_LIBRARY_PATH="" ./TiberianDawnHD-release-20250330-x86_64.AppImage --appimage-extract
 ./squashfs-root/AppRun
