26d64f7b4a3bff12846130a32abd97f203cf5ba4
David <d10sfan@gmail.com>
2025-05-04 13:53:46 -0500

    doom-runner - Cleanup

--- engines/doom-runner/assets/run-doom-runner.sh
+++ engines/doom-runner/assets/run-doom-runner.sh
@@ -4,12 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export LD_LIBRARY_PATH="$DIR/lib:$LD_LIBRARY_PATH"
 
-if [ ! -f ~/.config/DoomRunner/options.json ]; then
-    if [ ! -d ~/.config/DoomRunner ]; then
-        mkdir -p ~/.config/DoomRunner
-    fi
-    echo "options.json file detected, so creating"
-    cp -rfv "$DIR/options-template.json" ~/.config/DoomRunner/options.json
-fi
-
 "$DIR/DoomRunner" "$@"
