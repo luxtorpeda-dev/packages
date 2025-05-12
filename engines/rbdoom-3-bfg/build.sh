0c0eea60f03c54dca857c9d5a7d4b354ebd931f1
David <d10sfan@gmail.com>
2025-05-11 20:47:13 -0500

    >

--- engines/rbdoom-3-bfg/build.sh
+++ engines/rbdoom-3-bfg/build.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-wget https://sdk.lunarg.com/sdk/download/1.3.231.1/linux/vulkansdk-linux-x86_64-1.3.231.1.tar.gz
-tar xvf vulkansdk-linux-x86_64-1.3.231.1.tar.gz
-source 1.3.231.1/setup-env.sh
-
-sudo cp -r $VULKAN_SDK/include/vulkan/ /usr/local/include/
-sudo cp -P $VULKAN_SDK/lib/libvulkan.so* /usr/local/lib/
-sudo cp $VULKAN_SDK/lib/libVkLayer_*.so /usr/local/lib/
-sudo mkdir -p /usr/local/share/vulkan/explicit_layer.d
-sudo cp $VULKAN_SDK/etc/vulkan/explicit_layer.d/VkLayer_*.json /usr/local/share/vulkan/explicit_layer.d
-sudo ldconfig
-
 # CLONE PHASE
 git clone https://github.com/RobertBeckebans/RBDOOM-3-BFG.git source
 pushd source
