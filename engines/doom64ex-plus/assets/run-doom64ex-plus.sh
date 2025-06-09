e7f5c331780b8085073286e234a6ea5b06f572a4
d10sfan <4337981+d10sfan@users.noreply.github.com>
2025-06-08 20:50:43 -0500

    Switch to doom64ex-minus (#1998)
    * Switch to doom64ex-minus
    
    * ?

--- engines/doom64ex-plus/assets/run-doom64ex-plus.sh
+++ engines/doom64ex-plus/assets/run-doom64ex-plus.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./DOOM64EX-Plus "$@"
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./doom64ex-minus "$@"
--- engines/doom64ex-plus/build.sh
+++ engines/doom64ex-plus/build.sh
@@ -2,9 +2,9 @@
 
 # From https://gitlab.com/luxtorpeda/packages/gzdoom - See LICENSE file for more information
 # CLONE PHASE
-git clone https://github.com/atsb/Doom64EX-Plus.git source
+git clone https://git.sr.ht/~marcin-serwin/Doom64EX-Minus source
 pushd source
-git checkout "$COMMIT_TAG"
+git checkout "$COMMIT_HASH"
 popd
 
 # BUILD PHASE
@@ -13,8 +13,8 @@ make -j "$(nproc)"
 popd
 
 # COPY PHASE
-cp -rfv source/DOOM64EX-Plus "$diststart/1148590/dist/"
+cp -rfv source/doom64ex-minus "$diststart/1148590/dist/"
 cp -rfv assets/* "$diststart/1148590/dist/"
 
 cp -rfv source/doomsnd.sf2 "$diststart/1148590/dist/"
-cp -rfv source/doom64ex-plus.wad "$diststart/1148590/dist/"
+cp -rfv source/doom64ex-minus.wad "$diststart/1148590/dist/"
