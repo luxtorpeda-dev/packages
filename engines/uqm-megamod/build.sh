ab79c70abb90ed767657b58da6e46dec94769186
8f00ff (Vi) <8f00ff@proton.me>
2025-06-11 01:18:37 -0400

    moved UQM_PACKAGES and UQM_ADDONS to env.json

--- engines/uqm-megamod/build.sh
+++ engines/uqm-megamod/build.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-export UQM_ADDONS=(
-  "uqm-${COMPAT_VER}-3DOMusicRemastered.uqm"
-  "uqm-${COMPAT_VER}-3dovideo.uqm"
-  "mm-vols-space.uqm"
-  "mm-rmx-utwig.uqm"
-  "mm-${COMMIT_TAG}-3dovoice.uqm"
-  "mm-remix-timing.uqm"
-  "mm-${COMMIT_TAG}-3domode.uqm"
-  "mm-${COMMIT_TAG}-volasaurus-remix-pack.uqm"
-  "mm-${COMMIT_TAG}-hd-content.uqm"
-  "mm-${COMMIT_TAG}-hd-classic-pack.uqm"
-  "mm-${COMMIT_TAG}-dosmode.uqm"
-)
-
-export UQM_PACKAGES=(
-  "mm-${COMMIT_TAG}-content.uqm"
-)
-
 # CLONE PHASE
 git clone https://github.com/JHGuitarFreak/UQM-MegaMod.git source
 pushd source
@@ -40,14 +22,14 @@ popd
 cp -v source/UrQuanMasters "${diststart}/2645580/dist/uqm-megamod"
 cp -v assets/uqm-megamod.sh "${diststart}/2645580/dist/uqm-megamod.sh"
 
-mkdir -p "${diststart}/2645580/dist/megamod/content/addons"
-for i in ${UQM_ADDONS[@]}; do
-  cp -rvf "${i}" "${diststart}/2645580/dist/megamod/content/addons/${i}"
-done
-
 mkdir -p "${diststart}/2645580/dist/megamod/content/packages"
 for i in ${UQM_PACKAGES[@]}; do
   cp -rvf "${i}" "${diststart}/2645580/dist/megamod/content/packages/${i}"
+done
+
+mkdir -p "${diststart}/2645580/dist/megamod/content/addons"
+for i in ${UQM_ADDONS[@]}; do
+  cp -rvf "${i}" "${diststart}/2645580/dist/megamod/content/addons/${i}"
 done
 
 echo -e "${COMMIT_TAG}"'\n\n$Format:%ad$' > "${diststart}/2645580/dist/megamod/content/version"
