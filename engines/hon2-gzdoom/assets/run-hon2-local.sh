9925b77c80931b0b10a791bfa655d69f5b434f56
David <d10sfan@gmail.com>
2025-06-19 19:04:18 -0500

    Update run-hon2-local.sh

--- engines/hon2-gzdoom/assets/run-hon2-local.sh
+++ engines/hon2-gzdoom/assets/run-hon2-local.sh
@@ -3,4 +3,4 @@
 export SteamDeck=""
 unset SteamDeck
 
-LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./gzdoom "$@" +vid_backend 1
+LD_LIBRARY_PATH="lib:$LD_LIBRARY_PATH" ./engine-classic "$@" +vid_backend 1
