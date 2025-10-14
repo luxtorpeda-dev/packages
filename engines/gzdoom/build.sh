9fa72aa7baacc1c73c37951a7c5a34739249cf13
David <d10sfan@gmail.com>
2025-10-14 18:31:40 -0500

    UZDoom

--- engines/gzdoom/build.sh
+++ engines/gzdoom/build.sh
@@ -2,9 +2,9 @@
 
 # From https://gitlab.com/luxtorpeda/packages/gzdoom - See LICENSE file for more information
 # CLONE PHASE
-git clone https://github.com/zdoom/gzdoom.git source
+git clone https://github.com/UZDoom/UZDoom.git source
 pushd source
-git checkout "$COMMIT_TAG"
+git checkout "$COMMIT_HASH"
 popd
 
 hg clone https://heptapod.host/jp-lebreton/wadsmoosh
