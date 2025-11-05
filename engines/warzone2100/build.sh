a639281182ae907426eddd75cdb9449b1fd157af
d10sfan <me@d10sfan.com>
2025-11-04 21:10:28 -0600

    asd

--- engines/warzone2100/build.sh
+++ engines/warzone2100/build.sh
@@ -7,21 +7,6 @@ git checkout -f "$COMMIT_TAG"
 git submodule update --init --recursive
 popd
 
-INSTALL_DIR="/opt/vulkan"
-SDK_VERSION="1.4.328.1"
-ARCH="x86_64"
-SDK_FILE="vulkansdk-linux-${ARCH}-${SDK_VERSION}.tar.xz"
-SDK_URL="https://sdk.lunarg.com/sdk/download/${SDK_VERSION}/linux/${ARCH}"
-
-wget -q --show-progress https://sdk.lunarg.com/sdk/download/1.4.328.1/linux/vulkansdk-linux-x86_64-1.4.328.1.tar.xz -O "${SDK_FILE}"
-sudo tar -xf "${SDK_FILE}"
-
-export VULKAN_SDK="${INSTALL_DIR}/${SDK_VERSION}/x86_64"
-export PATH=$VULKAN_SDK/bin:$PATH
-export LD_LIBRARY_PATH=$VULKAN_SDK/lib:$LD_LIBRARY_PATH
-export VK_ICD_FILENAMES=$VULKAN_SDK/etc/vulkan/icd.d
-export VK_LAYER_PATH=$VULKAN_SDK/etc/explicit_layer.d
-
 # BUILD PHASE
 pushd "source"
 mkdir -p build
