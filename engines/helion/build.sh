1ce9b0c9fa5e278b1c56095a0396e0d35a95314b
d10sfan <me@d10sfan.com>
2025-11-30 21:00:38 -0600

    helion switch to built

--- engines/helion/build.sh
+++ engines/helion/build.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
 
-# SETUP DOTNET
-
-curl -L https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
-chmod +x ./dotnet-install.sh
-./dotnet-install.sh --channel 10.0
-export DOTNET_ROOT=$HOME/.dotnet
-export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
-
-# CLONE PHASE
-git clone https://github.com/Helion-Engine/Helion.git source
-pushd source
-git checkout "$COMMIT_TAG"
-popd
-
-# BUILD PHASE
-pushd "source/Client"
-dotnet publish -c Release -r linux-x64 -p:SelfContainedRelease=true
-popd
-
 # COPY PHASE
-cp -rfv source/Publish/linux-x64_SelfContained "$diststart/common/dist/helion"
 cp -rfv assets/* "$diststart/common/dist/"
