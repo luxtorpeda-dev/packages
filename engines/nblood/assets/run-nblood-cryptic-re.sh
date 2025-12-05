1a72937976578ffec333934c599c0bd11cd103de
d10sfan <me@d10sfan.com>
2025-12-04 21:25:39 -0600

    Add nblood & notblood to the new blood release

---
+++ engines/nblood/assets/run-nblood-cryptic-re.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -rsf nblood.pk3 DOS/BLOOD121/nblood.pk3
+
+cd DOS/BLOOD121
+
+../../nblood -usecwd -nosetup -ini CRYPTIC.INI
---
+++ engines/notblood/assets/run-notblood-re.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -rsf nblood.pk3 DOS/BLOOD121/notblood.pk3
+
+cd DOS/BLOOD121
+
+../../notblood -usecwd -nosetup
