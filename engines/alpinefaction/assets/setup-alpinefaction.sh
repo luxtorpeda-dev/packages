aa580534fea3f0a503e8b97b1e5172c5c7dd98f5
mv <mv@darkok.xyz>
2025-12-07 20:30:12 +0200

    Proton: replace dashfaction with alpinefaction

---
+++ engines/alpinefaction/assets/setup-alpinefaction.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ln -sf --backup "AlpineFaction/AlpineFactionLauncher.exe" "$PWD/RedFaction.exe"
--- engines/dashfaction/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# COPY PHASE
-mkdir "$diststart/20530/dist/DashFaction/"
-cp -rfv assets/* "$diststart/20530/dist/DashFaction/"
