6fdd5cc7c463a9f2a91465c34faac6a4ac601e4c
d10sfan <me@d10sfan.com>
2025-12-16 21:07:29 -0600

    Update build.sh

--- engines/vkquake/build.sh
+++ engines/vkquake/build.sh
@@ -15,7 +15,7 @@ sudo cp -r $VULKAN_SDK/include/vulkan/ /usr/local/include/
 sudo cp -P $VULKAN_SDK/lib/libvulkan.so* /usr/local/lib/
 sudo cp $VULKAN_SDK/lib/libVkLayer_*.so /usr/local/lib/
 sudo mkdir -p /usr/local/share/vulkan/explicit_layer.d
-sudo cp $VULKAN_SDK/etc/vulkan/explicit_layer.d/VkLayer_*.json /usr/local/share/vulkan/explicit_layer.d
+sudo cp $VULKAN_SDK/share/vulkan/explicit_layer.d/VkLayer_*.json /usr/local/share/vulkan/explicit_layer.d
 sudo ldconfig
 
 # CLONE PHASE
