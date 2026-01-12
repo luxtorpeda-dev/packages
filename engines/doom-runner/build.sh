0f37a4666c03c6186ef2209c82ac55e07c8a8c81
d10sfan <me@d10sfan.com>
2026-01-11 20:26:50 -0600

    asd

--- engines/doom-runner/build.sh
+++ engines/doom-runner/build.sh
@@ -6,8 +6,10 @@ pushd source
 git checkout -f "$COMMIT_HASH"
 popd
 
-export CXXFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas"
-export CFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas"
+export CXXFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas -I"$VCPKG_INSTALLED_PATH"/include"
+export CFLAGS="-m64 -mtune=generic -mfpmath=sse -msse -msse2 -pipe -Wno-unknown-pragmas -I"$VCPKG_INSTALLED_PATH"/include"
+export LDFLAGS=-L"$VCPKG_INSTALLED_PATH/lib"
+export LIBRARY_PATH="$VCPKG_INSTALLED_PATH/lib"
 
 # BUILD PHASE
 pushd "source"
