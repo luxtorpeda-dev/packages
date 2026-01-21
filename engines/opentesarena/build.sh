7794a1eb5412b164cf1ab1e430a96a8d533ec2db
d10sfan <me@d10sfan.com>
2026-01-20 20:54:06 -0600

    Update build.sh

--- engines/opentesarena/build.sh
+++ engines/opentesarena/build.sh
@@ -20,6 +20,7 @@ popd
 
 # COPY PHASE
 cp -rfv assets/* "$diststart/1812290/dist/"
+
 cp -rfv source/data/ "$diststart/1812290/dist/"
 cp -rfv source/options/ "$diststart/1812290/dist/"
 cp -rfv source/build/otesa "$diststart/1812290/dist/"
