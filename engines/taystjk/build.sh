bc40396a460f3523cea35563dad14bfad2c6d051
d10sfan <me@d10sfan.com>
2026-01-31 19:01:00 -0600

    taystjk

---
+++ engines/taystjk/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/taysta/TaystJK.git source
+pushd source
+git checkout "$COMMIT_HASH"
+popd
+
+# BUILD PHASE
+mkdir -p tmp
+pushd source
+mkdir build
+cd build
+cmake \
+    -DCMAKE_INSTALL_PREFIX=../../tmp \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+# COPY PHASE
+cp -rfv tmp/JediAcademy/* "$diststart/6020/dist/"
+cp -rfv assets/run-taystjk.sh "$diststart/6020/dist/"
