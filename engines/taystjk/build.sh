48c083a339f772e20c24b130d37c3facad2e8119
d10sfan <4337981+d10sfan@users.noreply.github.com>
2026-01-31 19:11:28 -0600

    taystjk (#2906)
    * taystjk
    
    * Update build.sh
    
    * asdas

---
+++ engines/taystjk/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/taysta/TaystJK.git source
+pushd source
+git checkout "$COMMIT_HASH"
+popd
+
+# BUILD PHASE
+mkdir -p tmp
+pushd source
+cd build
+cmake \
+    -DCMAKE_INSTALL_PREFIX=../../tmp \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    ..
+make -j "$(nproc)"
+make install
+popd
+
+# COPY PHASE
+cp -rfv tmp/JediAcademy/* "$diststart/6020/dist/"
+cp -rfv assets/run-taystjk-mp.sh "$diststart/6020/dist/"
