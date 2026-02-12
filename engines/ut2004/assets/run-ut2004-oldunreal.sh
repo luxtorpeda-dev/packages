c4279dfe4b584b61a9221a3b5996983a5dfffd3b
d10sfan <me@d10sfan.com>
2026-02-12 16:24:50 -0600

    Reapply "Update run-ut2004-oldunreal.sh"
    This reverts commit 6e3f2e5226d30bf45b8fb837cbf45e1b2a70750f.

--- engines/ut2004/assets/run-ut2004-oldunreal.sh
+++ engines/ut2004/assets/run-ut2004-oldunreal.sh
@@ -6,18 +6,6 @@ fi
 
 cd ../ # game tries to start in system directory, so have to get out and back to the normal directory
 
-if [ -f ~/.ut2004/System/User.ini ]; then
-    echo "detected user user.ini"
-
-    sed -i "s/InputClass=Class'foxWSFix.foxPlayerInput'/InputClass=Class'Engine.PlayerInput'/" ~/.ut2004/System/User.ini
-
-    if grep -Fxq "InputClass=Class'foxWSFix.foxPlayerInput'" ~/.ut2004/System/User.ini
-    then
-        echo "foxWSFix found"
-        sed -i "/InputClass=Class'foxWSFix.foxPlayerInput'/d" ~/.ut2004/System/User.ini
-    fi
-fi
-
 cd linuxdata-oldunreal/System
 
 ./UT2004
