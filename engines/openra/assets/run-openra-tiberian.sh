9244685a7d76c7dc199f50f3f18a7301423c3417
d10sfan <me@d10sfan.com>
2026-03-01 18:24:59 -0600

    Release bumps

--- engines/openra/assets/run-openra-tiberian.sh
+++ engines/openra/assets/run-openra-tiberian.sh
@@ -3,6 +3,6 @@
 rm -rf ./squashfs-root
 
 
-chmod +x TiberianDawnHD-release-20250330-x86_64.AppImage
-LD_LIBRARY_PATH="" ./TiberianDawnHD-release-20250330-x86_64.AppImage --appimage-extract
+chmod +x TiberianDawnHD-playtest-20260222-x86_64.AppImage
+LD_LIBRARY_PATH="" ./TiberianDawnHD-playtest-20260222-x86_64.AppImage --appimage-extract
 ./squashfs-root/AppRun
