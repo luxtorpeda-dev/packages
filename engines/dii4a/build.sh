d343ed9b3fbf3858fc8aabbb14e7d48cc3ac830b
d10sfan <me@d10sfan.com>
2026-03-01 18:30:11 -0600

    Update build.sh

--- engines/dii4a/build.sh
+++ engines/dii4a/build.sh
@@ -9,15 +9,15 @@ popd
 
 # BUILD PHASE
 pushd source
-cd Q3E/src/main/jni/doom3/neo
+cd doom3/neo
 cmake -B build -DCMAKE_SHARED_LINKER_FLAGS=-m64 -DCMAKE_BUILD_TYPE=Release CMakeLists.txt
 cmake --build build --config Release
 popd
 
 
 # COPY PHASE
-cp -rfv source/Q3E/src/main/jni/doom3/neo/build/lib*.so "$diststart/common/dist/"
-cp -rfv source/Q3E/src/main/jni/doom3/neo/build/Quake4 "$diststart/common/dist/"
-cp -rfv source/Q3E/src/main/jni/doom3/neo/build/Prey "$diststart/common/dist/"
+cp -rfv source/doom3/neo/build/lib*.so "$diststart/common/dist/"
+cp -rfv source/doom3/neo/build/Quake4 "$diststart/common/dist/"
+cp -rfv source/doom3/neo/build/Prey "$diststart/common/dist/"
 
 cp -rfv assets/* "$diststart/common/dist/"
