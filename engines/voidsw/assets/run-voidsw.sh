e1bb5c984eefe5ccf0d8a9c6218885c1b3fb77f2
alpabrz <alexanderbrzoska.dev@pm.me>
2026-03-04 04:32:36 +0100

    VoidSW: Add support for "Shadow Warrior Classic" (#3008)
    * VoidSW: Add support for "Shadow Warrior Classic"
    - Add choices to "Shadow Warrior Classic" so that one can select
      VoidSW
    - Add appid to VoidSW config
    - In run-voidsw.sh, add fallback option (since "Shadow Warrior
      Classic" seems to add some CLI options which we need to ignore)
    
    * Update packagessniper_v2.json
    
    ---------
    
    Co-authored-by: d10sfan <me@d10sfan.com>

--- engines/voidsw/assets/run-voidsw.sh
+++ engines/voidsw/assets/run-voidsw.sh
@@ -8,4 +8,6 @@ elif [ "$gamearg" = "-addon1" ]; then
     ./voidsw -gaddons/WT.GRP -usecwd -nosetup -addon1
 elif [ "$gamearg" = "-addon2" ]; then
     ./voidsw -gaddons/TD.grp -usecwd -nosetup -addon2
+else
+    ./voidsw -usecwd -nosetup
 fi
