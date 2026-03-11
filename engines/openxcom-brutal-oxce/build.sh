0242bb1317a37e8a5f2f1bc6ab08b63c1e6356d3
d10sfan <me@d10sfan.com>
2026-03-10 22:18:37 -0500

    Reapply "openxcom-brutal-oxce (#3058)"
    This reverts commit eddd3ae8a194806141a1a6b2c93aa00a9f2c0d20.

---
+++ engines/openxcom-brutal-oxce/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# CLONE PHASE
+git clone https://github.com/Xilmi/OpenXcom.git source
+pushd source
+git checkout "$COMMIT_HASH"
+popd
+
+cp -rfv "$pfx/lib/pkgconfig/sdl12_compat.pc" "$pfx/lib/pkgconfig/sdl.pc"
+
+# BUILD PHASE
+pushd source
+mkdir build
+cd build
+cmake \
+    -DCMAKE_INSTALL_PREFIX="$pfx" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH="$pfx" \
+    -DDEV_BUILD=OFF \
+    -DBUILD_PACKAGE=OFF \
+    ..
+make -j "$(nproc)"
+popd
+
+# COPY PHASE
+cp -rfv "source/build/bin/"* "$diststart/common/dist"
+cp -rfv ./assets/*.sh "$diststart/common/dist"
