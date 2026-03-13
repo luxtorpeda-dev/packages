4b84fb6447100998a0f1cc90f7927b8181ddd647
d10sfan <me@d10sfan.com>
2026-03-12 19:10:48 -0500

    ut2004 fix

--- engines/ut2004/assets/setup-ut2004-oldunreal.sh
+++ engines/ut2004/assets/setup-ut2004-oldunreal.sh
@@ -2,7 +2,7 @@
 
 create_relative_symlink () {
     local -r target=$1
-    local -r symlink="linuxdata-oldunreal/$target"
+    local -r symlink="linuxdata-oldunreal-v2/$target"
     mkdir -p "$(dirname "$symlink")"
     ln -rsf "$target" "$symlink"
 }
@@ -13,41 +13,48 @@ find {Web} -type f  | while read -r file_name ; do
     create_relative_symlink "$file_name"
 done
 
-mkdir -p linuxdata-oldunreal/Animations
-ln -rsf Animations/* linuxdata-oldunreal/Animations
+mkdir -p linuxdata-oldunreal-v2/Animations
+ln -rsf Animations/* linuxdata-oldunreal-v2/Animations
 
-mkdir -p linuxdata-oldunreal/Help
-ln -rsf Help/* linuxdata-oldunreal/Help
+mkdir -p linuxdata-oldunreal-v2/Help
+ln -rsf Help/* linuxdata-oldunreal-v2/Help
 
-mkdir -p linuxdata-oldunreal/Speech
-ln -rsf Speech/* linuxdata-oldunreal/Speech
+mkdir -p linuxdata-oldunreal-v2/Speech
+ln -rsf Speech/* linuxdata-oldunreal-v2/Speech
 
-mkdir -p linuxdata-oldunreal/System
-ln -rsf System/* linuxdata-oldunreal/System
+mkdir -p linuxdata-oldunreal-v2/System
+ln -rsf System/* linuxdata-oldunreal-v2/System
 
-mkdir -p linuxdata-oldunreal/Textures
-ln -rsf Textures/* linuxdata-oldunreal/Textures
+mkdir -p linuxdata-oldunreal-v2/Textures
+ln -rsf Textures/* linuxdata-oldunreal-v2/Textures
 
-ln -rsf Benchmark linuxdata-oldunreal/Benchmark
-ln -rsf ForceFeedback linuxdata-oldunreal/ForceFeedback
-ln -rsf KarmaData linuxdata-oldunreal/KarmaData
-ln -rsf Manual linuxdata-oldunreal/Manual
-ln -rsf maps linuxdata-oldunreal/Maps
-ln -rsf Music linuxdata-oldunreal/Music
-ln -rsf Prefabs linuxdata-oldunreal/Prefabs
-ln -rsf StaticMeshes linuxdata-oldunreal/StaticMeshes
-ln -rsf "ut2004 content 2" linuxdata-oldunreal/"ut2004 content 2"
+mkdir -p linuxdata-oldunreal-v2/Maps
+ln -rsf maps/* linuxdata-oldunreal-v2/Maps
 
-mkdir -p linuxdata-oldunreal/Sounds
-ln -rsf Sounds/* linuxdata-oldunreal/Sounds
+mkdir -p linuxdata-oldunreal-v2/StaticMeshes
+ln -rsf StaticMeshes/* linuxdata-oldunreal-v2/StaticMeshes
 
-cp -rfv linuxdata-oldunreal-binaries/* linuxdata-oldunreal
+ln -rsf Benchmark linuxdata-oldunreal-v2/Benchmark
+ln -rsf ForceFeedback linuxdata-oldunreal-v2/ForceFeedback
+ln -rsf KarmaData linuxdata-oldunreal-v2/KarmaData
+ln -rsf Manual linuxdata-oldunreal-v2/Manual
+ln -rsf Music linuxdata-oldunreal-v2/Music
+ln -rsf Prefabs linuxdata-oldunreal-v2/Prefabs
+ln -rsf "ut2004 content 2" linuxdata-oldunreal-v2/"ut2004 content 2"
+
+mkdir -p linuxdata-oldunreal-v2/Sounds
+ln -rsf Sounds/* linuxdata-oldunreal-v2/Sounds
+
+cp -rfv linuxdata-oldunreal-v2-binaries/* linuxdata-oldunreal-v2
     
-rm linuxdata-oldunreal/System/User.ini
-cp System/User.ini linuxdata-oldunreal/System/User.ini
+rm linuxdata-oldunreal-v2/System/User.ini
+cp System/User.ini linuxdata-oldunreal-v2/System/User.ini
 
-cp -rfv linuxdata-oldunreal/System/Default.ini linuxdata-oldunreal/System/Default.ini-backup
-grep -v "ut2004master2.epicgames.com" linuxdata-oldunreal/System/Default.ini > tmp.ini
-mv tmp.ini linuxdata-oldunreal/System/Default.ini
-sed -i "s/ut2004master1.epicgames.com/ut2004master.333networks.com/" linuxdata-oldunreal/System/Default.ini
-sed -i "s/GUI2K4.UT2K4MainMenu/GUI2K4.UT2K4MainMenuWS/" linuxdata-oldunreal/System/Default.ini
+cp -rfv linuxdata-oldunreal-v2/System/Default.ini linuxdata-oldunreal-v2/System/Default.ini-backup
+grep -v "ut2004master2.epicgames.com" linuxdata-oldunreal-v2/System/Default.ini > tmp.ini
+mv tmp.ini linuxdata-oldunreal-v2/System/Default.ini
+sed -i "s/ut2004master1.epicgames.com/ut2004master.333networks.com/" linuxdata-oldunreal-v2/System/Default.ini
+
+grep -q 'UT2K4MainMenuWS' linuxdata-oldunreal-v2/System/Default.ini || sed -i 's/GUI2K4.UT2K4MainMenu/GUI2K4.UT2K4MainMenuWS/' linuxdata-oldunreal-v2/System/Default.ini
+
+cp -rfv ./linuxdata-oldunreal-extra/* linuxdata-oldunreal-v2
