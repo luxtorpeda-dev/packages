29c1b0632551a24ac81c16c7bf39cf73d8036819
d10sfan <me@d10sfan.com>
2026-04-20 20:05:48 -0500

    Update build.sh

--- engines/odamex/build.sh
+++ engines/odamex/build.sh
@@ -7,23 +7,7 @@ git checkout "$COMMIT_TAG"
 git submodule update --init --recursive
 popd
 
-git clone https://github.com/Doom-Utils/deutex.git
-pushd deutex
-git checkout ef1c06a
-popd
-
 # BUILD PHASE
-pushd "deutex"
-./bootstrap
-./configure
-make
-make install
-popd
-
-pushd source/wad
-deutex -rgb 0 255 255 -doom2 bootstrap -build wadinfo.txt ../odamex.wad
-popd
-
 pushd "source"
 mkdir -p build
 cd build
@@ -39,5 +23,5 @@ popd
 # COPY PHASE
 cp -rfv "source/build/client/odamex" "$diststart/common/dist/"
 chmod +x "$diststart/common/dist/odamex"
-cp -rfv "source/odamex.wad" "$diststart/common/dist/"
+cp -rfv "source/build/client/odamex.wad" "$diststart/common/dist/"
 cp -rfv assets/* "$diststart/common/dist/"
